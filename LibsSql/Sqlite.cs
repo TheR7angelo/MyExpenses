de5f61b95aaec816a0b06ad68b2bfef30cc532cb
TheR7angelo <raphael.antunes@hotmail.fr>
2023-04-17 08:53:18 +0200

    refractor

--- LibsSql/Sqlite.cs
+++ LibsSql/Sqlite.cs
@@ -12,7 +12,6 @@ public class Sqlite
     {
         _connection = !File.Exists(dbPath) ? InitDataBase(dbPath) : new SQLiteConnection(dbPath, false);
         _connection.Execute("PRAGMA foreignkeys = ON");
-        
     }
 
     private static SQLiteConnection InitDataBase(string dbPath)
@@ -25,13 +24,13 @@ public class Sqlite
     }
 
     private static void InsertDefaultData(SQLiteConnection conn)
-    {
-        conn.InsertAll(GetDefault.GetWalletTypes());
-    }
+        => conn.InsertAll(GetDefault.GetDefaults());
 
     private static void CreateDefaultTables(SQLiteConnection connection)
     {
-        var baseTables = new List<string> { Category.Definition, WalletType.Definition };
+        var baseTables = new List<string>
+            { Category.Definition, WalletType.Definition, PaymentMode.Definition, Localisation.Definition };
+
         var tables = new List<string> { Wallet.Definition };
 
         foreach (var command in new List<List<string>> { baseTables, tables }
