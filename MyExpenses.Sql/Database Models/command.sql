6fa3e5dc8980257de7ad2a99dd28ff47d3278315
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-08 09:25:21 +0200

    ci(Update): Add new SQL tables to database model
    This commit introduces multiple new SQL tables to the MyExpenses
    database model. The tables include `t_account_type`, `t_account`,
    `t_category_type`,  `t_mode_payment`, `t_place`, and `t_history`. Each
    of these tables contributes to a more detailed and voracious data
    structure for financial tracking transactions.

---
+++ MyExpenses.Sql/Database Models/command.sql
@@ -0,0 +1,72 @@
+CREATE TABLE t_account_type
+(
+    id   INTEGER
+        constraint t_account_type_pk
+            PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE t_account
+(
+    id              INTEGER
+        CONSTRAINT t_account_pk
+            PRIMARY KEY AUTOINCREMENT,
+    name            TEXT,
+    account_type_fk INTEGER
+        CONSTRAINT t_account_t_account_type_id_fk
+            REFERENCES t_account_type
+);
+
+CREATE TABLE t_category_type
+(
+    id   INTEGER
+        CONSTRAINT t_category_type_pk
+            PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE t_mode_payment
+(
+    id   INTEGER
+        CONSTRAINT t_mode_payment_pk
+            PRIMARY KEY AUTOINCREMENT,
+    name TEXT
+);
+
+CREATE TABLE t_place
+(
+    id         INTEGER
+        CONSTRAINT t_place_pk
+            PRIMARY KEY AUTOINCREMENT,
+    name       TEXT,
+    number     TEXT,
+    street     TEXT,
+    postal     TEXT,
+    city       TEXT,
+    country    TEXT,
+    latitude   REAL,
+    longitude  REAL,
+    date_added DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE t_history
+(
+    id               INTEGER
+        CONSTRAINT t_history_pk
+            PRIMARY KEY AUTOINCREMENT,
+    compte_fk        INTEGER
+        CONSTRAINT t_history_t_account_id_fk
+            REFERENCES t_account,
+    "order"          TEXT,
+    category_type_fk INTEGER
+        CONSTRAINT t_history_t_category_type_id_fk
+            REFERENCES t_category_type,
+    mode_payment_fk  INTEGER
+        CONSTRAINT t_history_t_mode_payment_id_fk
+            REFERENCES t_mode_payment,
+    rising           REAL,
+    date             DATETIME,
+    place_fk         INTEGER
+        constraint t_history_t_place_id_fk
+            references t_place
+);
