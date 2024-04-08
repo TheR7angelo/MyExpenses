262e889f434a8017a1065762377440a58b102ae5
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-08 16:37:02 +0200

    ci(Update): Refactor code in MainWindow.xaml.cs file
    Code in MainWindow.xaml.cs file was significantly refactored for
    improved readability and maintainability. Changes include replacing the
    memory provider and data source with a writable layer for map features,
    adjusting the feature creation process to a more straightforward foreach
    loop, and moving the initialization components into the constructor
    method.

--- MyExpenses.Maps.Test/MainWindow.xaml.cs
+++ MyExpenses.Maps.Test/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System.IO;
 using Mapsui;
 using Mapsui.Layers;
-using Mapsui.Providers;
+using Mapsui.Projections;
 using Mapsui.Styles;
 using MyExpenses.Sql.Context;
 
@@ -9,52 +9,61 @@ namespace MyExpenses.Maps.Test;
 
 public partial class MainWindow
 {
+    private WritableLayer WritableLayer { get; }
+
+    private SymbolStyle PointStyle { get; }
+
     public MainWindow()
     {
+        InitializeComponent();
+
         const string icon =
             @"C:\Users\ZP6177\Documents\Programmation\C#\MyExpenses\MyExpenses.Maps.Test\Ressources\Sans titre - 1.png";
         var fileStream = new FileStream(icon, FileMode.Open);
         var bitmapId = BitmapRegistry.Instance.Register(fileStream);
 
-        var pointStyle = new SymbolStyle
+        PointStyle = new SymbolStyle
         {
             BitmapId = bitmapId,
             SymbolScale = 0.02
         };
 
-        var mapControl = new Mapsui.UI.Wpf.MapControl();
         var map = new Map { CRS = "EPSG:3857", BackColor = Color.Gray };
-        mapControl.Map = map;
-        mapControl.Map.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+        MapControl.Map = map;
+        MapControl.Map.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
 
         using var context = new DataBaseContext();
         var places = context.TPlaces
             .Where(s => s.Latitude != null && s.Latitude != 0 && s.Longitude != null && s.Longitude != 0)
             .ToList();
 
-        var features = places.Select(place => new PointFeature(place.Longitude ?? 0, place.Latitude ?? 0)
+        var features = new List<IFeature>();
+        foreach (var place in places)
         {
-            Styles = new List<IStyle>
+            var point = SphericalMercator.FromLonLat(place.Longitude ?? 0, place.Latitude ?? 0);
+            var feature = new PointFeature(point.x, point.y);
+            feature.Styles = new List<IStyle>
             {
-                pointStyle,
+                PointStyle,
                 new LabelStyle
                 {
                     Text = place.Name, Offset = new Offset { X = 0, Y = 30 },
                     Font = new Font { FontFamily = "Arial", Size = 12 },
                     Halo = new Pen { Color = Color.White, Width = 2 }
                 }
-            }
-        }).Cast<IFeature>().ToList();
+            };
+            features.Add(feature);
+        }
 
-        var memoryProvider = new MemoryProvider(features) { CRS = "EPSG:4326" };
-        var dataSource = new ProjectingProvider(memoryProvider) { CRS = "EPSG:3857" };
+        WritableLayer = new WritableLayer();
+        WritableLayer.AddRange(features);
+        WritableLayer.Style = null;
 
-        var layer = new Layer { DataSource = dataSource };
-        layer.Style = null;
+        MapControl.Map.Layers.Add(WritableLayer);
+    }
 
-        mapControl.Map.Layers.Add(layer);
+    void AddPointAndRefreshMap(double longitude, double latitude, string name)
+    {
 
-        InitializeComponent();
-        Content = mapControl;
     }
 }
