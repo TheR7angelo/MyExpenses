d29387177acb1a03aa2054013a66449a107e1d53
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-10 08:48:35 +0200

    ci(Update): Refactor SQL command in MyExpenses

--- MyExpenses.Sql/Database Models/command.sql
+++ MyExpenses.Sql/Database Models/command.sql
@@ -1,13 +1,3 @@
-﻿CREATE VIEW v_value_by_month_year_category AS
-SELECT  STRFTIME('%Y-%m', h.date) AS month_year, ct.name, ROUND(SUM(h.value), 2) AS total
-FROM t_history h
-         LEFT JOIN t_category_type ct
-                   ON h.category_type_fk = ct.id
-GROUP BY month_year, ct.name
-ORDER BY month_year;
-
-CREATE VIEW v_value_by_month_year AS
-SELECT STRFTIME('%Y-%m', h.date) AS month_year, ROUND(SUM(h.value), 2) AS total
-FROM t_history h
-GROUP BY month_year
-ORDER BY month_year;
+﻿SELECT ROUND(SUM(value), 2)
+FROM t_history
+WHERE pointed = 1;
