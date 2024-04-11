fc44d32a867b62a29d9eb77c780c4b92314b532d
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-11 08:54:48 +0200

    ci(Update): Update SQL views for better date sorting
    The SQL views 'v_value_by_month_year' and
    'v_value_by_month_year_category' have been updated. These views now
    group and sort by discrete 'year' and 'month' fields instead of the
    combined 'month-year' field. This change allows more precise control and
    flexibility in date based queries and sorts.

--- MyExpenses.Sql/Database Models/Definition.sql
+++ MyExpenses.Sql/Database Models/Definition.sql
@@ -125,16 +125,18 @@ BEGIN
     WHERE id = NEW.id;
 END;
 
+DROP VIEW IF EXISTS v_value_by_month_year;
 CREATE VIEW v_value_by_month_year AS
-SELECT STRFTIME('%Y-%m', h.date) AS month_year, ROUND(SUM(h.value), 2) AS total
+SELECT CAST(STRFTIME('%Y', h.date) AS INTEGER) AS year, CAST(STRFTIME('%m', h.date) AS INTEGER) AS month, ROUND(SUM(h.value), 2) AS total
 FROM t_history h
-GROUP BY month_year
-ORDER BY month_year;
+GROUP BY year, month
+ORDER BY year, month;
 
+DROP VIEW IF EXISTS v_value_by_month_year_category;
 CREATE VIEW v_value_by_month_year_category AS
-SELECT STRFTIME('%Y-%m', h.date) AS month_year, ct.name, ROUND(SUM(h.value), 2) AS total
+SELECT CAST(STRFTIME('%Y', h.date) AS INTEGER) AS year, CAST(STRFTIME('%m', h.date) AS INTEGER) AS month, ct.name, ROUND(SUM(h.value), 2) AS total
 FROM t_history h
          LEFT JOIN t_category_type ct
                    ON h.category_type_fk = ct.id
-GROUP BY month_year, ct.name
-ORDER BY month_year;
+GROUP BY year, month, ct.name
+ORDER BY year, month;
