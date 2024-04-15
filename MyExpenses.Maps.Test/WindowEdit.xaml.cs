42872d1de42c10613b0bd15fe0cfc6173209a7f3
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-15 13:50:36 +0200

    ci(Update): Add Mapsui layers and feature to WindowEdit
    Three new import statements have been added (Mapsui, Mapsui.Layers, and
    Mapsui.Styles) to support the addition of new layers and features to the
    map in WindowEdit.xaml.cs. The changes improve the WindowEdit
    constructor's map setup and the SetTplace function's marking
    functionality. Now, features can be added to the WritableLayer, and the
    map will automatically center and zoom on these features.

--- MyExpenses.Maps.Test/WindowEdit.xaml.cs
+++ MyExpenses.Maps.Test/WindowEdit.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using Mapsui.Layers;
+using Mapsui.Styles;
 using MyExpenses.Maps.Test.Utils;
 using MyExpenses.Models.Sql.Tables;
 using MyExpenses.Utils;
@@ -11,19 +13,30 @@ public partial class WindowEdit
 {
     public TPlace TPlace { get; } = new();
 
+    private WritableLayer WritableLayer { get; } = new WritableLayer { Style = null };
+
     public WindowEdit()
     {
         var map = MapStyle.GetMap(false);
+        map.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+
+        map.Layers.Add(WritableLayer);
+
         InitializeComponent();
+
         MapControl.Map = map;
-        MapControl.Map.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
     }
 
     public void SetTplace(TPlace newTPlace)
     {
         PropertyCopyHelper.CopyProperties(newTPlace, TPlace);
-    }
+        var feature = TPlace.ToPointFeature();
+        feature.Styles = new List<IStyle> { MapStyle.RedMarkerStyle };
 
+        WritableLayer.Add(feature);
+
+        MapControl.Map.Home = n => { n.CenterOnAndZoomTo(feature.Point, 1); };
+    }
 
     private void ButtonSearchByCoordinate_OnClick(object sender, RoutedEventArgs e)
     {
