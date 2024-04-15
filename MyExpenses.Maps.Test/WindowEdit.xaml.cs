1adc13aa5d838e23f01b8bfe143b4db4eced1f03
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-15 09:28:55 +0200

    ci(Update): Add TPlace model to WindowEdit
    The code modifies the WindowEdit class in MyExpenses.Maps.Test to
    include the TPlace model from the SQL Tables namespace. This model will
    be used to implement and initialize a new TPlace instance.

--- MyExpenses.Maps.Test/WindowEdit.xaml.cs
+++ MyExpenses.Maps.Test/WindowEdit.xaml.cs
@@ -1,7 +1,11 @@
-﻿namespace MyExpenses.Maps.Test;
+﻿using MyExpenses.Models.Sql.Tables;
+
+namespace MyExpenses.Maps.Test;
 
 public partial class WindowEdit
 {
+    public TPlace TPlace { get; init; } = new();
+
     public WindowEdit()
     {
         InitializeComponent();
