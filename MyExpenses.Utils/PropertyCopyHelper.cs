2cc883171ae92ac84db853391bbbc2d450039806
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-15 09:51:47 +0200

    ci(Update): Add 'MyExpenses.Utils' project and 'PropertyCopyHelper' class
    A new project called 'MyExpenses.Utils' has been added to the solution.
    This project includes a new file 'PropertyCopyHelper.cs', which contains
    a static class to help in copying properties from a source instance to a
    destination instance. This utility may enhance code reuse and
    maintainability across the solution.

---
+++ MyExpenses.Utils/PropertyCopyHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace MyExpenses.Utils;
+
+public static class PropertyCopyHelper
+{
+    public static void CopyProperties<TSource, TDestination>(TSource source, TDestination destination)
+        where TSource: class
+        where TDestination: class
+    {
+        var sourceProperties = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var destinationProperties = typeof(TDestination).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach(var sourceProperty in sourceProperties)
+        {
+            var destinationProperty = destinationProperties.FirstOrDefault(d => d.Name == sourceProperty.Name && d.PropertyType == sourceProperty.PropertyType);
+
+            if (destinationProperty == null || !destinationProperty.CanWrite) continue;
+            var value = sourceProperty.GetValue(source);
+            destinationProperty.SetValue(destination, value);
+        }
+    }
+}
