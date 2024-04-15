3142b90bdf7de41b4203499b629f5d0d20ef9ceb
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-15 13:55:31 +0200

    ci(Update): Set default User-Agent in HttpClient
    The implementation of GetHttpClient method in HttpClient.cs has been
    updated. The User-Agent is now set to "Test" by default if no specific
    User-Agent string is provided. This reduces the number of lines and
    simplifies the code.

--- MyExpenses.WebApi/HttpClient.cs
+++ MyExpenses.WebApi/HttpClient.cs
@@ -5,10 +5,8 @@ public abstract class Http
     protected static HttpClient GetHttpClient(string baseUrl, string? userAgent=null)
     {
         var httpClient = new HttpClient { BaseAddress = new Uri(baseUrl)};
-        if (!string.IsNullOrEmpty(userAgent))
-        {
-            httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
-        }
+        userAgent ??= "Test";
+        httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
         return httpClient;
     }
 
