07ec866945fd3382c1a7f736e8dbf9c129ea0910
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-16 11:44:30 +0200

    ci(Update): Implement navigation in Nominatim search results window
    Updated the 'WindowSelectNominatimSearchResult' class to support the
    INotifyPropertyChanged interface. Added function to navigate back and
    forth between search results. The navigation buttons and the displayed
    place's latitude are bound to properties in the UI.

--- MyExpenses.Maps.Test/SelectNominatimSearchResult/WindowSelectNominatimSearchResult.xaml.cs
+++ MyExpenses.Maps.Test/SelectNominatimSearchResult/WindowSelectNominatimSearchResult.xaml.cs
@@ -1,11 +1,33 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 using MyExpenses.Models.Sql.Tables;
 
 namespace MyExpenses.Maps.Test.SelectNominatimSearchResult;
 
-public partial class WindowSelectNominatimSearchResult
+public partial class WindowSelectNominatimSearchResult : INotifyPropertyChanged
 {
-    public ObservableCollection<TPlace> Places { get; } = [];
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    private List<TPlace> Places { get; set; } = [];
+
+    private TPlace _currentPlace { get; set; } = new();
+
+    public TPlace CurrentPlace
+    {
+        get => _currentPlace;
+        private set
+        {
+            _currentPlace = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private int Index { get; set; }
+    private int Total { get; set; }
 
     public WindowSelectNominatimSearchResult()
     {
@@ -14,9 +36,38 @@ public partial class WindowSelectNominatimSearchResult
 
     public void AddRange(IEnumerable<TPlace> places)
     {
-        foreach (var place in places)
-        {
-            Places.Add(place);
-        }
+        Places.AddRange(places);
+        CurrentPlace = Places.First();
+
+        Index = 1;
+        Total = Places.Count;
+
+        UpdateTitle();
+    }
+
+    private void UpdateCurrentPlace()
+    {
+        if (Index.Equals(0)) Index = Total;
+        if (Index.Equals(Total + 1)) Index = 1;
+
+        CurrentPlace = Places[Index-1];
+        UpdateTitle();
+    }
+
+    private void UpdateTitle()
+    {
+        Title = $"{Index}/{Total} - {CurrentPlace}";
+    }
+
+    private void ButtonGoBack_OnClick(object sender, RoutedEventArgs e)
+    {
+        Index--;
+        UpdateCurrentPlace();
+    }
+
+    private void ButtonGoNext_OnClick(object sender, RoutedEventArgs e)
+    {
+        Index++;
+        UpdateCurrentPlace();
     }
 }
