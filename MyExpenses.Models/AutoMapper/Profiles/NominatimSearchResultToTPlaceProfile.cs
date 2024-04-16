61b3b5d94d4a257ceeba88a3c1140a3bcaa4a52e
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-16 13:30:46 +0200

    ci(Update): Update mapping logic in NominatimSearchResultToTPlaceProfile
    The mapping approach for the 'Street' field in
    NominatimSearchResultToTPlaceProfile has been updated. The new logic
    ensures that if both 'Road' and 'Suburb' fields are not empty in the
    source, the street in the destination will include both, separated by a
    comma. This update provides a more accurate representation of the
    address.

--- MyExpenses.Models/AutoMapper/Profiles/NominatimSearchResultToTPlaceProfile.cs
+++ MyExpenses.Models/AutoMapper/Profiles/NominatimSearchResultToTPlaceProfile.cs
@@ -10,11 +10,20 @@ public class NominatimSearchResultToTPlaceProfile : Profile
     {
         CreateMap<NominatimSearchResult, TPlace>()
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-            .ForMember(dest => dest.Number, opt => opt.MapFrom(src => src.Address != null ? src.Address.HouseNumber : null))
-            .ForMember(dest => dest.Street, opt => opt.MapFrom(src => src.Address != null ? src.Address!.Road ?? src.Address!.Suburb : null))
-            .ForMember(dest => dest.Postal, opt => opt.MapFrom(src => src.Address != null ? src.Address!.Postcode : null))
+            .ForMember(dest => dest.Number,
+                opt => opt.MapFrom(src => src.Address != null ? src.Address.HouseNumber : null))
+            .ForMember(dest => dest.Street,
+                opt => opt.MapFrom(src =>
+                    src.Address != null
+                        ? !string.IsNullOrEmpty(src.Address.Road) && !string.IsNullOrEmpty(src.Address.Suburb)
+                            ? $"{src.Address.Road}, {src.Address.Suburb}"
+                            : src.Address.Road ?? src.Address.Suburb
+                        : null))
+            .ForMember(dest => dest.Postal,
+                opt => opt.MapFrom(src => src.Address != null ? src.Address!.Postcode : null))
             .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.Address != null ? src.Address!.City : null))
-            .ForMember(dest => dest.Country, opt => opt.MapFrom(src => src.Address != null ? src.Address!.Country : null))
+            .ForMember(dest => dest.Country,
+                opt => opt.MapFrom(src => src.Address != null ? src.Address!.Country : null))
             .ForMember(dest => dest.Latitude, opt => opt.MapFrom(src => src.Latitude))
             .ForMember(dest => dest.Longitude, opt => opt.MapFrom(src => src.Longitude))
             .ForMember(dest => dest.DateAdded, opt => opt.MapFrom(_ => DateTime.Now))
