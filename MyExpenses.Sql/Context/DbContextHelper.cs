e2be417c9be22f2cb97dcc3fe90dd39a52d2514a
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-18 16:46:28 +0200

    ci(Update): Add DbContextHelper with Upsert method
    The DbContextHelper.cs file was added to the MyExpenses.Sql project.
    This helper class contains two methods: AddOrEditPlace and Upsert. By
    using these, it allows for more easy handling of update and insert
    operations on the database context. The upsert operation either updates
    existing entities or adds a new one if it doesn't already exist.

---
+++ MyExpenses.Sql/Context/DbContextHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using MyExpenses.Models.Sql.Tables;
+
+namespace MyExpenses.Sql.Context;
+
+public static class DbContextHelper
+{
+    public static bool AddOrEditPlace(this TPlace place)
+    {
+        try
+        {
+            using var context = new DataBaseContext();
+            context.Upsert(place, s => s.Id == place.Id);
+            context.SaveChanges();
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+
+    public static void Upsert<TEntity>(this DbContext context, TEntity entity, Expression<Func<TEntity, bool>> predicate) where TEntity : class
+    {
+        var existingEntity = context.Set<TEntity>().AsNoTracking().FirstOrDefault(predicate);
+
+        if (existingEntity is null) context.Set<TEntity>().Add(entity);
+        else context.Set<TEntity>().Update(entity);
+    }
+}
