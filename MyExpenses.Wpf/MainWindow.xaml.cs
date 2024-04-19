9ca8ad30eb24603f733a1747f22d1bbbb800a8b4
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-19 16:50:36 +0200

    ci(Update): Add DayHistories data grid to MainWindow
    Added DayHistories as ObservableCollection in MainWindow.xaml.cs and
    created a new data grid in MainWindow.xaml to display them. Also,
    project references to MyExpenses.Models and MyExpenses.Sql were added in
    MyExpenses.Wpf.csproj.

--- MyExpenses.Wpf/MainWindow.xaml.cs
+++ MyExpenses.Wpf/MainWindow.xaml.cs
@@ -1,9 +1,18 @@
-﻿namespace MyExpenses.Wpf;
+﻿using System.Collections.ObjectModel;
+using MyExpenses.Models.Sql.Tables;
+using MyExpenses.Sql.Context;
+
+namespace MyExpenses.Wpf;
 
 public partial class MainWindow
 {
+    public ObservableCollection<THistory> DayHistories { get; }
+
     public MainWindow()
     {
+        using var context = new DataBaseContext();
+        DayHistories = new ObservableCollection<THistory>(context.THistories);
+
         InitializeComponent();
     }
 }
