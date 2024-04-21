a47386cc431de5c28637382fd0077f8c425e7bee
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-21 19:10:54 +0200

    ci(Update): Change ObservableCollection to List in MainWindow
    The ObservableCollection used for VHistories in MainWindow.xaml.cs has
    been replaced by List. This adjustment is made to simplify the code and
    improve performance as we don't need the features offered by
    ObservableCollection for this scenario.

--- MyExpenses.Wpf/MainWindow.xaml.cs
+++ MyExpenses.Wpf/MainWindow.xaml.cs
@@ -1,17 +1,16 @@
-﻿using System.Collections.ObjectModel;
-using MyExpenses.Models.Sql.Views;
+﻿using MyExpenses.Models.Sql.Views;
 using MyExpenses.Sql.Context;
 
 namespace MyExpenses.Wpf;
 
 public partial class MainWindow
 {
-    public ObservableCollection<VHistory> VHistories { get; }
+    public List<VHistory> VHistories { get; }
 
     public MainWindow()
     {
         using var context = new DataBaseContext();
-        VHistories = new ObservableCollection<VHistory>(context.VHistories.OrderByDescending(s => s.Date));
+        VHistories = new List<VHistory>(context.VHistories.OrderByDescending(s => s.Date));
 
         InitializeComponent();
     }
