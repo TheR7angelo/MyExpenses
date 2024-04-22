e67bdf410ff06d1bf720aec4d92bca9be63b2a29
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-22 09:19:47 +0200

    ci(Update): Add total by accounts to MainWindow
    In MainWindow.xaml.cs, a new property VTotalByAccounts was added to
    store a list of total values by account from DataBaseContext. This list
    is now being utilized in MainWindow.xaml within an ItemsControl element,
    replacing the hard-coded buttons for individual accounts with dynamic
    buttons that are bound to the data from VTotalByAccounts.

--- MyExpenses.Wpf/MainWindow.xaml.cs
+++ MyExpenses.Wpf/MainWindow.xaml.cs
@@ -6,10 +6,12 @@ namespace MyExpenses.Wpf;
 public partial class MainWindow
 {
     public List<VHistory> VHistories { get; }
+    public List<VTotalByAccount> VTotalByAccounts { get; }
 
     public MainWindow()
     {
         using var context = new DataBaseContext();
+        VTotalByAccounts = new List<VTotalByAccount>(context.VTotalByAccounts);
         VHistories = new List<VHistory>(context.VHistories.OrderByDescending(s => s.Date));
 
         InitializeComponent();
