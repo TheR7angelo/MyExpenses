510f150cdce80ae230adef3bc2b63226c11ccc37
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-22 11:31:58 +0200

    ci(Update): Add DashBoard page and update MainWindow accordingly
    The commit introduces a new DashBoard page to the WPF application and
    updates MainWindow to accommodate this change. The functionality and
    display previously present in MainWindow have been moved to the new
    DashBoard page. Key functionalities that are migrated include event
    handlers and property change notifications. The MainWindow now simply
    contains a frame to load the DashBoard page.

--- MyExpenses.Wpf/MainWindow.xaml.cs
+++ MyExpenses.Wpf/MainWindow.xaml.cs
@@ -1,67 +1,9 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows;
-using System.Windows.Controls;
-using MyExpenses.Models.Sql.Views;
-using MyExpenses.Sql.Context;
-using MyExpenses.Utils;
+﻿namespace MyExpenses.Wpf;
 
-namespace MyExpenses.Wpf;
-
-public partial class MainWindow : INotifyPropertyChanged
+public partial class MainWindow
 {
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-
-    public List<VHistory> VHistories { get; }
-    public List<VTotalByAccount> VTotalByAccounts { get; }
-
-    private double? _total { get; set; } = 0d;
-
-    public double? Total
-    {
-        get => _total;
-        set
-        {
-            _total = value;
-            OnPropertyChanged();
-            OnPropertyChanged(nameof(TotalStr));
-        }
-    }
-
-    public string TotalStr => Total.HasValue ? Total.Value.ToString("F2") : "0.00";
-
     public MainWindow()
     {
-        using var context = new DataBaseContext();
-        VTotalByAccounts = new List<VTotalByAccount>(context.VTotalByAccounts);
-        VHistories = new List<VHistory>(context.VHistories.OrderByDescending(s => s.Date));
-
         InitializeComponent();
     }
-
-    #region Function
-
-    private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
-    {
-        var radioButtons = ItemsControlVTotalAccount.FindVisualChildren<RadioButton>();
-        var radioButton = radioButtons.FirstOrDefault();
-        if (radioButton is null) return;
-        radioButton.IsChecked = true;
-    }
-
-    #endregion
-
-    #region Action
-
-    private void ToggleButtonVTotalAccount_OnChecked(object sender, RoutedEventArgs e)
-    {
-        var button = (RadioButton)sender;
-        var vTotalByAccount = (VTotalByAccount)button.DataContext;
-        Total = vTotalByAccount.Total;
-    }
-
-    #endregion
 }
---
+++ MyExpenses.Wpf/Pages/DashBoard.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using MyExpenses.Models.Sql.Views;
+using MyExpenses.Sql.Context;
+using MyExpenses.Utils;
+
+namespace MyExpenses.Wpf.Pages;
+
+public partial class DashBoard : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    public List<VHistory> VHistories { get; }
+    public List<VTotalByAccount> VTotalByAccounts { get; }
+
+    private double? _total { get; set; } = 0d;
+
+    public double? Total
+    {
+        get => _total;
+        set
+        {
+            _total = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(TotalStr));
+        }
+    }
+
+    public string TotalStr => Total.HasValue ? Total.Value.ToString("F2") : "0.00";
+
+    public DashBoard()
+    {
+        using var context = new DataBaseContext();
+        VTotalByAccounts = new List<VTotalByAccount>(context.VTotalByAccounts);
+        VHistories = new List<VHistory>(context.VHistories.OrderByDescending(s => s.Date));
+
+        InitializeComponent();
+    }
+
+    #region Function
+
+    private void DashBoard_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        var radioButtons = ItemsControlVTotalAccount.FindVisualChildren<RadioButton>();
+        var radioButton = radioButtons.FirstOrDefault();
+        if (radioButton is null) return;
+        radioButton.IsChecked = true;
+    }
+
+    #endregion
+
+    #region Action
+
+    private void ToggleButtonVTotalAccount_OnChecked(object sender, RoutedEventArgs e)
+    {
+        var button = (RadioButton)sender;
+        var vTotalByAccount = (VTotalByAccount)button.DataContext;
+        Total = vTotalByAccount.Total;
+    }
+
+    #endregion
+}
