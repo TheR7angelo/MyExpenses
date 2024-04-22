08521bc6df3416cd0808b68da18bd18fbf2b8167
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-22 09:20:09 +0200

    ci(Update): Implement INotifyPropertyChanged in MainWindow
    The MainWindow class now implements the INotifyPropertyChanged
    interface. This allows for data binding to the 'Total' property and its
    corresponding string representation 'TotalStr'. The interface
    implementation also includes the OnPropertyChanged method for property
    changes, as well as a new button click event that updates the 'Total'
    property. The MainWindow XAML code has been amended to reflect these
    changes.

--- MyExpenses.Wpf/MainWindow.xaml.cs
+++ MyExpenses.Wpf/MainWindow.xaml.cs
@@ -1,12 +1,36 @@
-﻿using MyExpenses.Models.Sql.Views;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using MyExpenses.Models.Sql.Views;
 using MyExpenses.Sql.Context;
 
 namespace MyExpenses.Wpf;
 
-public partial class MainWindow
+public partial class MainWindow : INotifyPropertyChanged
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
     public List<VHistory> VHistories { get; }
     public List<VTotalByAccount> VTotalByAccounts { get; }
+
+    private double? _total { get; set; } = 0d;
+
+    private double? Total
+    {
+        get => _total;
+        set
+        {
+            _total = value;
+            OnPropertyChanged();
+            OnPropertyChanged(nameof(TotalStr));
+        }
+    }
+
+    public string TotalStr => Total.HasValue ? Total.Value.ToString("F2") : "0.00";
 
     public MainWindow()
     {
@@ -16,4 +40,17 @@ public partial class MainWindow
 
         InitializeComponent();
     }
+
+    #region Action
+
+    private void ButtonVTotalAccount_OnClick(object sender, RoutedEventArgs e)
+    {
+        var button = (Button)sender;
+        var vTotalByAccount = (VTotalByAccount)button.DataContext;
+        Total = vTotalByAccount.Total;
+    }
+
+    #endregion
+
+
 }
