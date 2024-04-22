cf0a7cafc40d8517b291cf551edad6b0f9e12ced
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-22 10:02:48 +0200

    ci(Update): Add TotalToIconConverter class
    This new class implements IValueConverter, converting expense total
    amounts into different icons. This is the first step towards a more
    user-friendly display of financial data within the application. The
    conversion logic is contained within the Convert method.

---
+++ MyExpenses.Wpf/Converters/TotalToIconConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using MaterialDesignThemes.Wpf;
+
+namespace MyExpenses.Wpf.Converters;
+
+public class TotalToIconConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not double total) return PackIconKind.None; // Return a default icon or null
+        return total switch
+        {
+            < 0 => PackIconKind.WeatherPouring,
+            0 => PackIconKind.WeatherPartlyCloudy,
+            _ => PackIconKind.WhiteBalanceSunny
+        };
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
