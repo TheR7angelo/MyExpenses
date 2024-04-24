4de9e543172e9624a77b78363cbb8c242a5f133e
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-24 08:56:08 +0200

    ci(Update): Add localization resources for MainWindow
    New resource files (.resx) have been added for English and French
    languages in the MainWindow. These resource files will allow the
    application to support multi-language features. Menu items in the
    MainWindow.xaml have been updated to bind data from the resource file.

--- MyExpenses.Wpf/MainWindow.xaml.cs
+++ MyExpenses.Wpf/MainWindow.xaml.cs
@@ -2,6 +2,10 @@
 
 public partial class MainWindow
 {
+    public string MenuItemHeaderFile { get; } = Ressources.Resx.MainWindow.MainWindowRessource.MenuItemHeaderFile;
+    public string MenuItemHeaderExportDatabase { get; } = Ressources.Resx.MainWindow.MainWindowRessource.MenuItemHeaderExportDatabase;
+    public string MenuItemHeaderImportDatabase { get; } = Ressources.Resx.MainWindow.MainWindowRessource.MenuItemHeaderImportDatabase;
+
     public MainWindow()
     {
         InitializeComponent();
