be9fa2fa19150758039ec5fcb0a1cb9819f789ff
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-26 21:47:03 +0200

    ci(Update): Set default value for Date in THistory model
    The Date property in the THistory model within MyExpenses now has a
    default value set to the current date and time. This change ensures that
    if no specific date is provided when creating a record, the current date
    and time will be assigned automatically.

--- MyExpenses.Models/Sql/Tables/THistory.cs
+++ MyExpenses.Models/Sql/Tables/THistory.cs
@@ -26,7 +26,7 @@ public partial class THistory
     public double? Value { get; set; }
 
     [Column("date", TypeName = "DATETIME")]
-    public DateTime? Date { get; set; }
+    public DateTime? Date { get; set; } = DateTime.Now;
 
     [Column("place_fk")]
     public int? PlaceFk { get; set; }
