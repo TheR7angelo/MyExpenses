74d05f9d53cdbbf873c134e97ef58079c19db938
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-27 18:16:17 +0200

    ci(Update): Add logging capabilities using Serilog
    Introduced a new utility project which includes the configuration for
    the Serilog logger. Added references to this new utility project in the
    main application project. Enhanced the startup process of the
    application to instantiate the logger and manage its lifetime, notably
    at application's startup and shutdown, as well as during unhandled
    exceptions.

---
+++ MyExpenses.Utils/LoggerConfig.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using Serilog.Core;
+
+namespace MyExpenses.Utils;
+
+public static class LoggerConfig
+{
+    public static Logger CreateConfig()
+    {
+        Directory.CreateDirectory("log");
+
+        return new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File(Path.Join("log", $"log-{Guid.NewGuid()}.log"))
+            .CreateLogger();
+    }
+}
--- MyExpenses.Wpf/App.xaml.cs
+++ MyExpenses.Wpf/App.xaml.cs
@@ -1,6 +1,33 @@
-﻿namespace MyExpenses.Wpf;
+﻿using System.Windows;
+using Serilog;
+
+namespace MyExpenses.Wpf;
 
 
 public partial class App
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        Log.Logger = MyExpenses.Utils.LoggerConfig.CreateConfig();
+        Log.Information("Démmarrage de l'application");
+
+        AppDomain.CurrentDomain.ProcessExit += CurrentDomainOnProcessExit;
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+    }
+
+    private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        if (e.ExceptionObject is Exception ex) Log.Fatal(ex, "Une erreur inattendue est survenue");
+        else Log.Fatal("Une erreur inattendue sans exception associée est survenue");
+
+        Log.CloseAndFlush();
+    }
+
+    private static void CurrentDomainOnProcessExit(object? sender, EventArgs e)
+    {
+        Log.Information("Application quitter");
+        Log.CloseAndFlush();
+    }
 }
