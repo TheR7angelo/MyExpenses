38abdeb7914023c2d683c0f8f87b1ce90c16da6c
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-29 09:08:35 +0200

    ci(Update): Add account type name validation on focus lost
    Expanded functionality in the AddEditAccountType page to include
    validation of account type name on focus loss. A warning message is
    displayed if the user attempts to enter a duplicate account type name.
    Additional features include a new ComboBox for account types and
    interaction buttons for validation, cancellation and deletion.

--- MyExpenses.Wpf/Pages/AddEditAccountType.xaml.cs
+++ MyExpenses.Wpf/Pages/AddEditAccountType.xaml.cs
@@ -1,4 +1,6 @@
-﻿using MyExpenses.Models.Sql.Tables;
+﻿using System.Windows;
+using MyExpenses.Models.Sql.Tables;
+using MyExpenses.Sql.Context;
 
 namespace MyExpenses.Wpf.Pages;
 
@@ -6,8 +8,25 @@ public partial class AddEditAccountType
 {
     public TAccountType AccountType { get; } = new();
 
+    public List<TAccountType> AccountTypes { get; }
+
     public AddEditAccountType()
     {
+        using var context = new DataBaseContext();
+        AccountTypes = [..context.TAccountTypes];
+
         InitializeComponent();
     }
+
+    private void TextBoxAccountType_OnLostFocus(object sender, RoutedEventArgs e)
+    {
+        var accountTypeName = AccountType.Name;
+        if (string.IsNullOrEmpty(accountTypeName)) return;
+
+        var alreadyExist = CheckAccountName(accountTypeName);
+        if (alreadyExist) MessageBox.Show("Account type name already exist");
+    }
+
+    private bool CheckAccountName(string accountName)
+        => AccountTypes.Select(s => s.Name).Contains(accountName);
 }
