2cbfa32059b1ece8d6a5e41c2594f152656f1ab4
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-29 11:34:41 +0200

    ci(Update): Add new AddEditCategoryType window and corresponding resources
    This commit introduces a new window, AddEditCategoryType, with its
    corresponding XAML file. Additional resources required for this window
    like localized strings were also added. Moreover, an accompanying
    designer C# file and respective modifications to the project file were
    made to facilitate this new window.

---
+++ MyExpenses.Wpf/Windows/AddEditCategoryType.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MyExpenses.Models.Sql.Tables;
+using MyExpenses.Sql.Context;
+using MyExpenses.Wpf.Resources.Resx.AddEditCategoryType;
+using MyExpenses.Wpf.Resources.Resx.AddEditCurrency;
+
+namespace MyExpenses.Wpf.Windows;
+
+public partial class AddEditCategoryType
+{
+    #region Property
+
+    public TCategoryType CategoryType { get; } = new();
+
+    private List<TCategoryType> CategoryTypes { get; }
+
+    #endregion
+
+    #region Resx
+
+    public string TextBoxCategoryTypeName { get; } = AddEditCategoryTypeResources.TextBoxCategoryTypeName;
+    public string ButtonValidContent { get; } = AddEditCategoryTypeResources.ButtonValidContent;
+    public string ButtonCancelContent { get; } = AddEditCategoryTypeResources.ButtonCancelContent;
+
+    #endregion
+
+    public AddEditCategoryType()
+    {
+        using var context = new DataBaseContext();
+        CategoryTypes = [..context.TCategoryTypes];
+
+        InitializeComponent();
+    }
+
+    #region Function
+
+    private bool CheckCategoryTypeName(string accountName)
+        => CategoryTypes.Select(s => s.Name).Contains(accountName);
+
+    private void ShowErrorMessage()
+        => MessageBox.Show(AddEditCategoryTypeResources.MessageBoxCategoryAlreadyExists);
+
+    #endregion
+
+    #region Action
+
+    private void ButtonCancel_OnClick(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+        Close();
+    }
+
+    private void ButtonValid_OnClick(object sender, RoutedEventArgs e)
+    {
+        var categoryTypeName = CategoryType.Name;
+
+        if (string.IsNullOrEmpty(categoryTypeName)) return;
+
+        var alreadyExist = CheckCategoryTypeName(categoryTypeName);
+        if (alreadyExist) ShowErrorMessage();
+        else
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+
+    private void TextBoxCategoryType_OnPreviewLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+    {
+        var textBox = (TextBox)sender;
+
+        var categoryTypeName = textBox.Text;
+        if (string.IsNullOrEmpty(categoryTypeName)) return;
+
+        var alreadyExist = CheckCategoryTypeName(categoryTypeName);
+        if (alreadyExist) ShowErrorMessage();
+    }
+
+    #endregion
+}
