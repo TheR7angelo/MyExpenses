2d01d323b1a0e512db0ef7190e69a6c571e0d675
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-29 08:54:28 +0200

    ci(Update): Add resources for AddEditAccountType page
    This commit introduces embedded resources intended for localization
    purposes on the AddEditAccountType page. It includes the generation of
    designer code files for these resources and their referencing in the
    main project file. Furthermore, a new instance of TAccountType has been
    added to the AddEditAccountType class.

--- MyExpenses.Wpf/Pages/AddEditAccountType.xaml.cs
+++ MyExpenses.Wpf/Pages/AddEditAccountType.xaml.cs
@@ -1,7 +1,11 @@
-﻿namespace MyExpenses.Wpf.Pages;
+﻿using MyExpenses.Models.Sql.Tables;
+
+namespace MyExpenses.Wpf.Pages;
 
 public partial class AddEditAccountType
 {
+    public TAccountType AccountType { get; } = new();
+
     public AddEditAccountType()
     {
         InitializeComponent();
