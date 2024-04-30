052f9bb8f51f90796400fdbb85897f137897183b
TheR7angelo <raphael.antunes@hotmail.fr>
2024-04-30 11:38:50 +0200

    ci(Update): Add hierarchical tree view to LocationManagementPage
    The tree view retrieves groupings of places from the database and
    displays them hierarchically by country and city in
    LocationManagementPage. It also features binding that would update the
    tree view upon any change in the source data.

--- MyExpenses.Wpf/Pages/LocationManagementPage.xaml.cs
+++ MyExpenses.Wpf/Pages/LocationManagementPage.xaml.cs
@@ -1,9 +1,33 @@
-﻿namespace MyExpenses.Wpf.Pages;
+﻿using System.Collections.ObjectModel;
+using MyExpenses.Models.Sql.Groups;
+using MyExpenses.Sql.Context;
+
+namespace MyExpenses.Wpf.Pages;
 
 public partial class LocationManagementPage
 {
+    public ObservableCollection<CountryGroup> Places { get; }
+
+
     public LocationManagementPage()
     {
+        using var context = new DataBaseContext();
+        var places = context.TPlaces.OrderBy(s => s.Country).ThenBy(s => s.City).ToList();
+        var groupedPlacesByCountryCity = places
+            .GroupBy(country => country.Country)
+            .Select(country => new CountryGroup
+            {
+                Country = country.Key,
+                CityGroups = country.GroupBy(s => s.City)
+                    .Select(city => new CityGroup
+                    {
+                        City = city.Key,
+                        Places = city.ToList()
+                    }).ToList()
+            }).ToList();
+
+        Places = new ObservableCollection<CountryGroup>(groupedPlacesByCountryCity);
+
         InitializeComponent();
     }
 }
