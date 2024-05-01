618334e9f24c49429f786fbf5b7b8b2e058e5ac3
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-01 21:16:55 +0200

    ci(Update): Add documentation to TotalToIconConverter class
    Added comprehensive XML documentation comments to the
    TotalToIconConverter class, provided information about the class, its
    methods, and their respective parameters. Also adjusted the ConvertBack
    method to return null instead of throwing NotImplementedException.

--- MyExpenses.Wpf/Converters/TotalToIconConverter.cs
+++ MyExpenses.Wpf/Converters/TotalToIconConverter.cs
@@ -4,8 +4,19 @@ using MaterialDesignThemes.Wpf;
 
 namespace MyExpenses.Wpf.Converters;
 
+/// <summary>
+/// Converts a total value to an icon from the MaterialDesignThemes.Wpf library.
+/// </summary>
 public class TotalToIconConverter : IValueConverter
 {
+    /// <summary>
+    /// Converts a total value to an icon from the MaterialDesignThemes.Wpf library.
+    /// </summary>
+    /// <param name="value">The total value to be converted.</param>
+    /// <param name="targetType">The type of the target property.</param>
+    /// <param name="parameter">An optional parameter passed to the converter.</param>
+    /// <param name="culture">The culture to be used for conversion.</param>
+    /// <returns>The icon from the MaterialDesignThemes.Wpf library corresponding to the total value.</returns>
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         value ??= 0d;
@@ -18,8 +29,9 @@ public class TotalToIconConverter : IValueConverter
         };
     }
 
-    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        // No return
+        return null;
     }
 }
