9fca7da3748845e8b1e7f3d0cba8da86234a8cb7
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-03 16:19:54 +0200

    ci(Update): Add ValueSymbolConverter to DashBoardPage
    Added a new converter, ValueSymbolConverter, and applied it to a
    DataGridTextColumn in DashBoardPage.xaml. The converter combines values
    and symbols into a single string for display within the data grid.

---
+++ MyExpenses.Wpf/Converters/ValueSymbolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MyExpenses.Wpf.Converters;
+
+public class ValueSymbolConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        var value = ((double)values[0]).ToString("F2");
+        var symbol = values[1].ToString() ?? "";
+        return $"{value} {symbol}";
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
