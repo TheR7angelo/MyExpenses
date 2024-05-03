a175de9d3835bc023eae8d42f883316ff5eceb77
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-03 15:40:31 +0200

    ci(Update): Added NominatimSearchWindows to MyExpenses.Wpf project
    A new WPF window called NominatimSearchWindows has been added to the
    MyExpenses.Wpf project. It includes a search window interface with
    related back-end code. This window facilitates location management by
    allowing the user to search for and select a location. It integrates
    Mapsui for map rendering.

---
+++ MyExpenses.Wpf/Windows/LocationManagementWindows/NominatimSearchWindows.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using Mapsui.Layers;
+using Mapsui.Styles;
+using MyExpenses.Models.AutoMapper;
+using MyExpenses.Models.Sql.Tables;
+using MyExpenses.Wpf.Utils.Maps;
+
+namespace MyExpenses.Wpf.Windows.LocationManagementWindows;
+
+public partial class NominatimSearchWindows : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    private List<TPlace> Places { get; } = [];
+
+    private TPlace _currentPlace { get; set; } = new();
+
+    public TPlace CurrentPlace
+    {
+        get => _currentPlace;
+        private set
+        {
+            _currentPlace = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private int Index { get; set; }
+    private int Total { get; set; }
+
+    private WritableLayer WritableLayer { get; } = new() { Style = null };
+
+    public NominatimSearchWindows()
+    {
+        var map = MapsuiMapExtensions.GetMap(false);
+        map.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+        map.Layers.Add(WritableLayer);
+
+        InitializeComponent();
+
+        MapControl.Map = map;
+    }
+
+    public void AddRange(IEnumerable<TPlace> places)
+    {
+        Places.AddRange(places);
+        Index = 1;
+        Total = Places.Count;
+
+        UpdateCurrentPlace();
+    }
+
+    private void UpdateCurrentPlace()
+    {
+        if (Index.Equals(0)) Index = Total;
+        if (Index.Equals(Total + 1)) Index = 1;
+
+        CurrentPlace = Places[Index-1];
+        UpdatePointFeature();
+        UpdateTitle();
+    }
+
+    private void UpdatePointFeature()
+    {
+        var mapper = Mapping.Mapper;
+        var feature = mapper.Map<PointFeature>(CurrentPlace);
+        feature.Styles = new List<IStyle> { MapsuiStyleExtensions.RedMarkerStyle };
+        WritableLayer.Clear();
+        WritableLayer.Add(feature);
+        MapControl.Map.Home = n => { n.CenterOnAndZoomTo(feature.Point, 1); };
+        MapControl.Map.Navigator.CenterOn(feature.Point);
+        MapControl.Map.Navigator.ZoomTo(1);
+        MapControl.Refresh();
+    }
+
+    private void UpdateTitle()
+    {
+        Title = $"{Index}/{Total} - {CurrentPlace}";
+    }
+
+    private void ButtonGoBack_OnClick(object sender, RoutedEventArgs e)
+    {
+        Index--;
+        UpdateCurrentPlace();
+    }
+
+    private void ButtonGoNext_OnClick(object sender, RoutedEventArgs e)
+    {
+        Index++;
+        UpdateCurrentPlace();
+    }
+
+    private void ButtonCancel_OnClick(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+        Close();
+    }
+
+    private void ButtonValid_OnClick(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+        Close();
+    }
+}
