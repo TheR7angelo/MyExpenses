44c3a07bf85939f7bea368418329b1513d8c7c4c
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-04 19:11:35 +0200

    ci(Update): Add ObservableCollection in AccountManagementPage
    Added an ObservableCollection type variable to the
    AccountManagementPage. Also, imported the necessary namespaces for the
    ObservableCollection and the DatabaseContext. Now, the Accounts can be
    accessed directly from the context on page initialization.

--- MyExpenses.Wpf/Pages/AccountManagementPage.xaml.cs
+++ MyExpenses.Wpf/Pages/AccountManagementPage.xaml.cs
@@ -1,9 +1,18 @@
-﻿namespace MyExpenses.Wpf.Pages;
+﻿using System.Collections.ObjectModel;
+using MyExpenses.Models.Sql.Tables;
+using MyExpenses.Sql.Context;
+
+namespace MyExpenses.Wpf.Pages;
 
 public partial class AccountManagementPage
 {
+    public ObservableCollection<TAccount> Accounts { get; }
+
     public AccountManagementPage()
     {
+        using var context = new DataBaseContext();
+        Accounts = [..context.TAccounts];
+
         InitializeComponent();
     }
 }
