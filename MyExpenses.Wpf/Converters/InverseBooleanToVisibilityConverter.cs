c847bedbbf165328f5200f0eb863816418a9f15b
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-05 19:57:46 +0200

    ci(Update): Added InverseBooleanToVisibilityConverter and integrated into AddEditAccountWindow
    This update includes the introduction of the
    'InverseBooleanToVisibilityConverter' into the project, which is used
    specifically for the AddEditAccountWindow. This converter facilitates a
    more intuitive and flexible management of element visibility based on
    boolean values.

---
+++ MyExpenses.Wpf/Converters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MyExpenses.Wpf.Converters;
+
+public class InverseBooleanToVisibilityConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not bool b) return Visibility.Visible;
+
+        return b ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
