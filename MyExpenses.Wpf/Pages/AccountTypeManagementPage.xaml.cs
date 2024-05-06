0f570e8d896f5b2401a40b2b39813d6f552572fe
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-06 14:51:59 +0200

    ci(Update): Add AccountTypeManagementPage
    Add a new page called AccountTypeManagementPage for the WPF application.
    This page will provide functionalities for managing the account types in
    the application. The page includes two buttons for adding a new account
    type and managing the existing ones.

---
+++ MyExpenses.Wpf/Pages/AccountTypeManagementPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using MyExpenses.Models.Sql.Tables;
+using MyExpenses.Sql.Context;
+
+namespace MyExpenses.Wpf.Pages;
+
+public partial class AccountTypeManagementPage
+{
+    public ObservableCollection<TAccountType> AccountTypes { get; }
+
+    public required DashBoardPage DashBoardPage { get; init; }
+
+    public AccountTypeManagementPage()
+    {
+        using var context = new DataBaseContext();
+        AccountTypes = [..context.TAccountTypes];
+
+        InitializeComponent();
+    }
+
+    private void ButtonAddNewAccountType_OnClick(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("Add new acount type");
+    }
+
+    private void ButtonAccountType_OnClick(object sender, RoutedEventArgs e)
+    {
+        Console.WriteLine("Add new acount type");
+    }
+}
