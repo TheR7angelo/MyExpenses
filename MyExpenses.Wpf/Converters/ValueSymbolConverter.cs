e7c87392c1fe72cbb35d178d8788281a6b5e2569
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-06 08:39:21 +0200

    ci(Update): Refactor ValueSymbolConverter to handle empty symbols
    This commit refactors the Convert method in ValueSymbolConverter to
    better handle cases where the symbol is an empty string or null. It also
    modifies the ConvertBack method to simply return an empty array instead
    of throwing a NotImplementedException.

--- MyExpenses.Wpf/Converters/ValueSymbolConverter.cs
+++ MyExpenses.Wpf/Converters/ValueSymbolConverter.cs
@@ -8,12 +8,14 @@ public class ValueSymbolConverter : IMultiValueConverter
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
         var value = ((double)values[0]).ToString("F2");
-        var symbol = values[1].ToString() ?? "";
-        return $"{value} {symbol}";
+        var symbol = values[1].ToString() ?? string.Empty;
+
+        return !string.IsNullOrWhiteSpace(symbol) ? $"{value} {symbol}" : value;
     }
 
     public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        //pass
+        return [];
     }
 }
