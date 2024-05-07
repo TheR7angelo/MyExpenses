884880ad3f6729b5db24fd390da3d3e597a5bcbd
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-07 21:55:11 +0200

    ci(Update): Implement INotifyPropertyChanged in TColor table
    Added PropertyChanged dependency and implemented
    AddINotifyPropertyChangedInterface annotation to the TColor model. This
    allows the detection of changes to property values in the TColor table
    to better maintain data consistency.

--- MyExpenses.Models/Sql/Tables/TColor.cs
+++ MyExpenses.Models/Sql/Tables/TColor.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using PropertyChanged;
 
 namespace MyExpenses.Models.Sql.Tables;
 
+[AddINotifyPropertyChangedInterface]
 [Table("t_color")]
 public partial class TColor : ISql
 {
