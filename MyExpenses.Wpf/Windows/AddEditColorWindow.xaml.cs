562cb2182e4d3ded5c1e7a7bac837f63d38c9ebd
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-08 20:04:25 +0200

    ci(Update): Add ColorChanged event handler to ColorPickerControl
    This update links the ColorPickerControl in the AddEditColorWindow to a
    new ColorChanged event handler within the same window. This handler logs
    the hexadecimal code of the selected color to the console when a new
    color is selected.

--- MyExpenses.Wpf/Windows/AddEditColorWindow.xaml.cs
+++ MyExpenses.Wpf/Windows/AddEditColorWindow.xaml.cs
@@ -1,9 +1,16 @@
-﻿namespace MyExpenses.Wpf.Windows;
+﻿using MyExpenses.Wpf.UserControls;
+
+namespace MyExpenses.Wpf.Windows;
 
 public partial class AddEditColorWindow
 {
     public AddEditColorWindow()
     {
         InitializeComponent();
+    }
+
+    private void ColorPickerControl_OnColorChanged(object? sender, ColorChangedEventArgs e)
+    {
+        Console.WriteLine(e.HexadecimalCode);
     }
 }
