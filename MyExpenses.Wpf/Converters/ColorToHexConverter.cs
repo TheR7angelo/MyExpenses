a748cff18e21b120b13355d59873f2d810376720
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-08 19:02:35 +0200

    ci(Update): Add ColorToHexConverter class
    The new class ColorToHexConverter is introduced in the Converters
    directory. It implements the IValueConverter interface with methods for
    converting between Color and Hexadecimal representations, providing
    flexibility and efficiency when working with color values.

---
+++ MyExpenses.Wpf/Converters/ColorToHexConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using MyExpenses.Wpf.Utils;
+
+namespace MyExpenses.Wpf.Converters;
+
+public class ColorToHexConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is not Color color ? null : color.ToHexadecimal();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is not string str ? Colors.Black : str.ToColor();
+    }
+}
