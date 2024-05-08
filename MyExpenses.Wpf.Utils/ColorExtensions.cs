fc7c044ae0ca5a457e53ff17b18ce7664083e2df
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-08 17:24:29 +0200

    ci(Update): Added HSV to Color conversion method
    The codebase is now extended with a new method in ColorExtensions class
    that allows conversion from Hue, Saturation, and Value (HSV) color
    values to a System.Drawing.Color object. The added method considers
    various cases and returns a Color object corresponding to the HSV
    inputs.

--- MyExpenses.Wpf.Utils/ColorExtensions.cs
+++ MyExpenses.Wpf.Utils/ColorExtensions.cs
@@ -7,6 +7,74 @@ namespace MyExpenses.Wpf.Utils;
 public static class ColorExtensions
 {
     /// <summary>
+    /// Converts HSV values to a System.Drawing.Color object.
+    /// </summary>
+    /// <param name="hue">The hue value (0-360).</param>
+    /// <param name="saturation">The saturation value (0-1).</param>
+    /// <param name="value">The value (brightness) value (0-1).</param>
+    /// <param name="alpha">The alpha value (0-255). Default is 255.</param>
+    /// <returns>The System.Drawing.Color object representing the converted color.</returns>
+    public static Color ToColor(double hue, double saturation, double value, byte alpha = 255)
+    {
+        double r, g, b;
+        if (saturation == 0)
+        {
+            r = value;
+            g = value;
+            b = value;
+        }
+        else
+        {
+            const double tolerance = 0.00001;
+            if (Math.Abs(hue - 360.0) < tolerance) hue = 0;
+            else hue = hue / 60;
+
+            var i = (int)Math.Truncate(hue);
+            var f = hue - i;
+
+            var p = value * (1.0 - saturation);
+            var q = value * (1.0 - saturation * f);
+            var t = value * (1.0 - saturation * (1.0 - f));
+
+            switch (i)
+            {
+                case 0:
+                    r = value;
+                    g = t;
+                    b = p;
+                    break;
+                case 1:
+                    r = q;
+                    g = value;
+                    b = p;
+                    break;
+                case 2:
+                    r = p;
+                    g = value;
+                    b = t;
+                    break;
+                case 3:
+                    r = p;
+                    g = q;
+                    b = value;
+                    break;
+                case 4:
+                    r = t;
+                    g = p;
+                    b = value;
+                    break;
+                default:
+                    r = value;
+                    g = p;
+                    b = q;
+                    break;
+            }
+        }
+
+        return Color.FromArgb(alpha, (byte)(r * 255), (byte)(g * 255), (byte)(b * 255));
+    }
+
+    /// <summary>
     /// Converts a hexadecimal color code to a SkiaSharp.SKColor object.
     /// </summary>
     /// <param name="hexColor">The hexadecimal color code to convert.</param>
