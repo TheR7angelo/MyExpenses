410213f42898e9274ceb596955dc2f7171d7ec3a
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-08 18:18:12 +0200

    ci(Update): Add RoundThreeDecimalConverter for UI value formatting
    Introduced a new file, RoundThreeDecimalConverter.cs, a Converter class
    for formatting decimal values in the user interface. It rounds double
    values to three decimal places and also provides a ConvertBack method
    for handling string inputs.

---
+++ MyExpenses.Wpf/Converters/RoundThreeDecimalConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace MyExpenses.Wpf.Converters;
+
+public class RoundThreeDecimalConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double doubleValue)
+        {
+            return Math.Round(doubleValue, 3);
+        }
+
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string stringValue && double.TryParse(stringValue, out var doubleValue))
+        {
+            return doubleValue;
+        }
+
+        return value;
+    }
+}
