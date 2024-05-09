2b6312ea70f29bbd3bb027a1ec3c72369093cab6
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-09 22:45:16 +0200

    ci(Update): Expand ColorPickerControl features in AddEditColorWindow
    Added properties to AddEditColorWindow that provides labels for various
    color channels. These properties are binded to the ColorPickerControl
    within the AddEditColorWindow, facilitating a more detailed user
    interface.

--- MyExpenses.Wpf/Windows/AddEditColorWindow.xaml.cs
+++ MyExpenses.Wpf/Windows/AddEditColorWindow.xaml.cs
@@ -1,9 +1,20 @@
-﻿using MyExpenses.Wpf.UserControls;
+﻿using MyExpenses.Wpf.Resources.Resx.Windows.AddEditColorWindow;
+using MyExpenses.Wpf.UserControls;
 
 namespace MyExpenses.Wpf.Windows;
 
 public partial class AddEditColorWindow
 {
+    public string LabelRedChannel { get; } = AddEditColorWindowResources.LabelRedChannel;
+    public string LabelGreenChannel { get; } = AddEditColorWindowResources.LabelGreenChannel;
+    public string LabelBlueChannel { get; } = AddEditColorWindowResources.LabelBlueChannel;
+    public string LabelHueChannel { get; } = AddEditColorWindowResources.LabelHueChannel;
+    public string LabelSaturationChannel { get; } = AddEditColorWindowResources.LabelSaturationChannel;
+    public string LabelValueChannel { get; } = AddEditColorWindowResources.LabelValueChannel;
+    public string LabelAlphaChannel { get; } = AddEditColorWindowResources.LabelAlphaChannel;
+    public string LabelPreview { get; } = AddEditColorWindowResources.LabelPreview;
+    public string LabelHexadecimalCode { get; } = AddEditColorWindowResources.LabelHexadecimalCode;
+
     public AddEditColorWindow()
     {
         InitializeComponent();
