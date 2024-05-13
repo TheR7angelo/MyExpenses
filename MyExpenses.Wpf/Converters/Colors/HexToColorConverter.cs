8ebecb4d236b8ace37fd68dc25bf0f6fcb4f9cf8
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-13 13:41:03 +0200

    ci(Update): Add HexToColorConverter in Color Converters
    A new converter named HexToColorConverter has been added to the Color
    Converters. This converter handles the conversion of a hexadecimal color
    value to a Color object, as well as performing the reversed operation.
    It aims to simplify color handling within the application.

---
+++ MyExpenses.Wpf/Converters/Colors/HexToColorConverter.cs
@@ -0,0 +1,26 @@
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using MyExpenses.Wpf.Utils;
+
+namespace MyExpenses.Wpf.Converters.Colors;
+
+public class HexToColorConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string str) return null;
+
+        if (str.StartsWith('#')) str = str[1..];
+
+        if(str.Length > 8) str = str[..8];
+
+        var color = str.ToColor();
+        return color;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value is not Color color ? null : color.ToHexadecimal();
+    }
+}
