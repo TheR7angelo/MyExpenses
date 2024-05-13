78d28d9e83b6ff7b54777375e92b4dcc6d2464a3
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-13 21:33:32 +0200

    ci(Update): Add edit functionality for account types
    Added edit functionality to AccountTypeManagementPage. This includes
    handling click event of account type button, displaying the edit window,
    and processing the result. Commented code are placeholders for the
    actual implementation of account type modification.

--- MyExpenses.Wpf/Pages/AccountTypeManagementPage.xaml.cs
+++ MyExpenses.Wpf/Pages/AccountTypeManagementPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
+using System.Windows.Controls;
 using MyExpenses.Models.Sql.Tables;
 using MyExpenses.Sql.Context;
 using MyExpenses.Utils;
@@ -51,5 +52,30 @@ public partial class AccountTypeManagementPage
     private void ButtonAccountType_OnClick(object sender, RoutedEventArgs e)
     {
         Console.WriteLine("Edit acount type");
+
+        var button = (Button)sender;
+        if (button.DataContext is not TAccountType accountType) return;
+
+        var addEditAccountType = new AddEditAccountTypeWindow();
+        addEditAccountType.SetTAccountType(accountType);
+
+        var result = addEditAccountType.ShowDialog();
+        if (result != true) return;
+
+        // var newAccountType = addEditAccountType.AccountType;
+        //
+        // Log.Information("Attempting to inject the new account type \"{NewAccountTypeName}\"", newAccountType.Name);
+        // var (success, exception) = newAccountType.AddOrEdit();
+        // if (success)
+        // {
+        //     AccountTypes.AddAndSort(newAccountType, s => s.Name!);
+        //     Log.Information("Account type was successfully added");
+        //     MsgBox.Show(AccountTypeManagementPageResources.MessageBoxAddAccountTypeSuccess, MsgBoxImage.Check);
+        // }
+        // else
+        // {
+        //     Log.Error(exception, "An error occurred please retry");
+        //     MsgBox.Show(AccountTypeManagementPageResources.MessageBoxAddAccountTypeError, MsgBoxImage.Error);
+        // }
     }
 }
