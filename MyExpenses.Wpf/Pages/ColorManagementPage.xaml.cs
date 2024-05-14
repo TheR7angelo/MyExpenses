dc344adf15b41ac9f3931c0e07455332c31b9eb0
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-14 16:52:19 +0200

    ci(Update): Added ColorManagementPage functionality and layout
    This commit involves creating the ColorManagementPage in the WPF
    application. It includes integration with database for retrieving
    colors, handling of color collection changes, UI actions and their
    respective methods, and updating items' size. Corresponding XAML markup
    was updated to add a ScrollViewer, StackPanel, ItemsControl and other UI
    components.

--- MyExpenses.Wpf/Pages/ColorManagementPage.xaml.cs
+++ MyExpenses.Wpf/Pages/ColorManagementPage.xaml.cs
@@ -1,9 +1,70 @@
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Windows;
+using System.Windows.Controls;
+using MyExpenses.Models.Sql.Tables;
+using MyExpenses.Sql.Context;
+using MyExpenses.Wpf.Utils;
+
 namespace MyExpenses.Wpf.Pages;
 
 public partial class ColorManagementPage
 {
+    public ObservableCollection<TColor> Colors { get; }
+
+    //TODO work
     public ColorManagementPage()
     {
+        using var context = new DataBaseContext();
+        Colors = [..context.TColors.OrderBy(s => s.Name)];
+
         InitializeComponent();
+
+        Colors.CollectionChanged += Colors_OnCollectionChanged;
     }
+
+    #region Action
+
+    private void ButtonAddColor_OnClick(object sender, RoutedEventArgs e)
+    {
+        //TODO work
+    }
+
+    private void Colors_OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        UpdateItemsSize();
+    }
+
+    private void ItemsControl_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        UpdateItemsSize();
+    }
+
+    #endregion
+
+    #region Function
+
+    private void UpdateItemsSize()
+    {
+        var items = ItemsControl.Items;
+        double maxWidth = 0;
+
+        foreach (var item in items)
+        {
+            ItemsControl.UpdateLayout();
+            if (ItemsControl.ItemContainerGenerator.ContainerFromItem(item) is not FrameworkElement container) continue;
+            maxWidth = Math.Max(maxWidth, container.ActualWidth);
+        }
+
+        foreach (var item in items)
+        {
+            if (ItemsControl.ItemContainerGenerator.ContainerFromItem(item) is not ContentPresenter container) continue;
+            var button = Utilities.FindChild<Button>(container);
+            if (button is null) continue;
+
+            button.Width = maxWidth;
+        }
+    }
+
+    #endregion
 }
