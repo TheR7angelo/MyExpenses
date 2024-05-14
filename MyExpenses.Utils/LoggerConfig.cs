49a8551dcdf548dcb05a835ae2b9cc2a5b4736cf
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-14 09:05:10 +0200

    ci(Update): Add method to remove old log files
    This commit introduces a new method, `RemoveOldLog`, to the
    'LoggerConfig' file in the 'MyExpenses.Utils' project. This method
    deletes log files that are older than a specified number of days,
    helping to manage the size of logged data over time by automatically
    clearing out outdated information.

--- MyExpenses.Utils/LoggerConfig.cs
+++ MyExpenses.Utils/LoggerConfig.cs
@@ -29,4 +29,36 @@ public static class LoggerConfig
 
         return loggerConfiguration;
     }
+
+    /// <summary>
+    /// Removes log files that are older than a specified number of days.
+    /// </summary>
+    /// <param name="maxDay">The maximum age of log files to keep in days.</param>
+    /// <returns>The number of log files deleted.</returns>
+    public static int RemoveOldLog(int maxDay)
+    {
+        var today = DateTime.Now;
+
+        var deletedCount = 0;
+        var files = Directory.GetFiles(LogDirectoryPath);
+        foreach (var file in files)
+        {
+            var fileCreationTime = File.GetCreationTime(file);
+            var isToOld = fileCreationTime.AddDays(maxDay) < today;
+
+            if (!isToOld) continue;
+            try
+            {
+                File.Delete(file);
+
+                if (!File.Exists(file))  deletedCount += 1;
+            }
+            catch (Exception ex)
+            {
+                // log the exception or handle it
+            }
+        }
+
+        return deletedCount;
+    }
 }
