eddc6f4a7566f86577298c5504a3f00cb33df1bc
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-15 16:18:32 +0200

    ci(Update): Implement PropertyChanged notification in TCurrency model
    Implemented the INotifyPropertyChanged interface by using the
    AddINotifyPropertyChangedInterface attribute from PropertyChanged
    library. This allows the UI to update automatically when property values
    in the TCurrency model change.

--- MyExpenses.Models/Sql/Tables/TCurrency.cs
+++ MyExpenses.Models/Sql/Tables/TCurrency.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using PropertyChanged;
 
 namespace MyExpenses.Models.Sql.Tables;
 
+[AddINotifyPropertyChangedInterface]
 [Table("t_currency")]
 public partial class TCurrency : ISql
 {
