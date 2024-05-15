96139426d1041e8027a98b648cb64715ac23809d
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-15 13:54:37 +0200

    ci(Update): Add currency management functionality
    Added new references in CurrencyManagementPage.xaml.cs, an
    ObservableCollection for Currencies, and two event handlers for add and
    edit currency buttons. This paves the way for enhanced currency
    management capabilities in the application.

--- MyExpenses.Wpf/Pages/CurrencyManagementPage.xaml.cs
+++ MyExpenses.Wpf/Pages/CurrencyManagementPage.xaml.cs
@@ -1,11 +1,30 @@
+using System.Collections.ObjectModel;
+using System.Windows;
+using MyExpenses.Models.Sql.Tables;
+using MyExpenses.Sql.Context;
+
 namespace MyExpenses.Wpf.Pages;
 
 public partial class CurrencyManagementPage
 {
+    public ObservableCollection<TCurrency> Currencies { get; }
     public required DashBoardPage DashBoardPage { get; set; }
 
     public CurrencyManagementPage()
     {
+        using var context = new DataBaseContext();
+        Currencies = [..context.TCurrencies.OrderBy(s => s.Symbol)];
+
         InitializeComponent();
+    }
+
+    private void ButtonAddCurrency_OnClick(object sender, RoutedEventArgs e)
+    {
+        //TODO work
+    }
+
+    private void ButtonEditCurrency_OnClick(object sender, RoutedEventArgs e)
+    {
+        //TODO work
     }
 }
