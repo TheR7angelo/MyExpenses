319ad2d0106fc5f952079788fdb9543b48bd296f
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-17 08:31:49 +0200

    ci(Update): Corrected typo in log message in App.xaml.cs
    A misspelled log message in the App.xaml.cs file was amended, enhancing
    understandability of log statements when the configuration file is read.
    Now, a corrected log message, "Configuration read:", will be displayed
    appropriately.

--- MyExpenses.Wpf/App.xaml.cs
+++ MyExpenses.Wpf/App.xaml.cs
@@ -22,7 +22,7 @@ public partial class App
 
         Log.Information("Reading configuration file");
         var configuration = MyExpenses.Utils.Config.Configuration;
-        Log.Information("Configuration reade :{NewLine}{Configuration}", Environment.NewLine, configuration);
+        Log.Information("Configuration read :{NewLine}{Configuration}", Environment.NewLine, configuration);
 
         Log.Information("Apply log configuration");
         LoadLogConfiguration(configuration.Log);
