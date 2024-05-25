6440086435af9c9ab59e4085a5976db93a9ccfee
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-25 21:20:23 +0200

    ci(Update): Add documentation to conversion methods in Converter.cs
    Detailed documentation has been added to the conversion methods in the
    Converter.cs file. This includes information about method functionality
    and parameters, improving code clarity and readability.

--- MyExpenses.Utils/Sql/Converter.cs
+++ MyExpenses.Utils/Sql/Converter.cs
@@ -6,6 +6,11 @@ namespace MyExpenses.Utils.Sql;
 
 public static class Converter
 {
+    /// <summary>
+    /// Converts an integer to a TColor object.
+    /// </summary>
+    /// <param name="id">The id of the TColor.</param>
+    /// <returns>The TColor object with the specified id, or null if no TColor is found with the id.</returns>
     public static TColor? ToTColor(this int id)
     {
         using var context = new DataBaseContext();
@@ -14,6 +19,11 @@ public static class Converter
         return color;
     }
 
+    /// <summary>
+    /// Converts a VHistory object to a THistory object.
+    /// </summary>
+    /// <param name="vHistory">The VHistory object to convert.</param>
+    /// <returns>The THistory object that corresponds to the VHistory object, or null if no THistory is found with the specified id.</returns>
     public static THistory? ToTHistory(this VHistory vHistory)
     {
         using var context = new DataBaseContext();
@@ -22,6 +32,11 @@ public static class Converter
         return history;
     }
 
+    /// <summary>
+    /// Converts a TAccount object to a VTotalByAccount object.
+    /// </summary>
+    /// <param name="account">The TAccount object to convert.</param>
+    /// <returns>The VTotalByAccount object with the same Id as the TAccount object, or null if no VTotalByAccount is found with the Id.</returns>
     public static VTotalByAccount? ToVTotalByAccount(this TAccount account)
     {
         using var context = new DataBaseContext();
@@ -30,6 +45,11 @@ public static class Converter
         return vTotalByAccount;
     }
 
+    /// <summary>
+    /// Converts an integer representing an account id to a VTotalByAccount object.
+    /// </summary>
+    /// <param name="accountId">The id of the account.</param>
+    /// <returns>The VTotalByAccount object with the specified id, or null if no VTotalByAccount is found with the id.</returns>
     public static VTotalByAccount? ToVTotalByAccount(this int accountId)
     {
         using var context = new DataBaseContext();
@@ -38,6 +58,11 @@ public static class Converter
         return vTotalByAccount;
     }
 
+    /// <summary>
+    /// Converts a VTotalByAccount object to a TAccount object.
+    /// </summary>
+    /// <param name="vTotalByAccount">The VTotalByAccount object to convert.</param>
+    /// <returns>The TAccount object with the specified id, or null if no TAccount is found with the id.</returns>
     public static TAccount? ToTAccount(this VTotalByAccount vTotalByAccount)
     {
         using var context = new DataBaseContext();
@@ -46,6 +71,11 @@ public static class Converter
         return account;
     }
 
+    /// <summary>
+    /// Converts an integer to a TAccount object.
+    /// </summary>
+    /// <param name="accountId">The id of the TAccount.</param>
+    /// <returns>The TAccount object with the specified id, or null if no TAccount is found with the id.</returns>
     public static TAccount? ToTAccount(this int accountId)
     {
         using var context = new DataBaseContext();
