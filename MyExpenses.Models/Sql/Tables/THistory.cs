4f51e01909b858243693dc7a2eddddeeb394c548
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-26 19:19:53 +0200

    ci(Update): Set default value of Pointed property to false
    The Pointed property in the THistory class in the MyExpenses.Models.Sql
    namespace, specifically under the Tables folder, has been updated. The
    default value for this property has been set to false to ensure
    consistent behavior across all instances where this class is used.

--- MyExpenses.Models/Sql/Tables/THistory.cs
+++ MyExpenses.Models/Sql/Tables/THistory.cs
@@ -32,7 +32,7 @@ public partial class THistory : ISql
     public int? PlaceFk { get; set; }
 
     [Column("pointed", TypeName = "BOOLEAN")]
-    public bool? Pointed { get; set; }
+    public bool? Pointed { get; set; } = false;
 
     [Column("bank_transfer_fk")]
     public int? BankTransferFk { get; set; }
