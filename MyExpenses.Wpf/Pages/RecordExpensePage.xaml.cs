388022f8788a059286acf2bdcdadc77d458b0986
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-26 18:38:10 +0200

    ci(Update): Update RecordExpensePage layout and functionality
    The RecordExpensePage's layout structure was improved for better
    responsiveness and UX. Extra fields including a TextBox for description
    and a ComboBox for category types were added. A MouseClick event was
    also prepared for the buttons. On the code behind, model binding was
    expanded to cover the new elements.

--- MyExpenses.Wpf/Pages/RecordExpensePage.xaml.cs
+++ MyExpenses.Wpf/Pages/RecordExpensePage.xaml.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Windows;
 using MyExpenses.Models.Sql.Tables;
 using MyExpenses.Sql.Context;
 
@@ -12,19 +13,32 @@ public partial class RecordExpensePage
     public string DisplayMemberPathAccount { get; } = nameof(TAccount.Name);
     public string SelectedValuePathAccount { get; } = nameof(TAccount.Id);
 
+    public string DisplayMemberPathCategoryType { get; } = nameof(TCategoryType.Name);
+    public string SelectedValuePathCategoryType { get; } = nameof(TCategoryType.Id);
+
     //TODO work
     public string ComboBoxAccountHintAssist { get; } = "From account :";
+    //TODO work
+    public string TextBoxDescriptionHintAssist { get; } = "Description :";
+    //TODO work
+    public string ComboBoxCategoryHintAssist { get; } = "Category type :";
 
     public required DashBoardPage DashBoardPage { get; set; }
 
     public ObservableCollection<TAccount> Accounts { get; }
+    public ObservableCollection<TCategoryType> CategoryTypes { get; }
 
     public RecordExpensePage()
     {
         using var context = new DataBaseContext();
         Accounts = [..context.TAccounts.OrderBy(s => s.Name)];
+        CategoryTypes = [..context.TCategoryTypes.OrderBy(s => s.Name)];
 
         InitializeComponent();
     }
 
+    private void ButtonAccount_OnClick(object sender, RoutedEventArgs e)
+    {
+        //TODO work
+    }
 }
