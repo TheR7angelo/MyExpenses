a603b308e47e9d7a77589a4db551473c53a20cc9
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-26 16:53:56 +0200

    ci(Update): Add ComboBox and Row, Column definitions in RecordExpensePage
    Added ComboBox to RecordExpensePage XAML with its style, source and
    binding details. Included RowDefinition and ColumnDefinition to Grid.
    Also enhanced the RecordExpensePage code-behind file with additional
    namespaces and property details. Some unused code is commented out as
    part of this change.

--- MyExpenses.Wpf/Pages/RecordExpensePage.xaml.cs
+++ MyExpenses.Wpf/Pages/RecordExpensePage.xaml.cs
@@ -1,12 +1,30 @@
+using System.Collections.ObjectModel;
+using MyExpenses.Models.Sql.Tables;
+using MyExpenses.Sql.Context;
+
 namespace MyExpenses.Wpf.Pages;
 
 public partial class RecordExpensePage
 {
 
+    public THistory History { get; } = new();
+
+    public string DisplayMemberPathAccount { get; } = nameof(TAccount.Name);
+    public string SelectedValuePathAccount { get; } = nameof(TAccount.Id);
+
+    //TODO work
+    public string ComboBoxAccountHintAssist { get; } = "From account :";
+
     public required DashBoardPage DashBoardPage { get; set; }
+
+    public ObservableCollection<TAccount> Accounts { get; }
 
     public RecordExpensePage()
     {
+        using var context = new DataBaseContext();
+        Accounts = [..context.TAccounts.OrderBy(s => s.Name)];
+
         InitializeComponent();
     }
+
 }
