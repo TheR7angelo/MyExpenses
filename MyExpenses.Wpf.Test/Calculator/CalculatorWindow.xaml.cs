b7758cab791e8628509e3a0eb3c67585569e927b
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-31 13:52:13 +0200

    ci(Update): Update CalculatorWindow UI to use data binding
    The CalculatorWindow XAML and code-behind have been updated to use data
    binding. The TextCalculationResult DependencyProperty has been added to
    the CalculatorWindow class to hold the calculation result. The label
    previously containing a static "Hello World" message now binds to this
    property, allowing for dynamic display of calculation results.

--- MyExpenses.Wpf.Test/Calculator/CalculatorWindow.xaml.cs
+++ MyExpenses.Wpf.Test/Calculator/CalculatorWindow.xaml.cs
@@ -1,9 +1,21 @@
+using System.Windows;
+
 namespace MyExpenses.Wpf.Test.Calculator;
 
 public partial class CalculatorWindow
 {
+    public static readonly DependencyProperty TextCalculationResultProperty =
+        DependencyProperty.Register(nameof(TextCalculationResult), typeof(string), typeof(CalculatorWindow),
+            new PropertyMetadata(default(string)));
+
     public CalculatorWindow()
     {
         InitializeComponent();
+    }
+
+    public string TextCalculationResult
+    {
+        get => (string)GetValue(TextCalculationResultProperty);
+        set => SetValue(TextCalculationResultProperty, value);
     }
 }
