dc596743b2be2d3b828360bae69349dde441d239
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-31 11:39:10 +0200

    ci(Update): Refactor MainWindow in MyExpenses WPF Test
    This commit simplifies the MainWindow class in the MyExpenses.WPF.Test
    project. It removes unneeded usings and the inheritance from Window
    class.

--- MyExpenses.Wpf.Test/MainWindow.xaml.cs
+++ MyExpenses.Wpf.Test/MainWindow.xaml.cs
@@ -1,20 +1,6 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿namespace MyExpenses.Wpf.Test;
 
-namespace MyExpenses.Wpf.Test;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow
 {
     public MainWindow()
     {
