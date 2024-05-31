1c0452703c3a3adcdd559394e1be2b23c5403a27
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-31 11:41:00 +0200

    ci(Update): Add CalculatorWindow to MainWindow initialization
    This commit adds the CalculatorWindow to the MainWindow's initialization
    process. A new CalculatorWindow instance is now created and displayed
    when the MainWindow initializes.

--- MyExpenses.Wpf.Test/MainWindow.xaml.cs
+++ MyExpenses.Wpf.Test/MainWindow.xaml.cs
@@ -1,9 +1,15 @@
-﻿namespace MyExpenses.Wpf.Test;
+﻿using MyExpenses.Wpf.Test.Calculator;
+
+namespace MyExpenses.Wpf.Test;
 
 public partial class MainWindow
 {
     public MainWindow()
     {
         InitializeComponent();
+
+        var calculatorWindow = new CalculatorWindow();
+        calculatorWindow.ShowDialog();
+
     }
 }
