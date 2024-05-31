92ee6c60a14a047e446c4e9f4cc44a301e7ceeb5
TheR7angelo <raphael.antunes@hotmail.fr>
2024-05-31 15:36:58 +0200

    ci(Update): Refactor CalculatorWindow layout and cleanup code
    Simplified the layout of CalculatorWindow in the XAML file by
    uncommenting and refactoring the Grid definition. Existing commented
    code in the CalculatorWindow.xaml.cs file has also been uncommented and
    revised, resulting in a cleaner structure and improved readability.
    Unnecessary variable definitions have been removed, and the usage of an
    enumerator for operations has been corrected.

--- MyExpenses.Wpf.Test/Calculator/CalculatorWindow.xaml.cs
+++ MyExpenses.Wpf.Test/Calculator/CalculatorWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -5,96 +6,72 @@ namespace MyExpenses.Wpf.Test.Calculator;
 
 public partial class CalculatorWindow
 {
-    // public static readonly DependencyProperty TextCalculationResultProperty =
-    //     DependencyProperty.Register(nameof(TextCalculationResult), typeof(string), typeof(CalculatorWindow),
-    //         new PropertyMetadata(default(string)));
-    //
-    // public string TextCalculationResult
-    // {
-    //     get => (string)GetValue(TextCalculationResultProperty);
-    //     set => SetValue(TextCalculationResultProperty, value);
-    // }
-    //
-    // public static readonly DependencyProperty CalculationResultProperty =
-    //     DependencyProperty.Register(nameof(CalculationResult), typeof(double), typeof(CalculatorWindow),
-    //         new PropertyMetadata(default(double), PropertyChangedCallback));
-    //
-    // private static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    // {
-    //     Console.WriteLine(e.NewValue);
-    // }
-    //
-    // public double CalculationResult
-    // {
-    //     get => (double)GetValue(CalculationResultProperty);
-    //     set => SetValue(CalculationResultProperty, value);
-    // }
+    public static readonly DependencyProperty TextCalculationResultProperty =
+        DependencyProperty.Register(nameof(TextCalculationResult), typeof(string), typeof(CalculatorWindow),
+            new PropertyMetadata(default(string)));
+
+    public string TextCalculationResult
+    {
+        get => (string)GetValue(TextCalculationResultProperty);
+        set => SetValue(TextCalculationResultProperty, value);
+    }
 
     public CalculatorWindow()
     {
         InitializeComponent();
     }
 
-    // private void ButtonReversePolarity_OnClick(object sender, RoutedEventArgs e)
-    // {
-    //     CalculationResult = - CalculationResult;
-    // }
+    private Operator _currentOperator;
 
-    private Operator currentOperator;
-    private double firstNumber;
-    private double secondNumber;
+    private double _firstNumber;
+
+    private double _secondNumber;
 
     private void Button_Click(object sender, RoutedEventArgs e)
     {
-        var btn = sender as Button;
-        if (txtResult.Text != "0")
-        {
-            txtResult.Text = $"{txtResult.Text}{btn.Content}";
-        }
-        else
-        {
-            txtResult.Text = btn.Content.ToString();
-        }
+        if (sender is not Button btn) return;
+
+        txtResult.Text = txtResult.Text is not "0" ? $"{txtResult.Text}{btn.Content}" : btn.Content.ToString();
     }
 
     private void Button_Click_1(object sender, RoutedEventArgs e)
     {
-        firstNumber = 0;
-        secondNumber = 0;
-        currentOperator = 0;
+        _firstNumber = 0;
+        _secondNumber = 0;
+        _currentOperator = 0;
         txtResult.Text = "0";
     }
 
     private void Button_Click_2(object sender, RoutedEventArgs e)
     {
-        currentOperator = Operator.Add;
-        firstNumber = double.Parse(txtResult.Text);
+        _currentOperator = Operator.Add;
+        _firstNumber = double.Parse(txtResult.Text);
         txtResult.Text = "0";
     }
 
     private void Button_Click_3(object sender, RoutedEventArgs e)
     {
-        secondNumber = double.Parse(txtResult.Text);
-        txtResult.Text = GetResult(firstNumber, currentOperator, secondNumber);
+        _secondNumber = double.Parse(txtResult.Text);
+        txtResult.Text = GetResult(_firstNumber, _currentOperator, _secondNumber);
     }
 
     private string GetResult(double firstNumber, Operator currentOperator, double secondNumber)
     {
         if (currentOperator == Operator.Add)
         {
-            return (firstNumber + secondNumber).ToString();
+            return (firstNumber + secondNumber).ToString(CultureInfo.InvariantCulture);
         }
-        else if (currentOperator == Operator.Substract)
+        else if (currentOperator == Operator.Subtract)
         {
-            return (firstNumber - secondNumber).ToString();
+            return (firstNumber - secondNumber).ToString(CultureInfo.InvariantCulture);
         }
         else if (currentOperator == Operator.Multiply)
         {
-            return (firstNumber * secondNumber).ToString();
+            return (firstNumber * secondNumber).ToString(CultureInfo.InvariantCulture);
         }
         else if (currentOperator == Operator.Divide)
         {
-            return (firstNumber / secondNumber).ToString();
+            return (firstNumber / secondNumber).ToString(CultureInfo.InvariantCulture);
         }
         else
         {
@@ -104,28 +81,28 @@ public partial class CalculatorWindow
 
     private void Button_Click_4(object sender, RoutedEventArgs e)
     {
-        currentOperator = Operator.Substract;
-        firstNumber = double.Parse(txtResult.Text);
+        _currentOperator = Operator.Subtract;
+        _firstNumber = double.Parse(txtResult.Text);
         txtResult.Text = "0";
     }
 
     private void Button_Click_5(object sender, RoutedEventArgs e)
     {
-        currentOperator = Operator.Multiply;
-        firstNumber = double.Parse(txtResult.Text);
+        _currentOperator = Operator.Multiply;
+        _firstNumber = double.Parse(txtResult.Text);
         txtResult.Text = "0";
     }
 
     private void Button_Click_6(object sender, RoutedEventArgs e)
     {
-        currentOperator = Operator.Divide;
-        firstNumber = double.Parse(txtResult.Text);
+        _currentOperator = Operator.Divide;
+        _firstNumber = double.Parse(txtResult.Text);
         txtResult.Text = "0";
     }
 
     private void Button_Click_7(object sender, RoutedEventArgs e)
     {
-        txtResult.Text = (double.Parse(txtResult.Text) * -1).ToString();
+        txtResult.Text = (double.Parse(txtResult.Text) * -1).ToString(CultureInfo.InvariantCulture);
     }
 
     private void Button_Click_8(object sender, RoutedEventArgs e)
@@ -144,7 +121,7 @@ public partial class CalculatorWindow
 public enum Operator
 {
     Add,
-    Substract,
+    Subtract,
     Multiply,
     Divide
 }
