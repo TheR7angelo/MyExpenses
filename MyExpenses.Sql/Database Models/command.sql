ed41d14dc19eba8d17720e5906f5ecfb128fce35
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-03 11:38:58 +0200

    ci(Update): Update command.sql with new SQL queries
    Added SQL queries to command.sql to aggregate and summarize transaction
    history in 't_history' table. Now, it includes queries not only for
    pointed transactions, but also for unpointed transactions, and a grouped
    by monthly view.

--- MyExpenses.Sql/Database Models/command.sql
+++ MyExpenses.Sql/Database Models/command.sql
@@ -4,6 +4,17 @@ WHERE account_fk = 1 AND pointed = 1;
 
 SELECT ROUND(SUM(value), 2)
 FROM t_history
+WHERE account_fk = 1 AND pointed = 0;
+
+SELECT ROUND(SUM(value), 2)
+FROM t_history
 WHERE account_fk = 1;
 
 ALTER TABLE t_history RENAME compte_fk TO account_fk;
+
+
+SELECT STRFTIME('%Y-%m', th.date) AS date,
+       th.account_fk,
+       ROUND(SUM(th.value), 2) AS value
+FROM t_history th
+GROUP BY th.account_fk, STRFTIME('%Y-%m', th.date);
