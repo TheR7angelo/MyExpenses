1bb09d1b464327eacc97ebbe4fd3e9402343b430
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-05 14:39:40 +0200

    ci(Update): Add ToInt extension method in StringsExtensions
    A new extension method titled 'ToInt' is introduced in the
    StringsExtensions class. This method converts the given string to an
    equivalent 32-bit signed integer, throwing a FormatException if the
    string is not a valid representation of a number.

--- MyExpenses.Utils/Strings/StringsExtensions.cs
+++ MyExpenses.Utils/Strings/StringsExtensions.cs
@@ -3,6 +3,22 @@ namespace MyExpenses.Utils.Strings;
 public static class StringsExtensions
 {
     /// <summary>
+    /// Converts the specified string representation of a number to an equivalent 32-bit signed integer.
+    /// </summary>
+    /// <param name="input">The string to convert.</param>
+    /// <returns>The 32-bit signed integer equivalent to the number specified in the string.</returns>
+    /// <exception cref="FormatException">The string does not contain a valid representation of a number.</exception>
+    public static int ToInt(this string input)
+    {
+        if (int.TryParse(input, out var i))
+        {
+            return i;
+        }
+
+        throw new FormatException();
+    }
+
+    /// <summary>
     /// Converts the first character of a string to uppercase.
     /// </summary>
     /// <param name="input">The input string.</param>
