543a0affc4366d109fdae627e1632d6c7b4c8a13
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-06 08:48:07 +0200

    ci(Update): Add ExistingDatabase class and WelcomePage
    Introduced a new model class, ExistingDatabase, to handle existing
    database files. The WelcomePage in the WPF application has been updated
    to use this class for loading and displaying existing databases. Also
    added associated XAML file for WelcomePage.

---
+++ MyExpenses.Wpf/Pages/WelcomePage.xaml.cs
@@ -0,0 +1,24 @@
+using System.Collections.ObjectModel;
+using System.IO;
+using MyExpenses.Models.IO;
+
+namespace MyExpenses.Wpf.Pages;
+
+public partial class WelcomePage
+{
+    private string DirectoryDatabase { get; } = Path.GetFullPath("Databases");
+    public ObservableCollection<ExistingDatabase> ExistingDatabases { get; }
+
+    public WelcomePage()
+    {
+        if (!Directory.Exists(DirectoryDatabase)) Directory.CreateDirectory(DirectoryDatabase);
+
+        var existingDatabases = Directory
+            .GetFiles(DirectoryDatabase, "*.sqlite")
+            .Select(s => new ExistingDatabase { FilePath = s } );
+
+        ExistingDatabases = [..existingDatabases];
+
+        InitializeComponent();
+    }
+}
