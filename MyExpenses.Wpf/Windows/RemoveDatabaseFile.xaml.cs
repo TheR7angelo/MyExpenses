2ed013700dfb12fd8d5baf899fcc7874628a61bd
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-07 15:32:19 +0200

    ci(Update): Update RemoveDatabaseFile UI with localization resources
    Added localization resources for the "Valid" and "Cancel" buttons in the
    RemoveDatabaseFile window. These resources have been included in the new
    resx files created for English and French. The button labels now bind to
    these resources. The newly generated designer file was also added to the
    MyExpenses Wpf project.

--- MyExpenses.Wpf/Windows/RemoveDatabaseFile.xaml.cs
+++ MyExpenses.Wpf/Windows/RemoveDatabaseFile.xaml.cs
@@ -2,12 +2,16 @@ using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using MyExpenses.Models.IO;
+using MyExpenses.Wpf.Resources.Resx.Windows.RemoveDatabaseFile;
 using MyExpenses.Wpf.Utils;
 
 namespace MyExpenses.Wpf.Windows;
 
 public partial class RemoveDatabaseFile
 {
+    public string ButtonCancelContent { get; } = RemoveDatabaseFileResources.ButtonCancelCotent;
+    public string ButtonValidContent { get; } = RemoveDatabaseFileResources.ButtonValidContent;
+
     public ObservableCollection<ExistingDatabase> ExistingDatabases { get; } = [];
 
     internal List<ExistingDatabase> ExistingDatabasesToDelete { get; } = [];
