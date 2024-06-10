3b24e2eeabce974ff876be168757c792ffb1f1df
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-10 16:20:48 +0200

    ci(Update): Reformat SQL queries for improved readability
    The SQL queries in command.sql have been reformatted. Statements are now
    separated by new lines, thereby improving overall readability. The
    'ALTER TABLE' command has also been updated with a newline and
    indentation for consistency with the reformatted 'SELECT' statements.

--- MyExpenses.Sql/Database Models/command.sql
+++ MyExpenses.Sql/Database Models/command.sql
@@ -1,14 +1,16 @@
 ﻿SELECT ROUND(SUM(value), 2)
 FROM t_history
-WHERE account_fk = 1 AND pointed = 1;
+WHERE account_fk = 1
+  AND pointed = 1;
 
 SELECT ROUND(SUM(value), 2)
 FROM t_history
-WHERE account_fk = 1 AND pointed = 0;
+WHERE account_fk = 1
+  AND pointed = 0;
 
 SELECT ROUND(SUM(value), 2)
 FROM t_history
 WHERE account_fk = 1;
 
-ALTER TABLE t_history RENAME compte_fk TO account_fk;
-
+ALTER TABLE t_history
+    RENAME compte_fk TO account_fk;
