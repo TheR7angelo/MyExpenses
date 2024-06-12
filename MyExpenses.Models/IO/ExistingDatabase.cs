52e7e9ba52b270d0231d43dfa8027f3d0ecb0f94
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-12 09:55:24 +0200

    ci(Update): Add FileName property to ExistingDatabase class
    A FileName property has been added to the ExistingDatabase class. This
    new property utilizes the Path.GetFileName method to retrieve the name
    of the file from the FilePath. This enhancement improves file management
    within the application.

--- MyExpenses.Models/IO/ExistingDatabase.cs
+++ MyExpenses.Models/IO/ExistingDatabase.cs
@@ -3,5 +3,6 @@ namespace MyExpenses.Models.IO;
 public class ExistingDatabase
 {
     public string? FilePath { get; init; }
+    public string? FileName => Path.GetFileName(FilePath);
     public string? FileNameWithoutExtension => Path.GetFileNameWithoutExtension(FilePath);
 }
