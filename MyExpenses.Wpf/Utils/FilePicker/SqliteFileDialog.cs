17087d4a36330697541dd2647fca06e7c7ed271d
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-12 09:17:30 +0200

    ci(Update): Simplify SqliteFileDialog Extensions in FilePicker
    This commit streamlines the SqliteFileDialog by reducing the Extensions
    list to only include the ".sqlite" extension. The previous code
    contained an unnecessary list initialization and multiple database file
    types, which has now been cleaned up.

--- MyExpenses.Wpf/Utils/FilePicker/SqliteFileDialog.cs
+++ MyExpenses.Wpf/Utils/FilePicker/SqliteFileDialog.cs
@@ -3,8 +3,5 @@ namespace MyExpenses.Wpf.Utils.FilePicker;
 public class SqliteFileDialog(string? titleOpenFile = null, string? titleSaveFile = null, bool multiSelect = false)
     : AFileDialog(titleOpenFile, titleSaveFile, multiSelect, Extensions, "Fichier base donnée sqlite")
 {
-    private new static IEnumerable<string> Extensions => new List<string>
-    {
-        ".sqlite", "*.db"
-    };
+    private new static IEnumerable<string> Extensions => [".sqlite"];
 }
