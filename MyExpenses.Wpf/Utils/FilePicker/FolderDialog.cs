416a626494b7ab5fcdd0e2416e2dfbb0163432e7
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-12 09:55:53 +0200

    ci(Update): Add FolderDialog class in FilePicker utils
    A new class named FolderDialog is introduced in the FilePicker
    utilities. This class, implementing the IDialog interface, uses
    VistaFolderBrowserDialog for browsing folders. It provides functionality
    to get files and returns selected file paths. Saving files is not yet
    implemented.

---
+++ MyExpenses.Wpf/Utils/FilePicker/FolderDialog.cs
@@ -0,0 +1,35 @@
+using MyExpenses.Models.Ui.FilePicker;
+using Ookii.Dialogs.Wpf;
+
+namespace MyExpenses.Wpf.Utils.FilePicker;
+
+public class FolderDialog(string? titleOpenFile = null, bool multiSelect = false) : IDialog
+{
+    private readonly VistaFolderBrowserDialog _vistaFolderBrowserDialog = new()
+    {
+        Description = titleOpenFile,
+        UseDescriptionForTitle = true,
+        Multiselect = multiSelect
+    };
+
+    public IEnumerable<string> Extensions => Array.Empty<string>();
+
+    public string[]? GetFiles()
+    {
+        var path = GetFile();
+
+        if (string.IsNullOrEmpty(path)) return null;
+        return [path];
+    }
+
+    public string? GetFile()
+    {
+        var result = _vistaFolderBrowserDialog.ShowDialog();
+        return result is true ? _vistaFolderBrowserDialog.SelectedPath : null;
+    }
+
+    public string? SaveFile()
+    {
+        throw new NotImplementedException();
+    }
+}
