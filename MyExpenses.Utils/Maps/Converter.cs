8d6fa1e0d31b8ba0ae80dc584d3382d8b1c32f71
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-14 13:42:41 +0200

    ci(Update): Add conversion methods to Converter.cs
    Two new conversion methods have been added to the Converter class in the
    Maps utility. These methods convert a TPlace object to an MPoint object,
    and an IFeature object to a TPlace object respectively.

--- MyExpenses.Utils/Maps/Converter.cs
+++ MyExpenses.Utils/Maps/Converter.cs
@@ -1,4 +1,5 @@
 ﻿using Mapsui;
+using Mapsui.Layers;
 using MyExpenses.Models.AutoMapper;
 using MyExpenses.Models.Sql.Tables;
 
@@ -6,6 +7,23 @@ namespace MyExpenses.Utils.Maps;
 
 public static class Converter
 {
+    /// <summary>
+    /// Converts a TPlace object to an MPoint object.
+    /// </summary>
+    /// <param name="place">The TPlace object to convert.</param>
+    /// <returns>The converted MPoint object.</returns>
+    public static MPoint ToMPoint(this TPlace place)
+    {
+        var mapper = Mapping.Mapper;
+        var pointFeature = mapper.Map<PointFeature>(place);
+        return pointFeature.Point;
+    }
+
+    /// <summary>
+    /// Converts an IFeature object to a TPlace object.
+    /// </summary>
+    /// <param name="feature">The IFeature object to convert.</param>
+    /// <returns>The converted TPlace object.</returns>
     public static TPlace ToTPlace(this IFeature feature)
     {
         var mapper = Mapping.Mapper;
