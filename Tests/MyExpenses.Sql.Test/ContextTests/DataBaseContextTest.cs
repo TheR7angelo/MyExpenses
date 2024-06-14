6880d598a53e057fddc910bbbde328a930e7294c
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-14 16:38:04 +0200

    ci(Update): Add DbContextOptions test in DataBaseContextTest
    Added a new test method named DataBaseContextDbContextOptionsTest in the
    DataBaseContextTest file. The test checks if a DataBaseContext object,
    instantiated using DbContextOptions, is not null. A using directive for
    Microsoft.EntityFrameworkCore was also added.

--- Tests/MyExpenses.Sql.Test/ContextTests/DataBaseContextTest.cs
+++ Tests/MyExpenses.Sql.Test/ContextTests/DataBaseContextTest.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using MyExpenses.Sql.Context;
 
 namespace MyExpenses.Sql.Test.ContextTests;
@@ -5,6 +6,16 @@ namespace MyExpenses.Sql.Test.ContextTests;
 public class DataBaseContextTest
 {
     private readonly string _dbPathTest = Path.Join(Path.GetFullPath("Resources"), "Model - Using - Test.sqlite");
+
+    [Fact]
+    private void DataBaseContextDbContextOptionsTest()
+    {
+        DataBaseContext.FilePath = _dbPathTest;
+        var contextOption = new DbContextOptions<DataBaseContext>();
+        using var context = new DataBaseContext(contextOption);
+
+        Assert.NotNull(context);
+    }
 
     [Fact]
     private void DataBaseContextStringEmptyTest()
