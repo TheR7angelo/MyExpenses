17adb24dacb1ba4c8b34af7051c2a3c3fce979de
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-15 20:07:43 +0200

    ci(Update): Add resource properties to DashBoard2Page
    Added several properties for button titles, mostly to improve the
    maintainability of the code. These properties are taken from
    DashBoardPageResources, to ensure that all button titles are centralized
    and can be managed in one location.

--- MyExpenses.Wpf/Pages/DashBoard2Page.xaml.cs
+++ MyExpenses.Wpf/Pages/DashBoard2Page.xaml.cs
@@ -1,9 +1,24 @@
 ﻿using System.Windows;
+using MyExpenses.Wpf.Resources.Resx.Pages.DashBoardPage;
 
 namespace MyExpenses.Wpf.Pages;
 
 public partial class DashBoard2Page
 {
+    #region Button WrapPanel
+
+    public string ButtonAccountManagement { get; } = DashBoardPageResources.ButtonAccountManagement;
+    public string ButtonAccountTypeManagement { get; } = DashBoardPageResources.ButtonAccountTypeManagement;
+    public string ButtonCategoryTypeManagement { get; } = DashBoardPageResources.ButtonCategoryTypeManagement;
+    public string ButtonColorManagement { get; } = DashBoardPageResources.ButtonColorManagement;
+    public string ButtonCurrencyManagement { get; } = DashBoardPageResources.ButtonCurrencyManagement;
+    public string ButtonLocationManagement { get; } = DashBoardPageResources.ButtonLocationManagement;
+    public string ButtonModePaymentManagement { get; } = DashBoardPageResources.ButtonModePaymentManagement;
+    public string ButtonMakeBankTransfer { get; } = DashBoardPageResources.ButtonMakeBankTransfer;
+    public string ButtonRecordExpense { get; } = DashBoardPageResources.ButtonRecordExpense;
+
+    #endregion
+
     public DashBoard2Page()
     {
         InitializeComponent();
@@ -14,14 +29,11 @@ public partial class DashBoard2Page
     private void ButtonAccountManagement_OnClick(object sender, RoutedEventArgs e)
         => nameof(MainWindow.FrameBody).NavigateTo(typeof(AccountManagementPage));
 
-
     private void ButtonAccountTypeManagement_OnClick(object sender, RoutedEventArgs e)
         => nameof(MainWindow.FrameBody).NavigateTo(typeof(AccountTypeManagementPage));
 
-
     private void ButtonCategoryTypeManagement_OnClick(object sender, RoutedEventArgs e)
         => nameof(MainWindow.FrameBody).NavigateTo(typeof(CategoryTypeManagementPage));
-
 
     private void ButtonColorManagement_OnClick(object sender, RoutedEventArgs e)
         => nameof(MainWindow.FrameBody).NavigateTo(typeof(ColorManagementPage));
@@ -35,10 +47,8 @@ public partial class DashBoard2Page
     private void ButtonModePaymentManagement_OnClick(object sender, RoutedEventArgs e)
         => nameof(MainWindow.FrameBody).NavigateTo(typeof(ModePaymentManagementPage));
 
-
     private void ButtonMakeBankTransfer_OnClick(object sender, RoutedEventArgs e)
         => nameof(MainWindow.FrameBody).NavigateTo(typeof(BankTransferPage));
-
 
     private void ButtonRecordExpense_OnClick(object sender, RoutedEventArgs e)
         => nameof(MainWindow.FrameBody).NavigateTo(typeof(RecordExpensePage));
