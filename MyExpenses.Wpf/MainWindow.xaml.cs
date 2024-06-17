bd6f22ad0ea100211c5e70140170cf77dab67d40
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-17 14:12:07 +0200

    ci(Update): Remove color enumeration code in MainWindow
    The initialization code that created a list of color codes and saved it
    to the database in MainWindow.xaml.cs was removed. This code was
    unnecessary and was likely for testing purposes, as it created a
    black-listed color enumeration and saved each color to a new TColor
    object in the database.

--- MyExpenses.Wpf/MainWindow.xaml.cs
+++ MyExpenses.Wpf/MainWindow.xaml.cs
@@ -35,41 +35,6 @@ public partial class MainWindow
 
     public MainWindow()
     {
-        // var blackList = new List<KnownColor>
-        // {
-        //     KnownColor.Control, KnownColor.Desktop, KnownColor.Highlight, KnownColor.Info, KnownColor.Menu,
-        //     KnownColor.Transparent, KnownColor.Window, KnownColor.ActiveBorder, KnownColor.ActiveCaption,
-        //     KnownColor.AppWorkspace, KnownColor.ButtonFace, KnownColor.ButtonHighlight, KnownColor.ButtonShadow,
-        //     KnownColor.ControlDark, KnownColor.ControlLight, KnownColor.ControlText, KnownColor.GrayText,
-        //     KnownColor.HighlightText, KnownColor.HotTrack, KnownColor.InactiveBorder, KnownColor.InactiveCaption,
-        //     KnownColor.InactiveCaptionText, KnownColor.InfoText, KnownColor.MenuBar, KnownColor.MenuHighlight,
-        //     KnownColor.MenuText, KnownColor.ScrollBar, KnownColor.WindowFrame, KnownColor.WindowText,
-        //     KnownColor.ActiveCaptionText, KnownColor.ControlDarkDark, KnownColor.ControlLightLight,
-        //     KnownColor.GradientActiveCaption, KnownColor.GradientInactiveCaption
-        // };
-        // var knownColors = Enum.GetValues<KnownColor>()
-        //     .Where(s => !blackList.Contains(s))
-        //     .OrderBy(s => s.ToString())
-        //     .ToList();
-        //
-        // var colors = new List<TColor>();
-        // foreach (var knownColor in knownColors)
-        // {
-        //     var name = knownColor.ToString().SplitUpperCaseWord();
-        //     var color = Color.FromKnownColor(knownColor);
-        //     var hex = $"#{color.A:X2}{color.R:X2}{color.G:X2}{color.B:X2}";
-        //
-        //     colors.Add(new TColor
-        //     {
-        //         Name = name,
-        //         HexadecimalColorCode = hex
-        //     });
-        // }
-        //
-        // using var context = new DataBaseContext();
-        // context.TColors.AddRange(colors);
-        // context.SaveChanges();
-
         InitializeComponent();
 
         Navigator.CanGoBackChanged += Navigator_OnCanGoBackChanged;
