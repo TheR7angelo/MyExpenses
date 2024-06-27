539c59f3a1313d9b684292783c203e746716449f
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-27 14:07:52 +0200

    ci(Update): Add token refresh functionality to DropboxService
    A new method RefreshAccessTokenAuthentication() has been added to the
    DropboxService class to handle Dropbox token refreshing. Another change
    includes a refactor to GetAccessTokenAuthentication() and GetTempToken()
    to use DropboxKeys property instead of passing it as an argument. These
    changes aim to improve the reliability and maintainability of the
    service.

--- MyExpenses.WebApi/Dropbox/DropboxService.cs
+++ MyExpenses.WebApi/Dropbox/DropboxService.cs
@@ -10,10 +10,14 @@ public class DropboxService
 {
     public AccessTokenAuthentication? AccessTokenAuthentication { get; set; }
 
+    private DropboxKeys DropboxKeys { get; }
+
     private string FilePathSecretKeys { get; }
 
     public DropboxService()
     {
+        DropboxKeys = GetDropboxKeys();
+
         var directorySecretKeys = GenerateDirectorySecretKeys();
         FilePathSecretKeys = Path.Join(directorySecretKeys, "AccessTokenAuthentication.json");
 
@@ -23,20 +27,37 @@ public class DropboxService
         AccessTokenAuthentication = JsonConvert.DeserializeObject<AccessTokenAuthentication>(jsonStr);
     }
 
+    public async Task RefreshAccessTokenAuthentication()
+    {
+        using var httpClient = Http.GetHttpClient();
+        var requestData = new Dictionary<string, string>
+        {
+            { "grant_type", "refresh_token" },
+            { "refresh_token", AccessTokenAuthentication!.RefreshToken! },
+            { "client_id", DropboxKeys.AppKey! },
+            { "client_secret", DropboxKeys.AppSecret! }
+        };
+
+        var requestContent = new FormUrlEncodedContent(requestData);
+        var response = await httpClient.PostAsync("https://api.dropbox.com/oauth2/token", requestContent);
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        var accessTokenResponse = JsonConvert.DeserializeObject<AccessTokenAuthentication>(responseContent);
+        AccessTokenAuthentication.AccessToken = accessTokenResponse!.AccessToken;
+        AccessTokenAuthentication.ExpiresIn = accessTokenResponse.ExpiresIn;
+        AccessTokenAuthentication.TokenType = accessTokenResponse.TokenType;
+
+        var now = DateTime.Now;
+        AccessTokenAuthentication.DateCreated = now;
+        AccessTokenAuthentication.DateExpiration = now.AddSeconds(accessTokenResponse.ExpiresIn ?? 0);
+
+        await File.WriteAllTextAsync(FilePathSecretKeys, JsonConvert.SerializeObject(AccessTokenAuthentication, Formatting.Indented));
+    }
+
     public void AuthorizeApplication()
     {
-        var assembly = Assembly.GetAssembly(typeof(DropboxService))!;
-        var resources = assembly.GetManifestResourceNames();
-        var resourceName = resources.First(s => s.Contains("DropboxKeys.json"));
-
-        using var stream = assembly.GetManifestResourceStream(resourceName)!;
-        using var reader = new StreamReader(stream);
-        var jsonStr = reader.ReadToEnd();
-
-        var dropboxKeys = JsonConvert.DeserializeObject<DropboxKeys>(jsonStr)!;
-
-        var tempToken = GetTempToken(dropboxKeys)!;
-        var taskAccessTokenAuthentication = GetAccessTokenAuthentication(tempToken, dropboxKeys);
+        var tempToken = GetTempToken()!;
+        var taskAccessTokenAuthentication = GetAccessTokenAuthentication(tempToken);
         var accessTokenAuthentication = taskAccessTokenAuthentication.GetAwaiter().GetResult();
 
         if (accessTokenAuthentication is not null)
@@ -48,16 +69,29 @@ public class DropboxService
         File.WriteAllText(FilePathSecretKeys, JsonConvert.SerializeObject(accessTokenAuthentication, Formatting.Indented));
     }
 
-    private static async Task<AccessTokenAuthentication?> GetAccessTokenAuthentication(string tempToken, DropboxKeys dropboxKeys)
+    private static DropboxKeys GetDropboxKeys()
     {
-        using var httpClient = new HttpClient();
+        var assembly = Assembly.GetAssembly(typeof(DropboxService))!;
+        var resources = assembly.GetManifestResourceNames();
+        var resourceName = resources.First(s => s.Contains("DropboxKeys.json"));
+
+        using var stream = assembly.GetManifestResourceStream(resourceName)!;
+        using var reader = new StreamReader(stream);
+        var jsonStr = reader.ReadToEnd();
+
+        return JsonConvert.DeserializeObject<DropboxKeys>(jsonStr)!;
+    }
+
+    private async Task<AccessTokenAuthentication?> GetAccessTokenAuthentication(string tempToken)
+    {
+        using var httpClient = Http.GetHttpClient();
         var requestData = new Dictionary<string, string>
         {
             { "code", tempToken },
             { "grant_type", "authorization_code" },
-            { "client_id", dropboxKeys.AppKey! },
-            { "client_secret", dropboxKeys.AppSecret! },
-            { "redirect_uri", dropboxKeys.RedirectUri! }
+            { "client_id", DropboxKeys.AppKey! },
+            { "client_secret", DropboxKeys.AppSecret! },
+            { "redirect_uri", DropboxKeys.RedirectUri! }
         };
 
         var requestContent = new FormUrlEncodedContent(requestData);
@@ -69,13 +103,13 @@ public class DropboxService
 
     }
 
-    private static string? GetTempToken(DropboxKeys dropboxKeys)
+    private string? GetTempToken()
     {
         var httpListener = new HttpListener();
-        httpListener.Prefixes.Add(dropboxKeys.RedirectUri!);
+        httpListener.Prefixes.Add(DropboxKeys.RedirectUri!);
         httpListener.Start();
 
-        var uri = $"https://www.dropbox.com/oauth2/authorize?client_id={dropboxKeys.AppKey}&redirect_uri={dropboxKeys.RedirectUri}&response_type=code&token_access_type=offline";
+        var uri = $"https://www.dropbox.com/oauth2/authorize?client_id={DropboxKeys.AppKey}&redirect_uri={DropboxKeys.RedirectUri}&response_type=code&token_access_type=offline";
         var process = new Process();
         process.StartInfo.UseShellExecute = true;
         process.StartInfo.CreateNoWindow = false;
