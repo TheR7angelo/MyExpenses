a181c8510aa03f20a7f958ddcce98785557dcb43
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-27 10:37:05 +0200

    ci(Update): Remove redundant Dropbox authentication code
    The redundant Dropbox authentication code in the DropboxApiTest.cs class
    is removed. These changes simplify the Test method by directly using the
    new DropboxService instead of manually handling the token acquisition
    and management, which is already taken care of in the DropboxService
    class.

--- Tests/MyExpenses.WebApi.Test/Dropbox/DropboxApiTest.cs
+++ Tests/MyExpenses.WebApi.Test/Dropbox/DropboxApiTest.cs
@@ -16,46 +16,6 @@ public class DropboxApiTest(ITestOutputHelper testOutputHelper)
     [Fact]
     private async Task Test()
     {
-        // var directorySecretKeys = Path.GetFullPath("Api");
-        // directorySecretKeys = Path.Join(directorySecretKeys, "Dropbox");
-        //
-        // var directoryInfo = Directory.CreateDirectory(directorySecretKeys);
-        // directoryInfo = directoryInfo.Parent;
-        // if (directoryInfo is not null) directoryInfo.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
-        //
-        // var filePathSecretKeys = Path.Join(directorySecretKeys, "AccessTokenAuthentication.json");
-        //
-        // AccessTokenAuthentication? accessTokenAuthentication;
-        // if (!File.Exists(filePathSecretKeys))
-        // {
-        //     var assembly = Assembly.GetAssembly(typeof(MyExpenses.WebApi.Nominatim.Nominatim))!;
-        //     var resources = assembly.GetManifestResourceNames();
-        //     var resourceName = resources.First(s => s.Contains("DropboxKeys.json"));
-        //
-        //     await using var stream = assembly.GetManifestResourceStream(resourceName)!;
-        //     using var reader = new StreamReader(stream);
-        //     var jsonStr = await reader.ReadToEndAsync();
-        //
-        //      var dropboxKeys = JsonConvert.DeserializeObject<DropboxKeys>(jsonStr)!;
-        //
-        //     var tempToken = GetTempToken(dropboxKeys)!;
-        //     accessTokenAuthentication = await GetAccessTokenAuthentication(tempToken, dropboxKeys);
-        //     if (accessTokenAuthentication is not null)
-        //     {
-        //         accessTokenAuthentication.DateCreated = DateTime.Now;
-        //         accessTokenAuthentication.DateExpiration = DateTime.Now.AddSeconds(accessTokenAuthentication.ExpiresIn ?? 0);
-        //     }
-        //
-        //     await File.WriteAllTextAsync(filePathSecretKeys, JsonConvert.SerializeObject(accessTokenAuthentication, Formatting.Indented));
-        // }
-        // else
-        // {
-        //     var jsonStr = await File.ReadAllTextAsync(filePathSecretKeys);
-        //     accessTokenAuthentication = JsonConvert.DeserializeObject<AccessTokenAuthentication>(jsonStr);
-        // }
-        //
-        // if (accessTokenAuthentication is null) return;
-
         var dropboxService = new DropboxService();
         if (dropboxService.AccessTokenAuthentication is null) dropboxService.AuthorizeApplication();
 
@@ -72,54 +32,5 @@ public class DropboxApiTest(ITestOutputHelper testOutputHelper)
             using var memStream = new MemoryStream(Encoding.UTF8.GetBytes(content));
             await client.Files.UploadAsync("/test.txt", WriteMode.Overwrite.Instance, body: memStream);
         }
-    }
-
-    private static async Task<AccessTokenAuthentication?> GetAccessTokenAuthentication(string tempToken, DropboxKeys dropboxKeys)
-    {
-        using var httpClient = new HttpClient();
-        var requestData = new Dictionary<string, string>
-        {
-            { "code", tempToken },
-            { "grant_type", "authorization_code" },
-            { "client_id", dropboxKeys.AppKey! },
-            { "client_secret", dropboxKeys.AppSecret! },
-            { "redirect_uri", dropboxKeys.RedirectUri! }
-        };
-
-        var requestContent = new FormUrlEncodedContent(requestData);
-        var response = await httpClient.PostAsync("https://api.dropbox.com/oauth2/token", requestContent);
-        var responseContent = await response.Content.ReadAsStringAsync();
-
-        var accessTokenResponse = JsonConvert.DeserializeObject<AccessTokenAuthentication>(responseContent);
-        return accessTokenResponse;
-
-    }
-
-    private static string? GetTempToken(DropboxKeys dropboxKeys)
-    {
-        var httpListener = new HttpListener();
-        httpListener.Prefixes.Add(dropboxKeys.RedirectUri!);
-        httpListener.Start();
-
-        var uri = $"https://www.dropbox.com/oauth2/authorize?client_id={dropboxKeys.AppKey}&redirect_uri={dropboxKeys.RedirectUri}&response_type=code&token_access_type=offline";
-        var process = new Process();
-        process.StartInfo.UseShellExecute = true;
-        process.StartInfo.CreateNoWindow = false;
-        process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
-        process.StartInfo.FileName = uri;
-        process.Start();
-
-        var context = httpListener.GetContext();
-        var response = context.Response;
-
-        using var writer = new StreamWriter(response.OutputStream);
-        writer.Write("You've successfully authenticated!");
-
-        var tempToken = context.Request.QueryString["code"];
-
-        response.Close();
-        httpListener.Close();
-
-        return tempToken;
     }
 }
