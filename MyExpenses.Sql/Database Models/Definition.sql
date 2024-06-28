320657b80ae468911abeb6851469c0b572f66b2a
TheR7angelo <raphael.antunes@hotmail.fr>
2024-06-28 18:37:30 +0200

    ci(Update): Update date condition in SQL script
    The date comparisons in the Definition.sql script were modified.
    Specifically, the year comparison in the WHERE clause was adjusted to
    only fetch years that are less than the current one from history. An
    additional condition was also introduced to ensure month values are
    bounded within the current year.

--- MyExpenses.Sql/Database Models/Definition.sql
+++ MyExpenses.Sql/Database Models/Definition.sql
@@ -560,8 +560,9 @@ WITH all_periods AS (SELECT a.id                     AS account_fk,
                                                 SELECT '11'
                                                 UNION
                                                 SELECT '12')) m
-                     WHERE y.year <= (SELECT strftime('%Y', MAX(date)) FROM t_history)
-                         AND m.month <= (SELECT strftime('%m', MAX(date)) FROM t_history)),
+                     WHERE y.year < (SELECT strftime('%Y', MAX(date)) FROM t_history)
+                        OR (y.year == (SELECT strftime('%Y', MAX(date)) FROM t_history)
+                         AND m.month <= (SELECT strftime('%m', MAX(date)) FROM t_history))),
      monthly AS (SELECT ap.account_fk,
                         ap.account,
                         ap.period,
