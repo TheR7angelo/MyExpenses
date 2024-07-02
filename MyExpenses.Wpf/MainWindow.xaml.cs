5978bca2cbe79d3edf366169b969524362abafca
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-02 13:28:48 +0200

    ci(Update): Implement database export feature
    The database export functionality has now been added to the 'MyExpenses'
    application. This includes options to save the database locally or on
    Dropbox, leveraged by new utility classes. Also, error handling has been
    incorporated to safeguard the process against potential exceptions.

--- MyExpenses.Wpf/MainWindow.xaml.cs
+++ MyExpenses.Wpf/MainWindow.xaml.cs
@@ -1,8 +1,16 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
+using Dropbox.Api.Files;
+using MyExpenses.Models.Wpf.Save;
+using MyExpenses.Sql.Context;
 using MyExpenses.Utils.WindowStyle;
+using MyExpenses.WebApi.Dropbox;
 using MyExpenses.Wpf.Resources.Resx.Windows.MainWindow;
+using MyExpenses.Wpf.Utils.FilePicker;
+using MyExpenses.Wpf.Windows;
+using Serilog;
 
 namespace MyExpenses.Wpf;
 
@@ -67,10 +75,58 @@ public partial class MainWindow
         }
     }
 
-    private void MenuItemDatabaseExport_OnClick(object sender, RoutedEventArgs e)
+    private async void MenuItemDatabaseExport_OnClick(object sender, RoutedEventArgs e)
     {
         //TODO work
         Console.WriteLine("Need export");
+
+        var saveLocationWindow = new SaveLocationWindow();
+        saveLocationWindow.ShowDialog();
+
+        if (saveLocationWindow.DialogResult is not true) return;
+
+        var database = DataBaseContext.FilePath!;
+
+        try
+        {
+            //TODO wait screen
+            switch (saveLocationWindow.SaveLocationResult)
+            {
+                case SaveLocation.Local:
+                    await SaveToLocal(database);
+                    Log.Information("Database was successfully save to local");
+                    break;
+                case SaveLocation.Dropbox:
+                    await SaveToCloudAsync(database);
+                    Log.Information("Database was successfully save to cloud");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        catch (Exception exception)
+        {
+            Log.Error(exception, "An error occur please retry");
+        }
+
+        //TODO make messagebox result
+    }
+
+    private static async Task<FileMetadata> SaveToCloudAsync(string database)
+    {
+        var dropboxService = new DropboxService();
+        return await dropboxService.UploadFileAsync(database, DbContextBackup.CloudDirectoryBackupDatabase);
+    }
+
+    private static async Task SaveToLocal(string database)
+    {
+        var sqliteDialog = new SqliteFileDialog();
+        var selectedDialog = sqliteDialog.SaveFile();
+
+        if (string.IsNullOrEmpty(selectedDialog)) return;
+
+        selectedDialog = Path.ChangeExtension(selectedDialog, DbContextBackup.Extension);
+        await Task.Run(() => File.Copy(database, selectedDialog, true));
     }
 
     private void MenuItemDatabaseImport_OnClick(object sender, RoutedEventArgs e)
