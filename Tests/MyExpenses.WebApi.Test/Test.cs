8c5750126a5ddca038937fa53b8edcf8f209aabc
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-05 14:22:20 +0200

    ci(Update): Add GithubTest method in WebApi Test
    This update introduces the GithubTest method in the
    MyExpenses.WebApi.Test class. This new method was implemented to test
    the connection and fetch data from a specified GitHub repository,
    wherein the call response is validated and its content is printed.

---
+++ Tests/MyExpenses.WebApi.Test/Test.cs
@@ -0,0 +1,19 @@
+namespace MyExpenses.WebApi.Test;
+
+public class Test
+{
+    [Fact]
+    private async Task GithubTest()
+    {
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("User-Agent", "Anything");
+        httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", "Nope");
+
+        var response = await httpClient.GetAsync("https://api.github.com/repos/TheR7angelo/AutoFiberC6/releases/latest");
+
+        response.EnsureSuccessStatusCode();
+        var responseBody = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine(responseBody);
+    }
+}
