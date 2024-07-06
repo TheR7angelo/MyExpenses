cd20dc6caddc2389f916f19b2083855376f4c838
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-06 20:51:38 +0200

    ci(Update): Refactor convert method in EmptyStringTreeViewConverter
    The original convert method is abstracted into the ToUnknown function
    for better readability and possible future reuse. It is now called in
    the Convert method, enhancing the code's structure without changing its
    functionality.

--- MyExpenses.Wpf/Converters/EmptyStringTreeViewConverter.cs
+++ MyExpenses.Wpf/Converters/EmptyStringTreeViewConverter.cs
@@ -8,11 +8,16 @@ public class EmptyStringTreeViewConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        return value is string str && !string.IsNullOrWhiteSpace(str) ? str : EmptyStringTreeViewConverterResources.Unknown;
+        return ToUnknown(value);
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         return Binding.DoNothing;
+    }
+
+    public static string ToUnknown(object? value)
+    {
+        return value is string str && !string.IsNullOrWhiteSpace(str) ? str : EmptyStringTreeViewConverterResources.Unknown;
     }
 }
