4a82e7f49fc8922689c540b66feaa15933aad22f
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-07 12:02:16 +0200

    ci(Update): Add KML generation for single place in GoogleMaps tests
    This update introduces a new test method for generating a KML document
    representing a single place from the database. The generated KML
    includes the place's name and coordinates, along with some simple data
    attributes. The KML document is then saved to a file named
    "location.kml".

--- Tests/MyExpenses.WebApi.Test/Maps/GoogleMaps.cs
+++ Tests/MyExpenses.WebApi.Test/Maps/GoogleMaps.cs
@@ -41,10 +41,54 @@ public class StreetViewTest
     }
 
     [Fact]
+    private void GoToKmlPlace()
+    {
+        using var context = new DataBaseContext();
+        var place = context.TPlaces.First(s =>
+            s.Latitude != null && s.Latitude != 0 && s.Longitude != null && s.Longitude != 0);
+
+        XNamespace ns = "http://www.opengis.net/kml/2.2";
+
+        var kml = new XDocument(
+            new XDeclaration("1.0", "UTF-8", string.Empty),
+            new XElement(ns + "kml",
+                new XElement(ns + "Document", new XAttribute("id", "root_doc"),
+                    new XElement(ns + "Schema", new XAttribute("name", "location_data"),
+                        new XAttribute("id", "location_data"),
+                        new XElement(ns + "SimpleField", new XAttribute("name", "attribute1"),
+                            new XAttribute("type", "string")),
+                        new XElement(ns + "SimpleField", new XAttribute("name", "attribute2"),
+                            new XAttribute("type", "int")),
+                        new XElement(ns + "SimpleField", new XAttribute("name", "attribute3"),
+                            new XAttribute("type", "string"))
+                    ),
+                    new XElement(ns + "Placemark",
+                        new XElement(ns + "name", place.Name),
+                        new XElement(ns + "ExtendedData",
+                            new XElement(ns + "SchemaData", new XAttribute("schemaUrl", "#location_data"),
+                                new XElement(ns + "SimpleData", new XAttribute("name", "attribute1"), "value1"),
+                                new XElement(ns + "SimpleData", new XAttribute("name", "attribute2"), "123"),
+                                new XElement(ns + "SimpleData", new XAttribute("name", "attribute3"), "value3")
+                            )
+                        ),
+                        new XElement(ns + "Point",
+                            new XElement(ns + "coordinates",
+                                $"{place.Longitude!.Value.ToString(CultureInfo.InvariantCulture)},{place.Latitude!.Value.ToString(CultureInfo.InvariantCulture)},0")
+                        )
+                    )
+                )
+            )
+        );
+
+        kml.Save("location.kml");
+    }
+
+    [Fact]
     private void GoToKmlMultiPlace()
     {
         using var context = new DataBaseContext();
-        var places = context.TPlaces.Where(s => s.Latitude != null && s.Latitude != 0 && s.Longitude != null && s.Longitude != 0).ToList();
+        var places = context.TPlaces
+            .Where(s => s.Latitude != null && s.Latitude != 0 && s.Longitude != null && s.Longitude != 0).ToList();
 
         XNamespace ns = "https://www.opengis.net/kml/2.2";
 
@@ -57,11 +101,12 @@ public class StreetViewTest
 
         foreach (var place in places)
         {
-            kml.Root!.Element(ns+"Document")!.Add(
+            kml.Root!.Element(ns + "Document")!.Add(
                 new XElement(ns + "Placemark",
                     new XElement(ns + "name", place.Name),
                     new XElement(ns + "Point",
-                        new XElement(ns + "coordinates", $"{place.Longitude!.Value.ToString(CultureInfo.InvariantCulture)},{place.Latitude!.Value.ToString(CultureInfo.InvariantCulture)},0")
+                        new XElement(ns + "coordinates",
+                            $"{place.Longitude!.Value.ToString(CultureInfo.InvariantCulture)},{place.Latitude!.Value.ToString(CultureInfo.InvariantCulture)},0")
                     )
                 )
             );
