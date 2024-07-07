06b7bb7a69755cc22e7f3c33d1d751ed2cae6128
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-07 18:07:58 +0200

    ci(Update): Add KML file writing functionality
    New methods have been added in the Writer class for creating KML files
    from points. This includes methods to handle single points, multiple
    points, and the transformation of a Point into a KML file. The tests for
    these new functionalities have also been created in the WriterTest
    class.

---
+++ MyExpenses.IO/Sig/Kml/Writer.cs
@@ -0,0 +1,99 @@
+using System.Globalization;
+using System.IO.Compression;
+using System.Xml.Linq;
+using NetTopologySuite.Geometries;
+
+namespace MyExpenses.IO.Sig.Kml;
+
+public static class Writer
+{
+        private static XNamespace KmlNamespace => XNamespace.Get("http://www.opengis.net/kml/2.2");
+
+    public static void ToKmlFile(this IEnumerable<Point> points, string fileSavePath, string name = "Point")
+    {
+        var extension = Path.GetExtension(fileSavePath).ToLower();
+
+        var extensions = new List<string> { ".kml", ".kmz" };
+        if (!extensions.Contains(extension))
+        {
+            throw new ArgumentException($"The file extension must be .kml or .kmz. The provided extension was {extension}.");
+        }
+
+        var kml = new XDocument(
+            new XDeclaration("1.0", "UTF-8", string.Empty),
+            new XElement(KmlNamespace + "kml",
+                new XElement(KmlNamespace + "Document")
+            )
+        );
+
+        foreach (var obj in points.Select((point, i) => new { i, point }))
+        {
+            var indexedName = $"{name} {obj.i + 1}";
+            var (yInvariant, xInvariant) = obj.point.ToInvariantCoordinate();
+
+            kml.Root!.Element(KmlNamespace +"Document")!.Add(
+                new XElement(KmlNamespace + "Placemark",
+                    new XElement(KmlNamespace + "name", indexedName),
+                    new XElement(KmlNamespace + "Point",
+                        new XElement(KmlNamespace + "coordinates",
+                            $"{yInvariant}, {xInvariant}, 0")
+                    )
+                )
+            );
+        }
+
+        SaveToKmlKmzFile(fileSavePath, kml, extension);
+    }
+
+    public static void ToKmlFile(this Point point, string fileSavePath, string name = "Point")
+    {
+        var extension = Path.GetExtension(fileSavePath).ToLower();
+
+        var extensions = new List<string> { ".kml", ".kmz" };
+        if (!extensions.Contains(extension))
+        {
+            throw new ArgumentException($"The file extension must be .kml or .kmz. The provided extension was {extension}.");
+        }
+
+        var (yInvariant, xInvariant) = point.ToInvariantCoordinate();
+
+        var kml = new XDocument(
+            new XDeclaration("1.0", "UTF-8", string.Empty),
+            new XElement(KmlNamespace + "kml",
+                new XElement(KmlNamespace + "Placemark",
+                    new XElement(KmlNamespace + "name", name),
+                    new XElement(KmlNamespace + "Point",
+                        new XElement(KmlNamespace + "coordinates",
+                            $"{xInvariant}, {yInvariant}, 0")))));
+
+        SaveToKmlKmzFile(fileSavePath, kml, extension);
+    }
+
+    private static void SaveToKmlKmzFile(string fileSavePath, XDocument kml, string extension)
+    {
+        var tempFilePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+
+        tempFilePath = Path.ChangeExtension(tempFilePath, ".kml");
+        kml.Save(tempFilePath);
+
+        if (extension == ".kmz")
+        {
+            using var zip = ZipFile.Open(fileSavePath, ZipArchiveMode.Create);
+            zip.CreateEntryFromFile(tempFilePath, Path.GetFileName(tempFilePath));
+
+            File.Delete(tempFilePath);
+        }
+        else
+        {
+            File.Move(tempFilePath, fileSavePath, true);
+        }
+    }
+
+    private static (string YInvariant, string XInvariant) ToInvariantCoordinate(this Point point)
+    {
+        var yInvariant = point.Y.ToString(CultureInfo.InvariantCulture);
+        var xInvariant = point.X.ToString(CultureInfo.InvariantCulture);
+
+        return (yInvariant, xInvariant);
+    }
+}
---
+++ Tests/MyExpenses.IO.Test/Sig/Kml/WriterTest.cs
@@ -0,0 +1,82 @@
+using MyExpenses.IO.Sig.Kml;
+using MyExpenses.Models.IO.Sig.Keyhole_Markup_Language;
+using MyExpenses.Sql.Context;
+using NetTopologySuite.Geometries;
+
+namespace MyExpenses.IO.Test.Sig.Kml;
+
+public class WriterTest
+{
+    private Point Point { get; } = new(-0.5754690669950785, 44.837624634089);
+
+    [Fact]
+    private void GoToKml()
+    {
+        const string fileSavePath = "location.kml";
+        Point.ToKmlFile(fileSavePath);
+
+        Assert.True(File.Exists(fileSavePath));
+    }
+
+    [Fact]
+    private void GoToKmlMultiPoint()
+    {
+        using var context = new DataBaseContext();
+        var points = context.TPlaces
+            .Where(s => s.Latitude != null && s.Latitude != 0 && s.Longitude != null && s.Longitude != 0)
+            .Select(s => s.Geometry).ToList();
+
+        const string fileSavePath = "locations.kml";
+        points.ToKmlFile(fileSavePath);
+
+        Assert.True(File.Exists(fileSavePath));
+    }
+
+    [Fact]
+    private void GoToKmlPlace()
+    {
+        using var context = new DataBaseContext();
+        var place = context.TPlaces.First(s =>
+            s.Latitude != null && s.Latitude != 0 && s.Longitude != null && s.Longitude != 0);
+
+        var mapping = Models.AutoMapper.Mapping.Mapper;
+        var placeSig = mapping.Map<PlaceSig>(place);
+
+        var properties = placeSig.GetType().GetProperties();
+
+        // XNamespace ns = "http://www.opengis.net/kml/2.2";
+        //
+        // var kml = new XDocument(
+        //     new XDeclaration("1.0", "UTF-8", string.Empty),
+        //     new XElement(ns + "kml",
+        //         new XElement(ns + "Document", new XAttribute("id", "root_doc"),
+        //             new XElement(ns + "Schema", new XAttribute("name", "location_data"),
+        //                 new XAttribute("id", "location_data"),
+        //                 new XElement(ns + "SimpleField", new XAttribute("name", "attribute1"),
+        //                     new XAttribute("type", "string")),
+        //                 new XElement(ns + "SimpleField", new XAttribute("name", "attribute2"),
+        //                     new XAttribute("type", "int")),
+        //                 new XElement(ns + "SimpleField", new XAttribute("name", "attribute3"),
+        //                     new XAttribute("type", "string"))
+        //             ),
+        //             new XElement(ns + "Placemark",
+        //                 new XElement(ns + "name", place.Name),
+        //                 new XElement(ns + "ExtendedData",
+        //                     new XElement(ns + "SchemaData", new XAttribute("schemaUrl", "#location_data"),
+        //                         new XElement(ns + "SimpleData", new XAttribute("name", "attribute1"), "value1"),
+        //                         new XElement(ns + "SimpleData", new XAttribute("name", "attribute2"), "123"),
+        //                         new XElement(ns + "SimpleData", new XAttribute("name", "attribute3"), "value3")
+        //                     )
+        //                 ),
+        //                 new XElement(ns + "Point",
+        //                     new XElement(ns + "coordinates",
+        //                         $"{place.Longitude!.Value.ToString(CultureInfo.InvariantCulture)},{place.Latitude!.Value.ToString(CultureInfo.InvariantCulture)},0")
+        //                 )
+        //             )
+        //         )
+        //     )
+        // );
+        //
+        // kml.Save("location.kml");
+    }
+}
