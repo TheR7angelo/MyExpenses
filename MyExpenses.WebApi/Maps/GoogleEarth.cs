969823f7d919dfe5ad1e0ce0461adaf26492fa39
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-07 10:35:33 +0200

    ci(Update): Refactor GoogleEarth coordinate conversion
    The method for converting coordinates for use with Google Earth has been
    updated. The conversion now uses a separate function,
    `ToInvariantCoordinate()`, to simplify the `GoToGoogleEarthWeb()`
    method. This provides a better separation of concerns and improves
    overall readability of the code.

--- MyExpenses.WebApi/Maps/GoogleEarth.cs
+++ MyExpenses.WebApi/Maps/GoogleEarth.cs
@@ -12,8 +12,7 @@ public static class GoogleEarth
 
     public static void GoToGoogleEarthWeb(this Point point, int altitudeLevel = 200)
     {
-        var yInvariant = point.Y.ToString(CultureInfo.InvariantCulture);
-        var xInvariant = point.X.ToString(CultureInfo.InvariantCulture);
+        var (yInvariant, xInvariant) = point.ToInvariantCoordinate();
 
         var googleEarthUrl = $"https://earth.google.com/web/@{yInvariant},{xInvariant},{altitudeLevel}a,0d,30y,0h,0t,0r";
 
@@ -22,5 +21,13 @@ public static class GoogleEarth
             FileName = googleEarthUrl,
             UseShellExecute = true
         });
+    }
+
+    private static (string YInvariant, string XInvariant) ToInvariantCoordinate(this Point point)
+    {
+        var yInvariant = point.Y.ToString(CultureInfo.InvariantCulture);
+        var xInvariant = point.X.ToString(CultureInfo.InvariantCulture);
+
+        return (yInvariant, xInvariant);
     }
 }
