44bf40c695ece1f97b742e722743b68a41bb83b7
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-08 13:40:34 +0200

    ci(Update): Add YAxis and text paint to AccountValueTrendControl
    The YAxis has been added to the CartesianChart in
    AccountValueTrendControl.xaml along with label text paint. Additionally,
    labels are transformed in `SetXAxis` method to give users a friendlier
    format, and the same text paint is applied. Some necessary usings were
    also added to AccountValueTrendControl.xaml.cs.

--- MyExpenses.Wpf/UserControls/Analytics/AccountValueTrendControl.xaml.cs
+++ MyExpenses.Wpf/UserControls/Analytics/AccountValueTrendControl.xaml.cs
@@ -1,8 +1,12 @@
+using System.Globalization;
+using System.Windows.Media;
 using LiveChartsCore;
 using LiveChartsCore.Kernel.Sketches;
 using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
 using MyExpenses.Models.Sql.Views;
 using MyExpenses.Sql.Context;
+using SkiaSharp.Views.WPF;
 
 namespace MyExpenses.Wpf.UserControls.Analytics;
 
@@ -10,9 +14,17 @@ public partial class AccountValueTrendControl
 {
     public ISeries[] Series { get; set; } = null!;
     public ICartesianAxis[] XAxis { get; set; } = null!;
+    public ICartesianAxis[] YAxis { get; set; } = null!;
+
+    public SolidColorPaint TextPaint { get; }
 
     public AccountValueTrendControl()
     {
+        // TODO add listener color change
+        var brush = (SolidColorBrush)FindResource("MaterialDesignBody");
+        var wpfColor = brush.Color;
+        TextPaint = new SolidColorPaint(wpfColor.ToSKColor());
+
         SetChart();
 
         InitializeComponent();
@@ -30,11 +42,38 @@ public partial class AccountValueTrendControl
 
         SetSeries(groupsVAccountMonthlyCumulativeSums);
         SetXAxis(axis);
+        SetYAxis();
+    }
+
+    private void SetYAxis()
+    {
+        var axis = new Axis
+        {
+            LabelsPaint = TextPaint
+        };
+        YAxis = [axis];
     }
 
     private void SetXAxis(IEnumerable<string> labels)
     {
-        var axis = new Axis { Labels = labels.ToList() };
+        var transformedLabels = labels.Select(label =>
+        {
+            var labelSplit = label.Split('-');
+
+            if (!int.TryParse(labelSplit[0], out var year) || !int.TryParse(labelSplit[1], out var month))
+                throw new FormatException($"Invalid format for label '{label}'. Expected 'YYYY-MM' format.");
+
+            var d = new DateOnly(year, month, 1);
+            var newLabel = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(d.ToString("Y"));
+
+            return newLabel;
+        }).ToList();
+
+        var axis = new Axis
+        {
+            Labels = transformedLabels,
+            LabelsPaint = TextPaint
+        };
         XAxis = [axis];
     }
 
