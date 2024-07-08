578e3df11cecbd109fd3a0fb64dc673db5a61b52
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-08 11:42:36 +0200

    ci(Update): Add CartesianChart to AccountValueTrendControl
    A CartesianChart from LiveChartsCore has been added to the
    AccountValueTrendControl XAML and methods for setting up the Series and
    XAxis have been implemented in the corresponding .cs file. This provides
    a visual representation of trends in account values.

--- MyExpenses.Wpf/UserControls/Analytics/AccountValueTrendControl.xaml.cs
+++ MyExpenses.Wpf/UserControls/Analytics/AccountValueTrendControl.xaml.cs
@@ -1,10 +1,24 @@
+using LiveChartsCore;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.SkiaSharpView;
+using MyExpenses.Models.Sql.Views;
 using MyExpenses.Sql.Context;
 
 namespace MyExpenses.Wpf.UserControls.Analytics;
 
 public partial class AccountValueTrendControl
 {
+    public ISeries[] Series { get; set; } = null!;
+    public ICartesianAxis[] XAxis { get; set; } = null!;
+
     public AccountValueTrendControl()
+    {
+        SetChart();
+
+        InitializeComponent();
+    }
+
+    private void SetChart()
     {
         using var context = new DataBaseContext();
         var groupsVAccountMonthlyCumulativeSums = context.VAccountMonthlyCumulativeSums
@@ -12,6 +26,36 @@ public partial class AccountValueTrendControl
             .GroupBy(s => s.Account)
             .ToList();
 
-        InitializeComponent();
+        var axis = groupsVAccountMonthlyCumulativeSums.First().Select(s => s.Period!);
+
+        SetSeries(groupsVAccountMonthlyCumulativeSums);
+        SetXAxis(axis);
+    }
+
+    private void SetXAxis(IEnumerable<string> labels)
+    {
+        var axis = new Axis { Labels = labels.ToList() };
+        XAxis = [axis];
+    }
+
+    private void SetSeries(List<IGrouping<string?, VAccountMonthlyCumulativeSum>> groupsVAccountMonthlyCumulativeSums)
+    {
+        var series = new List<ISeries>();
+        foreach (var groupVAccountMonthlyCumulativeSums in groupsVAccountMonthlyCumulativeSums)
+        {
+            var name = groupVAccountMonthlyCumulativeSums.Key;
+            var values = groupVAccountMonthlyCumulativeSums.Select(s => (double)s.CumulativeSum!);
+
+            var lineSeries = new LineSeries<double>
+            {
+                Name = name,
+                Values = values,
+                Fill = null
+            };
+
+            series.Add(lineSeries);
+        }
+
+        Series = series.ToArray();
     }
 }
