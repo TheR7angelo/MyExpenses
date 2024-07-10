660e17bd7ddf0b36cd3a85a13967277942b591ae
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-10 15:12:41 +0200

    ci(Update): Add click events to ColorPickerWindow buttons
    The "Valid" and "Cancel" buttons in the ColorPickerWindow.xaml now have
    respective click events assigned to them. In addition,
    ButtonValid_OnClick and ButtonCancel_OnClick methods were implemented in
    the ColorPickerWindow.xaml.cs to handle these events. This gives
    functionality to these buttons, allowing them to perform actions when
    clicked.

--- MyExpenses.Wpf/Windows/ColorPickerWindow.xaml.cs
+++ MyExpenses.Wpf/Windows/ColorPickerWindow.xaml.cs
@@ -1,9 +1,29 @@
+using System.Windows;
+using System.Windows.Media;
+
 namespace MyExpenses.Wpf.Windows;
 
 public partial class ColorPickerWindow
 {
+    public Color? ColorResult { get; private set; }
+
     public ColorPickerWindow()
     {
         InitializeComponent();
+    }
+
+    private void ButtonValid_OnClick(object sender, RoutedEventArgs e)
+    {
+        ColorResult = ColorPickerControl.Color;
+        DialogResult = true;
+
+        Close();
+    }
+
+    private void ButtonCancel_OnClick(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+
+        Close();
     }
 }
