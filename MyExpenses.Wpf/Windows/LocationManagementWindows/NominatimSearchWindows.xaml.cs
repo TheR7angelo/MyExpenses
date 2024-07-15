9f618b1484cbad119537d7b9ffab63aa3102462a
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-15 09:32:57 +0200

    ci(Update): Update TextBox hints to be bindable properties
    TextBox hints in NominatimSearchWindows have been updated to bind to
    properties, providing greater flexibility for potential changes to hints
    in the future. Additionally, some unused code has been removed to neaten
    up the codebase. This allows for better maintainability and easier
    future alterations.

--- MyExpenses.Wpf/Windows/LocationManagementWindows/NominatimSearchWindows.xaml.cs
+++ MyExpenses.Wpf/Windows/LocationManagementWindows/NominatimSearchWindows.xaml.cs
@@ -1,36 +1,88 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Interop;
 using Mapsui.Layers;
 using Mapsui.Styles;
 using MyExpenses.Models.AutoMapper;
+using MyExpenses.Models.Config;
+using MyExpenses.Models.Config.Interfaces;
 using MyExpenses.Models.Sql.Tables;
 using MyExpenses.Utils.WindowStyle;
 using MyExpenses.Wpf.Utils.Maps;
 
 namespace MyExpenses.Wpf.Windows.LocationManagementWindows;
 
-public partial class NominatimSearchWindows : INotifyPropertyChanged
+public partial class NominatimSearchWindows
 {
-    public event PropertyChangedEventHandler? PropertyChanged;
+    public static readonly DependencyProperty PlaceNameHintAssistProperty = DependencyProperty.Register(
+        nameof(PlaceNameHintAssist),
+        typeof(string), typeof(NominatimSearchWindows), new PropertyMetadata(default(string)));
 
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    public string PlaceNameHintAssist
+    {
+        get => (string)GetValue(PlaceNameHintAssistProperty);
+        set => SetValue(PlaceNameHintAssistProperty, value);
+    }
 
-    private List<TPlace> Places { get; } = [];
+    public static readonly DependencyProperty PlaceNumberHintAssistProperty =
+        DependencyProperty.Register(nameof(PlaceNumberHintAssist), typeof(string), typeof(NominatimSearchWindows),
+            new PropertyMetadata(default(string)));
 
-    private TPlace _currentPlace { get; set; } = new();
+    public string PlaceNumberHintAssist
+    {
+        get => (string)GetValue(PlaceNumberHintAssistProperty);
+        set => SetValue(PlaceNumberHintAssistProperty, value);
+    }
+
+    public static readonly DependencyProperty CurrentPlaceProperty = DependencyProperty.Register(nameof(CurrentPlace),
+        typeof(TPlace), typeof(NominatimSearchWindows), new PropertyMetadata(default(TPlace)));
 
     public TPlace CurrentPlace
     {
-        get => _currentPlace;
-        private set
-        {
-            _currentPlace = value;
-            OnPropertyChanged();
-        }
+        get => (TPlace)GetValue(CurrentPlaceProperty);
+        set => SetValue(CurrentPlaceProperty, value);
     }
+
+    public static readonly DependencyProperty PlaceStreetHintAssistProperty =
+        DependencyProperty.Register(nameof(PlaceStreetHintAssist), typeof(string), typeof(NominatimSearchWindows),
+            new PropertyMetadata(default(string)));
+
+    public string PlaceStreetHintAssist
+    {
+        get => (string)GetValue(PlaceStreetHintAssistProperty);
+        set => SetValue(PlaceStreetHintAssistProperty, value);
+    }
+
+    public static readonly DependencyProperty PlacePostalHintAssistProperty =
+        DependencyProperty.Register(nameof(PlacePostalHintAssist), typeof(string), typeof(NominatimSearchWindows),
+            new PropertyMetadata(default(string)));
+
+    public string PlacePostalHintAssist
+    {
+        get => (string)GetValue(PlacePostalHintAssistProperty);
+        set => SetValue(PlacePostalHintAssistProperty, value);
+    }
+
+    public static readonly DependencyProperty PlaceCityHintAssistProperty =
+        DependencyProperty.Register(nameof(PlaceCityHintAssist), typeof(string), typeof(NominatimSearchWindows),
+            new PropertyMetadata(default(string)));
+
+    public string PlaceCityHintAssist
+    {
+        get => (string)GetValue(PlaceCityHintAssistProperty);
+        set => SetValue(PlaceCityHintAssistProperty, value);
+    }
+
+    public static readonly DependencyProperty PlaceCountryHintAssistProperty =
+        DependencyProperty.Register(nameof(PlaceCountryHintAssist), typeof(string), typeof(NominatimSearchWindows),
+            new PropertyMetadata(default(string)));
+
+    public string PlaceCountryHintAssist
+    {
+        get => (string)GetValue(PlaceCountryHintAssistProperty);
+        set => SetValue(PlaceCountryHintAssistProperty, value);
+    }
+
+    private List<TPlace> Places { get; } = [];
 
     private int Index { get; set; }
     private int Total { get; set; }
@@ -43,12 +95,24 @@ public partial class NominatimSearchWindows : INotifyPropertyChanged
         map.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
         map.Layers.Add(WritableLayer);
 
+        Interface.LanguageChanged += Interface_OnLanguageChanged;
         InitializeComponent();
 
         var hWnd = new WindowInteropHelper(GetWindow(this)!).EnsureHandle();
         hWnd.SetWindowCornerPreference(DwmWindowCornerPreference.Round);
 
         MapControl.Map = map;
+    }
+
+    private void Interface_OnLanguageChanged(object sender, ConfigurationLanguageChangedEventArgs e)
+    {
+        //TODO work
+        PlaceNameHintAssist = "Name :";
+        PlaceNumberHintAssist = "Number :";
+        PlaceStreetHintAssist = "Street :";
+        PlacePostalHintAssist = "Postal code :";
+        PlaceCityHintAssist = "City :";
+        PlaceCountryHintAssist = "Country :";
     }
 
     public void AddRange(IEnumerable<TPlace> places)
@@ -65,7 +129,7 @@ public partial class NominatimSearchWindows : INotifyPropertyChanged
         if (Index.Equals(0)) Index = Total;
         if (Index.Equals(Total + 1)) Index = 1;
 
-        CurrentPlace = Places[Index-1];
+        CurrentPlace = Places[Index - 1];
         UpdatePointFeature();
         UpdateTitle();
     }
