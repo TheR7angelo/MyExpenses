9e44ee6ced15c973b4685bd361b45d78e2369d48
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-28 21:23:02 +0200

    ci(Update): Add VacuumDatabaseUpdateWindow for database maintenance
    Introduce a new VacuumDatabaseUpdateWindow class and XAML layout to
    support database maintenance activities. This window initializes with a
    collection of SizeDatabases and configures essential properties and
    styles for a consistent user interface experience.

---
+++ MyExpenses.Wpf/Windows/VacuumDatabaseUpdateWindow.xaml.cs
@@ -0,0 +1,22 @@
+using System.Collections.ObjectModel;
+using MyExpenses.Models.IO;
+using MyExpenses.Utils.Collection;
+
+namespace MyExpenses.Wpf.Windows;
+
+public partial class VacuumDatabaseUpdateWindow
+{
+    public ObservableCollection<SizeDatabase> SizeDatabases = [];
+
+    public VacuumDatabaseUpdateWindow(SizeDatabase sizeDatabase)
+    {
+        SizeDatabases.Add(sizeDatabase);
+        InitializeComponent();
+    }
+
+    public VacuumDatabaseUpdateWindow(IEnumerable<SizeDatabase> sizeDatabases)
+    {
+        SizeDatabases.AddRangeAndSort(sizeDatabases, s => s.FileNameWithoutExtension);
+        InitializeComponent();
+    }
+}
