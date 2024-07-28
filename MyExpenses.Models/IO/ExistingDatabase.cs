de4b42b886e4f22562d2e8923d4a2bfa96b7558f
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-28 18:41:59 +0200

    ci(Update): Refactor ExistingDatabase class to use constructor
    Updated the ExistingDatabase class to initialize properties via a
    constructor to ensure proper instantiation. Made properties private to
    encapsulate the data and added a new FileInfo property for enhanced file
    information handling.

--- MyExpenses.Models/IO/ExistingDatabase.cs
+++ MyExpenses.Models/IO/ExistingDatabase.cs
@@ -2,7 +2,17 @@ namespace MyExpenses.Models.IO;
 
 public class ExistingDatabase
 {
-    public string? FilePath { get; init; }
-    public string? FileName => Path.GetFileName(FilePath);
-    public string? FileNameWithoutExtension => Path.GetFileNameWithoutExtension(FilePath);
+    public string FilePath { get; private set; }
+    public string FileName { get; private set; }
+    public string FileNameWithoutExtension { get; private set; }
+    public FileInfo FileInfo { get; private set; }
+
+    public ExistingDatabase(string filePath)
+    {
+        FilePath = filePath;
+        FileName = Path.GetFileName(filePath);
+        FileNameWithoutExtension = Path.GetFileNameWithoutExtension(FilePath);
+        FileInfo = new FileInfo(filePath);
+    }
+
 }
