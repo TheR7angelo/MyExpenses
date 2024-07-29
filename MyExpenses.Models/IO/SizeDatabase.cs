ec7fa88f15ca2f3de483de53c161df5f2139fceb
TheR7angelo <raphael.antunes@hotmail.fr>
2024-07-29 15:06:40 +0200

    ci(Update): Add size change tracking to SizeDatabase
    Modified `SizeDatabase` class to include private fields `_oldSize` and
    `_newSize` with properties to update size change. Introduced
    `GainInBytes` property to reflect the difference, implementing a method
    to calculate and format the size change.

--- MyExpenses.Models/IO/SizeDatabase.cs
+++ MyExpenses.Models/IO/SizeDatabase.cs
@@ -1,8 +1,55 @@
+using MyExpenses.Models.Resources.Resx.IO;
+
 namespace MyExpenses.Models.IO;
 
 public class SizeDatabase
 {
     public required string FileNameWithoutExtension { get; init; }
-    public required long OldSize { get; init; }
-    public required long NewSize { get; init; }
+    private long _oldSize;
+    private long _newSize;
+
+    public long OldSize
+    {
+        get => _oldSize;
+        set
+        {
+            _oldSize = value;
+            UpdateGainInBytes();
+        }
+    } // in bytes
+
+    public long NewSize
+    {
+        get => _newSize;
+        set
+        {
+            _newSize = value;
+            UpdateGainInBytes();
+        }
+    } // in bytes
+
+    public string GainInBytes { get; private set; } = "0.00 o";
+
+    private void UpdateGainInBytes()
+    {
+        var gainInBytes = _oldSize - _newSize;
+
+        var absoluteGain = Math.Abs(gainInBytes);
+        string[] units =
+        [
+            SizeDatabaseResources.ByteUnit, SizeDatabaseResources.KiloByteUnit, SizeDatabaseResources.MegaByteUnit,
+            SizeDatabaseResources.GigaByteUnit, SizeDatabaseResources.TeraByteUnit, SizeDatabaseResources.PetaByteUnit,
+            SizeDatabaseResources.ExaByteUnit, SizeDatabaseResources.ZettaByteUnit, SizeDatabaseResources.YottaByteUnit
+        ];
+        var unitIndex = 0;
+        while (absoluteGain >= 1024 && unitIndex < units.Length - 1)
+        {
+            absoluteGain /= 1024;
+            ++unitIndex;
+        }
+
+        var sign = gainInBytes >= 0 ? "-" : "+";
+
+        GainInBytes = $"{sign}{absoluteGain:F2} {units[unitIndex]}";
+    }
 }
