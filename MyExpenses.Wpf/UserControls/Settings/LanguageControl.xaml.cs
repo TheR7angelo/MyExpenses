e4f8137e905804d8377c3aae002143b8e9107945
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-05 15:39:28 +0200

    ci(Update): Add language support for ComboBox hints and 24h label
    Implemented data bindings for ComboBox hint and 24-hour format label in
    `LanguageControl.xaml`. Added corresponding dependency properties and
    updated language dynamically via event handlers in
    `LanguageControl.xaml.cs`.

--- MyExpenses.Wpf/UserControls/Settings/LanguageControl.xaml.cs
+++ MyExpenses.Wpf/UserControls/Settings/LanguageControl.xaml.cs
@@ -1,5 +1,7 @@
 using System.Globalization;
 using System.Windows;
+using MyExpenses.Models.Config;
+using MyExpenses.Models.Config.Interfaces;
 using MyExpenses.Sql.Context;
 using MyExpenses.Utils;
 
@@ -7,6 +9,30 @@ namespace MyExpenses.Wpf.UserControls.Settings;
 
 public partial class LanguageControl
 {
+    #region Resx
+
+    public static readonly DependencyProperty ComboBoxLanguageSelectorHintAssistProperty =
+        DependencyProperty.Register(nameof(ComboBoxLanguageSelectorHintAssist), typeof(string), typeof(LanguageControl),
+            new PropertyMetadata(default(string)));
+
+    public string ComboBoxLanguageSelectorHintAssist
+    {
+        get => (string)GetValue(ComboBoxLanguageSelectorHintAssistProperty);
+        set => SetValue(ComboBoxLanguageSelectorHintAssistProperty, value);
+    }
+
+    public static readonly DependencyProperty LabelIs24HFormatProperty =
+        DependencyProperty.Register(nameof(LabelIs24HFormat), typeof(string), typeof(LanguageControl),
+            new PropertyMetadata(default(string)));
+
+    public string LabelIs24HFormat
+    {
+        get => (string)GetValue(LabelIs24HFormatProperty);
+        set => SetValue(LabelIs24HFormatProperty, value);
+    }
+
+    #endregion
+
     public static readonly DependencyProperty CultureInfoSelectedProperty =
         DependencyProperty.Register(nameof(CultureInfoSelected), typeof(CultureInfo), typeof(LanguageControl),
             new PropertyMetadata(default(CultureInfo)));
@@ -43,6 +69,19 @@ public partial class LanguageControl
         var configuration = Config.Configuration;
         Is24Hours = configuration.Interface.Clock.Is24Hours;
 
+        Interface.LanguageChanged += Interface_OnLanguageChanged;
+        UpdateLanguage();
+
         InitializeComponent();
+    }
+
+    private void Interface_OnLanguageChanged(object sender, ConfigurationLanguageChangedEventArgs e)
+        => UpdateLanguage();
+
+    private void UpdateLanguage()
+    {
+        // TODO work
+        ComboBoxLanguageSelectorHintAssist = "Choice of application language";
+        LabelIs24HFormat = "Is 24h format";
     }
 }
