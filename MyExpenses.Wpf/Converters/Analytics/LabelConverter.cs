79b42e21945245611a6816ac70653189e88bdea0
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-07 14:30:43 +0200

    ci(Update): Refactor label transformation for XAxis in AccountValueTrendControl
    Enhanced the `SetXAxis` and `UpdateLanguage` methods to ensure labels
    are transformed to and from Title Case Date Format correctly. Added a
    new extension method `ToTransformLabelsToTitleCaseDateFormatConvertBack`
    to facilitate the reverse transformation.

--- MyExpenses.Wpf/Converters/Analytics/LabelConverter.cs
+++ MyExpenses.Wpf/Converters/Analytics/LabelConverter.cs
@@ -21,4 +21,21 @@ public static class LabelConverter
 
         return transformedLabels;
     }
+
+    public static List<string> ToTransformLabelsToTitleCaseDateFormatConvertBack(this IEnumerable<string> labels)
+    {
+        var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+
+        var transformedLabels = labels.Select(label =>
+        {
+            DateTime dateTime = default;
+            var culture = cultures.FirstOrDefault(c => DateTime.TryParseExact(label, "MMMM yyyy", c, DateTimeStyles.None, out dateTime));
+
+            if (culture is null) throw new FormatException($"Invalid format for label '{label}'. Cannot determine the original culture.");
+
+            return dateTime.ToString("yyyy-MM");
+        }).ToList();
+
+        return transformedLabels;
+    }
 }
