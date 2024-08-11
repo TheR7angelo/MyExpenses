2ddbec2e93f969528321a2c965a60454968bd918
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-11 18:20:55 +0200

    ci(Update): Add dynamic title binding and language support
    Updated `StackedTotalEllipseControl.xaml` to bind title properties
    dynamically and introduced `UpdateLanguage` method in code-behind for
    setting default titles. Additionally, language change event handling was
    added to support real-time updates.

--- MyExpenses.Wpf/UserControls/Analytics/AccountTotalEllipseControl/StackedTotalEllipseControl.xaml.cs
+++ MyExpenses.Wpf/UserControls/Analytics/AccountTotalEllipseControl/StackedTotalEllipseControl.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using MyExpenses.Models.Config;
+using MyExpenses.Models.Config.Interfaces;
 using MyExpenses.Models.Sql.Views;
 
 namespace MyExpenses.Wpf.UserControls.Analytics.AccountTotalEllipseControl;
@@ -15,8 +17,53 @@ public partial class StackedTotalEllipseControl
         set => SetValue(VTotalByAccountProperty, value);
     }
 
+    public static readonly DependencyProperty TitleTotalTotalNotPointedProperty =
+        DependencyProperty.Register(nameof(TitleTotalTotalNotPointed), typeof(string),
+            typeof(StackedTotalEllipseControl), new PropertyMetadata(default(string)));
+
+    public string TitleTotalTotalNotPointed
+    {
+        get => (string)GetValue(TitleTotalTotalNotPointedProperty);
+        set => SetValue(TitleTotalTotalNotPointedProperty, value);
+    }
+
+    public static readonly DependencyProperty TitleTotalTotalProperty =
+        DependencyProperty.Register(nameof(TitleTotalTotal), typeof(string), typeof(StackedTotalEllipseControl),
+            new PropertyMetadata(default(string)));
+
+    public string TitleTotalTotal
+    {
+        get => (string)GetValue(TitleTotalTotalProperty);
+        set => SetValue(TitleTotalTotalProperty, value);
+    }
+
+    public static readonly DependencyProperty TitleTotalTotalPointedProperty =
+        DependencyProperty.Register(nameof(TitleTotalTotalPointed), typeof(string), typeof(StackedTotalEllipseControl),
+            new PropertyMetadata(default(string)));
+
+    public string TitleTotalTotalPointed
+    {
+        get => (string)GetValue(TitleTotalTotalPointedProperty);
+        set => SetValue(TitleTotalTotalPointedProperty, value);
+    }
+
     public StackedTotalEllipseControl()
     {
+        UpdateLanguage();
+
         InitializeComponent();
+
+        Interface.LanguageChanged += InterfaceOnLanguageChanged;
+    }
+
+    private void InterfaceOnLanguageChanged(object sender, ConfigurationLanguageChangedEventArgs e)
+        => UpdateLanguage();
+
+    private void UpdateLanguage()
+    {
+        // TODO work
+        TitleTotalTotalNotPointed = "TotalNotPointed";
+        TitleTotalTotal = "Total";
+        TitleTotalTotalPointed = "TotalPointed";
     }
 }
