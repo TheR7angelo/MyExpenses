04d882b58f209041bf5395f1faaff8849f81e4a4
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-12 10:56:03 +0200

    ci(Update): Add dynamic tab creation for account category summaries
    Replaced static binding of accounts with dynamic tab creation. Each tab
    now initializes with an `AccountCategorySumControl` and sets its chart
    based on the account's data. This refactor improves maintainability and
    ensures charts are updated correctly.

--- MyExpenses.Wpf/UserControls/Analytics/AccountsCategorySumControl.xaml.cs
+++ MyExpenses.Wpf/UserControls/Analytics/AccountsCategorySumControl.xaml.cs
@@ -1,18 +1,30 @@
-using System.Collections.ObjectModel;
-using MyExpenses.Models.Sql.Tables;
+using System.Windows.Controls;
 using MyExpenses.Sql.Context;
 
 namespace MyExpenses.Wpf.UserControls.Analytics;
 
 public partial class AccountsCategorySumControl
 {
-    public ObservableCollection<TAccount> Accounts { get; }
+    // public ObservableCollection<TAccount> Accounts { get; }
 
     public AccountsCategorySumControl()
     {
         using var context = new DataBaseContext();
-        Accounts = [..context.TAccounts.OrderBy(s => s.Name)];
+        var accounts = context.TAccounts.OrderBy(s => s.Name);
 
         InitializeComponent();
+
+        foreach (var account in accounts)
+        {
+            var accountCategorySumControl = new AccountCategorySumControl { Account = account };
+            accountCategorySumControl.SetChart();
+
+            var tabItem = new TabItem
+            {
+                Header = account.Name,
+                Content = accountCategorySumControl
+            };
+            TabControl.Items.Add(tabItem);
+        }
     }
 }
