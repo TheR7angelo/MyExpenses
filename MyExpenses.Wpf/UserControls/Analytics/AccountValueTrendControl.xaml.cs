38d64b0624cc9731796f26c36e5e4123aec85e0a
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-13 13:40:53 +0200

    ci(Update): Fix redundant Series assignment in AccountValueTrendControl
    The code originally assigned an incomplete series collection before
    assigning the correct array. This change removes the redundant
    assignment to ensure that only the correctly-formed series array is
    used.

--- MyExpenses.Wpf/UserControls/Analytics/AccountValueTrendControl.xaml.cs
+++ MyExpenses.Wpf/UserControls/Analytics/AccountValueTrendControl.xaml.cs
@@ -185,6 +185,7 @@ public partial class AccountValueTrendControl
             CheckBoxesTrend.Add(checkBoxTrend);
         }
 
+        Series = [..series];
         Series = series.ToArray();
     }
 
