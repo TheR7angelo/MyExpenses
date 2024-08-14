e5c6ef8ff33b45481e95a6afced4f73381f6a5f6
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-14 08:47:18 +0200

    ci(Update): Refactor SQL formatting for consistency
    Separated DELETE statement from table name for better readability and
    adjusted spacing in INSERT statements for consistency. Corrected spacing
    in VALUES clause to enhance code clarity and maintain uniform formatting
    standards across the file.

--- MyExpenses.Sql/Database Models/Command.sql
+++ MyExpenses.Sql/Database Models/Command.sql
@@ -15,10 +15,11 @@ WHERE account_fk = 1;
 ALTER TABLE t_history
     RENAME compte_fk TO account_fk;
 
-DELETE FROM t_supported_languages;
+DELETE
+FROM t_supported_languages;
 INSERT INTO t_supported_languages(id, code, native_name, english_name, default_language, date_added)
-VALUES (1,'en-001','English (World)','English (World)', true,'2024-07-12 09:19:06'),
-       (2,'fr-FR','Français (France)','French (France)', false,'2024-07-12 09:19:06');
+VALUES (1, 'en-001', 'English (World)', 'English (World)', true, '2024-07-12 09:19:06'),
+       (2, 'fr-FR', 'Français (France)', 'French (France)', false, '2024-07-12 09:19:06');
 
 SELECT load_extension('mod_spatialite');
 
@@ -27,7 +28,7 @@ FROM t_place;
 
 
 INSERT INTO t_geometry_columns(f_table_name, f_geometry_column, type, coord_dimension, srid)
-VALUES('t_place', 'geometry', 'POINT', 2, 4326);
+VALUES ('t_place', 'geometry', 'POINT', 2, 4326);
 
 
 SELECT *
