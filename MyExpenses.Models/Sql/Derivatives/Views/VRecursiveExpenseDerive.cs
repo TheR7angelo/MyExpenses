3471c0724d2f0d1568fe83faccf2a30ed8368e49
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-15 22:17:49 +0200

    ci(Update): Add VRecursiveExpenseDerive class
    Introduced VRecursiveExpenseDerive class to extend VRecursiveExpense
    within the `Sql/Derivatives/Views` directory. Removed unnecessary folder
    includes from MyExpenses.Models.csproj.

---
+++ MyExpenses.Models/Sql/Derivatives/Views/VRecursiveExpenseDerive.cs
@@ -0,0 +1,8 @@
+using MyExpenses.Models.Sql.Bases.Views;
+
+namespace MyExpenses.Models.Sql.Derivatives.Views;
+
+public class VRecursiveExpenseDerive : VRecursiveExpense
+{
+    public bool RecursiveToAdd { get; set; }
+}
