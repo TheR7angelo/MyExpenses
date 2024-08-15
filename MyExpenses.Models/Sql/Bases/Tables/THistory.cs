2f61d7afec78fdf29dd3eabef6132dad4c28d9fe
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-15 22:50:05 +0200

    ci(Update): Update navigation properties in THistory.cs
    Removed unnecessary namespace qualifiers from the navigation properties
    to improve code readability and maintainability. This change ensures a
    more consistent and cleaner codebase.

--- MyExpenses.Models/Sql/Bases/Tables/THistory.cs
+++ MyExpenses.Models/Sql/Bases/Tables/THistory.cs
@@ -57,15 +57,15 @@ public partial class THistory : ISql
 
     [ForeignKey("AccountFk")]
     [InverseProperty("THistories")]
-    public virtual Bases.Tables.TAccount? AccountFkNavigation { get; set; }
+    public virtual TAccount? AccountFkNavigation { get; set; }
 
     [ForeignKey("BankTransferFk")]
     [InverseProperty("THistories")]
-    public virtual Bases.Tables.TBankTransfer? BankTransferFkNavigation { get; set; }
+    public virtual TBankTransfer? BankTransferFkNavigation { get; set; }
 
     [ForeignKey("CategoryTypeFk")]
     [InverseProperty("THistories")]
-    public virtual Bases.Tables.TCategoryType? CategoryTypeFkNavigation { get; set; }
+    public virtual TCategoryType? CategoryTypeFkNavigation { get; set; }
 
     [ForeignKey("ModePaymentFk")]
     [InverseProperty("THistories")]
