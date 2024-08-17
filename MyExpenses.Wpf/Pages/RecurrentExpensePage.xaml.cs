a9c7fa3df91762ca082e555cf15094e7d5420188
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-17 19:42:49 +0200

    ci(Update): Add Recurrent Expense Page
    Implemented the Recurrent Expense Page with data grid functionality.
    Added resource files for localization in English and French. Initialized
    the necessary data bindings, grid columns, and localized headers in XAML
    and its code-behind.

---
+++ MyExpenses.Wpf/Pages/RecurrentExpensePage.xaml.cs
@@ -0,0 +1,112 @@
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using FilterDataGrid;
+using MyExpenses.Models.Config;
+using MyExpenses.Models.Config.Interfaces;
+using MyExpenses.Models.Sql.Bases.Views;
+using MyExpenses.Sql.Context;
+using MyExpenses.Utils.Collection;
+using MyExpenses.Wpf.Resources.Resx.Pages.RecurrentExpensePage;
+using MyExpenses.Wpf.Utils.FilterDataGrid;
+
+namespace MyExpenses.Wpf.Pages;
+
+public partial class RecurrentExpensePage
+{
+    #region DependencyProperty
+
+    public static readonly DependencyProperty LocalLanguageProperty = DependencyProperty.Register(nameof(LocalLanguage),
+        typeof(Local), typeof(RecurrentExpensePage), new PropertyMetadata(default(Local)));
+
+    public Local LocalLanguage
+    {
+        get => (Local)GetValue(LocalLanguageProperty);
+        set => SetValue(LocalLanguageProperty, value);
+    }
+
+    public static readonly DependencyProperty DateFormatStringProperty =
+        DependencyProperty.Register(nameof(DateFormatString), typeof(string), typeof(RecurrentExpensePage),
+            new PropertyMetadata(default(string)));
+
+    public string DateFormatString
+    {
+        get => (string)GetValue(DateFormatStringProperty);
+        set => SetValue(DateFormatStringProperty, value);
+    }
+
+    #endregion
+
+    private DataGridRow? DataGridRow { get; set; }
+
+    public ObservableCollection<VRecursiveExpense> VRecursiveExpenses { get; }
+
+    public RecurrentExpensePage()
+    {
+        UpdateLocalLanguage();
+
+        InitializeComponent();
+
+        UpdateLanguage();
+        VRecursiveExpenses = [];
+        FilterDataGrid.ItemsSource = VRecursiveExpenses;
+
+        UpdateDataGrid();
+
+        Interface.LanguageChanged += Interface_OnLanguageChanged;
+    }
+
+    #region Action
+
+    private void DataGridRow_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        => DataGridRow = sender as DataGridRow;
+
+    private void Interface_OnLanguageChanged(object sender, ConfigurationLanguageChangedEventArgs e)
+        => UpdateLanguage();
+
+    #endregion
+
+    #region Function
+
+    private void UpdateDataGrid()
+    {
+        using var context = new DataBaseContext();
+        var records = context.VRecursiveExpenses
+            .OrderBy(s => s.IsActive)
+            .ThenBy(s => s.StartDate)
+            .ToList();
+
+        VRecursiveExpenses.Clear();
+        VRecursiveExpenses.AddRange(records);
+    }
+
+    private void UpdateLanguage()
+    {
+        DateFormatString = "dd/MM/yyyy";
+        UpdateLocalLanguage();
+
+        DataGridTextColumnAccount.Header = RecurrentExpensePageResources.DataGridTextColumnAccountHeader;
+        DataGridTextColumnDescription.Header = RecurrentExpensePageResources.DataGridTextColumnDescriptionHeader;
+        DataGridTextColumnNote.Header = RecurrentExpensePageResources.DataGridTextColumnNoteHeader;
+        DataGridTemplateColumnCategory.Header = RecurrentExpensePageResources.DataGridTemplateColumnCategoryHeader;
+        DataGridTextColumnModePayment.Header = RecurrentExpensePageResources.DataGridTextColumnModePaymentHeader;
+        DataGridTemplateColumnValue.Header = RecurrentExpensePageResources.DataGridTemplateColumnValueHeader;
+        DataGridTextColumnStartDate.Header = RecurrentExpensePageResources.DataGridTextColumnStartDateHeader;
+        DataGridTextColumnRecursiveTotal.Header = RecurrentExpensePageResources.DataGridTextColumnRecursiveTotalHeader;
+        DataGridTextColumnRecursiveCount.Header = RecurrentExpensePageResources.DataGridTextColumnRecursiveCountHeader;
+        DataGridTextColumnFrequency.Header = RecurrentExpensePageResources.DataGridTextColumnFrequencyHeader;
+        DataGridTextColumnNextDueDate.Header = RecurrentExpensePageResources.DataGridTextColumnNextDueDateHeader;
+        DataGridTextColumnPlace.Header = RecurrentExpensePageResources.DataGridTextColumnPlaceHeader;
+        DataGridCheckBoxColumnIsActive.Header = RecurrentExpensePageResources.DataGridCheckBoxColumnIsActiveHeader;
+    }
+
+    private void UpdateLocalLanguage()
+    {
+        var currentCulture = CultureInfo.CurrentCulture;
+        LocalLanguage = currentCulture.ToLocal();
+    }
+
+    #endregion
+}
