4762466bd6ce623b03906396ff0780b623d909a3
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-18 14:19:54 +0200

    ci(Update): Add ToDouble extension method to convert string to double
    This new method converts a string to a nullable double, supporting
    various number formats. It uses CultureInfo.InvariantCulture for parsing
    to ensure consistent behavior across different locales.

--- MyExpenses.Utils/Strings/StringsExtensions.cs
+++ MyExpenses.Utils/Strings/StringsExtensions.cs
@@ -1,7 +1,26 @@
+using System.Globalization;
+
 namespace MyExpenses.Utils.Strings;
 
 public static class StringsExtensions
 {
+    /// <summary>
+    /// Converts a string to a double value (nullable).
+    /// </summary>
+    /// <param name="input">The input string.</param>
+    /// <param name="output">The converted double value (nullable).</param>
+    /// <returns>A boolean indicating if the conversion was successful.</returns>
+    public static bool ToDouble(this string input, out double? output)
+    {
+        var separators = new[] { ',' };
+        input = separators.Aggregate(input, (current, separator) => current.Replace(separator, '.'));
+
+        var success = double.TryParse(input, NumberStyles.Any, CultureInfo.InvariantCulture,  out var result);
+        output = success ? result : null;
+
+        return success;
+    }
+
     /// <summary>
     /// Converts a string to an int value (nullable).
     /// </summary>
