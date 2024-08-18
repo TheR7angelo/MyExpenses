89a27d2298563e4d75429d76331d3bf343eb81e9
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-18 16:18:17 +0200

    ci(Update): Add ERecursiveFrequency property to TRecursiveFrequency
    Introduced a new property, `ERecursiveFrequency`, to the
    `TRecursiveFrequency` class. This property is marked as `[NotMapped]`
    and casts the `Id` to the `ERecursiveFrequency` enum type.

--- MyExpenses.Models/Sql/Bases/Tables/TRecursiveFrequency.cs
+++ MyExpenses.Models/Sql/Bases/Tables/TRecursiveFrequency.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using MyExpenses.Models.Sql.Bases.Enums;
 
 namespace MyExpenses.Models.Sql.Bases.Tables;
 
@@ -9,6 +10,10 @@ public partial class TRecursiveFrequency
     [Key]
     [Column("id")]
     public int Id { get; set; }
+
+    [NotMapped]
+    public ERecursiveFrequency ERecursiveFrequency
+        => (ERecursiveFrequency)Id;
 
     [Column("frequency")]
     public string? Frequency { get; set; }
