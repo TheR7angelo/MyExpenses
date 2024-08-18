eb0281b630d645b7090adcadd582278e96d4073a
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-18 22:33:28 +0200

    ci(Update): Add action buttons for editing and deleting records
    This commit introduces buttons in the `RecurrentExpensePage` to allow
    users to edit and delete records directly from the data grid. This
    enhancement replaces the menu item for record deletion and streamlines
    user interactions.

--- MyExpenses.Wpf/Pages/RecurrentExpensePage.xaml.cs
+++ MyExpenses.Wpf/Pages/RecurrentExpensePage.xaml.cs
@@ -96,6 +96,22 @@ public partial class RecurrentExpensePage
         UpdateDataGrid();
     }
 
+    private void ButtonDeleteRecord_OnClick(object sender, RoutedEventArgs e)
+    {
+        var button = (Button)sender;
+        if (button.DataContext is not VRecursiveExpense vRecurrentExpense) return;
+
+        DeleteRecord(vRecurrentExpense);
+    }
+
+    private void ButtonEditRecord_OnClick(object sender, RoutedEventArgs e)
+    {
+        var button = (Button)sender;
+        if (button.DataContext is not VRecursiveExpense vRecurrentExpense) return;
+
+        EditRecurrentExpenseWindow(vRecurrentExpense);
+    }
+
     private void DataGridRow_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
         => DataGridRow = sender as DataGridRow;
 
@@ -223,60 +239,4 @@ public partial class RecurrentExpensePage
     }
 
     #endregion
-
-    private void MenuItemDeleteRecord_OnClick(object sender, RoutedEventArgs e)
-    {
-        if (DataGridRow?.DataContext is not VRecursiveExpense vRecurrentExpense) return;
-
-        DeleteRecord(vRecurrentExpense);
-    }
-
-    private void DeleteRecord(VRecursiveExpense vRecurrentExpense)
-    {
-        var response = MsgBox.Show(RecurrentExpensePageResources.MessageBoxDeleteQuestion,
-            MsgBoxImage.Question, MessageBoxButton.YesNoCancel);
-        if (response is not MessageBoxResult.Yes) return;
-
-        var recurrentExpense = vRecurrentExpense.Id.ToISql<TRecursiveExpense>()!;
-        Log.Information("Attempting to remove the recursive expense \"{RecursiveExpenseDescription}\"",
-            recurrentExpense.Description);
-        var (success, exception) = recurrentExpense.Delete();
-
-        if (success)
-        {
-            Log.Information("Recursive expense was successfully removed");
-            MsgBox.Show(RecurrentExpensePageResources.MessageBoxDeleteRecursiveExpenseNoUseSuccess,
-                MsgBoxImage.Check);
-
-            UpdateDataGrid();
-            return;
-        }
-
-        if (exception!.InnerException is SqliteException
-            {
-                SqliteExtendedErrorCode: SQLitePCL.raw.SQLITE_CONSTRAINT_FOREIGNKEY
-            })
-        {
-            Log.Error("Foreign key constraint violation");
-
-            response = MsgBox.Show(RecurrentExpensePageResources.MessageBoxDeleteRecursiveExpenseUseQuestion,
-                MsgBoxImage.Question, MessageBoxButton.YesNoCancel);
-
-            if (response is not MessageBoxResult.Yes) return;
-
-            Log.Information(
-                "Attempting to remove the recursive expense \"{RecursiveExpenseDescription}\" with all relative element",
-                recurrentExpense.Description);
-            recurrentExpense.Delete(true);
-            Log.Information("Recursive expense and all relative element was successfully removed");
-            MsgBox.Show(RecurrentExpensePageResources.MessageBoxDeleteRecursiveExpenseUseSuccess,
-                MsgBoxImage.Check);
-
-            UpdateDataGrid();
-            return;
-        }
-
-        Log.Error(exception, "An error occurred please retry");
-        MsgBox.Show(RecurrentExpensePageResources.MessageBoxDeleteRecursiveExpenseError, MsgBoxImage.Error);
-    }
 }
