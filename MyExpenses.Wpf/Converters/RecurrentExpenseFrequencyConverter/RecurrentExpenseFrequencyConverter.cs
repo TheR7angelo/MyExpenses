c4829c27fd415c71db06e0b0809a20248a1efabd
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-18 22:13:43 +0200

    ci(Update): Implement ConvertBack method in RecurrentExpenseFrequencyConverter
    Previously, the ConvertBack method threw a NotImplementedException. This
    change returns Binding.DoNothing instead, preventing runtime exceptions
    and improving stability.

--- MyExpenses.Wpf/Converters/RecurrentExpenseFrequencyConverter/RecurrentExpenseFrequencyConverter.cs
+++ MyExpenses.Wpf/Converters/RecurrentExpenseFrequencyConverter/RecurrentExpenseFrequencyConverter.cs
@@ -21,6 +21,6 @@ public class RecurrentExpenseFrequencyConverter : IValueConverter
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        return Binding.DoNothing;
     }
 }
