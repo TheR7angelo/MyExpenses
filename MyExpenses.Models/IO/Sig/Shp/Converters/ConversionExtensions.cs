2c4412116c8563f030a6db45f584809c7c9f3845
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-19 20:31:00 +0200

    ci(Update): Add shape file reading and writing support
    Implemented classes and methods for reading and writing shapefiles,
    including conversions for boolean and datetime values. Updated project
    files to include required dependencies and added a CSV resource for
    geographic coordinate systems.

---
+++ MyExpenses.Models/IO/Sig/Shp/Converters/ConversionExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace MyExpenses.Models.IO.Sig.Shp.Converters;
+
+public static class ConversionExtensions
+{
+    public static object? ConvertTo(this object? value, Type targetType)
+    {
+        if (value == null)
+            return targetType.IsValueType ? Activator.CreateInstance(targetType) : null;
+
+        if (targetType.IsInstanceOfType(value))
+        {
+            return value;
+        }
+
+        if ((targetType == typeof(bool) || targetType == typeof(bool?) ) &&
+            value is string or int or double)
+        {
+            return value.ToBool();
+        }
+
+        try
+        {
+            return Convert.ChangeType(value, targetType);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
---
+++ MyExpenses.Models/IO/Sig/Shp/Converters/DateTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace GeoReso.Models.IO.Shape.Converters;
+
+public class DateTimeConverter
+{
+    private readonly string[] _formats =
+        { "dd/MM/yyyy HH:mm", "MM/dd/yyyy HH:mm", "yyyy/MM/dd HH:mm:ss.fff", "yyyy/MM/dd HH:mm:ss",
+            "yyyy-MM-dd HH:mm", "MM-dd-yyyy HH:mm", "yyyy-MM-dd HH:mm:ss.fff", "yyyy-MM-dd HH:mm:ss" };
+
+    public DateTime? ConvertFromString(string? text)
+    {
+        if (string.IsNullOrEmpty(text)) return null;
+
+        if (DateTime.TryParseExact(text, _formats, System.Globalization.CultureInfo.InvariantCulture,
+                System.Globalization.DateTimeStyles.None, out var result))
+        {
+            return result;
+        }
+
+        return null;
+    }
+}
