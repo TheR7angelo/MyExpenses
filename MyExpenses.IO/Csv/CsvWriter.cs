b8a83d23d7172fc6079f79c8c82c3f6771724f95
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-19 16:52:31 +0200

    ci(Update): Add CsvWriter utility for CSV file creation
    Introduced a new CsvWriter class for writing CSV files. This class
    includes a method `WriteCsv` which handles converting a collection of
    records into a CSV format and saving it to a specified file path. Proper
    exception handling ensures errors are logged and the method returns a
    boolean success flag.

---
+++ MyExpenses.IO/Csv/CsvWriter.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using CsvHelper.Configuration;
+
+namespace MyExpenses.IO.Csv;
+
+public static class CsvWriter
+{
+    private static CsvConfiguration CsvConfiguration()
+    {
+        return new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            NewLine = Environment.NewLine,
+            Delimiter = ";"
+        };
+    }
+
+    public static bool WriteCsv<T>(this IEnumerable<T> records, string filePath)
+    {
+        filePath = Path.ChangeExtension(filePath, ".csv");
+
+        using var writer = new StreamWriter(filePath);
+        using var csv = new CsvHelper.CsvWriter(writer, CsvConfiguration());
+
+        try
+        {
+            csv.WriteRecords(records);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+}
