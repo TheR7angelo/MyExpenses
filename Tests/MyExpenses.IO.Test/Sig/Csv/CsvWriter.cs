17d6eeb9a2df6535d9133b3cdd00dbee2dcc8221
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-19 16:43:56 +0200

    ci(Update): Add CsvWriter test class for exporting mapped tables
    This commit introduces a CsvWriter test class in the MyExpenses.IO.Test
    project. It tests the functionality of exporting mapped database tables
    to CSV format by utilizing AutoMapper for conversions. The
    implementation includes a dynamic mechanism for handling various table
    types and their corresponding mappings.

---
+++ Tests/MyExpenses.IO.Test/Sig/Csv/CsvWriter.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using MyExpenses.Models.AutoMapper;
+using MyExpenses.Models.IO.Export;
+using MyExpenses.Models.IO.Export.Sql.Tables;
+using MyExpenses.Models.Sql.Bases.Tables;
+using MyExpenses.Sql.Context;
+
+namespace MyExpenses.IO.Test.Sig.Csv;
+
+public class CsvWriter
+{
+    [Fact]
+    public void Test()
+    {
+        using var context = new DataBaseContext();
+
+        var tables = context.Model.GetEntityTypes()
+            .SelectMany(s => s.GetTableMappings())
+            .Select(s => s.TypeBase)
+            .Select(s => s.ClrType)
+            .ToList();
+
+        var methodInfo = typeof(DataBaseContext)
+            .GetMethods()
+            .First(m => m is { Name: "Set", IsGenericMethod: true } && m.GetParameters().Length == 0);
+
+        var mapper = Mapping.Mapper;
+
+        var typeSwitch = new TypeSwitch()
+            .Case<TAccount>(s => mapper.Map<ExportTAccount>(s))
+            .Case<TAccountType>(s => mapper.Map<ExportTAccountType>(s))
+            .Case<TBankTransfer>(s => mapper.Map<ExportTBankTransfer>(s))
+            .Case<TCategoryType>(s => mapper.Map<ExportTCategoryType>(s))
+            .Case<TColor>(s => mapper.Map<ExportTColor>(s))
+            .Case<TCurrency>(s => mapper.Map<ExportTCurrency>(s))
+            .Case<TGeometryColumn>(s => mapper.Map<ExportTGeometryColumn>(s))
+            .Case<THistory>(s => mapper.Map<ExportTHistory>(s))
+            .Case<TModePayment>(s => mapper.Map<ExportTModePayment>(s))
+            .Case<TPlace>(s => mapper.Map<ExportTPlace>(s))
+            .Case<TRecursiveExpense>(s => mapper.Map<ExportTRecursiveExpense>(s))
+            .Case<TRecursiveFrequency>(s => mapper.Map<ExportTRecursiveFrequency>(s))
+            .Case<TSpatialRefSy>(s => mapper.Map<ExportTSpatialRefSy>(s))
+            .Case<TSupportedLanguage>(s => mapper.Map<ExportTSupportedLanguage>(s))
+            .Case<TVersion>(s => mapper.Map<ExportTVersion>(s));
+
+        var exportRecords = new List<ExportRecord>();
+        foreach (var table in tables)
+        {
+            var genericSetMethod = methodInfo.MakeGenericMethod(table);
+            dynamic dynamicRecords = genericSetMethod.Invoke(context, null)!;
+
+            var records = new List<object?>();
+            foreach (var dynamicRecord in dynamicRecords)
+            {
+                var tmp = typeSwitch.Switch(dynamicRecord);
+                records.Add(tmp);
+            }
+
+            var name = table.GetCustomAttribute<TableAttribute>()!.Name;
+            exportRecords.Add(new ExportRecord { Name = name, Records = records });
+        }
+
+        Console.WriteLine("hey");
+    }
+}
+
+public class TypeSwitch
+{
+    readonly Dictionary<Type, Func<object, object>> _matches = new();
+
+    public TypeSwitch Case<T>(Func<T, object> action)
+    {
+        _matches.Add(typeof(T), (x) => action((T)x));
+        return this;
+    }
+
+    public object Switch(object x)
+    {
+        return _matches[x.GetType()](x);
+    }
+}
