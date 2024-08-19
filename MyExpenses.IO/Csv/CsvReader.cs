ccdbddba949abef5e08e4a1ea9cd532e1ea106a9
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-19 16:49:47 +0200

    ci(Update): Add CsvReader to handle CSV file reading
    Introduce a static CsvReader class that supports reading CSV files with
    automatic encoding detection using Ude.NetStandard. Updated the project
    file to include the necessary dependencies for CsvHelper and
    Ude.NetStandard.

---
+++ MyExpenses.IO/Csv/CsvReader.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+using System.Text;
+using CsvHelper.Configuration;
+using Ude;
+
+namespace MyExpenses.IO.Csv;
+
+public static class CsvReader
+{
+    static CsvReader()
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+    }
+
+    public static IEnumerable<T> ReadCsv<T>(this string filePath)
+    {
+        var csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HeaderValidated = null,
+            MissingFieldFound = null,
+            DetectDelimiter = true
+        };
+
+        var encodingName = GetEncodingName(filePath);
+        var encoding = Encoding.GetEncoding(encodingName);
+
+
+        using var streamReader = new StreamReader(filePath, encoding, true,
+            new FileStreamOptions { Access = FileAccess.Read, Share = FileShare.ReadWrite });
+
+        using var reader = new CsvHelper.CsvReader(streamReader, csvConfiguration);
+        var records = reader.GetRecords<T>();
+
+        return records.ToList();
+    }
+
+    private static string GetEncodingName(string filePath)
+    {
+        using var fileStream = File.OpenRead(filePath);
+
+        var detector = new CharsetDetector();
+        detector.Feed(fileStream);
+        detector.DataEnd();
+
+        var encodingName = string.IsNullOrEmpty(detector.Charset)
+            ? "UTF-8"
+            : detector.Charset;
+        return encodingName;
+    }
+}
