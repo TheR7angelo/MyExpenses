dd368abc3a66ec1b63b367be05cc968ba5bcdd90
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-21 21:33:13 +0200

    ci(Update): Refactor Excel worksheet generation logic
    Simplified the `ToExcelWorksheet` method and extracted helper methods
    for setting headers, values, date styles, and table styles. This
    enhancement improves code readability and maintainability by breaking
    down complex logic into smaller, more manageable functions.

--- MyExpenses.IO/Excel/ImportExportDataTableExcel.cs
+++ MyExpenses.IO/Excel/ImportExportDataTableExcel.cs
@@ -14,7 +14,6 @@ public static class ImportExportDataTableExcel
         ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
     }
 
-    // TODO simplify
     public static bool ToExcelWorksheet(this IEnumerable<ExportRecord> exportRecords, string filePath)
     {
         filePath = Path.ChangeExtension(filePath, ".xlsx");
@@ -26,54 +25,14 @@ public static class ImportExportDataTableExcel
         {
             var worksheet = package.Workbook.Worksheets.Add(exportRecord.Name);
 
-            var column = 1;
             var properties = exportRecord.Records.First()!.GetType().GetProperties();
-            var headers = new List<PropertyInfo>();
-            foreach (var propertyInfo in properties)
-            {
-                var header = propertyInfo.GetCustomAttribute<DisplayNameAttribute>()?.DisplayName;
-                if (string.IsNullOrEmpty(header)) continue;
+            var headers = worksheet.SetHeaders(properties);
 
-                worksheet.Cells[1, column].Value = header;
-                headers.Add(propertyInfo);
-                column++;
-            }
+            worksheet.SetValue(exportRecord, properties, headers);
 
-            var row = 2;
-            foreach (var record in exportRecord.Records)
-            {
-                column = 1;
-                foreach (var propertyInfo in properties)
-                {
-                    if (!headers.Contains(propertyInfo)) continue;
+            worksheet.SetDateStyle(headers);
 
-                    var value = propertyInfo.GetValue(record);
-                    worksheet.Cells[row, column].Value = value;
-
-                    column++;
-                }
-
-                row++;
-            }
-
-            foreach (var propertyInfo in headers)
-            {
-                if (propertyInfo.PropertyType == typeof(DateTime) || propertyInfo.PropertyType == typeof(DateTime?))
-                {
-                    var index = headers.IndexOf(propertyInfo) + 1;
-                    worksheet.Column(index).Style.Numberformat.Format = "yyyy-mm-dd hh:mm:ss";
-                }
-
-                if (propertyInfo.PropertyType == typeof(DateOnly) || propertyInfo.PropertyType == typeof(DateOnly?))
-                {
-                    var index = headers.IndexOf(propertyInfo) + 1;
-                    worksheet.Column(index).Style.Numberformat.Format = "yyyy-mm-dd";
-                }
-            }
-
-            var dataRange = worksheet.Cells[1, 1, row - 1, headers.Count];
-            var table = worksheet.Tables.Add(dataRange, $"{exportRecord.Name}_table");
-            table.TableStyle = TableStyles.Medium7;
+            var dataRange = worksheet.SetExcelTableStyle(headers, exportRecord);
 
             dataRange.AutoFitColumns();
         }
@@ -81,5 +40,70 @@ public static class ImportExportDataTableExcel
         package.SaveAs(filePath);
 
         return false;
+    }
+
+    private static ExcelRange SetExcelTableStyle(this ExcelWorksheet worksheet, List<PropertyInfo> headers, ExportRecord exportRecord)
+    {
+        var maxRow = worksheet.Dimension.End.Row;
+        var dataRange = worksheet.Cells[1, 1, maxRow, headers.Count];
+        var table = worksheet.Tables.Add(dataRange, $"{exportRecord.Name}_table");
+        table.TableStyle = TableStyles.Medium7;
+        return dataRange;
+    }
+
+    private static void SetDateStyle(this ExcelWorksheet worksheet, List<PropertyInfo> headers)
+    {
+        foreach (var propertyInfo in headers)
+        {
+            if (propertyInfo.PropertyType == typeof(DateTime) || propertyInfo.PropertyType == typeof(DateTime?))
+            {
+                var index = headers.IndexOf(propertyInfo) + 1;
+                worksheet.Column(index).Style.Numberformat.Format = "yyyy-mm-dd hh:mm:ss";
+            }
+
+            if (propertyInfo.PropertyType == typeof(DateOnly) || propertyInfo.PropertyType == typeof(DateOnly?))
+            {
+                var index = headers.IndexOf(propertyInfo) + 1;
+                worksheet.Column(index).Style.Numberformat.Format = "yyyy-mm-dd";
+            }
+        }
+    }
+
+    private static void SetValue(this ExcelWorksheet worksheet, ExportRecord exportRecord, PropertyInfo[] properties,
+        List<PropertyInfo> headers)
+    {
+        var row = 2;
+        foreach (var record in exportRecord.Records)
+        {
+            var column = 1;
+            foreach (var propertyInfo in properties)
+            {
+                if (!headers.Contains(propertyInfo)) continue;
+
+                var value = propertyInfo.GetValue(record);
+                worksheet.Cells[row, column].Value = value;
+
+                column++;
+            }
+
+            row++;
+        }
+    }
+
+    private static List<PropertyInfo> SetHeaders(this ExcelWorksheet worksheet, PropertyInfo[] properties)
+    {
+        var column = 1;
+        var headers = new List<PropertyInfo>();
+        foreach (var propertyInfo in properties)
+        {
+            var header = propertyInfo.GetCustomAttribute<DisplayNameAttribute>()?.DisplayName;
+            if (string.IsNullOrEmpty(header)) continue;
+
+            worksheet.Cells[1, column].Value = header;
+            headers.Add(propertyInfo);
+            column++;
+        }
+
+        return headers;
     }
 }
