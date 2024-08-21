eaeabb6e3c719dc668d92661b5edfb027aa7ef72
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-21 22:01:25 +0200

    ci(Update): Update KmlWriter to include display names in KML output
    Introduced logic to dynamically fetch and use a display name property
    when generating KML files. If a display name is present, it replaces the
    default naming convention in the KML elements. Added utility function to
    retrieve the display name property from ISig objects.

--- MyExpenses.IO/Sig/Kml/KmlWriter.cs
+++ MyExpenses.IO/Sig/Kml/KmlWriter.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using System.IO.Compression;
+using System.Reflection;
 using System.Xml.Linq;
 using MyExpenses.Models.IO.Sig.Interfaces;
+using MyExpenses.Models.Sig;
 using MyExpenses.Utils.Maps;
 using NetTopologySuite.Geometries;
 
@@ -28,9 +31,14 @@ public static class KmlWriter
                     new XAttribute("id", "root_doc"),
                     schemaElement)));
 
+        var displayNameProperty = GetDisplayNameProperty(fields, firstSig);
+
         foreach (var obj in enumerable.Select((point, i) => new { i, point }))
         {
-            var indexedName = $"{geomType} {obj.i + 1}";
+            var displayName = displayNameProperty is null
+                ? $"{geomType} {obj.i + 1}"
+                : displayNameProperty.GetValue(obj.point);
+
             var (xInvariant, yInvariant) = ((Point)obj.point.Geometry!).ToInvariantCoordinate();
 
             var kmlAttribute = obj.point.CreateKmlAttribute(filenameWithoutExtension);
@@ -38,7 +46,7 @@ public static class KmlWriter
             kml.Root!.Element(KmlUtils.KmlNamespace +"Document")!.Add(
                 new XElement(KmlUtils.KmlNamespace + "Placemark",
                     kmlAttribute,
-                    new XElement(KmlUtils.KmlNamespace + "name", indexedName),
+                    new XElement(KmlUtils.KmlNamespace + "name", displayName),
                     new XElement(KmlUtils.KmlNamespace + "Point",
                         new XElement(KmlUtils.KmlNamespace + "coordinates",
                             $"{yInvariant}, {xInvariant}")
@@ -48,6 +56,26 @@ public static class KmlWriter
         }
 
         SaveToKmlKmzFile(fileSavePath, kml, extension);
+    }
+
+    private static PropertyInfo? GetDisplayNameProperty(Dictionary<string, DbField> fields, ISig firstSig)
+    {
+        const string displayName = "name";
+
+        PropertyInfo? displayNameProperty = null;
+        if (!fields.ContainsKey(displayName)) return displayNameProperty;
+
+        var properties = firstSig.GetType().GetProperties();
+        foreach (var propertyInfo in properties)
+        {
+            if (propertyInfo.GetValueByProperty<ColumnAttribute>() is not string columnName) continue;
+            if (columnName is not displayName) continue;
+
+            displayNameProperty = propertyInfo;
+            break;
+        }
+
+        return displayNameProperty;
     }
 
     public static void ToKmlFile(this ISig sig, string fileSavePath, string geomType = "Point")
