78be14666b517437fdc088292688cfceaf5ddfef
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-22 16:34:15 +0200

    ci(Update): Add AccountsModePaymentMonthlySumControl with data visualization
    This commit introduces a new user control
    `AccountsModePaymentMonthlySumControl`, which provides monthly payment
    data visualization for different accounts. It includes supporting XAML
    and C# files, and integrates this control into the AnalyticsPage by
    adding a new tab. Additionally, some unused code in
    `AccountsCategorySumControl` has been removed.

--- MyExpenses.Wpf/UserControls/Analytics/AccountsModePaymentMonthlySumControl.xaml.cs
+++ MyExpenses.Wpf/UserControls/Analytics/AccountsModePaymentMonthlySumControl.xaml.cs
@@ -1,11 +1,11 @@
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 using MyExpenses.Sql.Context;
 
 namespace MyExpenses.Wpf.UserControls.Analytics;
 
-public partial class AccountsCategorySumControl
+public partial class AccountsModePaymentMonthlySumControl
 {
-    public AccountsCategorySumControl()
+    public AccountsModePaymentMonthlySumControl()
     {
         using var context = new DataBaseContext();
         var accounts = context.TAccounts.OrderBy(s => s.Name);
@@ -14,12 +14,12 @@ public partial class AccountsCategorySumControl
 
         foreach (var account in accounts)
         {
-            var accountCategorySumControl = new AccountCategorySumControl(account.Id);
+            var accountModePaymentMonthlySumControl = new AccountModePaymentMonthlySumControl(account.Id);
 
             var tabItem = new TabItem
             {
                 Header = account.Name,
-                Content = accountCategorySumControl
+                Content = accountModePaymentMonthlySumControl
             };
             TabControl.Items.Add(tabItem);
         }
