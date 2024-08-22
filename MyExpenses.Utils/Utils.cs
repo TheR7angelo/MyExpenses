274adb094d52de6011d2afd5aa020bc4280e349c
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-22 11:39:58 +0200

    ci(Update): Add utility to open files in Windows
    Introduced a new static utility class with a method to start files using
    Windows Explorer. This method throws a PlatformNotSupportedException if
    used on non-Windows platforms.

---
+++ MyExpenses.Utils/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+namespace MyExpenses.Utils;
+
+public static class Utils
+{
+    [SupportedOSPlatform("windows")]
+    public static void StartFile(this string file)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Process.Start(new ProcessStartInfo("explorer", file) { UseShellExecute = true });
+        }
+        else
+        {
+            throw new PlatformNotSupportedException("This method is not supported on this platform.");
+        }
+    }
+}
