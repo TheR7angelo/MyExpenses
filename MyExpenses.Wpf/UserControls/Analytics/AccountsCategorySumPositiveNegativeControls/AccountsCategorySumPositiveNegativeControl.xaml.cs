de796b40a2159f941e13037b4ecf82d807255d51
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-25 12:22:39 +0200

    ci(Update): Add AccountCategorySumPositiveNegativeControl component
    Implemented the AccountCategorySumPositiveNegativeControl component to
    display positive and negative sums for each account category. Integrated
    the component into the AnalyticsPage with dynamically generated tabs for
    each account.

---
+++ MyExpenses.Wpf/UserControls/Analytics/AccountsCategorySumPositiveNegativeControls/AccountsCategorySumPositiveNegativeControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using MyExpenses.Sql.Context;
+
+namespace MyExpenses.Wpf.UserControls.Analytics.AccountsCategorySumPositiveNegativeControls;
+
+public partial class AccountsCategorySumPositiveNegativeControl
+{
+    public AccountsCategorySumPositiveNegativeControl()
+    {
+        using var context = new DataBaseContext();
+        var accounts = context.TAccounts.OrderBy(s => s.Name);
+
+        InitializeComponent();
+
+        foreach (var account in accounts)
+        {
+            var accountModePaymentMonthlySumControl = new AccountCategorySumPositiveNegativeControl(account.Id);
+
+            var tabItem = new TabItem
+            {
+                Header = account.Name,
+                Content = accountModePaymentMonthlySumControl
+            };
+            TabControl.Items.Add(tabItem);
+        }
+    }
+}
