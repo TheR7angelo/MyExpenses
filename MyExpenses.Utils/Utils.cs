63ac1ae2a9612007def8219527bb8cd59d359a29
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-26 09:44:30 +0200

    ci(Update): Add XML documentation for StartFile method
    Added XML comments to provide a summary, parameter description, and
    exception details for the StartFile method in Utils.cs. This improves
    code readability and helps developers understand the method's usage and
    support.

--- MyExpenses.Utils/Utils.cs
+++ MyExpenses.Utils/Utils.cs
@@ -6,6 +6,11 @@ namespace MyExpenses.Utils;
 
 public static class Utils
 {
+    /// <summary>
+    /// Opens the specified file in the default file explorer on the Windows platform.
+    /// </summary>
+    /// <param name="file">The path to the file to be opened.</param>
+    /// <exception cref="PlatformNotSupportedException">Thrown if the method is called on a platform other than Windows.</exception>
     [SupportedOSPlatform("windows")]
     public static void StartFile(this string file)
     {
