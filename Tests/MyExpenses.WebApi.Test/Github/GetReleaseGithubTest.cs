291881a4520ebb77c28abc74345233c426413fb8
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-26 11:54:51 +0200

    ci(Update): Add GitHub API models and test for releases
    Introduced new models to interact with the GitHub API, including
    `Author`, `Release`, `Asset`, and `Uploader`. Also, add a unit test to
    fetch and display release information from a GitHub repository.

---
+++ Tests/MyExpenses.WebApi.Test/Github/GetReleaseGithubTest.cs
@@ -0,0 +1,31 @@
+﻿using MyExpenses.Models.WebApi.Github;
+using Newtonsoft.Json;
+using Xunit.Abstractions;
+
+namespace MyExpenses.WebApi.Test.Github;
+
+public class GetReleaseGithubTest(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public async Task GetRelease()
+    {
+        const string url = "https://api.github.com/repos/ravibpatel/AutoUpdater.NET/releases";
+
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("User-Agent", "CSharpApp");
+        var response = await httpClient.GetAsync(url);
+
+        var content = await response.Content.ReadAsStringAsync();
+        var releases = JsonConvert.DeserializeObject<List<Release>>(content)!;
+
+        foreach (var release in releases)
+        {
+            var releaseName = release.Name;
+            var body = release.Body;
+
+            testOutputHelper.WriteLine($"Release: {releaseName}");
+            testOutputHelper.WriteLine($"Notes de release:\n{body}");
+            testOutputHelper.WriteLine("==================================");
+        }
+    }
+}
