537a2491066abcd2b486f21563cf4142dc1e30ed
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-26 11:10:24 +0200

    ci(Update): Add ToHtmlTest for converting Markdown to HTML
    This commit introduces a new test file, ToHtmlTest.cs, that verifies the
    conversion of a Markdown document to HTML. It reads a Markdown file,
    processes it with the Markdig library, and then wraps the converted HTML
    content in a custom-styled HTML template. The final output is written to
    a test.html file for validation.

---
+++ Tests/MyExpenses.IO.Test/Html/ToHtmlTest.cs
@@ -0,0 +1,79 @@
+﻿using System.Reflection;
+using Markdig;
+using MyExpenses.Utils;
+
+namespace MyExpenses.IO.Test.Html;
+
+public class ToHtmlTest
+{
+    [Fact]
+    public void HtmlToHtmlTest()
+    {
+        var executablePath = Assembly.GetExecutingAssembly().Location;
+        var directory = executablePath.GetParentDirectory(6);
+
+        var mdPath = Path.Join(directory, "TODO LIST.md");
+
+        var content = File.ReadAllText(mdPath);
+
+        var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+        var htmlText = Markdown.ToHtml(content, pipeline);
+
+        const string backgroundColor = "#1e1f22";
+        const string foregroundColor = "#bcbec4";
+        const string huePrimaryColor = "#FFFFFF";
+
+        var fullHtml = $$"""
+                         <html>
+                         <head>
+                             <meta charset="UTF-8">
+                             <style>
+                         
+                            :root{
+                                  --backgroundColor: {{backgroundColor}};
+                                  --foregroundColor: {{foregroundColor}};
+                                  --huePrimaryColor: {{huePrimaryColor}};
+                              }
+                              
+                              ::-webkit-scrollbar {
+                                  width: 10px;
+                              }
+                              
+                              ::-webkit-scrollbar-track {
+                                  background: var(--backgroundColor);
+                              }
+                              
+                              ::-webkit-scrollbar-thumb {
+                                  background: var(--foregroundColor);
+                              }
+                              
+                              ::-webkit-scrollbar-thumb:hover {
+                                  background: var(--foregroundColor);
+                              }
+                         
+                              body{
+                                  background-color: var(--backgroundColor);
+                              }
+                         
+                              blockquote{
+                                  box-shadow: -5px 0 0 rgba(94, 158, 255, 100%);
+                                  padding: 5px;
+                                  background-color: background-color: var(--backgroundColor);
+                                  color: var(--foregroundColor);
+                                  border-radius: 5px;
+                                  border-top: 2px solid var(--huePrimaryColor);
+                                  border-right: 2px solid var(--huePrimaryColor);
+                                  border-bottom: 2px solid var(--huePrimaryColor);
+                                  font-size: 12px;
+                              }
+                          </style>
+                         </head>
+                         <body>
+                             {{htmlText}}
+                         </body>
+                         </html>
+                         """;
+
+        File.WriteAllText("test.html", fullHtml);
+    }
+}
