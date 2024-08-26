f8732f5fa635063158d058574c3eb779c1d7ca69
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-26 13:46:12 +0200

    ci(Update): Add GitHubClient to fetch release notes
    Introduced a new GitHubClient class to abstract GitHub API calls for
    fetching release notes. Refactored existing test to use the new
    GitHubClient, improving code readability and maintainability.

---
+++ MyExpenses.WebApi/GitHub/GitHubClient.cs
@@ -0,0 +1,26 @@
+﻿using MyExpenses.Models.WebApi.Github;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace MyExpenses.WebApi.GitHub;
+
+public class GitHubClient : Http
+{
+    private HttpClient HttpClient { get; } = GetHttpClient("https://api.github.com/repos/");
+
+    public async Task<List<Release>?> GetReleaseNotes(string owner, string repo)
+    {
+        var response = await HttpClient.GetAsync($"{owner}/{repo}/releases");
+
+        if (!response.IsSuccessStatusCode)
+        {
+            Log.Error("GitHub API returned unexpected response\n{ResponseReasonPhrase}", response.ReasonPhrase);
+            return null;
+        }
+
+        var content = await response.Content.ReadAsStringAsync();
+        var releases = JsonConvert.DeserializeObject<List<Release>>(content)!;
+
+        return releases;
+    }
+}
