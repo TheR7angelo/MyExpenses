653f84938e4ad21d96ff7b4920ba24ee9b9b7d5a
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-26 11:10:08 +0200

    ci(Update): Add GetParentDirectory method to Utils class
    Introduced a new utility method `GetParentDirectory` to navigate up a
    specified number of levels in the directory structure. This method
    enhances file path manipulation by providing an easy way to retrieve
    parent directories.

--- MyExpenses.Utils/Utils.cs
+++ MyExpenses.Utils/Utils.cs
@@ -7,6 +7,23 @@ namespace MyExpenses.Utils;
 public static class Utils
 {
     /// <summary>
+    /// Retrieves the parent directory of the specified path.
+    /// </summary>
+    /// <param name="path">The path for which the parent directory will be retrieved.</param>
+    /// <param name="depth">The number of levels up in the directory structure that will be traversed.
+    /// Default is 1.</param>
+    /// <returns>The parent directory of the specified path.</returns>
+    public static string GetParentDirectory(this string path, int depth = 1)
+    {
+        for (var i = 1; i <= depth; i++)
+        {
+            path = Path.GetDirectoryName(path)!;
+        }
+
+        return path;
+    }
+
+    /// <summary>
     /// Opens the MyExpenses GitHub page.
     /// </summary>
     /// <remarks>
