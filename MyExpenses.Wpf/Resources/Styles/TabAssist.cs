b05464d9529fb50d34041c44237801a82f31b251
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-27 21:34:00 +0200

    ci(Update): Add custom TabControl styles and properties
    Introduced new XAML resources and styles for customized TabControl
    appearance and behavior. Added TabAssist class to support attached
    properties for filled tab styles and uniform tab width functionality.

---
+++ MyExpenses.Wpf/Resources/Styles/TabAssist.cs
@@ -0,0 +1,51 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MyExpenses.Wpf.Resources.Styles;
+
+public static class TabAssist
+{
+    public static readonly DependencyProperty HasFilledTabProperty = DependencyProperty.RegisterAttached(
+        "HasFilledTab", typeof(bool), typeof(TabAssist), new PropertyMetadata(false));
+
+    public static void SetHasFilledTab(DependencyObject element, bool value)
+        => element.SetValue(HasFilledTabProperty, value);
+
+    public static bool GetHasFilledTab(DependencyObject element)
+        => (bool)element.GetValue(HasFilledTabProperty);
+
+    public static readonly DependencyProperty HasUniformTabWidthProperty = DependencyProperty.RegisterAttached(
+        "HasUniformTabWidth", typeof(bool), typeof(TabAssist), new PropertyMetadata(false));
+
+    public static void SetHasUniformTabWidth(DependencyObject element, bool value)
+        => element.SetValue(HasUniformTabWidthProperty, value);
+
+    public static bool GetHasUniformTabWidth(DependencyObject element)
+        => (bool)element.GetValue(HasUniformTabWidthProperty);
+
+    public static readonly DependencyProperty HeaderPanelMarginProperty = DependencyProperty.RegisterAttached(
+        "HeaderPanelMargin", typeof(Thickness), typeof(TabAssist), new PropertyMetadata(default(Thickness)));
+
+    public static void SetHeaderPanelMargin(DependencyObject element, Thickness value)
+        => element.SetValue(HeaderPanelMarginProperty, value);
+
+    public static Thickness GetHeaderPanelMargin(DependencyObject element)
+        => (Thickness) element.GetValue(HeaderPanelMarginProperty);
+
+    internal static Visibility GetBindableIsItemsHost(DependencyObject obj)
+        => (Visibility)obj.GetValue(BindableIsItemsHostProperty);
+
+    internal static void SetBindableIsItemsHost(DependencyObject obj, Visibility value)
+        => obj.SetValue(BindableIsItemsHostProperty, value);
+
+    internal static readonly DependencyProperty BindableIsItemsHostProperty =
+        DependencyProperty.RegisterAttached("BindableIsItemsHost", typeof(Visibility), typeof(TabAssist), new PropertyMetadata(Visibility.Collapsed, OnBindableIsItemsHostChanged));
+
+    private static void OnBindableIsItemsHostChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is Panel panel)
+        {
+            panel.IsItemsHost = (Visibility)e.NewValue == Visibility.Visible;
+        }
+    }
+}
