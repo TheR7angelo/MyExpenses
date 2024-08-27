22cbdbaae0574a1320ce11ed2b0fdcb45d9f4ea9
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-27 09:35:42 +0200

    ci(Update): Add DateOnlyToStringConverter for localized date formatting
    Implemented DateOnlyToStringConverter to format DateOnly values as
    strings, using localized date formats stored in resource files. Updated
    XAML and project files to include and utilize the new converter. This
    improves the display of date values in user interfaces by respecting
    cultural differences.

---
+++ MyExpenses.Wpf/Converters/DateOnlyToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using MyExpenses.Wpf.Resources.Resx.Converters.DateOnlyToStringConverter;
+
+namespace MyExpenses.Wpf.Converters;
+
+public class DateOnlyToStringConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        var format = DateOnlyToStringConverterResources.DateFormat;
+
+        if (value is DateOnly dateOnly) return dateOnly.ToString(format);
+
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string dateString) return value;
+        return DateTime.TryParse(dateString, culture, DateTimeStyles.None, out var result) ? result : value;
+    }
+}
