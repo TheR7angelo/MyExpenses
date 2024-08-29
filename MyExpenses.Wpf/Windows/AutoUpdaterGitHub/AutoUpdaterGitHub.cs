e7bd2577cd5820c3c98d45a855ddbfb992a829cc
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-29 09:36:57 +0200

    ci(Update): Add GitHub auto-update feature
    Introduced the `AutoUpdaterGitHub` class to handle checking for updates
    from a specified GitHub repository. Removed the inline update check in
    `WelcomePage` and replaced it with a call to the newly added
    `CheckUpdateGitHub` method. This refactoring simplifies the
    `WelcomePage` code and centralizes update logic in a dedicated class.

---
+++ MyExpenses.Wpf/Windows/AutoUpdaterGitHub/AutoUpdaterGitHub.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using MyExpenses.WebApi.GitHub;
+
+namespace MyExpenses.Wpf.Windows.AutoUpdaterGitHub;
+
+public static class AutoUpdaterGitHub
+{
+    // Juste for testing
+    private const string ApplicationOwner = "microsoft";
+    // Juste for testing
+    private const string ApplicationRepository = "PowerToys";
+
+    public static void CheckUpdateGitHub()
+    {
+        Task.Run(async () => { await CheckUpdateGitHubAsync(); });
+    }
+
+    private static async Task CheckUpdateGitHubAsync()
+    {
+        using var gitHubClient = new GitHubClient();
+
+        try
+        {
+            var releasesNotes = await gitHubClient.GetReleaseNotes(ApplicationOwner, ApplicationRepository);
+            if (releasesNotes is null) return; // Juste for testing
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private static void Initialize()
+    {
+        const string releasesUrl = @"C:\Users\ZP6177\Documents\Programmation\C#\MyExpenses\Tests\MyExpenses.IO.Test\bin\Debug\net8.0\test.html";
+
+        var autoUpdaterGitHubWindow = new AutoUpdaterGitHubWindow(releasesUrl)
+        {
+            Owner = Application.Current.MainWindow,
+        };
+        autoUpdaterGitHubWindow.ShowDialog();
+    }
+}
