88798f39a94f213898e41f9b59c328073235adfb
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-29 10:12:49 +0200

    ci(Update): Add XML documentation comments to Resources class methods
    Updated the Resources.cs file to include XML documentation comments for
    all methods. This enhances code readability and provides better context
    for developers on what each method does and the parameters they accept.

--- MyExpenses.Wpf/Utils/Resources.cs
+++ MyExpenses.Wpf/Utils/Resources.cs
@@ -7,12 +7,21 @@ namespace MyExpenses.Wpf.Utils;
 
 public static class Resources
 {
+    /// <summary>
+    /// Retrieves the resource object associated with the specified resource key.
+    /// </summary>
+    /// <param name="resourceKey">The key of the resource objects to retrieve.</param>
+    /// <returns>The resource object associated with the specified key.</returns>
     private static object GetFindResource(this string resourceKey)
     {
         var frameworkElement = new System.Windows.FrameworkElement();
         return frameworkElement.FindResource(resourceKey);
     }
 
+    /// <summary>
+    /// Retrieves the Material Design Paper color for Mapsui styles.
+    /// </summary>
+    /// <returns>The Material Design Paper color for Mapsui styles.</returns>
     public static Mapsui.Styles.Color GetMaterialDesignPaperMapsUiStylesColor()
     {
         var brush = "MaterialDesignPaper".GetSolidColorBrush();
@@ -21,6 +30,10 @@ public static class Resources
         return backColor;
     }
 
+    /// <summary>
+    /// Retrieves the SkiaSharp color object associated with the MaterialDesignBody resource.
+    /// </summary>
+    /// <returns>The SkiaSharp color object associated with the MaterialDesignBody resource.</returns>
     public static SKColor GetMaterialDesignBodySkColor()
     {
         var wpfColor = "MaterialDesignBody".GetColor();
@@ -28,18 +41,41 @@ public static class Resources
         return skColor;
     }
 
+    /// <summary>
+    /// Retrieves the SolidColorBrush object associated with the specified resource key.
+    /// </summary>
+    /// <param name="resourceKey">The key of the resource objects to retrieve.</param>
+    /// <returns>The SolidColorBrush object associated with the specified key.</returns>
     private static SolidColorBrush GetSolidColorBrush(this string resourceKey)
         => (SolidColorBrush)resourceKey.GetFindResource();
 
+    /// <summary>
+    /// Retrieves the color object associated with the specified resource key.
+    /// </summary>
+    /// <param name="resourceKey">The key of the color object to retrieve.</param>
+    /// <returns>The color object associated with the specified key.</returns>
     private static Color GetColor(this string resourceKey)
         => ((SolidColorBrush)resourceKey.GetFindResource()).Color;
 
+    /// <summary>
+    /// Retrieves the hexadecimal code associated with the SkiaSharp color object of the MaterialDesignBody resource.
+    /// </summary>
+    /// <returns>The hexadecimal code associated with the SkiaSharp color object of the MaterialDesignBody resource.</returns>
     public static string GetMaterialDesignBodyColorHexadecimal()
         => "MaterialDesignBody".GetHexadecimalCode();
 
+    /// <summary>
+    /// Retrieves the hexadecimal code associated with the SkiaSharp color object of the MaterialDesignPaper resource.
+    /// </summary>
+    /// <returns>The hexadecimal code associated with the SkiaSharp color object of the MaterialDesignPaper resource.</returns>
     public static string GetMaterialDesignPaperColorHexadecimal()
         => "MaterialDesignPaper".GetHexadecimalCode();
 
+    /// <summary>
+    /// Retrieves the hexadecimal code associated with the SkiaSharp color object of the specified resource key.
+    /// </summary>
+    /// <param name="resourceKey">The key of the resource object to retrieve the hexadecimal code for.</param>
+    /// <returns>The hexadecimal code associated with the SkiaSharp color object of the specified resource key.</returns>
     private static string GetHexadecimalCode(this string resourceKey)
     {
         var wpfColor = resourceKey.GetColor();
