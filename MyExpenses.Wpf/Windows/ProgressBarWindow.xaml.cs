2c454520dc8751075148af1b994848fdc491a985
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-31 16:09:57 +0200

    ci(Update): Refactor speed progress update logic
    Extract speed progress calculation into a separate method using a timer.
    This refactoring improves code readability and encapsulates the logic
    for updating speed progress, ensuring the timer is stopped when the
    download is complete.

--- MyExpenses.Wpf/Windows/ProgressBarWindow.xaml.cs
+++ MyExpenses.Wpf/Windows/ProgressBarWindow.xaml.cs
@@ -1,6 +1,7 @@
 using System.Windows;
 using MyExpenses.WebApi;
 using MyExpenses.Wpf.Utils;
+using Timer = System.Timers.Timer;
 
 namespace MyExpenses.Wpf.Windows;
 
@@ -33,12 +34,8 @@ public partial class ProgressBarWindow
     public async Task StartProgressBarDownload(string url, string destinationFile, bool overwrite = false)
     {
         IProgress<double> percentProgress = new Progress<double>(d => { ProgressBarPercent.Value = d; });
-        IProgress<(double NormalizeBytes, string NormalizeBytesUnit)> speedProgress =
-            new Progress<(double NormalizeBytes, string NormalizeBytesUnit)>(d =>
-            {
-                var roundNormalizeBytes = Math.Round(d.NormalizeBytes);
-                SpeedProgress = $"{roundNormalizeBytes} {d.NormalizeBytesUnit}/s";
-            });
+
+        var speedTimer = GetSpeedProgress(out var speedProgress);
 
         CancellationTokenSource?.Dispose();
         CancellationTokenSource = new CancellationTokenSource();
@@ -47,8 +44,32 @@ public partial class ProgressBarWindow
             speedProgress: speedProgress, cancellationToken: CancellationTokenSource.Token);
 
         DownloadIsDone = true;
+        speedTimer.Stop();
 
         Thread.Sleep(TimeSpan.FromSeconds(2.5));
+    }
+
+    private Timer GetSpeedProgress(out IProgress<(double NormalizeBytes, string NormalizeBytesUnit)> speedProgress)
+    {
+        double latestSpeed = 0;
+        var latestUnit = "";
+        var speedTimer = new Timer(TimeSpan.FromSeconds(2.5));
+        speedTimer.Elapsed += (_, _) =>
+        {
+            Dispatcher.Invoke(() =>
+            {
+                var roundNormalizeBytes = Math.Round(latestSpeed, 1);
+                SpeedProgress = $"{roundNormalizeBytes} {latestUnit}/s";
+            });
+        };
+        speedTimer.Start();
+
+        speedProgress = new Progress<(double NormalizeBytes, string NormalizeBytesUnit)>(d =>
+        {
+            latestSpeed = d.NormalizeBytes;
+            latestUnit = d.NormalizeBytesUnit;
+        });
+        return speedTimer;
     }
 
     private void ProgressBarWindow_OnClosed(object? sender, EventArgs e)
