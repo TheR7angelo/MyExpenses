1dfdb6413c09023eb1d054968144bd6c86f8c411
TheR7angelo <raphael.antunes@hotmail.fr>
2024-08-31 15:55:09 +0200

    ci(Update): Add download speed display to ProgressBarWindow
    Introduced a new DependencyProperty for displaying download speed.
    Handled cancellation of the download if the window is closed
    prematurely.

--- MyExpenses.Wpf/Windows/ProgressBarWindow.xaml.cs
+++ MyExpenses.Wpf/Windows/ProgressBarWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using MyExpenses.WebApi;
 using MyExpenses.Wpf.Utils;
 
@@ -5,6 +6,21 @@ namespace MyExpenses.Wpf.Windows;
 
 public partial class ProgressBarWindow
 {
+    #region DependencyProperty
+
+    public static readonly DependencyProperty SpeedProgressProperty = DependencyProperty.Register(nameof(SpeedProgress),
+        typeof(string), typeof(ProgressBarWindow), new PropertyMetadata(default(string)));
+
+    public string SpeedProgress
+    {
+        get => (string)GetValue(SpeedProgressProperty);
+        set => SetValue(SpeedProgressProperty, value);
+    }
+
+    #endregion
+
+    private CancellationTokenSource? CancellationTokenSource { get; set; }
+    private bool DownloadIsDone { get; set; }
 
     //TODO title
     public ProgressBarWindow()
@@ -17,10 +33,29 @@ public partial class ProgressBarWindow
     public async Task StartProgressBarDownload(string url, string destinationFile, bool overwrite = false)
     {
         IProgress<double> percentProgress = new Progress<double>(d => { ProgressBarPercent.Value = d; });
+        IProgress<(double NormalizeBytes, string NormalizeBytesUnit)> speedProgress =
+            new Progress<(double NormalizeBytes, string NormalizeBytesUnit)>(d =>
+            {
+                var roundNormalizeBytes = Math.Round(d.NormalizeBytes);
+                SpeedProgress = $"{roundNormalizeBytes} {d.NormalizeBytesUnit}/s";
+            });
 
-        await Http.DownloadFileWithReportAsync(url, destinationFile, overwrite, percentProgress: percentProgress);
+        CancellationTokenSource?.Dispose();
+        CancellationTokenSource = new CancellationTokenSource();
+
+        await Http.DownloadFileWithReportAsync(url, destinationFile, overwrite, percentProgress: percentProgress,
+            speedProgress: speedProgress, cancellationToken: CancellationTokenSource.Token);
+
+        DownloadIsDone = true;
 
         Thread.Sleep(TimeSpan.FromSeconds(2.5));
     }
 
+    private void ProgressBarWindow_OnClosed(object? sender, EventArgs e)
+    {
+        if (DownloadIsDone) return;
+
+        CancellationTokenSource?.Cancel();
+        CancellationTokenSource?.Dispose();
+    }
 }
