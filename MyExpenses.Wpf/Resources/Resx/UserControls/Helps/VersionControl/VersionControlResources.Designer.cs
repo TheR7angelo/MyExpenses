a820d8ab3842c1f5a05c050b673fc05504dfc23e
TheR7angelo <raphael.antunes@hotmail.fr>
2024-09-02 16:02:12 +0200

    ci(Update): Move VersionControlResources to subdirectory
    Relocated VersionControlResources files to the VersionControl
    subdirectory. This change ensures better organization by grouping
    related resources together and updating appropriate metadata references
    in the project file.

--- MyExpenses.Wpf/Resources/Resx/UserControls/Helps/VersionControl/VersionControlResources.Designer.cs
+++ MyExpenses.Wpf/Resources/Resx/UserControls/Helps/VersionControl/VersionControlResources.Designer.cs
@@ -7,10 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MyExpenses.Wpf.Resources.Resx.UserControls.Helps {
-    using System;
-    
-    
+namespace MyExpenses.Wpf.Resources.Resx.UserControls.Helps.VersionControl {
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -38,7 +35,7 @@ namespace MyExpenses.Wpf.Resources.Resx.UserControls.Helps {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MyExpenses.Wpf.Resources.Resx.UserControls.Helps.VersionControlResources", typeof(VersionControlResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MyExpenses.Wpf.Resources.Resx.UserControls.Helps.VersionControl.VersionControlResources", typeof(VersionControlResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
