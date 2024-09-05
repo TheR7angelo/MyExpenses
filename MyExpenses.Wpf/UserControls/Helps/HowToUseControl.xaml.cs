2fabf5805181b50fc20ee6264e40e67c930b7d65
TheR7angelo <raphael.antunes@hotmail.fr>
2024-09-05 11:14:05 +0200

    ci(Update): Add culture-specific PDF loading to HowToUseControl
    Implemented functionality to load culture-specific PDFs from the
    "Resources/How to use" directory. This enhancement incorporates dynamic
    culture detection and updates based on available PDF files for different
    cultures.

--- MyExpenses.Wpf/UserControls/Helps/HowToUseControl.xaml.cs
+++ MyExpenses.Wpf/UserControls/Helps/HowToUseControl.xaml.cs
@@ -1,9 +1,38 @@
+using System.Globalization;
+using System.IO;
+
 namespace MyExpenses.Wpf.UserControls.Helps;
 
 public partial class HowToUseControl
 {
+    public List<CultureInfo> CultureInfos { get; }
     public HowToUseControl()
     {
+        CultureInfos = GetCultureInfoHowToUse();
+
         InitializeComponent();
+    }
+
+    private static List<CultureInfo> GetCultureInfoHowToUse()
+    {
+        var directory = Path.GetFullPath("Resources");
+        directory = Path.Join(directory, "How to use");
+
+        var results = new List<CultureInfo>();
+        var allCulture = CultureInfo.GetCultures(CultureTypes.AllCultures);
+        var files = Directory.GetFiles(directory, "*.pdf");
+        foreach (var file in files)
+        {
+            var filename = Path.GetFileNameWithoutExtension(file);
+            var filenameSplit = filename.Split('_');
+            if (filenameSplit.Length < 1) continue;
+
+            var cultureName = filenameSplit[1];
+
+            var cultureInfo = allCulture.FirstOrDefault(c => c.EnglishName.Contains(cultureName, StringComparison.CurrentCultureIgnoreCase));
+            if (cultureInfo is not null) results.Add(cultureInfo);
+        }
+
+        return results;
     }
 }
