49f6fa591eb06df67d63c219411e707f0feb13ac
TheR7angelo <raphael.antunes@hotmail.fr>
2024-09-16 10:49:34 +0200

    ci(Update): Add Word file selection and transformation functionality
    This commit introduces the ability to select a Word document and
    transform it by modifying its contents using a zip archive approach. The
    UI has been updated to include buttons for selecting the Word file and
    initiating the transformation, along with a TextBox to display the
    selected file path.

---
+++ Helpers/MyExpenses.Wpf.WordRelativePath/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace MyExpenses.Wpf.WordRelativePath;
+
+
+public partial class MainWindow
+{
+    public static readonly DependencyProperty FilePathProperty = DependencyProperty.Register(nameof(FilePath),
+        typeof(string), typeof(MainWindow), new PropertyMetadata(default(string)));
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    public string FilePath
+    {
+        get => (string)GetValue(FilePathProperty);
+        set => SetValue(FilePathProperty, value);
+    }
+
+    private void ButtonSelectWord_OnClick(object sender, RoutedEventArgs e)
+    {
+        var fileDialog = new OpenFileDialog { Multiselect = false, Filter = "Word file (.docx)|*.docx" };
+
+        var result = fileDialog.ShowDialog();
+        if (result is not true) return;
+
+        FilePath = fileDialog.FileName;
+    }
+
+    private void ButtonTransform_OnClick(object sender, RoutedEventArgs e)
+    {
+        var temp = Path.GetFullPath("temp.zip");
+
+        var now = DateTime.Now;
+        var directory = Path.GetDirectoryName(FilePath)!;
+        var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(FilePath);
+        var filePath = Path.Join(directory, $"{fileNameWithoutExtension}_{now:yyyy-MM-dd}.docx");
+
+        File.Copy(FilePath, temp, true);
+
+        EditZipArchive(temp);
+
+        File.Move(temp, filePath, true);
+    }
+
+    private void EditZipArchive(string temp)
+    {
+        using var fileStream = new FileStream(temp, FileMode.Open);
+        using var zipArchive = new ZipArchive(fileStream, ZipArchiveMode.Update);
+
+        var entries = zipArchive.Entries.Where(x => x.FullName.StartsWith("word/media")).ToList();
+        foreach (var entry in entries)
+        {
+            entry.Delete();
+        }
+
+        // var xml = zipArchive.Entries.FirstOrDefault(x => x.FullName.Equals("word/_rels/document.xml.rels"));
+        // if (xml is null) return;
+        //
+        // var entryStream = xml.Open();
+        // var reader = new StreamReader(entryStream);
+        // var document = XDocument.Load(reader);
+        //
+        // var relationShips = document.Element(XName.Get("Relationships",
+        //     "http://schemas.openxmlformats.org/package/2006/relationships"));
+        //
+        // var wordDirectory = new DirectoryInfo(Path.GetDirectoryName(FilePath)!).Name.Replace(" ", "%20");
+        // foreach (var relationShip in relationShips!.Elements(XName.Get("Relationship", "http://schemas.openxmlformats.org/package/2006/relationships")))
+        // {
+            // var targetAttr = relationShip.Attribute("Target");
+            // if (targetAttr is null) continue;
+            //
+            // var value = targetAttr.Value;
+            // if (value.StartsWith("file:"))
+            // {
+            //     var relativePath = value.Split(wordDirectory).Last();
+            //     relativePath = $"{wordDirectory}{relativePath}";
+            //     targetAttr.Value = relativePath;
+            // }
+        // }
+        //
+        // var writer = new StreamWriter(entryStream);
+        // document.Save(writer);
+        // writer.Flush();
+    }
+}
