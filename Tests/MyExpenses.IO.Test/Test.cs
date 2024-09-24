8562e20073297aacf34f52af993c844f1b69ce9a
TheR7angelo <raphael.antunes@hotmail.fr>
2024-09-24 17:00:38 +0200

    ci(Update): Refactor export account handling and add validation
    Revamped the export logic to replace recursive frequency handling with
    account-related validations. Now, accounts are validated against account
    types and currencies directly, streamlining table collection creation
    and worksheet management. Also added a new hidden worksheet for Boolean
    value testing.

--- Tests/MyExpenses.IO.Test/Test.cs
+++ Tests/MyExpenses.IO.Test/Test.cs
@@ -1,8 +1,11 @@
 using System.Reflection;
 using MyExpenses.IO.Excel;
+using MyExpenses.Models.Sql.Bases.Views.Exports;
 using MyExpenses.Sql.Context;
 using MyExpenses.Utils;
 using OfficeOpenXml;
+using OfficeOpenXml.DataValidation.Contracts;
+using OfficeOpenXml.Table;
 
 namespace MyExpenses.IO.Test;
 
@@ -21,54 +24,28 @@ public class Test
         using var package = new ExcelPackage();
         var workbook = package.Workbook;
 
-        var exportVRecursiveFrequency = context.ExportVRecursiveFrequencies.AsEnumerable();
+        var worksheet = workbook.Worksheets.Add("param");
+        worksheet.Cells["A1"].Value = "Boolean value";
+        worksheet.Cells["A2"].Value = false;
+        worksheet.Cells["A3"].Value = true;
+        var range = worksheet.Cells["A1:A3"];
+        worksheet.SetExcelTableStyle(range, "Boolean_value");
+        worksheet.Hidden = eWorkSheetHidden.VeryHidden;
+
+        // var exportVRecursiveFrequency = context.ExportVRecursiveFrequencies.AsEnumerable();
         var exportVAccountType = context.ExportVAccountTypes.AsEnumerable();
+        var exportVCurrency = context.ExportVCurrencies.AsEnumerable();
+        var exportVAccount = context.ExportVAccounts.AsEnumerable();
 
-        _ = workbook.SetTableCollection(exportVRecursiveFrequency, context);
-        var exportVAccountTypeRange = workbook.SetTableCollection(exportVAccountType, context);
+        var exportVAccountTypeTable = workbook.AddTableCollection(exportVAccountType, context);
+        var exportVCurrencyTable = workbook.AddTableCollection(exportVCurrency, context);
+        var exportVAccountRange = workbook.AddTableCollection(exportVAccount, context);
 
+        exportVAccountRange.AddListValidation(exportVAccountTypeTable, typeof(ExportVAccount), nameof(ExportVAccount.AccountType), nameof(ExportVAccountType.Name));
+        exportVAccountRange.AddListValidation(exportVCurrencyTable, typeof(ExportVAccount), nameof(ExportVAccount.Currency), nameof(ExportVCurrency.Symbol));
+
+        // _ = workbook.SetTableCollection(exportVRecursiveFrequency, context);
         const string filePath = "Test.xlsx";
         package.SaveAs(filePath);
     }
-
-
-    // [Fact]
-    // private void ToExcel()
-    // {
-    //     var executablePath = Assembly.GetExecutingAssembly().Location;
-    //     var path = executablePath.GetParentDirectory(6);
-    //     var dbFile = Path.Combine(path, "MyExpenses.Wpf", "bin", "Debug", "net8.0-windows", "Databases", "Model - Using.sqlite");
-    //     using var context = new DataBaseContext(dbFile);
-    //
-    //     using var package = new ExcelPackage();
-    //     var workbook = package.Workbook;
-    //     var worksheet = workbook.Worksheets.Add("Sheet1");
-    //
-    //     var records = from ta in context.TAccounts
-    //                   join tat in context.TAccountTypes on ta.AccountTypeFk equals tat.Id
-    //                   join tc in context.TCurrencies on ta.CurrencyFk equals tc.Id
-    //                   select new
-    //                   {
-    //                       ta.Id,
-    //                       AccountName = ta.Name,
-    //                       AccountType = tat.Name,
-    //                       tc.Symbol,
-    //                       ta.Active,
-    //                       ta.DateAdded
-    //                   };
-    //     var results = records.AsEnumerable();
-    //     var range = worksheet.Cells["A1"].LoadFromCollection(results, true);
-    //     SetExcelTableStyle(worksheet, range, "test");
-    //
-    //     const int accountTypeColumnIndex = 3;
-    //
-    //     var options = context.TAccountTypes.Select(ct => ct.Name).Distinct().AsEnumerable();
-    //     var concatenatedOptions = string.Join(',', options);
-    //
-    //     var validation = worksheet.DataValidations.AddListValidation(worksheet.Cells[2, accountTypeColumnIndex, worksheet.Dimension.End.Row, accountTypeColumnIndex].Address);
-    //     validation.Formula.Values.Add(concatenatedOptions);
-    //
-    //     const string filePath = "test.xlsx";
-    //     package.SaveAs(filePath);
-    // }
 }
