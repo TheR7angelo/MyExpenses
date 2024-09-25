0a38681d3d5749392de1654589e3b03210a7c987
TheR7angelo <raphael.antunes@hotmail.fr>
2024-09-25 10:50:08 +0200

    ci(Update): Switch to context-based data export and simplify Excel handling
    Replaced IEnumerable exportRecords with DataBaseContext to streamline
    data fetching directly from the database. Introduced color-coded level
    differentiation and list validation for various export types, enhancing
    the readability and integrity of Excel data. Simplified table styles,
    date formatting, and value setting while removing redundant code
    sections.

--- MyExpenses.IO/Excel/ImportExportDataTableExcel.cs
+++ MyExpenses.IO/Excel/ImportExportDataTableExcel.cs
@@ -1,109 +1,84 @@
-using System.ComponentModel;
-using System.Reflection;
-using MyExpenses.Models.IO.Export;
+using System.Drawing;
+using MyExpenses.Models.Sql.Bases.Views.Exports;
+using MyExpenses.Sql.Context;
 using OfficeOpenXml;
-using OfficeOpenXml.Table;
 using LicenseContext = OfficeOpenXml.LicenseContext;
 
 namespace MyExpenses.IO.Excel;
 
 public static class ImportExportDataTableExcel
 {
+    private static readonly Color ColorLevel1 = Color.Aqua;
+    private static readonly Color ColorLevel2 = Color.Blue;
+    private static readonly Color ColorLevel3 = Color.Green;
+
     static ImportExportDataTableExcel()
     {
         ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
     }
 
-    public static bool ToExcelWorksheet(this IEnumerable<ExportRecord> exportRecords, string filePath)
+    public static bool ToExcelWorksheet(this DataBaseContext context, string filePath)
     {
         filePath = Path.ChangeExtension(filePath, ".xlsx");
 
         using var package = new ExcelPackage();
-        using var workbook = package.Workbook;
+        var workbook = package.Workbook;
 
-        foreach (var exportRecord in exportRecords)
-        {
-            var worksheet = package.Workbook.Worksheets.Add(exportRecord.Name);
+        var exportVAccountType = context.ExportVAccountTypes.AsEnumerable();
+        var exportVCurrency = context.ExportVCurrencies.AsEnumerable();
+        var exportVAccount = context.ExportVAccounts.AsEnumerable();
+        var exportVColor = context.ExportVColors.AsEnumerable();
+        var exportVCategoryType = context.ExportVCategoryTypes.AsEnumerable();
+        var exportVBankTransfer = context.ExportVBankTransfers.AsEnumerable();
+        var exportVModePayment = context.ExportVModePayments.AsEnumerable();
+        var exportVPlace = context.ExportVPlaces.AsEnumerable();
+        var exportVRecursiveFrequency = context.ExportVRecursiveFrequencies.AsEnumerable();
+        var exportVRecursiveExpense = context.ExportVRecursiveExpenses.AsEnumerable();
+        var exportVHistory = context.ExportVHistories.AsEnumerable();
 
-            var properties = exportRecord.Records.First()!.GetType().GetProperties();
-            var headers = worksheet.SetHeaders(properties);
+        var exportVHistoryTable = workbook.AddTableCollection(exportVHistory, context, ColorLevel3);
+        var exportVBankTransferTypeTable = workbook.AddTableCollection(exportVBankTransfer, context, ColorLevel3);
+        var exportVRecursiveExpenseTable = workbook.AddTableCollection(exportVRecursiveExpense, context, ColorLevel3);
 
-            worksheet.SetValue(exportRecord, properties, headers);
+        var exportVAccountTable = workbook.AddTableCollection(exportVAccount, context, ColorLevel2);
+        var exportVCategoryTypeTable = workbook.AddTableCollection(exportVCategoryType, context, ColorLevel2);
 
-            worksheet.SetDateStyle(headers);
+        var exportVAccountTypeTable = workbook.AddTableCollection(exportVAccountType, context, ColorLevel1);
+        var exportVCurrencyTable = workbook.AddTableCollection(exportVCurrency, context, ColorLevel1);
+        var exportVColorTable = workbook.AddTableCollection(exportVColor, context, ColorLevel1);
+        var exportVModePaymentTable = workbook.AddTableCollection(exportVModePayment, context, ColorLevel1);
+        var exportVPlaceTable = workbook.AddTableCollection(exportVPlace, context, ColorLevel1);
+        var exportVRecursiveFrequencyTable = workbook.AddTableCollection(exportVRecursiveFrequency, context, ColorLevel1);
 
-            var dataRange = worksheet.SetExcelTableStyle(headers, exportRecord);
+        var booleanTable = workbook.AddBooleanTable();
 
-            dataRange.AutoFitColumns();
-        }
+        exportVAccountTable.AddListValidation(exportVAccountTypeTable, typeof(ExportVAccount), nameof(ExportVAccount.AccountType), nameof(ExportVAccountType.Name));
+        exportVAccountTable.AddListValidation(exportVCurrencyTable, typeof(ExportVAccount), nameof(ExportVAccount.Currency), nameof(ExportVCurrency.Symbol));
+        exportVAccountTable.AddListValidationTrueFalse(booleanTable, typeof(ExportVAccount), nameof(ExportVAccount.Active));
+
+        exportVCategoryTypeTable.AddListValidation(exportVColorTable, typeof(ExportVCategoryType), nameof(ExportVCategoryType.ColorName), nameof(ExportVColor.Name));
+
+        exportVBankTransferTypeTable.AddListValidation(exportVAccountTable, typeof(ExportVBankTransfer), nameof(ExportVBankTransfer.FromAccountName), nameof(ExportVAccount.Name));
+        exportVBankTransferTypeTable.AddListValidation(exportVAccountTable, typeof(ExportVBankTransfer), nameof(ExportVBankTransfer.ToAccountName), nameof(ExportVAccount.Name));
+
+        exportVRecursiveExpenseTable.AddListValidation(exportVAccountTable, typeof(ExportVRecursiveExpense), nameof(ExportVRecursiveExpense.AccountName), nameof(ExportVAccount.Name));
+        exportVRecursiveExpenseTable.AddListValidation(exportVCategoryTypeTable, typeof(ExportVRecursiveExpense), nameof(ExportVRecursiveExpense.CategoryType), nameof(ExportVCategoryType.Name));
+        exportVRecursiveExpenseTable.AddListValidation(exportVModePaymentTable, typeof(ExportVRecursiveExpense), nameof(ExportVRecursiveExpense.ModePayment), nameof(ExportVModePayment.Name));
+        exportVRecursiveExpenseTable.AddListValidation(exportVPlaceTable, typeof(ExportVRecursiveExpense), nameof(ExportVRecursiveExpense.PlaceName), nameof(ExportVPlace.Name));
+        exportVRecursiveExpenseTable.AddListValidation(exportVRecursiveFrequencyTable, typeof(ExportVRecursiveExpense), nameof(ExportVRecursiveExpense.Frequency), nameof(ExportVRecursiveFrequency.Frequency));
+        exportVRecursiveExpenseTable.AddListValidationTrueFalse(booleanTable, typeof(ExportVRecursiveExpense), nameof(ExportVRecursiveExpense.IsActive));
+        exportVRecursiveExpenseTable.AddListValidationTrueFalse(booleanTable, typeof(ExportVRecursiveExpense), nameof(ExportVRecursiveExpense.ForceDeactivate));
+
+        exportVHistoryTable.AddListValidation(exportVAccountTable, typeof(ExportVHistory), nameof(ExportVHistory.AccountName), nameof(ExportVAccount.Name));
+        exportVHistoryTable.AddListValidation(exportVCategoryTypeTable, typeof(ExportVHistory), nameof(ExportVHistory.CategoryType), nameof(ExportVCategoryType.Name));
+        exportVHistoryTable.AddListValidation(exportVModePaymentTable, typeof(ExportVHistory), nameof(ExportVHistory.ModePayment), nameof(ExportVModePayment.Name));
+        exportVHistoryTable.AddListValidation(exportVPlaceTable, typeof(ExportVHistory), nameof(ExportVHistory.Place), nameof(ExportVPlace.Name));
+        exportVHistoryTable.AddListValidationTrueFalse(booleanTable, typeof(ExportVHistory), nameof(ExportVHistory.Pointed));
+
+        exportVHistoryTable.OrderTable(typeof(ExportVHistory), nameof(ExportVHistory.Date), eSortOrder.Descending);
 
         package.SaveAs(filePath);
 
-        return false;
-    }
-
-    private static ExcelRange SetExcelTableStyle(this ExcelWorksheet worksheet, List<PropertyInfo> headers, ExportRecord exportRecord)
-    {
-        var maxRow = worksheet.Dimension.End.Row;
-        var dataRange = worksheet.Cells[1, 1, maxRow, headers.Count];
-        var table = worksheet.Tables.Add(dataRange, $"{exportRecord.Name}_table");
-        table.TableStyle = TableStyles.Medium7;
-        return dataRange;
-    }
-
-    private static void SetDateStyle(this ExcelWorksheet worksheet, List<PropertyInfo> headers)
-    {
-        foreach (var propertyInfo in headers)
-        {
-            if (propertyInfo.PropertyType == typeof(DateTime) || propertyInfo.PropertyType == typeof(DateTime?))
-            {
-                var index = headers.IndexOf(propertyInfo) + 1;
-                worksheet.Column(index).Style.Numberformat.Format = "yyyy-mm-dd hh:mm:ss";
-            }
-
-            if (propertyInfo.PropertyType == typeof(DateOnly) || propertyInfo.PropertyType == typeof(DateOnly?))
-            {
-                var index = headers.IndexOf(propertyInfo) + 1;
-                worksheet.Column(index).Style.Numberformat.Format = "yyyy-mm-dd";
-            }
-        }
-    }
-
-    private static void SetValue(this ExcelWorksheet worksheet, ExportRecord exportRecord, PropertyInfo[] properties,
-        List<PropertyInfo> headers)
-    {
-        var row = 2;
-        foreach (var record in exportRecord.Records)
-        {
-            var column = 1;
-            foreach (var propertyInfo in properties)
-            {
-                if (!headers.Contains(propertyInfo)) continue;
-
-                var value = propertyInfo.GetValue(record);
-                worksheet.Cells[row, column].Value = value;
-
-                column++;
-            }
-
-            row++;
-        }
-    }
-
-    private static List<PropertyInfo> SetHeaders(this ExcelWorksheet worksheet, PropertyInfo[] properties)
-    {
-        var column = 1;
-        var headers = new List<PropertyInfo>();
-        foreach (var propertyInfo in properties)
-        {
-            var header = propertyInfo.GetCustomAttribute<DisplayNameAttribute>()?.DisplayName;
-            if (string.IsNullOrEmpty(header)) continue;
-
-            worksheet.Cells[1, column].Value = header;
-            headers.Add(propertyInfo);
-            column++;
-        }
-
-        return headers;
+        return true;
     }
 }
