fe407a755a3c25d0bb5692b21d366c2912d6274b
TheR7angelo <raphael.antunes@hotmail.fr>
2024-09-25 10:56:19 +0200

    ci(Update): Remove export functionality
    Deleted the Common export logic and related ExportRecord class along
    with the associated AutoMapper profiles and export table classes. This
    cleanup removes outdated code that is no longer necessary for the project's
    structure.

--- MyExpenses.Models/IO/Export/ExportRecord.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyExpenses.Models.IO.Export;
-
-public class ExportRecord
-{
-    public required string Name { get; set; }
-    public required Type Source { get; set; }
-    public List<object?> Records { get; set; } = [];
-}
