388673b80c42f805d66727e50e66398b39833543
TheR7angelo <raphael.antunes@hotmail.fr>
2024-09-28 10:40:42 +0200

    ci(Update): Add initial MyExpenses.Smartphones project
    Introduces the MyExpenses.Smartphones project with initial setup files
    including MainPage, AppShell, styles, assets, fonts, and configuration.
    This establishes the basic structure and resources needed for the
    application.

---
+++ MyExpenses.Smartphones/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyExpenses.Smartphones;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MyExpenses.Smartphones/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MyExpenses.Smartphones;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
