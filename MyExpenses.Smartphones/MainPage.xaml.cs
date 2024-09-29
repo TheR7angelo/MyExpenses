71b24da1e1a04a80abe191e33ef0f8ecceb02211
TheR7angelo <raphael.antunes@hotmail.fr>
2024-09-29 19:30:17 +0200

    ci(Update): Add data binding for MainPage version label
    Introduced a new bindable property `LabelContent` in `MainPage.xaml.cs`
    to display the database version. Removed the static image in
    `MainPage.xaml` and added a label bound to `LabelContent`.

--- MyExpenses.Smartphones/MainPage.xaml.cs
+++ MyExpenses.Smartphones/MainPage.xaml.cs
@@ -1,11 +1,29 @@
-﻿namespace MyExpenses.Smartphones;
+﻿using MyExpenses.Sql.Context;
+
+namespace MyExpenses.Smartphones;
 
 public partial class MainPage
 {
     private int _count;
 
+    public static readonly BindableProperty LabelContentProperty =
+        BindableProperty.Create(nameof(LabelContent), typeof(string), typeof(MainPage), default(string));
+
+    public string LabelContent
+    {
+        get => (string)GetValue(LabelContentProperty);
+        set => SetValue(LabelContentProperty, value);
+    }
+
     public MainPage()
     {
+        var dbFilePath = DbContextBackup.LocalFilePathDataBaseModel;
+        dbFilePath = Path.Join(FileSystem.AppDataDirectory, dbFilePath);
+        var z = File.Exists(dbFilePath);
+        using var context = new DataBaseContext(dbFilePath);
+
+        LabelContent = context.TVersions.First().Version!.ToString();
+
         InitializeComponent();
     }
 
