a657fb2964dfe232ff117465adf3076d09d9b3bd
TheR7angelo <raphael.antunes@hotmail.fr>
2024-09-30 16:06:05 +0200

    ci(Update): Add support for database file models
    Introduced a new method to include database file models in the
    FileManager. Created storage paths and handled file writing with
    necessary directory creation. Adjust WritePackageFile method to use
    synchronous CopyTo for compatibility.

--- MyExpenses.Smartphones/FileManager.cs
+++ MyExpenses.Smartphones/FileManager.cs
@@ -4,16 +4,29 @@ public class FileManager
 {
     public void AddAllFiles()
     {
-
+        AddDatabaseFileModels();
     }
 
-    private async Task WritePackageFile(string packageFile, string storagePath)
+    private void AddDatabaseFileModels()
+    {
+        var packageFile = Path.Join("Database Models", "Model.sqlite");
+        var storagePath = Path.Join(FileSystem.AppDataDirectory, packageFile);
+
+        WritePackageFile(packageFile, storagePath)
+            .ConfigureAwait(false).GetAwaiter().GetResult();
+    }
+
+    private static async Task WritePackageFile(string packageFile, string storagePath)
     {
         var packageExist = await FileSystem.Current.AppPackageFileExistsAsync(packageFile);
         if (!packageExist) return;
 
+        var parentPath = Path.GetDirectoryName(storagePath)!;
+        Directory.CreateDirectory(parentPath);
+
         await using var stream = await FileSystem.Current.OpenAppPackageFileAsync(packageFile);
         await using var fileStream = File.Create(storagePath);
-        await stream.CopyToAsync(fileStream);
+        // ReSharper disable once MethodHasAsyncOverload
+        stream.CopyTo(fileStream);
     }
 }
