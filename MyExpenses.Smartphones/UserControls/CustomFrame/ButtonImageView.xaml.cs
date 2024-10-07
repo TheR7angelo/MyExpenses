a0d7eadaa6c14a172f7323d3fda87d19689e235d
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-07 15:23:19 +0200

    ci(Update): Add ButtonImageView control for customizable image buttons
    This commit introduces a new ButtonImageView control in the CustomFrame
    namespace. The control allows customization of image and label
    properties like geometry, size, and layout options, making it versatile
    for UI development. This includes XAML and code-behind files to bind
    these properties dynamically.

---
+++ MyExpenses.Smartphones/UserControls/CustomFrame/ButtonImageView.xaml.cs
@@ -0,0 +1,87 @@
+using Microsoft.Maui.Controls.Shapes;
+
+namespace MyExpenses.Smartphones.UserControls.CustomFrame;
+
+public partial class ButtonImageView
+{
+    public static readonly BindableProperty GeometrySourceProperty = BindableProperty.Create(nameof(GeometrySource),
+        typeof(Geometry), typeof(ButtonImageView), default(Geometry));
+
+    public Geometry GeometrySource
+    {
+        get => (Geometry)GetValue(GeometrySourceProperty);
+        set => SetValue(GeometrySourceProperty, value);
+    }
+
+    public static readonly BindableProperty VerticalLabelOptionsProperty =
+        BindableProperty.Create(nameof(VerticalLabelOptions), typeof(LayoutOptions), typeof(ButtonImageView),
+            default(LayoutOptions));
+
+    public LayoutOptions VerticalLabelOptions
+    {
+        get => (LayoutOptions)GetValue(VerticalLabelOptionsProperty);
+        set => SetValue(VerticalLabelOptionsProperty, value);
+    }
+
+    public static readonly BindableProperty HorizontalLabelOptionsProperty =
+        BindableProperty.Create(nameof(HorizontalLabelOptions), typeof(LayoutOptions), typeof(ButtonImageView),
+            default(LayoutOptions));
+
+    public LayoutOptions HorizontalLabelOptions
+    {
+        get => (LayoutOptions)GetValue(HorizontalLabelOptionsProperty);
+        set => SetValue(HorizontalLabelOptionsProperty, value);
+    }
+
+    public static readonly BindableProperty LabelTextProperty =
+        BindableProperty.Create(nameof(LabelText), typeof(string), typeof(ButtonImageView), default(string));
+
+    public string LabelText
+    {
+        get => (string)GetValue(LabelTextProperty);
+        set => SetValue(LabelTextProperty, value);
+    }
+
+    public static readonly BindableProperty ImageHeightRequestProperty =
+        BindableProperty.Create(nameof(ImageHeightRequest), typeof(double), typeof(ButtonImageView), default(double));
+
+    public double ImageHeightRequest
+    {
+        get => (double)GetValue(ImageHeightRequestProperty);
+        set => SetValue(ImageHeightRequestProperty, value);
+    }
+
+    public static readonly BindableProperty ImageWidthRequestProperty =
+        BindableProperty.Create(nameof(ImageWidthRequest), typeof(double), typeof(ButtonImageView), default(double));
+
+    public double ImageWidthRequest
+    {
+        get => (double)GetValue(ImageWidthRequestProperty);
+        set => SetValue(ImageWidthRequestProperty, value);
+    }
+
+    public static readonly BindableProperty VerticalImageOptionsProperty =
+        BindableProperty.Create(nameof(VerticalImageOptions), typeof(LayoutOptions), typeof(ButtonImageView),
+            default(LayoutOptions));
+
+    public LayoutOptions VerticalImageOptions
+    {
+        get => (LayoutOptions)GetValue(VerticalImageOptionsProperty);
+        set => SetValue(VerticalImageOptionsProperty, value);
+    }
+
+    public static readonly BindableProperty HorizontalImageOptionsProperty =
+        BindableProperty.Create(nameof(HorizontalImageOptions), typeof(LayoutOptions), typeof(ButtonImageView),
+            default(LayoutOptions));
+
+    public LayoutOptions HorizontalImageOptions
+    {
+        get => (LayoutOptions)GetValue(HorizontalImageOptionsProperty);
+        set => SetValue(HorizontalImageOptionsProperty, value);
+    }
+
+    public ButtonImageView()
+    {
+        InitializeComponent();
+    }
+}
