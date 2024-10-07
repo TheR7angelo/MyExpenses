4d1ab9ec71255b1d0a84cc45d97455ed10f883ae
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-07 16:26:10 +0200

    ci(Update): Add StringToBoolConverter for dynamic Label visibility
    Introduced StringToBoolConverter to convert string inputs to boolean
    values, making the Label visibility conditional based on the presence of
    text. Registered the converter in ButtonImageView.xaml and bound Label's
    IsVisible property to it.

---
+++ MyExpenses.Smartphones/Resources/Styles/Converters/StringToBoolConverter.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+
+namespace MyExpenses.Smartphones.Resources.Styles.Converters;
+
+public class StringToBoolConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string str)
+        {
+            return !string.IsNullOrEmpty(str);
+        }
+        return false;
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return Binding.DoNothing;
+    }
+}
