31dae9b02e16a07b2089690ab9cf9d4f1ab0bb1a
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-07 16:59:13 +0200

    ci(Update): Add CustomFrame ButtonImageView to project configuration
    Included ButtonImageView.xaml and ButtonImageView.xaml.cs under proper
    ItemGroup in the project file to ensure their correct handling by the
    designer and compiler. This change helps maintain code organization and
    coherence within the solution.

---
+++ MyExpenses.Smartphones/UserControls/CustomFrame/ButtonImageView.xaml.cs
@@ -0,0 +1,77 @@
+using Microsoft.Maui.Controls.Shapes;
+
+namespace MyExpenses.Smartphones.UserControls.CustomFrame;
+
+public sealed partial class ButtonImageView
+{
+    public static readonly BindableProperty GeometryColorProperty =
+        BindableProperty.Create(nameof(GeometryColor), typeof(Color), typeof(ButtonImageTextView), default(Color));
+
+    public Color GeometryColor
+    {
+        get => (Color)GetValue(GeometryColorProperty);
+        set => SetValue(GeometryColorProperty, value);
+    }
+
+    public static readonly BindableProperty GeometrySourceProperty = BindableProperty.Create(nameof(GeometrySource),
+        typeof(Geometry), typeof(ButtonImageTextView), default(Geometry));
+
+    public Geometry GeometrySource
+    {
+        get => (Geometry)GetValue(GeometrySourceProperty);
+        set => SetValue(GeometrySourceProperty, value);
+    }
+
+    public static readonly BindableProperty ImageHeightRequestProperty =
+        BindableProperty.Create(nameof(ImageHeightRequest), typeof(double), typeof(ButtonImageTextView), default(double));
+
+    public double ImageHeightRequest
+    {
+        get => (double)GetValue(ImageHeightRequestProperty);
+        set => SetValue(ImageHeightRequestProperty, value);
+    }
+
+    public static readonly BindableProperty ImageWidthRequestProperty =
+        BindableProperty.Create(nameof(ImageWidthRequest), typeof(double), typeof(ButtonImageTextView), default(double));
+
+    public double ImageWidthRequest
+    {
+        get => (double)GetValue(ImageWidthRequestProperty);
+        set => SetValue(ImageWidthRequestProperty, value);
+    }
+
+    public static readonly BindableProperty VerticalImageOptionsProperty =
+        BindableProperty.Create(nameof(VerticalImageOptions), typeof(LayoutOptions), typeof(ButtonImageTextView),
+            default(LayoutOptions));
+
+    public LayoutOptions VerticalImageOptions
+    {
+        get => (LayoutOptions)GetValue(VerticalImageOptionsProperty);
+        set => SetValue(VerticalImageOptionsProperty, value);
+    }
+
+    public static readonly BindableProperty HorizontalImageOptionsProperty =
+        BindableProperty.Create(nameof(HorizontalImageOptions), typeof(LayoutOptions), typeof(ButtonImageTextView),
+            default(LayoutOptions));
+
+    public LayoutOptions HorizontalImageOptions
+    {
+        get => (LayoutOptions)GetValue(HorizontalImageOptionsProperty);
+        set => SetValue(HorizontalImageOptionsProperty, value);
+    }
+
+    public event EventHandler? Clicked;
+
+    public ButtonImageView()
+    {
+        InitializeComponent();
+    }
+
+    private void OnClicked()
+    {
+        Clicked?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void TapGestureRecognizer_Tapped(object? sender, TappedEventArgs e)
+        => OnClicked();
+}
