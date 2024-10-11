0aa72d25417f081316c9a5237b37ad88713fdb8c
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-11 08:47:02 +0200

    ci(Update): Add validation checks for database filename
    Ensure the database filename is not empty, doesn't contain incorrect
    characters, and doesn't already exist in the list of existing
    databases. Display appropriate error messages when validation fails.

--- MyExpenses.Smartphones/ContentPages/AddDatabaseFileContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/AddDatabaseFileContentPage.xaml.cs
@@ -1,5 +1,6 @@
 using System.Windows.Input;
 using MyExpenses.Models.IO;
+using MyExpenses.Utils.Strings;
 
 namespace MyExpenses.Smartphones.ContentPages;
 
@@ -47,8 +48,28 @@ public partial class AddDatabaseFileContentPage
 
     private async void ButtonValid_OnClicked(object? sender, EventArgs e)
     {
-        _taskCompletionSource.SetResult(true);
-        await Navigation.PopAsync();
+        if (string.IsNullOrEmpty(DatabaseFilename))
+        {
+            //TODO trad
+            await DisplayAlert("Error", "Database file name cannot be empty", "Ok");
+            return;
+        }
+
+        var containsIncorrectChar = DatabaseFilename.CheckFilenameContainsIncorrectChar();
+        if (containsIncorrectChar)
+        {
+            //TODO trad
+            await DisplayAlert("Error", "Database file name contains incorrect characters", "Ok");
+        }
+
+        //TODO trad
+        var alreadyExist = ExistingDatabases.Select(s => s.FileNameWithoutExtension).Contains(DatabaseFilename);
+        if (alreadyExist) await DisplayAlert("Error", "Database file name already exist", "Ok");
+        else
+        {
+            _taskCompletionSource.SetResult(true);
+            await Navigation.PopAsync();
+        }
     }
 
     private async void ButtonCancel_OnClicked(object? sender, EventArgs e)
