a70eac51da7cfed5ec48c00ecb466db8b9f1772f
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-11 09:37:53 +0200

    ci(Update): Add CustomEntryControl for database filename input
    Introduced a new CustomEntryControl to replace the standard Entry for
    better placeholder handling in AddDatabaseFileContentPage.xaml. This
    includes defining new bindable properties and behaviors within
    CustomEntryControl.xaml and CustomEntryControl.xaml.cs.

---
+++ MyExpenses.Smartphones/UserControls/CustomEntryControl.xaml.cs
@@ -0,0 +1,40 @@
+namespace MyExpenses.Smartphones.UserControls;
+
+public partial class CustomEntryControl
+{
+    public static readonly BindableProperty TextProperty =
+        BindableProperty.Create(nameof(Text), typeof(string), typeof(CustomEntryControl), null);
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public static readonly BindableProperty PlaceholderTextProperty =
+        BindableProperty.Create(nameof(PlaceholderText), typeof(string), typeof(CustomEntryControl), string.Empty);
+
+    public string PlaceholderText
+    {
+        get => (string)GetValue(PlaceholderTextProperty);
+        set => SetValue(PlaceholderTextProperty, value);
+    }
+
+    public static readonly BindableProperty IsPlaceholderVisibleProperty =
+        BindableProperty.Create(nameof(IsPlaceholderVisible), typeof(bool), typeof(CustomEntryControl), true);
+
+    public bool IsPlaceholderVisible
+    {
+        get => (bool)GetValue(IsPlaceholderVisibleProperty);
+        set => SetValue(IsPlaceholderVisibleProperty, value);
+    }
+
+    public CustomEntryControl()
+    {
+        InitializeComponent();
+    }
+
+    private void InputView_OnTextChanged(object? sender, TextChangedEventArgs e)
+        => IsPlaceholderVisible = string.IsNullOrEmpty(e.NewTextValue);
+
+}
