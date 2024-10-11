6f18941fc009be93ea47821b05f75c9608c366b2
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-11 09:43:08 +0200

    ci(Update): Add BooleanToInverseConverter for value conversion
    Introduced a new BooleanToInverseConverter class implementing
    IValueConverter. This converter inverts boolean values during data
    binding operations, enhancing UI flexibility.

---
+++ MyExpenses.Smartphones/Resources/Styles/Converters/BooleanToInverseConverter.cs
@@ -0,0 +1,18 @@
+using System.Globalization;
+
+namespace MyExpenses.Smartphones.Resources.Styles.Converters;
+
+public class BooleanToInverseConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue) return !boolValue;
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool boolValue) return !boolValue;
+        return value;
+    }
+}
