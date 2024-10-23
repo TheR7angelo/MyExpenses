dfc91c4aab417c2e686d4ffbc2f0a1cdcefb64e9
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-23 10:27:44 +0200

    ci(Update): Simplify DashBoardContentPage class
    Removed unnecessary using directives and cleaned up the
    DashBoardContentPage class definition. This reduces clutter and
    potentially improves compile time.

--- MyExpenses.Smartphones/ContentPages/DashBoardContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/DashBoardContentPage.xaml.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace MyExpenses.Smartphones.ContentPages;
 
-public partial class DashBoardContentPage : ContentPage
+public partial class DashBoardContentPage
 {
     public DashBoardContentPage()
     {
