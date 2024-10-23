d0c29b24b43c41fda5f4ce56904cc2cbb54c8c68
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-23 15:56:52 +0200

    ci(Update): Add real-time location display to DashBoardContentPage
    Implemented a background task to fetch and update the location every 5
    seconds. Introduced the `LabelLocation` property bound to a new Label in
    the XAML file to reflect these updates.

--- MyExpenses.Smartphones/ContentPages/DashBoardContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/DashBoardContentPage.xaml.cs
@@ -1,9 +1,37 @@
+using MyExpenses.Maui.Utils;
+
 namespace MyExpenses.Smartphones.ContentPages;
 
 public partial class DashBoardContentPage
 {
+    public static readonly BindableProperty LabelLocationProperty = BindableProperty.Create(nameof(LabelLocation),
+        typeof(string), typeof(DashBoardContentPage), default(string));
+
     public DashBoardContentPage()
     {
         InitializeComponent();
+
+        Task.Run(async () =>
+        {
+            while (true)
+            {
+                var location = await SensorRequestUtils.GetLocation();
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    var text = location is null
+                        ? "Null"
+                        : location.ToString();
+                    LabelLocation = text;
+                });
+
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+        });
+    }
+
+    public string LabelLocation
+    {
+        get => (string)GetValue(LabelLocationProperty);
+        set => SetValue(LabelLocationProperty, value);
     }
 }
