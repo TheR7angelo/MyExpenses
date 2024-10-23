e6f69203e0861186afe0fc76fd7673a65120f304
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-23 15:56:30 +0200

    ci(Update): Add season determination based on hemisphere
    Introduced `GetSeason` method to determine the current season based on
    the specified hemisphere. Also added the `ESeason` enum to represent the
    four seasons: Spring, Summer, Autumn, and Winter.

--- MyExpenses.Maui.Utils/SensorRequestUtils.cs
+++ MyExpenses.Maui.Utils/SensorRequestUtils.cs
@@ -20,4 +20,32 @@ public static class SensorRequestUtils
             ? EHemisphere.Northern
             : EHemisphere.Southern;
     }
+
+    public static ESeason? GetSeason(this EHemisphere? hemisphere)
+    {
+        if (hemisphere is null) return null;
+
+        var now = DateTime.Now;
+        var springStart = new DateTime(now.Year, 3, 21);
+        var summerStart = new DateTime(now.Year, 6, 21);
+        var autumnStart = new DateTime(now.Year, 9, 21);
+        var winterStart = new DateTime(now.Year, 12, 21);
+
+        ESeason currentSeason;
+
+        if (now >= springStart && now < summerStart) currentSeason = ESeason.Spring;
+        else if (now >= summerStart && now < autumnStart) currentSeason = ESeason.Summer;
+        else if (now >= autumnStart && now < winterStart) currentSeason = ESeason.Autumn;
+        else currentSeason = ESeason.Winter;
+
+        if (hemisphere is not EHemisphere.Southern) return currentSeason;
+        return currentSeason switch
+        {
+            ESeason.Spring => ESeason.Autumn,
+            ESeason.Summer => ESeason.Winter,
+            ESeason.Autumn => ESeason.Spring,
+            ESeason.Winter => ESeason.Summer,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
 }
---
+++ MyExpenses.Models/Maui/Sensor/Location/ESeason.cs
@@ -0,0 +1,9 @@
+namespace MyExpenses.Models.Maui.Sensor.Location;
+
+public enum ESeason
+{
+    Spring,
+    Summer,
+    Autumn,
+    Winter
+}
