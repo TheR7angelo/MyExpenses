044c70781e35e7dbaed2f5dfa1b11d5ae6e4825f
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-23 09:19:07 +0200

    ci(Update): Add Dashboard Content Page
    Introduced `DashBoardContentPage` to the ContentPages directory and
    updated `DashBoardShell.xaml` to include it. Added a gradient background
    for Flyout and configured `ShellContent` to use the new page.

---
+++ MyExpenses.Smartphones/ContentPages/DashBoardContentPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExpenses.Smartphones.ContentPages;
+
+public partial class DashBoardContentPage : ContentPage
+{
+    public DashBoardContentPage()
+    {
+        InitializeComponent();
+    }
+}
