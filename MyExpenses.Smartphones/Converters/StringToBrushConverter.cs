33b663c38a61e6089383f2eb2f1320ab3b3b92d5
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-28 14:05:01 +0100

    ci(Update): Add StringToBrushConverter for string to Brush conversion
    This new converter class transforms string representations of colors
    into Brush objects using Microsoft.Maui controls. It includes error
    handling to return a default brush if the conversion fails.

---
+++ MyExpenses.Smartphones/Converters/StringToBrushConverter.cs
@@ -0,0 +1,28 @@
+using System.Drawing;
+using System.Globalization;
+using Brush = Microsoft.Maui.Controls.Brush;
+using Color = Microsoft.Maui.Graphics.Color;
+
+namespace MyExpenses.Smartphones.Converters;
+
+public class StringToBrushConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not string colorString || string.IsNullOrWhiteSpace(colorString)) return Brush.Default;
+        try
+        {
+            var solidColorBrush = new SolidColorBrush(Color.FromArgb(colorString));
+            return solidColorBrush;
+        }
+        catch (Exception)
+        {
+            return Brush.Default;
+        }
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
