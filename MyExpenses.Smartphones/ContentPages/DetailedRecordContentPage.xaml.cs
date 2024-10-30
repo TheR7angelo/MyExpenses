c4913729d3c36c53dfe091d8f4f5dbbe4042bbe4
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-30 09:16:48 +0100

    ci: Add CurrencySymbol property to DetailedRecordContentPage
    Introduce a new CurrencySymbol property to store and display the
    currency symbol according to the associated account. Also, update the
    constructor to initialize this property based on the account's currency
    information from the database.

--- MyExpenses.Smartphones/ContentPages/DetailedRecordContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/DetailedRecordContentPage.xaml.cs
@@ -16,6 +16,8 @@ public partial class DetailedRecordContentPage
         set => SetValue(HistoryProperty, value);
     }
 
+    public string CurrencySymbol { get; private set; } = null!;
+
     public ObservableCollection<TModePayment> ModePayments { get; private set; } = [];
 
     public DetailedRecordContentPage(int historyPk)
@@ -37,6 +39,9 @@ public partial class DetailedRecordContentPage
     {
         using var context = new DataBaseContext();
         ModePayments.AddRange(context.TModePayments);
+
+        var account = context.TAccounts.First(s => s.Id.Equals(History.AccountFk));
+        CurrencySymbol = context.TCurrencies.First(s => s.Id.Equals(account.CurrencyFk)).Symbol!;
 
         InitializeComponent();
     }
