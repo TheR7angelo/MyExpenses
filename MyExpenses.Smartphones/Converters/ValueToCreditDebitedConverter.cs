8dfe90a20a9658ebf97361dfa2f8907212a2229e
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-30 09:20:43 +0100

    ci(Update): Add ValueToCreditDebitedConverter and value symbol display
    Introduced ValueToCreditDebitedConverter to label credit/debit values
    based on their sign. Updated the XAML to use this converter and added a
    new row in the grid layout to include the value symbol. Also included
    necessary resource files and updated the project file configuration.

---
+++ MyExpenses.Smartphones/Converters/ValueToCreditDebitedConverter.cs
@@ -0,0 +1,22 @@
+using System.Globalization;
+using MyExpenses.Smartphones.Resources.Resx.Converters.ValueToCreditDebitedConverter;
+
+namespace MyExpenses.Smartphones.Converters;
+
+public class ValueToCreditDebitedConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is not double doubleValue) return null;
+
+        return doubleValue >= 0
+            ? ValueToCreditDebitedConverterResources.Credited
+            : ValueToCreditDebitedConverterResources.Debited;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        // No return
+        return Binding.DoNothing;
+    }
+}
