e88a6ce246d7e13b7a25fd39b5536140c8b71049
TheR7angelo <raphael.antunes@hotmail.fr>
2024-10-31 09:39:59 +0100

    ci(Update): Revise t_place table structure and triggers
    Restructured the t_place table to include new fields for better data
    management. Added triggers to ensure the date_added column is
    consistently formatted. This update also includes cleaning up the
    temporary table t_place_2 used during the migration.

--- MyExpenses.Sql/Database Models/Command.sql
+++ MyExpenses.Sql/Database Models/Command.sql
@@ -31,3 +31,58 @@ FROM t_place;
 SELECT *
 FROM v_history h
 WHERE h.date BETWEEN '2024-08-01' AND '2024-09-01';
+
+CREATE TABLE t_place_2 AS
+SELECT * FROM t_place;
+
+DROP TABLE IF EXISTS t_place;
+CREATE TABLE t_place
+(
+    id             INTEGER
+        CONSTRAINT t_place_pk
+            PRIMARY KEY AUTOINCREMENT,
+    name           TEXT,
+    number         TEXT,
+    street         TEXT,
+    postal         TEXT,
+    city           TEXT,
+    country        TEXT,
+    latitude       REAL,
+    longitude      REAL,
+    is_open        BOOLEAN NOT NULL DEFAULT TRUE,
+    can_be_deleted BOOLEAN DEFAULT TRUE,
+    date_added     DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TRIGGER IF EXISTS after_insert_on_t_place;
+CREATE TRIGGER after_insert_on_t_place
+    AFTER INSERT
+    ON t_place
+    FOR EACH ROW
+BEGIN
+    UPDATE t_place
+    SET date_added = CASE
+                         WHEN typeof(NEW.date_added) = 'integer' THEN datetime(NEW.date_added / 1000, 'unixepoch')
+                         ELSE NEW.date_added
+        END
+    WHERE id = NEW.id;
+END;
+
+DROP TRIGGER IF EXISTS after_update_on_t_place;
+CREATE TRIGGER after_update_on_t_place
+    AFTER UPDATE
+    ON t_place
+    FOR EACH ROW
+BEGIN
+    UPDATE t_place
+    SET date_added = CASE
+                         WHEN typeof(NEW.date_added) = 'integer' THEN datetime(NEW.date_added / 1000, 'unixepoch')
+                         ELSE NEW.date_added
+        END
+    WHERE id = NEW.id;
+END;
+
+INSERT INTO t_place
+SELECT * FROM t_place_2;
+
+DROP TABLE t_place_2;
