126fcf1d3d4cc444077549871b3fccd83ec26cb5
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-05 14:28:38 +0100

    ci(Update): Add EmptyStringTreeViewConverter class
    Introduced a new converter to handle empty strings in TreeView
    components. This converter returns "Unknown" for any null or whitespace
    string values, providing a default value for display. Added support for
    binding scenarios where conversion back is not needed.

---
+++ MyExpenses.Smartphones/Converters/EmptyStringTreeViewConverter.cs
@@ -0,0 +1,22 @@
+using System.Globalization;
+using MyExpenses.Smartphones.Resources.Resx.Converters.EmptyStringTreeViewConverter;
+
+namespace MyExpenses.Smartphones.Converters;
+
+public class EmptyStringTreeViewConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return ToUnknown(value);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return Binding.DoNothing;
+    }
+
+    public static string ToUnknown(object? value)
+    {
+        return value is string str && !string.IsNullOrWhiteSpace(str) ? str : EmptyStringTreeViewConverterResources.Unknown;
+    }
+}
