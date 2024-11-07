6d5419f44514281783d57af8b58d5b9fb7558635
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-07 10:28:34 +0100

    ci(Update): Add localization support for 'Close' button
    Introduced a new BindableProperty for the 'Close' button text to support
    localization. Updated the XAML to bind the button text and handle
    language changes dynamically.

--- MyExpenses.Smartphones/ContentPages/CustomPopups/CustomPopupFilterCategories.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/CustomPopups/CustomPopupFilterCategories.xaml.cs
@@ -1,12 +1,24 @@
 using System.Collections.Immutable;
 using MyExpenses.Models.AutoMapper;
+using MyExpenses.Models.Config;
+using MyExpenses.Models.Config.Interfaces;
 using MyExpenses.Models.Sql.Derivatives.Views;
+using MyExpenses.Smartphones.Resources.Resx.ContentPages.CustomPopups.CustomPopupFilterCategories;
 using MyExpenses.Sql.Context;
 
 namespace MyExpenses.Smartphones.ContentPages.CustomPopups;
 
 public partial class CustomPopupFilterCategories
 {
+    public static readonly BindableProperty ButtonCloseTextProperty = BindableProperty.Create(nameof(ButtonCloseText),
+        typeof(string), typeof(CustomPopupFilterCategories), default(string));
+
+    public string ButtonCloseText
+    {
+        get => (string)GetValue(ButtonCloseTextProperty);
+        set => SetValue(ButtonCloseTextProperty, value);
+    }
+
     public List<VCategoryDerive> VCategoryDerives { get; }
 
     public CustomPopupFilterCategories()
@@ -16,8 +28,20 @@ public partial class CustomPopupFilterCategories
         using var context = new DataBaseContext();
         VCategoryDerives = [..context.VCategories.Select(s => mapper.Map<VCategoryDerive>(s))];
 
+        UpdateLanguage();
         InitializeComponent();
+
+        Interface.LanguageChanged += Interface_OnLanguageChanged;
     }
+
+    private void Interface_OnLanguageChanged(object sender, ConfigurationLanguageChangedEventArgs e)
+        => UpdateLanguage();
+
+    private void UpdateLanguage()
+    {
+        ButtonCloseText = CustomPopupFilterCategoriesResources.ButtonCloseText;
+    }
+
 
     public ImmutableList<VCategoryDerive> GetVCategoryDerivesChecked()
         => VCategoryDerives.Where(s => s.IsChecked).ToImmutableList();
