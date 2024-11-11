ea57367f87d769fb1fa81db6fb965f6c64b096e6
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-11 15:50:59 +0100

    ci(Update): Add total account summary and icon binding
    Integrated account summary calculation and binding in
    AccountManagementContentPage. Added converter and UI elements for
    displaying total account values and corresponding icons.

--- MyExpenses.Smartphones/ContentPages/AccountManagementContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/AccountManagementContentPage.xaml.cs
@@ -1,9 +1,28 @@
+using System.Collections.ObjectModel;
+using MyExpenses.Models.Sql.Bases.Views;
+using MyExpenses.Sql.Context;
+
 namespace MyExpenses.Smartphones.ContentPages;
 
 public partial class AccountManagementContentPage
 {
+    public static readonly BindableProperty TotalAllAccountProperty = BindableProperty.Create(nameof(TotalAllAccount),
+        typeof(double), typeof(AccountManagementContentPage), default(double));
+
+    public double TotalAllAccount
+    {
+        get => (double)GetValue(TotalAllAccountProperty);
+        set => SetValue(TotalAllAccountProperty, value);
+    }
+
+    public ObservableCollection<VTotalByAccount> VTotalByAccounts { get; }
+
     public AccountManagementContentPage()
     {
+        using var context = new DataBaseContext();
+        VTotalByAccounts = [..context.VTotalByAccounts];
+        TotalAllAccount = VTotalByAccounts.Sum(s => s.Total) ?? 0d;
+
         InitializeComponent();
     }
 }
