65755cf9dd303a445378665f1e977c0eac051d42
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-12 18:24:32 +0100

    ci(Update): Add year and month selection with date navigation buttons
    Introduced properties and logic for selecting and navigating year and
    month. Added buttons to add, remove, and reset to the current month,
    enhancing the user interface for bank transfer summaries.

--- MyExpenses.Smartphones/ContentPages/BankTransferSummaryContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/BankTransferSummaryContentPage.xaml.cs
@@ -1,9 +1,160 @@
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using MyExpenses.Models.Config;
+using MyExpenses.Models.Config.Interfaces;
+using MyExpenses.Smartphones.Resources.Resx.ContentPages.DashBoardContentPage;
+using MyExpenses.Sql.Context;
+using MyExpenses.Utils.Collection;
+using MyExpenses.Utils.Strings;
+
 namespace MyExpenses.Smartphones.ContentPages;
 
 public partial class BankTransferSummaryContentPage
 {
+
+    public static readonly BindableProperty SelectedYearProperty = BindableProperty.Create(nameof(SelectedYear),
+        typeof(string), typeof(BankTransferSummaryContentPage), default(string));
+
+    public string SelectedYear
+    {
+        get => (string)GetValue(SelectedYearProperty);
+        set => SetValue(SelectedYearProperty, value);
+    }
+
+    public static readonly BindableProperty SelectedMonthProperty = BindableProperty.Create(nameof(SelectedMonth),
+        typeof(string), typeof(BankTransferSummaryContentPage), default(string));
+
+    public string SelectedMonth
+    {
+        get => (string)GetValue(SelectedMonthProperty);
+        set => SetValue(SelectedMonthProperty, value);
+    }
+
+    public ObservableCollection<string> Years { get; }
+    public ObservableCollection<string> Months { get; } = [];
+
     public BankTransferSummaryContentPage()
     {
+        UpdateMonthLanguage();
+
+        var now = DateTime.Now;
+        using var context = new DataBaseContext();
+        Years =
+        [
+            ..context
+                .TBankTransfers
+                .Where(s => s.Date.HasValue)
+                .Select(s => s.Date!.Value.Year)
+                .Distinct()
+                .OrderByDescending(y => y)
+                .Select(y => y.ToString())
+        ];
+
+        if (Years.Count.Equals(0))
+        {
+            Years.Add(DateTime.Now.Year.ToString());
+        }
+
+        UpdateLanguage();
         InitializeComponent();
+
+        Interface.LanguageChanged += Interface_OnLanguageChanged;
+    }
+
+    private void Interface_OnLanguageChanged(object sender, ConfigurationLanguageChangedEventArgs e)
+    {
+        UpdateLanguage();
+        UpdateMonthLanguage();
+    }
+
+    private void UpdateLanguage()
+    {
+        //TODO work
+    }
+
+    private void UpdateMonthLanguage()
+    {
+        var currentCulture = CultureInfo.CurrentCulture;
+
+        var months = currentCulture.DateTimeFormat.MonthNames
+            .Where(s => !string.IsNullOrEmpty(s))
+            .Select(s => s.ToFirstCharUpper()).ToImmutableArray();
+
+        if (Months.Count is 0)
+        {
+            Months.AddRange(months);
+        }
+        else
+        {
+            var selectedMonth = Months.FirstOrDefault(month => month.Equals(SelectedMonth)) ?? string.Empty;
+            for (var i = 0; i < months.Length; i++)
+            {
+                Months[i] = months[i];
+            }
+
+            SelectedMonth = selectedMonth;
+        }
+    }
+
+    private async void ButtonAddMonth_OnClick(object? sender, EventArgs e)
+    {
+        var date = GetDateOnlyFilter();
+        date = date.AddMonths(1);
+
+        var result = UpdateFilterDate(date);
+
+        if (result) return;
+
+        await DisplayAlert(DashBoardContentPageResources.MessageBoxAddMonthErrorTitle,
+            DashBoardContentPageResources.MessageBoxAddMonthErrorMessage,
+            DashBoardContentPageResources.MessageBoxAddMonthErrorOkButton);
+    }
+
+    private void ButtonDateNow_OnClick(object? sender, EventArgs e)
+    {
+        var now = DateOnly.FromDateTime(DateTime.Now);
+        UpdateFilterDate(now);
+    }
+
+    private async void ButtonRemoveMonth_OnClick(object? sender, EventArgs e)
+    {
+        var date = GetDateOnlyFilter();
+        date = date.AddMonths(-1);
+
+        var result = UpdateFilterDate(date);
+
+        if (result) return;
+
+        await DisplayAlert(DashBoardContentPageResources.MessageBoxRemoveMonthErrorTitle,
+            DashBoardContentPageResources.MessageBoxRemoveMonthErrorMessage,
+            DashBoardContentPageResources.MessageBoxRemoveMonthErrorOkButton);
+    }
+
+    private DateOnly GetDateOnlyFilter()
+    {
+        var monthIndex = string.IsNullOrEmpty(SelectedMonth)
+            ? DateTime.Now.Month
+            : Months.IndexOf(SelectedMonth) + 1;
+
+        var year = string.IsNullOrEmpty(SelectedYear)
+            ? DateTime.Now.Year
+            : int.Parse(SelectedYear);
+
+        var date = DateOnly.Parse($"{year}/{monthIndex}/01");
+        return date;
+    }
+
+    private bool UpdateFilterDate(DateOnly date)
+    {
+        var yearStr = date.Year.ToString();
+        if (!Years.Contains(yearStr)) return false;
+
+        if (!yearStr.Equals(SelectedYear)) SelectedYear = yearStr;
+
+        var monthIndex = date.Month - 1;
+        SelectedMonth = Months[monthIndex];
+
+        return true;
     }
 }
