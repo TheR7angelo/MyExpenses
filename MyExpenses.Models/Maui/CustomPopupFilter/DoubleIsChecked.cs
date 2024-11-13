3aa75f4fff7bb8b9d85313878e4aa7c848733fbe
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-13 10:32:47 +0100

    ci(Update): Add equality members to DoubleIsChecked class
    Implemented custom equality methods to compare instances of the
    DoubleIsChecked class. This includes 'Equals' methods for both the class
    type and general objects, as well as an override for 'GetHashCode'.

--- MyExpenses.Models/Maui/CustomPopupFilter/DoubleIsChecked.cs
+++ MyExpenses.Models/Maui/CustomPopupFilter/DoubleIsChecked.cs
@@ -4,4 +4,25 @@ public class DoubleIsChecked
 {
     public double? DoubleValue { get; set; }
     public bool IsChecked { get; set; }
+
+    public bool Equals(DoubleIsChecked? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return DoubleValue == other.DoubleValue && IsChecked == other.IsChecked;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is DoubleIsChecked other)
+        {
+            return Equals(other);
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(DoubleValue, IsChecked);
+    }
 }
