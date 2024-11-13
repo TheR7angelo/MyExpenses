9bc1c509f84a673c408912e8b6a29a1a9270d7cb
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-13 13:35:54 +0100

    ci(Update): Implement equality members in StringIsChecked class
    Add Equals and GetHashCode methods to StringIsChecked class to support
    accurate comparisons and ensure object properties are correctly
    evaluated. This enhances the class functionality and helps prevent
    potential issues with object comparisons.

--- MyExpenses.Models/Maui/CustomPopupFilter/StringIsChecked.cs
+++ MyExpenses.Models/Maui/CustomPopupFilter/StringIsChecked.cs
@@ -4,4 +4,25 @@ public class StringIsChecked
 {
     public string? StringValue { get; set; }
     public bool IsChecked { get; set; }
+
+    public bool Equals(StringIsChecked? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return StringValue == other.StringValue && IsChecked == other.IsChecked;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is StringIsChecked other)
+        {
+            return Equals(other);
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(StringValue, IsChecked);
+    }
 }
