2c3b2e3384e9143104f5fc575a804e571a6394b4
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-14 15:41:30 +0100

    feat(Update): Add XML documentation comments to ObjectComparer methods
    This commit adds XML documentation comments to all methods in the
    ObjectComparer class. These comments provide detailed descriptions,
    parameter explanations, and return value information to improve code
    readability and maintainability. This enhancement will aid developers in
    understanding the purpose and usage of each method more effectively.

--- MyExpenses.Utils/Objects/ObjectComparer.cs
+++ MyExpenses.Utils/Objects/ObjectComparer.cs
@@ -2,6 +2,13 @@ namespace MyExpenses.Utils.Objects;
 
 public static class ObjectComparer
 {
+    /// <summary>
+    /// Compares two objects to determine if they are equal by checking their properties and fields.
+    /// </summary>
+    /// <param name="obj1">The first object to compare.</param>
+    /// <param name="obj2">The second object to compare.</param>
+    /// <typeparam name="T">The type of the objects being compared.</typeparam>
+    /// <returns>True if the objects are equal; otherwise, false.</returns>
     public static bool AreEqual<T>(this T obj1, T obj2)
     {
         if (obj1 is null || obj2 is null) return obj1 is null && obj2 is null;
@@ -14,6 +21,14 @@ public static class ObjectComparer
         return true;
     }
 
+    /// <summary>
+    /// Compares the properties of two objects to determine if they're equal.
+    /// </summary>
+    /// <param name="type">The type of the objects being compared.</param>
+    /// <param name="obj1">The first object to compare.</param>
+    /// <param name="obj2">The second object to compare.</param>
+    /// <typeparam name="T">The type of objects being compared.</typeparam>
+    /// <returns>True if all properties of the objects are equal; otherwise, false.</returns>
     private static bool ArePropertiesEqual<T>(Type type, T obj1, T obj2)
     {
         foreach (var property in type.GetProperties())
@@ -27,6 +42,14 @@ public static class ObjectComparer
         return true;
     }
 
+    /// <summary>
+    /// Compares the fields of two objects to determine if they're equal.
+    /// </summary>
+    /// <param name="type">The type of the objects being compared.</param>
+    /// <param name="obj1">The first object to compare.</param>
+    /// <param name="obj2">The second object to compare.</param>
+    /// <typeparam name="T">The type of objects being compared.</typeparam>
+    /// <return>True if all fields of the objects are equal; otherwise, false.</return>
     private static bool AreFieldsEqual<T>(Type type, T obj1, T obj2)
     {
         foreach (var field in type.GetFields())
@@ -40,6 +63,12 @@ public static class ObjectComparer
         return true;
     }
 
+    /// <summary>
+    /// Compares two values to determine if they are equal, handling special cases for collections and null values.
+    /// </summary>
+    /// <param name="value1">The first value to compare.</param>
+    /// <param name="value2">The second value to compare.</param>
+    /// <return>True if the values are equal; otherwise, false.</return>
     private static bool ValuesAreEqual(object? value1, object? value2)
     {
         if (value1 is null && value2 is null) return true;
@@ -53,7 +82,12 @@ public static class ObjectComparer
         return value1.Equals(value2);
     }
 
-
+    /// <summary>
+    /// Compares two collections of objects for equality by checking if they contain the same elements in the same order.
+    /// </summary>
+    /// <param name="collection1">The first collection to compare.</param>
+    /// <param name="collection2">The second collection to compare.</param>
+    /// <return>True if the collections are equal; otherwise, false.</return>
     private static bool CollectionsAreEqual(IEnumerable<object> collection1, IEnumerable<object> collection2)
     {
         using var enumerator1 = collection1.GetEnumerator();
