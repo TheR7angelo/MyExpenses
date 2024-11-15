3f836411315bf9fad915d7b4813a35477512a863
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-15 10:28:38 +0100

    ci(Update): Add multiline support and clear button to CustomEntryControl
    Added properties for multiline text input and a clear button
    functionality to CustomEntryControl. Updated XAML and code-behind to
    accommodate new features, including necessary bindings and gesture
    recognizers.

--- MyExpenses.Smartphones/UserControls/TextInputs/CustomEntryControl/CustomEntryControl.xaml.cs
+++ MyExpenses.Smartphones/UserControls/TextInputs/CustomEntryControl/CustomEntryControl.xaml.cs
@@ -1,7 +1,27 @@
+using MyExpenses.Smartphones.PackIcons;
+
 namespace MyExpenses.Smartphones.UserControls.TextInputs.CustomEntryControl;
 
 public partial class CustomEntryControl
 {
+    public static readonly BindableProperty HasMultilineProperty =
+        BindableProperty.Create(nameof(HasMultiline), typeof(bool), typeof(CustomEntryControl), default(bool));
+
+    public bool HasMultiline
+    {
+        get => (bool)GetValue(HasMultilineProperty);
+        set => SetValue(HasMultilineProperty, value);
+    }
+
+    public static readonly BindableProperty HasClearButtonProperty =
+        BindableProperty.Create(nameof(HasClearButton), typeof(bool), typeof(CustomEntryControl), default(bool));
+
+    public bool HasClearButton
+    {
+        get => (bool)GetValue(HasClearButtonProperty);
+        set => SetValue(HasClearButtonProperty, value);
+    }
+
     public static readonly BindableProperty TextProperty =
         BindableProperty.Create(nameof(Text), typeof(string), typeof(CustomEntryControl), null);
 
@@ -29,6 +49,8 @@ public partial class CustomEntryControl
         set => SetValue(IsPlaceholderVisibleProperty, value);
     }
 
+    public EPackIcons CloseCircle { get; } = EPackIcons.CloseCircle;
+
     public CustomEntryControl()
     {
         InitializeComponent();
@@ -37,4 +59,6 @@ public partial class CustomEntryControl
     private void InputView_OnTextChanged(object? sender, TextChangedEventArgs e)
         => IsPlaceholderVisible = string.IsNullOrEmpty(e.NewTextValue);
 
+    private void TapGestureRecognizer_OnTapped(object? sender, TappedEventArgs e)
+        => Text = string.Empty;
 }
