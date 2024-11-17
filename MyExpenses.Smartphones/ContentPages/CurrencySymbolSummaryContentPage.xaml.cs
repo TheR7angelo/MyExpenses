01e3ae07d0108f12a3d4c6ba6dc9cd2be8deeecf
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-17 19:16:01 +0100

    ci(Update): Refactor currency symbol management for better UX
    Improved the handling of currency symbols by introducing helper methods
    `RefreshCurrencies`, `ShowCustomPopupEntryForCurrency`,
    `HandleCurrencyResult`, `HandleCurrencyEdit`, and
    `HandleCurrencyDelete`. These changes streamline the user experience for
    editing and deleting currency symbols, incorporating validation and
    confirmation prompts.

--- MyExpenses.Smartphones/ContentPages/CurrencySymbolSummaryContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/CurrencySymbolSummaryContentPage.xaml.cs
@@ -1,11 +1,15 @@
 using System.Collections.ObjectModel;
+using CommunityToolkit.Maui.Views;
 using MyExpenses.Models.Config;
 using MyExpenses.Models.Config.Interfaces;
+using MyExpenses.Models.Maui.CustomPopup;
 using MyExpenses.Models.Sql.Bases.Tables;
+using MyExpenses.Smartphones.ContentPages.CustomPopups;
 using MyExpenses.Smartphones.Resources.Resx.ContentPages.CurrencySymbolSummaryContentPage;
 using MyExpenses.Sql.Context;
 using MyExpenses.Utils;
 using MyExpenses.Utils.Collection;
+using MyExpenses.Utils.Objects;
 using Serilog;
 
 namespace MyExpenses.Smartphones.ContentPages;
@@ -39,18 +43,26 @@ public partial class CurrencySymbolSummaryContentPage
         set => SetValue(PlaceholderTextProperty, value);
     }
 
-    public ObservableCollection<TCurrency> Currencies { get; }
     public int MaxLength { get; } = 24;
+
+    public ObservableCollection<TCurrency> Currencies { get; } = [];
 
     public CurrencySymbolSummaryContentPage()
     {
-        using var context = new DataBaseContext();
-        Currencies = [..context.TCurrencies.OrderBy(s => s.Symbol)];
+        RefreshCurrencies();
 
         UpdateLanguage();
         InitializeComponent();
 
         Interface.LanguageChanged += Interface_OnLanguageChanged;
+    }
+
+    private void RefreshCurrencies()
+    {
+        Currencies.Clear();
+
+        using var context = new DataBaseContext();
+        Currencies.AddRange(context.TCurrencies.OrderBy(s => s.Symbol));
     }
 
     private void Interface_OnLanguageChanged(object sender, ConfigurationLanguageChangedEventArgs e)
@@ -63,10 +75,84 @@ public partial class CurrencySymbolSummaryContentPage
         ButtonValidText = CurrencySymbolSummaryContentPageResources.ButtonValidText;
     }
 
-    // TODO work
     private async void ButtonSymbol_OnClicked(object? sender, EventArgs e)
     {
-        await DisplayAlert("Symbol", $"Symbol: {((Button)sender!).Text}", "Ok");
+        if (sender is not Button button) return;
+        if (button.BindingContext is not TCurrency currency) return;
+
+        var tempCurrency = currency.DeepCopy()!;
+        await ShowCustomPopupEntryForCurrency(tempCurrency);
+    }
+
+    private async Task ShowCustomPopupEntryForCurrency(TCurrency currency)
+    {
+        var placeHolder = "Device Value";
+
+        var customPopupEntry = new CustomPopupEntry { MaxLenght = MaxLength, PlaceholderText = placeHolder, EntryText = currency.Symbol!, CanDelete = true };
+        await this.ShowPopupAsync(customPopupEntry);
+
+        var result = await customPopupEntry.ResultDialog;
+        if (result is ECustomPopupEntryResult.Cancel) return;
+
+        currency.Symbol = customPopupEntry.EntryText;
+        await HandleCurrencyResult(currency, result);
+        RefreshCurrencies();
+    }
+
+    private async Task HandleCurrencyResult(TCurrency currency, ECustomPopupEntryResult result)
+    {
+        var json = currency.ToJson();
+        if (result is ECustomPopupEntryResult.Valid)
+        {
+            var validate = await ValidateCurrencySymbol(currency.Symbol!);
+            if (!validate) return;
+
+            var response = await DisplayAlert("Question", "Do you really want to edit this currency ?", "Yes", "No");
+            if (!response) return;
+
+            Log.Information("Attempt to edit currency symbol : {Symbol}", json);
+            await HandleCurrencyEdit(currency);
+
+            return;
+        }
+
+        var deleteResponse = await DisplayAlert("Question",
+            "Do you really want to delete this currency ?\nAll account who use this currency will also be deleted",
+            "Yes", "No");
+        if (!deleteResponse) return;
+
+        Log.Information("Attempt to delete currency symbol : {Symbol}", json);
+        await HandleCurrencyDelete(currency);
+    }
+
+    private async Task HandleCurrencyEdit(TCurrency currency)
+    {
+        var (success, exception) = currency.AddOrEdit();
+        if (success)
+        {
+            Log.Information("Currency symbol was successfully edited");
+            await DisplayAlert("Success", "Currency symbol was successfully edited", "Ok");
+        }
+        else
+        {
+            Log.Error(exception, "An error occurred while editing currency symbol");
+            await DisplayAlert("Error", "An error occurred while editing currency symbol, please retry", "Ok");
+        }
+    }
+
+    private async Task HandleCurrencyDelete(TCurrency currency)
+    {
+        var (success, exception) = currency.Delete(true);
+        if (success)
+        {
+            Log.Information("Currency symbol and all related accounts were successfully deleted");
+            await DisplayAlert("Success", "Currency symbol and all related accounts were successfully deleted", "Ok");
+        }
+        else
+        {
+            Log.Error(exception, "An error occurred while deleting currency symbol");
+            await DisplayAlert("Error", "An error occurred while deleting currency symbol, please retry", "Ok");
+        }
     }
 
     private async void ButtonValid_OnClicked(object? sender, EventArgs e)
@@ -111,9 +197,13 @@ public partial class CurrencySymbolSummaryContentPage
         }
     }
 
-    private async Task<bool> ValidateCurrencySymbol()
+    private async Task<bool> ValidateCurrencySymbol(string? currencySymbol = null)
     {
-        if (SymbolText.Equals(string.Empty))
+        var symbolToTest = string.IsNullOrWhiteSpace(currencySymbol)
+            ? SymbolText
+            : currencySymbol;
+
+        if (string.IsNullOrWhiteSpace(symbolToTest))
         {
             await DisplayAlert(
                 CurrencySymbolSummaryContentPageResources.MessageBoxValidateCurrencySymbolErrorEmptyTitle,
@@ -122,7 +212,7 @@ public partial class CurrencySymbolSummaryContentPage
             return false;
         }
 
-        var alreadyExist = Currencies.Any(s => s.Symbol!.Equals(SymbolText));
+        var alreadyExist = Currencies.Any(s => s.Symbol!.Equals(symbolToTest));
         if (alreadyExist)
         {
             await DisplayAlert(
