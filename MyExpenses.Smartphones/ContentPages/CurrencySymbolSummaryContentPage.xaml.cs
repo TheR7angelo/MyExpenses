af30de3d4673ff1eb1ccb1cb0a92264c2fd4ce03
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-17 13:03:19 +0100

    ci(Update): Add currency symbol display and click functionality
    Integrated a list of currency symbols from the database and displayed
    them using a FlexLayout. Added functionality to show an alert with the
    symbol upon clicking any currency button.

--- MyExpenses.Smartphones/ContentPages/CurrencySymbolSummaryContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/CurrencySymbolSummaryContentPage.xaml.cs
@@ -1,9 +1,23 @@
+using System.Collections.ObjectModel;
+using MyExpenses.Models.Sql.Bases.Tables;
+using MyExpenses.Sql.Context;
+
 namespace MyExpenses.Smartphones.ContentPages;
 
 public partial class CurrencySymbolSummaryContentPage
 {
+    public ObservableCollection<TCurrency> Currencies { get; }
+
     public CurrencySymbolSummaryContentPage()
     {
+        using var context = new DataBaseContext();
+        Currencies = [..context.TCurrencies.OrderBy(s => s.Symbol)];
+
         InitializeComponent();
+    }
+
+    private async void ButtonSymbol_OnClicked(object? sender, EventArgs e)
+    {
+        await DisplayAlert("Symbol", $"Symbol: {((Button)sender!).Text}", "Ok");
     }
 }
