def67bb010ac0971913ed3da3c89937592aef710
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-18 11:29:09 +0100

    ci(Update): Add BackCommand for improved navigation handling
    Implemented a BackCommand in AccountTypeSummaryContentPage to handle
    back navigation using a command pattern. This includes defining the
    command in both XAML and code-behind, ensuring a consistent and isolated
    navigation approach.

--- MyExpenses.Smartphones/ContentPages/AccountTypeSummaryContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/AccountTypeSummaryContentPage.xaml.cs
@@ -1,9 +1,26 @@
+using System.Windows.Input;
+
 namespace MyExpenses.Smartphones.ContentPages;
 
 public partial class AccountTypeSummaryContentPage
 {
+    public ICommand BackCommand { get; set; }
+
+    private readonly TaskCompletionSource<bool> _taskCompletionSource = new();
+
+    public Task<bool> ResultDialog
+        => _taskCompletionSource.Task;
+    
     public AccountTypeSummaryContentPage()
     {
+        BackCommand = new Command(OnBackCommandPressed);
+        
         InitializeComponent();
+    }
+
+    private async void OnBackCommandPressed()
+    {
+        _taskCompletionSource.SetResult(true);
+        await Navigation.PopAsync();
     }
 }
