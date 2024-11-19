57bef37cf5a58a52a1923ad80206c27c0edfc2f3
TheR7angelo <raphael.antunes@hotmail.fr>
2024-11-19 21:09:50 +0100

    ci(Update): Implement category type validation and color update
    Added validation logic for category types and updated the color
    properties to bind correctly with the UI. Also integrated logging for
    better tracking of category type addition and potential errors.

--- MyExpenses.Smartphones/ContentPages/AddEditCategoryTypesContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/AddEditCategoryTypesContentPage.xaml.cs
@@ -4,8 +4,11 @@ using MyExpenses.Models.Config;
 using MyExpenses.Models.Config.Interfaces;
 using MyExpenses.Models.Sql.Bases.Tables;
 using MyExpenses.Models.Sql.Bases.Views;
+using MyExpenses.Smartphones.Resources.Resx.ContentPages.AccountTypeSummaryContentPage;
 using MyExpenses.Sql.Context;
+using MyExpenses.Utils;
 using MyExpenses.Utils.Collection;
+using Serilog;
 
 namespace MyExpenses.Smartphones.ContentPages;
 
@@ -29,10 +32,20 @@ public partial class AddEditCategoryTypesContentPage
         set => SetValue(PlaceholderTextProperty, value);
     }
 
+    public static readonly BindableProperty SelectedHexadecimalColorCodeProperty =
+        BindableProperty.Create(nameof(SelectedHexadecimalColorCode), typeof(string),
+            typeof(AddEditCategoryTypesContentPage), "#00000000");
+
+    public string SelectedHexadecimalColorCode
+    {
+        get => (string)GetValue(SelectedHexadecimalColorCodeProperty);
+        set => SetValue(SelectedHexadecimalColorCodeProperty, value);
+    }
+
     public static readonly BindableProperty SelectedColorProperty = BindableProperty.Create(nameof(SelectedColor),
         typeof(TColor), typeof(AddEditCategoryTypesContentPage), default(TColor));
 
-    public TColor SelectedColor
+    public TColor? SelectedColor
     {
         get => (TColor)GetValue(SelectedColorProperty);
         set => SetValue(SelectedColorProperty, value);
@@ -103,5 +116,100 @@ public partial class AddEditCategoryTypesContentPage
     {
         _taskCompletionSource.SetResult(true);
         await Navigation.PopAsync();
+    }
+
+    private async void ButtonValid_OnClicked(object? sender, EventArgs e)
+    {
+        var validate = await ValidateCategoryType();
+        if (!validate) return;
+
+        // TODO trad
+        var response = await DisplayAlert(
+            AccountTypeSummaryContentPageResources.MesageBoxAddNewAccountTypeQuestionTitle,
+            string.Format(AccountTypeSummaryContentPageResources.MesageBoxAddNewAccountTypeQuestionMessage,
+                CategoryTypeName),
+            AccountTypeSummaryContentPageResources.MesageBoxAddNewAccountTypeQuestionYesButton,
+            AccountTypeSummaryContentPageResources.MesageBoxAddNewAccountTypeQuestionNoButton);
+        if (!response) return;
+
+        var newCategoryTypeType = new TCategoryType
+        {
+            Name = CategoryTypeName,
+            ColorFk = SelectedColor?.Id,
+            DateAdded = DateTime.Now
+        };
+
+        var json = newCategoryTypeType.ToJson();
+        Log.Information("Attempt to add new category type : {CategoryType}", json);
+        var (success, exception) = newCategoryTypeType.AddOrEdit();
+        if (success)
+        {
+            Log.Information("New category type was successfully added");
+
+            await using var context = new DataBaseContext();
+            var newVCategory = context.VCategories.First(s => s.Id.Equals(newCategoryTypeType.Id));
+            Categories.AddAndSort(newVCategory, s => s.CategoryName!);
+
+            CategoryTypeName = string.Empty;
+            SelectedColor = null;
+
+            // TODO trad
+            await DisplayAlert(
+                AccountTypeSummaryContentPageResources.MesageBoxAddNewAccountTypeSuccessTitle,
+                AccountTypeSummaryContentPageResources.MesageBoxAddNewAccountTypeSuccessMessage,
+                AccountTypeSummaryContentPageResources.MesageBoxAddNewAccountTypeSuccessOkButton);
+        }
+        else
+        {
+            Log.Error(exception, "An error occurred while adding new category type");
+            // TODO trad
+            await DisplayAlert(
+                AccountTypeSummaryContentPageResources.MesageBoxAddNewAccountTypeErrorTitle,
+                AccountTypeSummaryContentPageResources.MesageBoxAddNewAccountTypeErrorMessage,
+                AccountTypeSummaryContentPageResources.MesageBoxAddNewAccountTypeErrorOkButton);
+        }
+    }
+
+    private async Task<bool> ValidateCategoryType(string? categoryTypeName = null)
+    {
+        var categoryTypeNameToTest = string.IsNullOrWhiteSpace(categoryTypeName)
+            ? CategoryTypeName
+            : categoryTypeName;
+
+        if (string.IsNullOrWhiteSpace(categoryTypeNameToTest))
+        {
+            // TODO trad
+            await DisplayAlert(
+                AccountTypeSummaryContentPageResources.MessageBoxValidateAccountTypeErrorEmptyTitle,
+                AccountTypeSummaryContentPageResources.MessageBoxValidateAccountTypeErrorEmptyMessage,
+                AccountTypeSummaryContentPageResources.MessageBoxValidateAccountTypeErrorEmptyOkButton);
+            return false;
+        }
+
+        if (SelectedColor is null)
+        {
+            await DisplayAlert("Error", "Please select a color", "Ok");
+            return false;
+        }
+
+        var alreadyExist = Categories.Any(s => s.CategoryName!.Equals(categoryTypeNameToTest));
+        if (alreadyExist)
+        {
+            // TODO trad
+            await DisplayAlert(
+                AccountTypeSummaryContentPageResources.MessageBoxValidateAccountTypeErrorAlreadyExistTitle,
+                AccountTypeSummaryContentPageResources.MessageBoxValidateAccountTypeErrorAlreadyExistMessage,
+                AccountTypeSummaryContentPageResources.MessageBoxValidateAccountTypeErrorAlreadyExistOkButton);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void PickerColor_OnSelectedIndexChanged(object? sender, EventArgs e)
+    {
+        SelectedHexadecimalColorCode = SelectedColor is null
+            ? "#00000000"
+            : SelectedColor.HexadecimalColorCode!;
     }
 }
