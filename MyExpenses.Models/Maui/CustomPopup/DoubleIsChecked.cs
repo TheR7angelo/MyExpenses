f2388f434a5fbd8fc012aef9bfeae2ef0861334e
TheR7angelo <raphael.antunes@hotmail.fr>
2024-12-26 19:17:29 +0100

    ci(Update): Update properties and method in DoubleIsChecked class
    Changed `DoubleValue` to use `init` for immutability. Made `Equals`
    method private and adjusted it to use `Equals` for better null handling
    consistency.

--- MyExpenses.Models/Maui/CustomPopup/DoubleIsChecked.cs
+++ MyExpenses.Models/Maui/CustomPopup/DoubleIsChecked.cs
@@ -2,14 +2,14 @@ namespace MyExpenses.Models.Maui.CustomPopup;
 
 public class DoubleIsChecked
 {
-    public double? DoubleValue { get; set; }
+    public double? DoubleValue { get; init; }
     public bool IsChecked { get; set; }
 
-    public bool Equals(DoubleIsChecked? other)
+    private bool Equals(DoubleIsChecked? other)
     {
         if (other is null) return false;
         if (ReferenceEquals(this, other)) return true;
-        return DoubleValue == other.DoubleValue && IsChecked == other.IsChecked;
+        return DoubleValue.Equals(other.DoubleValue) && IsChecked == other.IsChecked;
     }
 
     public override bool Equals(object? obj)
