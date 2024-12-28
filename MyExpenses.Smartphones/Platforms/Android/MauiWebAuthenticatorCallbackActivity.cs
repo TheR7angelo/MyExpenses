91d77b765d6abbdf69ff0927f3864769b88a82da
TheR7angelo <raphael.antunes@hotmail.fr>
2024-12-28 12:33:27 +0100

    refactor(Update): Fix syntax issues in Android IntentFilter attribute
    Updated the IntentFilter attribute in
    MauiWebAuthenticatorCallbackActivity to use correct array initializer
    syntax. This resolves compilation errors caused by improper formatting
    of DataSchemes and Categories.

--- MyExpenses.Smartphones/Platforms/Android/MauiWebAuthenticatorCallbackActivity.cs
+++ MyExpenses.Smartphones/Platforms/Android/MauiWebAuthenticatorCallbackActivity.cs
@@ -5,9 +5,9 @@ namespace MyExpenses.Smartphones;
 
 [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
 [IntentFilter(
-    new[] { Android.Content.Intent.ActionView },
-    Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
-    DataSchemes = new[] { "myexpenses" },
+    [Android.Content.Intent.ActionView],
+    Categories = [Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable],
+    DataSchemes = ["myexpenses"],
     DataHost = "redirect"
 )]
 public class WebAuthenticationCallbackActivity : WebAuthenticatorCallbackActivity
