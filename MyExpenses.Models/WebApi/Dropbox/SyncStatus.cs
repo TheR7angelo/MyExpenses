855340268b3ddd4804a914ab0a317bc3bd76ea81
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-03 11:03:10 +0100

    ci(Update): Add XML summary for SyncStatus enum
    Added a summary to clarify the purpose of the SyncStatus enum,
    explaining its role in representing the synchronization status in
    Dropbox integration. This improves code readability and maintainability.

--- MyExpenses.Models/WebApi/Dropbox/SyncStatus.cs
+++ MyExpenses.Models/WebApi/Dropbox/SyncStatus.cs
@@ -1,5 +1,8 @@
 namespace MyExpenses.Models.WebApi.DropBox;
 
+/// <summary>
+/// Represents the synchronization status of a database or file in a Dropbox integration context.
+/// </summary>
 public enum SyncStatus
 {
     LocalIsOutdated,
