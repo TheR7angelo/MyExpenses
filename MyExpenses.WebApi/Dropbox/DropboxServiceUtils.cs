1ed5a0a8d529c43ad9a5251223ffffa8db4acac9
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-03 08:48:46 +0100

    ci(Update): Add XML documentation for Dropbox utility methods
    Added summaries to clarify the purpose and functionality of
    `GenerateDirectorySecretKeys` and `IsDropboxEnabled` methods. This
    improves code readability and provides better context for developers
    using these utilities.

--- MyExpenses.WebApi/Dropbox/DropboxServiceUtils.cs
+++ MyExpenses.WebApi/Dropbox/DropboxServiceUtils.cs
@@ -5,6 +5,11 @@ public static class DropboxServiceUtils
     private static string DirectorySecretKeys { get; } = GenerateDirectorySecretKeys();
     public static string FilePathSecretKeys { get; } = Path.Join(DirectorySecretKeys, "AccessTokenAuthentication.json");
 
+    /// <summary>
+    /// Creates and returns the directory path designated for storing Dropbox secret keys.
+    /// This includes ensuring the directory exists and setting its attributes to hidden.
+    /// </summary>
+    /// <returns>A string representing the path to the directory for Dropbox secret keys.</returns>
     private static string GenerateDirectorySecretKeys()
     {
         var directorySecretKeys = Path.Join(AppContext.BaseDirectory, "Api", "Dropbox");
@@ -15,6 +20,10 @@ public static class DropboxServiceUtils
         return directorySecretKeys;
     }
 
+    /// <summary>
+    /// Determines whether the Dropbox integration is enabled by checking the existence of the secret keys file.
+    /// </summary>
+    /// <returns>A boolean value indicating whether the Dropbox integration is enabled.</returns>
     public static bool IsDropboxEnabled()
         => File.Exists(FilePathSecretKeys);
 }
