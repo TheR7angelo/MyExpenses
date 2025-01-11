4e133cb276b405fb6178f233079b31740da309ae
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-11 21:17:54 +0100

    ci(Update): Refactor InvertBooleanConverter to use utility methods
    Replaced inline conversion logic with calls to utility methods from
    Utils.Converters. This improves code reuse and ensures consistency
    across conversions.

--- MyExpenses.Smartphones/Converters/InvertBooleanConverter.cs
+++ MyExpenses.Smartphones/Converters/InvertBooleanConverter.cs
@@ -1,12 +1,11 @@
 using System.Globalization;
-using MyExpenses.Utils.Converters;
 
 namespace MyExpenses.Smartphones.Converters;
 
 public class InvertBooleanConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        => value.Convert();
+        => Utils.Converters.InvertBooleanConverter.Convert(value);
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-        => value.ConvertBack();
+        => Utils.Converters.InvertBooleanConverter.ConvertBack(value);
 }
