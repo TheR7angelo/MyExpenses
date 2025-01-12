ede1951f8d15cfdd3f1357d5daab8b09b1563eea
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-12 20:32:45 +0100

    ci(Update): Fix and finalize InverseBooleanToVisibilityConverter logic
    Refined the ConvertBack method to properly handle Visibility input,
    ensuring correct type checks and accurate returns. Added ReSharper
    comments to suppress warnings about boxing allocations for better code
    clarity and maintainability.

--- MyExpenses.Wpf/Converters/InverseBooleanToVisibilityConverter.cs
+++ MyExpenses.Wpf/Converters/InverseBooleanToVisibilityConverter.cs
@@ -8,14 +8,18 @@ public class InverseBooleanToVisibilityConverter : IValueConverter
 {
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
+        // ReSharper disable once HeapView.BoxingAllocation
         if (value is not bool b) return Visibility.Visible;
 
+        // ReSharper disable once HeapView.BoxingAllocation
         return b ? Visibility.Collapsed : Visibility.Visible;
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        // pass
-        return null;
+        if (value is not Visibility visibility) return value;
+
+        // ReSharper disable once HeapView.BoxingAllocation
+        return visibility is Visibility.Collapsed;
     }
 }
