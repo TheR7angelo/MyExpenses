aa63f433f1edb9c6758b42be724f62f1ee33b67f
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-12 21:01:24 +0100

    ci(Update): Add ReSharper annotations to reduce boxing allocations
    ReSharper annotations were added to several DependencyProperty
    registration methods to suppress warnings related to boxing allocations.
    This helps improve code readability and ensures better performance by
    avoiding unnecessary allocations.

--- MyExpenses.Wpf/Resources/Styles/TabAssist.cs
+++ MyExpenses.Wpf/Resources/Styles/TabAssist.cs
@@ -5,27 +5,33 @@ namespace MyExpenses.Wpf.Resources.Styles;
 
 public static class TabAssist
 {
+    // ReSharper disable once HeapView.BoxingAllocation
     public static readonly DependencyProperty HasFilledTabProperty = DependencyProperty.RegisterAttached(
         "HasFilledTab", typeof(bool), typeof(TabAssist), new PropertyMetadata(false));
 
+    // ReSharper disable once HeapView.BoxingAllocation
     public static void SetHasFilledTab(DependencyObject element, bool value)
         => element.SetValue(HasFilledTabProperty, value);
 
     public static bool GetHasFilledTab(DependencyObject element)
         => (bool)element.GetValue(HasFilledTabProperty);
 
+    // ReSharper disable once HeapView.BoxingAllocation
     public static readonly DependencyProperty HasUniformTabWidthProperty = DependencyProperty.RegisterAttached(
         "HasUniformTabWidth", typeof(bool), typeof(TabAssist), new PropertyMetadata(false));
 
+    // ReSharper disable once HeapView.BoxingAllocation
     public static void SetHasUniformTabWidth(DependencyObject element, bool value)
         => element.SetValue(HasUniformTabWidthProperty, value);
 
     public static bool GetHasUniformTabWidth(DependencyObject element)
         => (bool)element.GetValue(HasUniformTabWidthProperty);
 
+    // ReSharper disable once HeapView.BoxingAllocation
     public static readonly DependencyProperty HeaderPanelMarginProperty = DependencyProperty.RegisterAttached(
         "HeaderPanelMargin", typeof(Thickness), typeof(TabAssist), new PropertyMetadata(default(Thickness)));
 
+    // ReSharper disable once HeapView.BoxingAllocation
     public static void SetHeaderPanelMargin(DependencyObject element, Thickness value)
         => element.SetValue(HeaderPanelMarginProperty, value);
 
@@ -35,9 +41,11 @@ public static class TabAssist
     internal static Visibility GetBindableIsItemsHost(DependencyObject obj)
         => (Visibility)obj.GetValue(BindableIsItemsHostProperty);
 
+    // ReSharper disable once HeapView.BoxingAllocation
     internal static void SetBindableIsItemsHost(DependencyObject obj, Visibility value)
         => obj.SetValue(BindableIsItemsHostProperty, value);
 
+    // ReSharper disable once HeapView.BoxingAllocation
     internal static readonly DependencyProperty BindableIsItemsHostProperty =
         DependencyProperty.RegisterAttached("BindableIsItemsHost", typeof(Visibility), typeof(TabAssist), new PropertyMetadata(Visibility.Collapsed, OnBindableIsItemsHostChanged));
 
