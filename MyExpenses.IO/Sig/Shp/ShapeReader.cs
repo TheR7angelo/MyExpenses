99f4ff2b82111e01d64cefa4e0f454917007a57f
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-15 14:19:23 +0100

    ci(Update): Remove unused and commented-out code from ShapeReader
    Eliminated legacy commented-out methods and static constructor to
    improve code clarity and maintainability. Updated `ReadFeatures` method
    signature to include an optional spatial reference parameter. This
    cleanup reduces clutter and makes the class more concise.

--- MyExpenses.IO/Sig/Shp/ShapeReader.cs
+++ MyExpenses.IO/Sig/Shp/ShapeReader.cs
@@ -16,13 +16,6 @@ public static class ShapeReader
 {
     private static readonly DateTimeConverter DateTimeConverterObject = new();
 
-    static ShapeReader()
-    {
-        // var csvFile = Path.GetFullPath("Resources");
-        // csvFile = Path.Combine(csvFile, "Projections", "ProjectionSystem.csv");
-        // GeographicCoordinateSystems = csvFile.ReadCsv<GeographicCoordinateSystem>().ToList();
-    }
-
     public static (List<T> Features, string? Projection) ReadShapeFile<T>(this string filePath) where T : class, ISig
     {
         //TODO work
@@ -47,7 +40,6 @@ public static class ShapeReader
             Log.Warning("Fichier .prj introuvable");
         }
 
-        // var features = filePath.ReadFeatures(geographicCoordinateSystem);
         var features = filePath.ReadFeatures();
 
         //TODO work
@@ -55,40 +47,6 @@ public static class ShapeReader
 
         return (features.ToList<T>(), projection);
     }
-
-    // public static (List<Feature> Features, string? Projection) ReadShapeFile(this string filePath)
-    // {
-    //     //TODO work
-    //     Log.Information("Lecture du shape \"{FilePath}\"", filePath);
-    //
-    //     string? projection = null;
-    //     GeographicCoordinateSystem? geographicCoordinateSystem = null;
-    //     var prjFilePath = Path.ChangeExtension(filePath, "prj");
-    //     if (File.Exists(prjFilePath))
-    //     {
-    //         //TODO work
-    //         Log.Information("Tentative de lecture du fichier .prj");
-    //
-    //         projection = File.ReadAllText(prjFilePath);
-    //         geographicCoordinateSystem = GetGeographicCoordinateSystemFromPrj(projection);
-    //
-    //         //TODO work
-    //         Log.Information("Système trouvé => {Name}", geographicCoordinateSystem?.Name);
-    //     }
-    //     else
-    //     {
-    //         //TODO work
-    //         Log.Warning("Fichier .prj introuvable");
-    //     }
-    //
-    //     // var features = filePath.ReadFeatures(geographicCoordinateSystem);
-    //     var features = filePath.ReadFeatures();
-    //
-    //     //TODO work
-    //     Log.Information("Shape \"{FilePath}\" lu, Nombre entité: {NbCount}", filePath, features.Count);
-    //
-    //     return (features, projection);
-    // }
 
     private static TSpatialRefSy? GetGeographicCoordinateSystemFromPrj(this string projectionString)
     {
@@ -152,64 +110,7 @@ public static class ShapeReader
         }
     }
 
-    // private static List<T> ToList<T>(this IEnumerable<Feature> features) where T : class
-    // {
-    //     var results = new List<T>();
-    //
-    //     foreach (var feature in features)
-    //     {
-    //         var tmp = (ISig)Activator.CreateInstance<T>();
-    //         tmp.Geometry = feature.Geometry;
-    //
-    //         foreach (var name in feature.Attributes.GetNames())
-    //         {
-    //             var tmpName = name;
-    //             if (tmpName.Length > 10) tmpName = tmpName[..10];
-    //
-    //             var property = tmpName.GetPropertiesInfoByName<T, ColumnAttribute>();
-    //             if (property is null) continue;
-    //             if (!property.CanWrite) continue;
-    //
-    //             var value = feature.Attributes[tmpName];
-    //
-    //             try
-    //             {
-    //                 property.SetValue(tmp, value);
-    //             }
-    //             catch (Exception)
-    //             {
-    //                 var targetType = property.PropertyType;
-    //                 try
-    //                 {
-    //                     value = value.ConvertTo(targetType);
-    //                 }
-    //                 catch (Exception e)
-    //                 {
-    //                     Console.WriteLine($"{name}");
-    //                     Console.WriteLine(e);
-    //                     throw;
-    //                 }
-    //
-    //                 try
-    //                 {
-    //                     property.SetValue(tmp, value);
-    //                 }
-    //                 catch (Exception exception)
-    //                 {
-    //                     Console.WriteLine($"{name}");
-    //                     Console.WriteLine(exception);
-    //                     throw;
-    //                 }
-    //             }
-    //         }
-    //
-    //         results.Add((tmp as T)!);
-    //     }
-    //
-    //     return results;
-    // }
-
-    private static List<Feature> ReadFeatures(this string filePath) //, GeographicCoordinateSystem? geographicCoordinateSystem = null)
+    private static List<Feature> ReadFeatures(this string filePath, TSpatialRefSy? spatialRef = null)
     {
         var features = new List<Feature>();
 
@@ -242,7 +143,6 @@ public static class ShapeReader
             {
                 str = str.Trim('*', ' ');
                 value = string.IsNullOrEmpty(str) ? null : str;
-                // value = value is not null ? DateTimeConverterObject.ConvertFromString((string?)value) : value;
                 // ReSharper disable once HeapView.BoxingAllocation
                 var dateConvert = value is not null ? DateTimeConverterObject.ConvertFromString((string?)value) : value;
 
