3d5e1215b67fa65c5cc76c41c31c218571c15de3
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-17 12:00:00 +0100

    refactor(Update): Refactor CSV configuration into a static property
    Centralized the CSV configuration as a static property to improve code
    reuse and maintainability. Updated the CSV reading method to use the
    shared configuration, reducing redundancy and enhancing consistency
    across the application.

--- MyExpenses.IO/Csv/CsvReader.cs
+++ MyExpenses.IO/Csv/CsvReader.cs
@@ -7,6 +7,22 @@ namespace MyExpenses.IO.Csv;
 
 public static class CsvReader
 {
+    /// <summary>
+    /// Provides the configuration options for CSV file operations used within the application.
+    /// </summary>
+    /// <remarks>
+    /// This property is a static instance of <see cref="CsvHelper.Configuration.CsvConfiguration"/> preconfigured
+    /// to use the invariant culture and to handle specific CSV parsing features. The configuration disables header
+    /// validation and missing field handling while enabling automatic detection of the delimiter.
+    /// </remarks>
+    // ReSharper disable once HeapView.ObjectAllocation.Evident
+    private static CsvConfiguration CsvConfiguration { get; } = new (CultureInfo.InvariantCulture)
+    {
+        HeaderValidated = null,
+        MissingFieldFound = null,
+        DetectDelimiter = true
+    };
+
     static CsvReader()
     {
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
@@ -20,20 +36,13 @@ public static class CsvReader
     /// <returns>An <see cref="IEnumerable{T}"/> containing the mapped objects from the CSV file.</returns>
     public static IEnumerable<T> ReadCsv<T>(this string filePath)
     {
-        var csvConfiguration = new CsvConfiguration(CultureInfo.InvariantCulture)
-        {
-            HeaderValidated = null,
-            MissingFieldFound = null,
-            DetectDelimiter = true
-        };
-
         var encodingName = GetEncodingName(filePath);
         var encoding = Encoding.GetEncoding(encodingName);
 
         using var streamReader = new StreamReader(filePath, encoding, true,
             new FileStreamOptions { Access = FileAccess.Read, Share = FileShare.ReadWrite });
 
-        using var reader = new CsvHelper.CsvReader(streamReader, csvConfiguration);
+        using var reader = new CsvHelper.CsvReader(streamReader, CsvConfiguration);
         var records = reader.GetRecords<T>();
 
         return records.ToList();
