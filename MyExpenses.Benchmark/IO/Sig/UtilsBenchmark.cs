faef56dacd9b148cc5a96bb563e5965836f7a0a8
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-19 14:26:57 +0100

    ci(Update): Add benchmark tests for DbfType field type mapping
    Introduce the `UtilsBenchmark` class to compare performance of
    dictionary and switch-based approaches for determining field types from
    `DbfType`. The benchmarks use BenchmarkDotNet to provide detailed
    performance metrics. This helps evaluate and optimize the efficiency of
    field type mapping.

---
+++ MyExpenses.Benchmark/IO/Sig/UtilsBenchmark.cs
@@ -0,0 +1,62 @@
+using BenchmarkDotNet.Attributes;
+using NetTopologySuite.IO.Esri.Dbf;
+
+namespace MyExpenses.Benchmark.IO.Sig;
+
+[MemoryDiagnoser]
+[ThreadingDiagnoser]
+[AllStatisticsColumn]
+[HtmlExporter]
+[MarkdownExporter]
+[RankColumn]
+public class UtilsBenchmark
+{
+    private static readonly Dictionary<DbfType, string> FieldTypeMap = new()
+    {
+        { DbfType.Numeric, "long" },
+        { DbfType.Float, "double" },
+        { DbfType.Character, "string" },
+        { DbfType.Date, "DateTime" },
+        { DbfType.Logical, "int" }
+    };
+
+    private static readonly DbfType[] TestDbfTypes =
+        [DbfType.Numeric, DbfType.Float, DbfType.Character, DbfType.Date, DbfType.Logical];
+
+
+    private static string GetDbFieldTypeFromDictionary(DbfType dbfType)
+    {
+        return FieldTypeMap[dbfType];
+    }
+
+    private static string GetDbFieldTypeFromSwitch(DbfType dbfType)
+    {
+        return dbfType switch
+        {
+            DbfType.Numeric => "long",
+            DbfType.Float => "double",
+            DbfType.Character => "string",
+            DbfType.Date => "DateTime",
+            DbfType.Logical => "int",
+            _ => throw new ArgumentOutOfRangeException(nameof(dbfType), dbfType, @"Unsupported DbfType")
+        };
+    }
+
+    [Benchmark]
+    public void BenchmarkDictionary()
+    {
+        foreach (var dbfType in TestDbfTypes)
+        {
+            _ = GetDbFieldTypeFromDictionary(dbfType);
+        }
+    }
+
+    [Benchmark]
+    public void BenchmarkSwitch()
+    {
+        foreach (var dbfType in TestDbfTypes)
+        {
+            _ = GetDbFieldTypeFromSwitch(dbfType);
+        }
+    }
+}
