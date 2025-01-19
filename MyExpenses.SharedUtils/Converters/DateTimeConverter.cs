7019e3019e891bf92319dc321af826080db94df3
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-19 12:58:48 +0100

    ci(Update): Refactor and relocate DateTimeConverter class
    Move the DateTimeConverter class from MyExpenses.Models to
    MyExpenses.SharedUtils to improve reusability across projects. Updated
    the code with minor naming adjustments and added XML documentation for
    better clarity and maintainability.

---
+++ MyExpenses.SharedUtils/Converters/DateTimeConverter.cs
@@ -0,0 +1,43 @@
+﻿namespace MyExpenses.SharedUtils.Converters;
+
+public class DateTimeConverter
+{
+    private readonly string[] _supportedFormats =
+    [
+        "dd/MM/yyyy HH:mm",
+        "MM/dd/yyyy HH:mm",
+        "yyyy/MM/dd HH:mm:ss.fff",
+        "yyyy/MM/dd HH:mm:ss",
+        "yyyy-MM-dd HH:mm",
+        "MM-dd-yyyy HH:mm",
+        "yyyy-MM-dd HH:mm:ss.fff",
+        "yyyy-MM-dd HH:mm:ss"
+    ];
+
+    private static readonly System.Globalization.CultureInfo InvariantCulture =
+        System.Globalization.CultureInfo.InvariantCulture;
+
+    /// <summary>
+    /// Converts a string to a nullable DateTime, using predefined formats.
+    /// </summary>
+    /// <param name="input">The string representation of a date and time.</param>
+    /// <returns>
+    /// A nullable <see cref="DateTime"/> object if the conversion is successful; otherwise, <c>null</c>.
+    /// </returns>
+    public DateTime? ConvertFromString(string? input)
+    {
+        if (string.IsNullOrWhiteSpace(input)) return null;
+
+        if (DateTime.TryParseExact(
+                input,
+                _supportedFormats,
+                InvariantCulture,
+                System.Globalization.DateTimeStyles.None,
+                out var result))
+        {
+            return result;
+        }
+
+        return null;
+    }
+}
