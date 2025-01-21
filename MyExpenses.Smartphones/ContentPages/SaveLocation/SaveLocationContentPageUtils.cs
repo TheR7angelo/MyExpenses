b3e5383c50da82295120040a14571859586ecdd1
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-21 13:31:39 +0100

    ci(Update): Refactor SaveLocationContentPage initialization
    Add necessary instantiation of SaveLocationContentPage to enable user
    interaction for selecting a save location. This change ensures the
    dialog is properly displayed and the user’s choice is awaited via the
    ResultDialog property.

--- MyExpenses.Smartphones/ContentPages/SaveLocation/SaveLocationContentPageUtils.cs
+++ MyExpenses.Smartphones/ContentPages/SaveLocation/SaveLocationContentPageUtils.cs
@@ -22,6 +22,10 @@ public static class SaveLocationContentPageUtils
 
     private static async Task<Models.Wpf.Save.SaveLocation?> GetSaveLocation(this SaveLocationMode saveLocationMode)
     {
+        // ReSharper disable once HeapView.ObjectAllocation.Evident
+        // Necessary instantiation of SaveLocationContentPage to present the save location dialog to the user.
+        // This creates and pushes the page to the navigation stack, allowing the user to interact with the UI
+        // and select a save location. The ResultDialog property is awaited to retrieve the user’s choice.
         var saveLocationContentPage = new SaveLocationContentPage(saveLocationMode);
         await Shell.Current.Navigation.PushAsync(saveLocationContentPage);
         var result = await saveLocationContentPage.ResultDialog;
