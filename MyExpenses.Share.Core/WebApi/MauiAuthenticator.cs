36041ea66c875e1c0188546e5d97ae3219678c88
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-21 08:48:19 +0100

    ci(Update): Refactor MauiAuthenticator to improve readability
    Reorganized Uri allocation for clarity and readability, separating
    authorization and callback URLs into distinct variables. Added comments
    explaining WebAuthenticatorOptions usage and ReSharper suppression
    rationale.

--- MyExpenses.Share.Core/WebApi/MauiAuthenticator.cs
+++ MyExpenses.Share.Core/WebApi/MauiAuthenticator.cs
@@ -8,13 +8,23 @@ public class MauiAuthenticator : IAuthenticator
 {
     public async Task<string?> AuthenticateAsync(DropboxKeys dropboxKeys, Pkce pkceData)
     {
+        // ReSharper disable HeapView.ObjectAllocation.Evident
+        // Normal allocation of an authorization URI for Dropbox OAuth2
         var authUri = new Uri($"https://www.dropbox.com/oauth2/authorize?client_id={dropboxKeys.AppKey}&redirect_uri={dropboxKeys.RedirectUri}&response_type=code&code_challenge={pkceData.CodeChallenge}&code_challenge_method=S256&token_access_type=offline");
 
+        // Normal allocation of a callback URI
+        var callBackUrl = new Uri(dropboxKeys.RedirectUri!);
+        // ReSharper restore HeapView.ObjectAllocation.Evident
+
         var result = await WebAuthenticator.Default.AuthenticateAsync(
+            // ReSharper disable once HeapView.ObjectAllocation.Evident
+            // Usage of WebAuthenticatorOptions is required to configure the authentication process.
+            // This class allows specifying the authentication URL, the callback URL (redirect URI),
+            // and additional parameters such as ephemeral browser sessions
             new WebAuthenticatorOptions
             {
                 Url = authUri,
-                CallbackUrl = new Uri(dropboxKeys.RedirectUri!),
+                CallbackUrl = callBackUrl,
                 PrefersEphemeralWebBrowserSession = true
             });
 
