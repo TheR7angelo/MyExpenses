e175542834e92056253e6c4166a822a5b95b55ed
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-21 08:45:25 +0100

    ci(Update): Add comments to clarify authenticator allocation logic
    Comments were added to explain the allocation process for WPF and MAUI
    authenticators in the switch statement. ReSharper directives were also
    included to suppress and restore HeapView warnings for object
    allocation. These changes enhance code readability and maintainability.

--- MyExpenses.Share.Core/WebApi/AuthenticatorFactory.cs
+++ MyExpenses.Share.Core/WebApi/AuthenticatorFactory.cs
@@ -6,11 +6,15 @@ public static class AuthenticatorFactory
 {
     public static IAuthenticator CreateAuthenticator(this ProjectSystem projectSystem)
     {
+        // ReSharper disable HeapView.ObjectAllocation.Evident
         return projectSystem switch
         {
+            // Normal allocation of a WPF Authenticator
             ProjectSystem.Wpf => new WpfAuthenticator(),
+            // Normal allocation of a MAUI Authenticator
             ProjectSystem.Maui => new MauiAuthenticator(),
             _ => throw new ArgumentOutOfRangeException(nameof(projectSystem), projectSystem, null)
         };
+        // ReSharper restore HeapView.ObjectAllocation.Evident
     }
 }
