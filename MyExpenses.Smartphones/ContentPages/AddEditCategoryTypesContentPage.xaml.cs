6ac283d08792a49b706f9bf2efaaf28f8a445725
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-22 20:55:41 +0100

    refactor(Update): Refactor object initialization for clarity and safety
    Ensure required properties are initialized using object initializers for
    `TCategoryType` and `CustomPopupEditCategory`. This enhances code
    readability, minimizes the risk of uninitialized states, and adapts to
    dynamic application needs effectively.

--- MyExpenses.Smartphones/ContentPages/AddEditCategoryTypesContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/AddEditCategoryTypesContentPage.xaml.cs
@@ -151,6 +151,10 @@ public partial class AddEditCategoryTypesContentPage
             AddEditCategoryTypesContentPageResources.MesageBoxAddNewCategoryTypeQuestionNoButton);
         if (!response) return;
 
+        // ReSharper disable once HeapView.ObjectAllocation.Evident
+        // Using an object initializer here ensures that the TCategoryType instance is constructed
+        // and all required properties (Name, ColorFk, and DateAdded) are initialized in one concise
+        // and readable statement. This approach minimizes the chance of missing a property initialization.
         var newCategoryTypeType = new TCategoryType
         {
             Name = CategoryTypeName,
@@ -241,6 +245,12 @@ public partial class AddEditCategoryTypesContentPage
         if (sender is not Border border) return;
         if (border.BindingContext is not VCategory category) return;
 
+        // ReSharper disable once HeapView.ObjectAllocation.Evident
+        // A new instance of CustomPopupEditCategory is explicitly created here to allow customization
+        // of the popup's behavior with specific properties such as MaxLength and CanDelete. This allocation
+        // is necessary as each invocation may require a uniquely configured popup based on the application's
+        // state or user interactions. The usage of an object initializer ensures these properties are set
+        // immediately, avoiding uninitialized or invalid states for the popup.
         var customPopupEditCategory = new CustomPopupEditCategory { MaxLenght = MaxLength, CanDelete = true };
         customPopupEditCategory.SetVCategory(category);
         await this.ShowPopupAsync(customPopupEditCategory);
