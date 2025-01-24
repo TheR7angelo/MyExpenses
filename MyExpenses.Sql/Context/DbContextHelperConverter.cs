8986bec92ade9f4d1a70fb746ece7fad4abad8f1
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-24 10:40:46 +0100

    ci(Update): Refactor ToISql<T> method to ensure consistency across DBs
    Added comments to clarify the use of `UpdateAllDefaultValues()` for
    maintaining default value consistency across all databases. Highlighted
    the importance of handling multiple databases in a controlled,
    sequential manner.

--- MyExpenses.Sql/Context/DbContextHelperConverter.cs
+++ MyExpenses.Sql/Context/DbContextHelperConverter.cs
@@ -12,6 +12,11 @@ public static class DbContextHelperConverter
     /// <returns>The ISql object of type T with the specified id, or null if no ISql object is found with the id.</returns>
     public static T? ToISql<T>(this int id) where T : class, ISql
     {
+        // ReSharper disable once HeapView.ObjectAllocation.Evident
+        // This method iterates through all existing databases to apply updates using their respective contexts.
+        // The use of `UpdateAllDefaultValues()` is necessary to ensure the default values remain consistent
+        // across all databases, and `SaveChanges()` is called to persist these changes. This approach is essential
+        // given the need to handle multiple databases in a controlled and sequential manner.
         using var context = new DataBaseContext();
         var result = context.Set<T>().FirstOrDefault(s => s.Id == id);
 
