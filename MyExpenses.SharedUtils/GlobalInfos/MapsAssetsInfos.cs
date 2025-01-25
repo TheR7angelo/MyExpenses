0ede223b4cd9ff195d6255ef7ee2da20e8cef6a8
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-25 12:51:46 +0100

    ci(Update): Update MapsAssetsBasePath to include "Resources" directory
    The MapsAssetsBasePath path now reflects an additional "Resources"
    directory in its structure. This change ensures proper alignment with
    the updated file organization and prevents potential path resolution
    issues.

--- MyExpenses.SharedUtils/GlobalInfos/MapsAssetsInfos.cs
+++ MyExpenses.SharedUtils/GlobalInfos/MapsAssetsInfos.cs
@@ -2,7 +2,7 @@ namespace MyExpenses.SharedUtils.GlobalInfos;
 
 public static class MapsAssetsInfos
 {
-    private static string MapsAssetsBasePath => Path.Join(OsInfos.OsBasePath, "Assets", "Maps");
+    private static string MapsAssetsBasePath => Path.Join(OsInfos.OsBasePath, "Resources", "Assets", "Maps");
 
     public static string BleuMarkerFilePath => Path.Join(MapsAssetsBasePath, "BlueMarker.svg");
     public static string GreenMarkerFilePath => Path.Join(MapsAssetsBasePath, "GreenMarker.svg");
