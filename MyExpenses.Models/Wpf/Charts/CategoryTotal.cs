264f31d7564ce21d60b829c12f5853900d9b4fa2
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-27 10:52:29 +0100

    ci(Update): Refactor CategoryTotal to support property change notifications
    Changed properties from `init` to `set` and added
    `AddINotifyPropertyChangedInterface` attribute for automatic property
    change notifications. This facilitates better data binding and improves
    reactivity in the UI.

--- MyExpenses.Models/Wpf/Charts/CategoryTotal.cs
+++ MyExpenses.Models/Wpf/Charts/CategoryTotal.cs
@@ -1,12 +1,15 @@
+using PropertyChanged;
+
 namespace MyExpenses.Models.Wpf.Charts;
 
+[AddINotifyPropertyChangedInterface]
 public class CategoryTotal
 {
     public string? Name { get; init; }
-    public string? HexadecimalColor { get; init; }
-    public double? Percentage { get; init; }
-    public double? Value { get; init; }
-    public string? Symbol { get; init; }
+    public string? HexadecimalColor { get; set; }
+    public double? Percentage { get; set; }
+    public double? Value { get; set; }
+    public string? Symbol { get; set; }
 
     public string ValueSymbol => Value is not null ? Value + " " + Symbol : 0d + " " + Symbol;
 }
