f77e7d3ccdfa30ffb028af4620fb4384b5ff4987
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-27 14:02:20 +0100

    ci(Update): Change HowToUseCulturePath from class to struct
    Refactored HowToUseCulturePath to improve performance and memory usage
    by switching from a class to a struct. This change is suitable as the
    type primarily stores value-like data.

--- MyExpenses.Models/Wpf/Helps/HowToUseCulturePath.cs
+++ MyExpenses.Models/Wpf/Helps/HowToUseCulturePath.cs
@@ -2,7 +2,7 @@ using System.Globalization;
 
 namespace MyExpenses.Models.Wpf.Helps;
 
-public class HowToUseCulturePath
+public struct HowToUseCulturePath
 {
     public required CultureInfo CultureInfo { get; init; }
     public required string Path { get; init; }
