d211e3fa6de33768bff8746285872a3d3f765bc6
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-28 14:45:50 +0100

    ci(Update): Enhance database version handling with proper context usage
    Added detailed comments explaining the necessity of creating a new
    `DataBaseContext` instance within the `SetDatabaseVersionValue` method.
    Ensured proper disposal of the context using the `using` statement for
    resource management.

--- MyExpenses.Wpf/UserControls/Helps/VersionControl.xaml.cs
+++ MyExpenses.Wpf/UserControls/Helps/VersionControl.xaml.cs
@@ -122,6 +122,10 @@ public partial class VersionControl
 
     private void SetDatabaseVersionValue()
     {
+        // ReSharper disable once HeapView.ObjectAllocation.Evident
+        // The creation of a new DataBaseContext instance (via `new DataBaseContext()`) is necessary to interact with the database.
+        // This context provides the connection to the database and allows querying or updating data.
+        // The `using` statement ensures that the context is disposed of properly after its use, freeing up resources like database connections.
         using var context = new DataBaseContext(DatabaseInfos.LocalFilePathDataBaseModel);
         var version = context.TVersions.First();
 
