9f5e5a422765edda3357141b2397620f3ab87a54
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-29 09:23:12 +0100

    ci(Update): Add XML documentation to `CreateColumnSeries` method
    Added detailed summary, parameter, and return descriptions to the
    `CreateColumnSeries` method, providing clarity on its usage. This
    enhances code readability and support for developers utilizing this
    method.

--- MyExpenses.Share.Core/Analysis/Commons.cs
+++ MyExpenses.Share.Core/Analysis/Commons.cs
@@ -7,7 +7,17 @@ namespace MyExpenses.Share.Core.Analysis;
 
 public static class Commons
 {
-    public static ColumnSeries<T> CreateColumnSeries<T>(string name, IEnumerable<T> values, SolidColorPaint? solidColorPaint,
+    /// <summary>
+    /// Creates a new column series with the specified properties.
+    /// </summary>
+    /// <typeparam name="T">The type of the values in the column series.</typeparam>
+    /// <param name="name">The name of the column series.</param>
+    /// <param name="values">The collection of values to be displayed in the column series.</param>
+    /// <param name="solidColorPaint">The solid color paint used to fill the columns in the series. Null if no color is specified.</param>
+    /// <param name="tooltipFormatter">An optional function to format the tooltip labels associated with the series data points.</param>
+    /// <returns>A new instance of <see cref="ColumnSeries{T}" /> configured with the provided properties.</returns>
+    public static ColumnSeries<T> CreateColumnSeries<T>(string name, IEnumerable<T> values,
+        SolidColorPaint? solidColorPaint,
         Func<ChartPoint<T, RoundedRectangleGeometry, LabelGeometry>, string>? tooltipFormatter)
     {
         // ReSharper disable once HeapView.ObjectAllocation.Evident
