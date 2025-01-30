187410ddc49260cdb50f59e63594258959dd11ce
TheR7angelo <raphael.antunes@hotmail.fr>
2025-01-30 10:12:12 +0100

    ci(Update): Add methods for generating and calculating linear trends
    Introduced `CreateLinearTrendLine` to generate trend line values from a
    dataset and `CalculateLinearTrend` for computing slope and intercept of
    linear trends. Both methods enhance the utility of numerical trend
    analysis within the application. Added XML documentation for better code
    clarity and usage guidance.

--- MyExpenses.SharedUtils/Maths/TrendUtils.cs
+++ MyExpenses.SharedUtils/Maths/TrendUtils.cs
@@ -2,6 +2,11 @@ namespace MyExpenses.SharedUtils.Maths;
 
 public static class TrendUtils
 {
+    /// <summary>
+    /// Generates a linear trend line based on the provided collection of numerical values.
+    /// </summary>
+    /// <param name="values">The sequence of numerical values for which the trend line is to be calculated.</param>
+    /// <returns>An enumerable collection of calculated trend line values.</returns>
     public static IEnumerable<double> CreateLinearTrendLine(this IEnumerable<double> values)
     {
         var enumerable = values as double[] ?? values.ToArray();
@@ -13,6 +18,12 @@ public static class TrendUtils
         return trendValues;
     }
 
+    /// <summary>
+    /// Calculates the slope and intercept of a linear trend based on the provided x and y datasets.
+    /// </summary>
+    /// <param name="xData">The array of independent variable values (x-axis).</param>
+    /// <param name="yData">The array of dependent variable values (y-axis).</param>
+    /// <returns>A tuple containing the slope (a) and the intercept (b) of the linear trend.</returns>
     private static (double a, double b) CalculateLinearTrend(this double[] xData, double[] yData)
     {
         double sumX = 0, sumY = 0, sumXy = 0, sumXx = 0;
