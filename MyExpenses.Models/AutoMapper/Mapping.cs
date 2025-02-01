01536cae0a7f853d5f3ca853e71215d4890a1c97
TheR7angelo <raphael.antunes@hotmail.fr>
2025-02-01 20:49:28 +0100

    ci(Update): Add XML documentation for Mapping class and its members
    Introduced detailed XML comments to enhance code readability and
    maintainability. Descriptions were added for the class, the static
    Mapper property, and the Configure method, explaining purpose and
    functionality.

--- MyExpenses.Models/AutoMapper/Mapping.cs
+++ MyExpenses.Models/AutoMapper/Mapping.cs
@@ -3,8 +3,18 @@ using AutoMapper;
 
 namespace MyExpenses.Models.AutoMapper;
 
+/// <summary>
+/// Provides mappings for objects used within the application.
+/// </summary>
 public static class Mapping
 {
+    /// <summary>
+    /// Provides access to a shared instance of the AutoMapper IMapper.
+    /// </summary>
+    /// <remarks>
+    /// This property allows mapping between object models using AutoMapper.
+    /// It is configured to be accessed as a static singleton for convenience.
+    /// </remarks>
     public static IMapper Mapper { get; }
 
     static Mapping()
@@ -17,6 +27,11 @@ public static class Mapping
 
         Mapper = mapperConfiguration.CreateMapper();
     }
+
+    /// <summary>
+    /// Configures the AutoMapper with the necessary profiles from the executing assembly.
+    /// </summary>
+    /// <param name="cfg">The configuration expression used to define mapping profiles.</param>
     private static void Configure(IMapperConfigurationExpression cfg)
     {
         var executingAssembly = Assembly.GetExecutingAssembly();
