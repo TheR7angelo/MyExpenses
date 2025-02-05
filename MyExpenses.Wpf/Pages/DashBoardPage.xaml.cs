9998f8b3d933f04d263ced249283a5059a5d8e7a
TheR7angelo <raphael.antunes@hotmail.fr>
2025-02-05 21:24:33 +0100

    refactor(Update): Refactor pie chart update logic into PieChartManager
    Moved pie chart and category total update logic to a new PieChartManager
    class, improving modularity and clarity. This refactoring simplifies
    DashboardPage by reducing internal complexity and isolating pie
    chart-related responsibilities.

--- MyExpenses.Wpf/Pages/DashBoardPage.xaml.cs
+++ MyExpenses.Wpf/Pages/DashBoardPage.xaml.cs
@@ -4,15 +4,11 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using FilterDataGrid;
-using LiveChartsCore;
-using LiveChartsCore.SkiaSharpView;
-using LiveChartsCore.SkiaSharpView.Painting;
 using MyExpenses.Models.Config.Interfaces;
 using MyExpenses.Models.Sql.Bases.Tables;
 using MyExpenses.Models.Sql.Bases.Views;
 using MyExpenses.Models.Sql.Queries;
 using MyExpenses.Models.Wpf.Charts;
-using MyExpenses.Share.Core.Analysis;
 using MyExpenses.SharedUtils.Collection;
 using MyExpenses.SharedUtils.Properties;
 using MyExpenses.Sql.Context;
@@ -253,6 +249,9 @@ public partial class DashBoardPage
     }
 
     public ObservableCollection<CategoryTotal> CategoryTotals { get; } = [];
+
+    private PieChartManager PieChartManager { get; }
+
     public ObservableCollection<string> Years { get; }
     public ObservableCollection<string> Months { get; } = [];
 
@@ -370,6 +369,10 @@ public partial class DashBoardPage
         RefreshAccountTotal();
 
         InitializeComponent();
+
+        // ReSharper disable once HeapView.ObjectAllocation.Evident
+        PieChartManager = new PieChartManager(PieChart, CategoryTotals);
+
         UpdateLanguage();
 
         UpdatePieChartLegendTextPaint();
@@ -679,7 +682,8 @@ public partial class DashBoardPage
 
         var filteredData = accountName.GetFilteredVDetailTotalCategories(monthInt, yearInt);
         var categoriesTotals = filteredData.CalculateCategoryTotals(out var grandTotal);
-        UpdateChartUi(categoriesTotals, grandTotal);
+
+        PieChartManager.UpdateChartUi(categoriesTotals, grandTotal);
     }
 
     private void RefreshRadioButtonSelected()
@@ -722,111 +726,6 @@ public partial class DashBoardPage
         if (radioButtons.Count == 0) return null;
 
         return radioButtons.FirstOrDefault(s => s.IsChecked == true)?.Content as string;
-    }
-
-    private void UpdateChartUi(IEnumerable<CategoryTotalData> categoriesTotals, double grandTotal)
-    {
-        var series = (ObservableCollection<ISeries>)PieChart.Series;
-
-        var existingSeries = series
-            .OfType<PieSeries<double>>()
-            .ToDictionary(s => (string)s.Tag!, s => s);
-
-        var categoryTotalsDict = CategoryTotals.ToDictionary(ct => ct.Name!, ct => ct);
-
-        var categoryTotalDatas = categoriesTotals as CategoryTotalData[] ?? categoriesTotals.ToArray();
-
-        foreach (var categoryTotal in categoryTotalDatas)
-        {
-            var absTotal = Math.Abs(categoryTotal.Total);
-            var percentage = grandTotal != 0 ? Math.Round(absTotal / grandTotal * 100, 2) : 0;
-
-            UpdateOrCreatePieSeries(series, existingSeries, categoryTotal, absTotal, percentage);
-            UpdateOrCreateCategoryTotal(CategoryTotals, categoryTotalsDict, categoryTotal, percentage);
-        }
-
-        RemoveObsoleteElements(series, existingSeries, CategoryTotals, categoryTotalsDict, categoryTotalDatas);
-    }
-
-    private static void UpdateOrCreatePieSeries(ObservableCollection<ISeries> series, Dictionary<string, PieSeries<double>> existingSeries,
-        CategoryTotalData categoryTotal, double absTotal, double percentage)
-    {
-        var func = categoryTotal.Symbol!.CreateDoughnutLabelFunc();
-        var solidColorPaint = categoryTotal.HexadecimalColorCode.ToSolidColorPaint();
-
-        if (existingSeries.TryGetValue(categoryTotal.Category!, out var pieSeries))
-        {
-            pieSeries.Values = [absTotal];
-            pieSeries.Name = $"{categoryTotal.Category} ({percentage}%)";
-            pieSeries.ToolTipLabelFormatter = func;
-
-            if (pieSeries.Fill is not SolidColorPaint solidColorPaintPaint || solidColorPaintPaint.Color != solidColorPaint?.Color)
-            {
-                pieSeries.Fill = solidColorPaint;
-            }
-        }
-        else
-        {
-            // ReSharper disable once HeapView.ObjectAllocation.Evident
-            // The PieSeries instance is created with the specified values to handle pie series operations.
-            pieSeries = new PieSeries<double>
-            {
-                Values = [absTotal],
-                Name = $"{categoryTotal.Category} ({percentage}%)",
-                ToolTipLabelFormatter = func,
-                Fill = solidColorPaint,
-                Tag = categoryTotal.Category
-            };
-
-            series.Add(pieSeries);
-        }
-    }
-
-    private static void UpdateOrCreateCategoryTotal(ObservableCollection<CategoryTotal> categoryTotals, Dictionary<string, CategoryTotal> categoryTotalsDict,
-        CategoryTotalData categoryTotal, double percentage)
-    {
-        if (categoryTotalsDict.TryGetValue(categoryTotal.Category!, out var existingCategoryTotal))
-        {
-            existingCategoryTotal.HexadecimalColor = categoryTotal.HexadecimalColorCode;
-            existingCategoryTotal.Percentage = percentage;
-            existingCategoryTotal.Value = categoryTotal.Total;
-            existingCategoryTotal.Symbol = categoryTotal.Symbol;
-        }
-        else
-        {
-            // ReSharper disable once HeapView.ObjectAllocation.Evident
-            // The CategoryTotal instance is created with the specified values to handle category total operations.
-            var newCategoryTotal = new CategoryTotal
-            {
-                Name = categoryTotal.Category,
-                HexadecimalColor = categoryTotal.HexadecimalColorCode,
-                Percentage = percentage,
-                Value = categoryTotal.Total,
-                Symbol = categoryTotal.Symbol
-            };
-
-            categoryTotals.Add(newCategoryTotal);
-            categoryTotalsDict[categoryTotal.Category!] = newCategoryTotal;
-        }
-    }
-
-    private static void RemoveObsoleteElements(ObservableCollection<ISeries> series,
-        Dictionary<string, PieSeries<double>> existingSeries,
-        ObservableCollection<CategoryTotal> categoryTotals,
-        Dictionary<string, CategoryTotal> categoryTotalsDict,
-        CategoryTotalData[] categoryTotalDatas)
-    {
-        var updatedCategories = categoryTotalDatas.Select(ct => ct.Category!).ToHashSet();
-
-        foreach (var keyToRemove in existingSeries.Keys.Except(updatedCategories))
-        {
-            series.Remove(existingSeries[keyToRemove]);
-        }
-
-        foreach (var category in categoryTotalsDict.Keys.Except(updatedCategories))
-        {
-            categoryTotals.Remove(categoryTotalsDict[category]);
-        }
     }
 
     private void UpdateMonthLanguage()
