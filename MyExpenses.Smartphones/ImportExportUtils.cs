69669638e7af1002dde3f7b624ef3502c6c79a60
TheR7angelo <raphael.antunes@hotmail.fr>
2025-05-16 14:29:07 +0200

    ci(Update): Refactor cloud and local database deletion functionality
    Moved `DeleteCloudFilesAsync` and `DeleteLocalDatabases` methods from
    `MyExpenses.Smartphones` to `MyExpenses.Core`. Centralizing these
    operations in the core ensures better code reuse and simplifies future
    maintenance. Updated necessary imports to support the changes.

--- MyExpenses.Smartphones/ImportExportUtils.cs
+++ MyExpenses.Smartphones/ImportExportUtils.cs
@@ -85,41 +85,6 @@ public static class ImportExportUtils
         existingDatabases.RefreshExistingDatabases(projectSystem);
     }
 
-    /// <summary>
-    /// Deletes a collection of cloud-stored database files from the specified cloud directory.
-    /// </summary>
-    /// <param name="databasesToDelete">A collection of database entries representing the files to be deleted from the cloud storage.</param>
-    /// <returns>A task that represents the asynchronous delete operation.</returns>
-    public static async Task DeleteCloudFilesAsync(this IEnumerable<ExistingDatabase> databasesToDelete)
-    {
-        var files = databasesToDelete.Select(db => db.FileName).ToArray();
-        Log.Information("Preparing to delete the following files: {Files}", files);
-
-        var dropboxService = await DropboxService.CreateAsync(ProjectSystem.Maui);
-        _ = await dropboxService.DeleteFilesAsync(files, DatabaseInfos.CloudDirectoryBackupDatabase);
-
-        Log.Information("Files successfully deleted from Dropbox");
-    }
-
-    /// <summary>
-    /// Deletes the specified local databases and their corresponding backup directories
-    /// from the local file system, if they exist.
-    /// </summary>
-    /// <param name="databasesToDelete">A collection of database entries to be deleted from the local file system.</param>
-    public static void DeleteLocalDatabases(this IEnumerable<ExistingDatabase> databasesToDelete)
-    {
-        foreach (var database in databasesToDelete.Where(database => !string.IsNullOrEmpty(database.FilePath) && File.Exists(database.FilePath)))
-        {
-            File.Delete(database.FilePath);
-
-            var backupDirectory = Path.Join(DatabaseInfos.LocalDirectoryBackupDatabase, database.FileNameWithoutExtension);
-            if (Directory.Exists(backupDirectory))
-            {
-                Directory.Delete(backupDirectory, true);
-            }
-        }
-    }
-
     #endregion
 
     #region Export
