d24877bf7aa54b2733e3e74ed8b7be2e99522701
TheR7angelo <raphael.antunes@hotmail.fr>
2025-05-19 16:06:45 +0200

    ci(Update): Add Vaccum class for database vacuum event handling
    Introduces a static Vaccum class with an event and delegate specifically
    designed for triggering database vacuum operations. This enables
    flexible and centralized handling of vacuum-related events in the SQL
    models.

---
+++ MyExpenses.Models/Sql/Vaccum.cs
@@ -0,0 +1,11 @@
+﻿namespace MyExpenses.Models.Sql;
+
+public class Vaccum
+{
+    public delegate void VaccumDatabaseEventHandler();
+
+    public static event VaccumDatabaseEventHandler? VaccumDatabase;
+
+    public static void OnVacuumDatabase()
+        => VaccumDatabase?.Invoke();
+}
