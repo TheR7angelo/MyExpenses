13dd230bd4a0f18e8e0e75592c4303e724664f44
TheR7angelo <raphael.antunes@hotmail.fr>
2025-05-28 08:38:24 +0200

    ci(Update): Refactor color picker popup initialization
    Simplify the instantiation of the ColorPickerPopup by replacing
    redundant commentary and unused object initialization with a property
    assignment. This improves code readability and maintains functionality.

--- MyExpenses.Smartphones/ContentPages/ColorManagementContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/ColorManagementContentPage.xaml.cs
@@ -38,13 +38,7 @@ public partial class ColorManagementContentPage
         if (sender is not Border border) return;
         if (border.BindingContext is not TColor color) return;
 
-        // ReSharper disable once HeapView.ObjectAllocation.Evident
-        // A new instance of CustomPopupEditCategory is explicitly created here to allow customization
-        // of the popup's behavior with specific properties such as MaxLength and CanDelete. This allocation
-        // is necessary as each invocation may require a uniquely configured popup based on the application's
-        // state or user interactions. The usage of an object initializer ensures these properties are set
-        // immediately, avoiding uninitialized or invalid states for the popup.
-        var colorPickerPopup = new ColorPickerPopup(); // { MaxLenght = MaxLength, CanDelete = true };
+        var colorPickerPopup = new ColorPickerPopup { EditColor = true };
         colorPickerPopup.SetColor(color);
         await this.ShowPopupAsync(colorPickerPopup);
 
