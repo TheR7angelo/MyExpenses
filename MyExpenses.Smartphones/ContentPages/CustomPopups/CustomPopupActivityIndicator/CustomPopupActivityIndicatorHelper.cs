46628bcb5943b096252d39b63357241f3265c93d
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-12 08:59:47 +0200

    ci(Refactor): Comment on unused CustomPopupActivityIndicator logic
    Temporarily commented on unused `CustomPopupActivityIndicatorHelper` methods and related fields to reduce unnecessary code execution. Simplified placeholder logic while maintaining the option to restore functionality later.

--- MyExpenses.Smartphones/ContentPages/CustomPopups/CustomPopupActivityIndicator/CustomPopupActivityIndicatorHelper.cs
+++ MyExpenses.Smartphones/ContentPages/CustomPopups/CustomPopupActivityIndicator/CustomPopupActivityIndicatorHelper.cs
@@ -1,45 +1,39 @@
 using CommunityToolkit.Maui.Extensions;
-using CommunityToolkit.Maui.Views;
 using Serilog;
 
 namespace MyExpenses.Smartphones.ContentPages.CustomPopups.CustomPopupActivityIndicator;
 
 public static class CustomPopupActivityIndicatorHelper
 {
-    private static CustomPopupActivityIndicator? _customPopupActivityIndicator;
-    private static bool _isOpen;
+    // private static CustomPopupActivityIndicator? _customPopupActivityIndicator;
+    // private static bool _isOpen;
 
     public static void ShowCustomPopupActivityIndicator(this Page contentPage, string messageToDisplay)
     {
-        // ReSharper disable once HeapView.ObjectAllocation.Evident
-        // Necessary allocation of a new CustomPopupActivityIndicator instance.
-        // This is required to dynamically display a popup activity indicator with a custom message.
-        _customPopupActivityIndicator = new CustomPopupActivityIndicator { LabelTextToDisplay = messageToDisplay };
-        _customPopupActivityIndicator.Opened += CustomPopupActivityIndicatorOnOpened;
-        // contentPage.ShowPopupAsync(_customPopupActivityIndicator);
-
-        _ = contentPage.ShowPopupAsync(_customPopupActivityIndicator);
+        // // ReSharper disable once HeapView.ObjectAllocation.Evident
+        // // Necessary allocation of a new CustomPopupActivityIndicator instance.
+        // // This is required to dynamically display a popup activity indicator with a custom message.
+        // _customPopupActivityIndicator = new CustomPopupActivityIndicator { LabelTextToDisplay = messageToDisplay };
+        // _customPopupActivityIndicator.Opened += CustomPopupActivityIndicatorOnOpened;
+        // _customPopupActivityIndicator.Closed += CustomPopupActivityIndicatorOnClosed;
+        // contentPage.ShowPopup(_customPopupActivityIndicator);
     }
 
-    private static void CustomPopupActivityIndicatorOnOpened(object? sender, EventArgs e)
-    {
-        Log.Information("CustomPopupActivityIndicator opened");
-        _isOpen = true;
-    }
+    // private static void CustomPopupActivityIndicatorOnClosed(object? sender, EventArgs e)
+    // {
+    //     Log.Information("CustomPopupActivityIndicator closed");
+    //     _isOpen = false;
+    // }
+    //
+    // private static void CustomPopupActivityIndicatorOnOpened(object? sender, EventArgs e)
+    // {
+    //     Log.Information("CustomPopupActivityIndicator opened");
+    //     _isOpen = true;
+    // }
 
     public static void CloseCustomPopupActivityIndicator()
     {
-        while (true)
-        {
-            if (_isOpen)
-            {
-                _customPopupActivityIndicator?.CloseAsync();
-                _isOpen = false;
-                Log.Information("CustomPopupActivityIndicator closed");
-                return;
-            }
-            Thread.Sleep(TimeSpan.FromMilliseconds(100));
-        }
-
+        // Log.Information("CustomPopupActivityIndicator close requested");
+        // _customPopupActivityIndicator?.CloseAsync();
     }
 }
