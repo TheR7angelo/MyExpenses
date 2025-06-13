1cefe25ba25a228be5e2d756e29d40d45b0f7228
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-13 15:06:20 +0200

    ci(Feature): Add PieChartManager for managing dynamic Pie Chart UI updates
    Introduced `PieChartManager` to handle Pie Chart updates and category totals synchronization. Added LiveChartsCore.SkiaSharpView.Maui package and project reference to `MyExpenses.Share.Core` for enhanced charting capabilities.

---
+++ MyExpenses.Maui.Utils/PieChartManager.cs
@@ -0,0 +1,25 @@
+using System.Collections.ObjectModel;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Maui;
+using MyExpenses.Models.Wpf.Charts;
+using MyExpenses.Share.Core.Analysis;
+
+namespace MyExpenses.Maui.Utils;
+
+public class PieChartManager(PieChart pieChart, ObservableCollection<CategoryTotal> categoryTotals)
+{
+    private PieChart PieChart { get; } = pieChart;
+
+    private ObservableCollection<CategoryTotal> CategoryTotals { get; } = categoryTotals;
+
+    /// <summary>
+    /// Updates the UI of the pie chart and the associated category totals based on the provided data.
+    /// </summary>
+    /// <param name="categoriesTotals">A collection of category total data, each containing category details, total values, symbols, and color codes.</param>
+    /// <param name="grandTotal">The grand total value used to calculate percentages for the pie chart and category totals.</param>
+    public void UpdateChartUi(IEnumerable<CategoryTotalData> categoriesTotals, double grandTotal)
+    {
+        var series = (ObservableCollection<ISeries>)PieChart.Series;
+        series.UpdateChartUi(CategoryTotals, categoriesTotals, grandTotal);;
+    }
+}
