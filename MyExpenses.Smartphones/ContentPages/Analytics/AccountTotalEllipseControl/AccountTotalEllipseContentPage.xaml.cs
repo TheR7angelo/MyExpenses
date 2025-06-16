2aadfa8786c180f7aac0f6be2fe01a6b5bfe0ebe
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-16 11:12:36 +0200

    ci(Feature): Add AccountTotalEllipseContentPage with orientation management
    Introduced `AccountTotalEllipseContentPage` to the Analytics module, including orientation handling via `DeviceOrientationService`. Enhanced support for landscape mode during page lifecycle.

---
+++ MyExpenses.Smartphones/ContentPages/Analytics/AccountTotalEllipseControl/AccountTotalEllipseContentPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MyExpenses.Smartphones.ContentPages.Analytics.AccountTotalEllipseControl;
+
+public partial class AccountTotalEllipseContentPage
+{
+    private readonly DeviceOrientationService _deviceOrientationService;
+
+    public AccountTotalEllipseContentPage()
+    {
+        _deviceOrientationService = new DeviceOrientationService();
+
+        InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        _deviceOrientationService.SetDeviceOrientation(DisplayOrientation.Landscape);
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+
+        _deviceOrientationService.SetDeviceOrientation(DisplayOrientation.Unknown);
+    }
+}
