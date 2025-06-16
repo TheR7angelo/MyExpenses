bcb81e0493d0ce55530eeb5f610448ae9f58fc29
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-16 11:12:53 +0200

    ci(Feature): Add ButtonTextAccountTotalEllipse property and navigation support
    Introduced `ButtonTextAccountTotalEllipse` bindable property to `GeneralAnalysesContentPage`. Add the corresponding button in XAML with navigation to `AccountTotalEllipseContentPage`. Updated language resources in `UpdateLanguage`.

--- MyExpenses.Smartphones/ContentPages/GeneralAnalysesContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/GeneralAnalysesContentPage.xaml.cs
@@ -1,11 +1,22 @@
 ﻿using MyExpenses.Models.Config.Interfaces;
 using MyExpenses.SharedUtils.Resources.Resx.AnalyticsManagement;
 using MyExpenses.Smartphones.ContentPages.Analytics;
+using MyExpenses.Smartphones.ContentPages.Analytics.AccountTotalEllipseControl;
 
 namespace MyExpenses.Smartphones.ContentPages;
 
 public partial class GeneralAnalysesContentPage
 {
+    public static readonly BindableProperty ButtonTextAccountTotalEllipseProperty =
+        BindableProperty.Create(nameof(ButtonTextAccountTotalEllipse), typeof(string),
+            typeof(GeneralAnalysesContentPage));
+
+    public string ButtonTextAccountTotalEllipse
+    {
+        get => (string)GetValue(ButtonTextAccountTotalEllipseProperty);
+        set => SetValue(ButtonTextAccountTotalEllipseProperty, value);
+    }
+
     public static readonly BindableProperty ButtonTextAccountAnalyzedByMonthProperty =
         BindableProperty.Create(nameof(ButtonTextAccountAnalyzedByMonth), typeof(string),
             typeof(GeneralAnalysesContentPage));
@@ -31,8 +42,12 @@ public partial class GeneralAnalysesContentPage
     private void UpdateLanguage()
     {
         ButtonTextAccountAnalyzedByMonth = AnalyticsManagementResources.ButtonTextAnalyseByCategoryAndMonth;
+        ButtonTextAccountTotalEllipse = AnalyticsManagementResources.TabItemAccountTotalEllipseControlHeader;
     }
 
     private void ButtonAccountAnalyzedByMonth_OnClicked(object? sender, EventArgs e)
         => _ = typeof(AccountAnalyzedByMonthContentPage).NavigateToAsync();
+
+    private void ButtonAccountTotalEllipse_OnClicked(object? sender, EventArgs e)
+        => _ = typeof(AccountTotalEllipseContentPage).NavigateToAsync();
 }
