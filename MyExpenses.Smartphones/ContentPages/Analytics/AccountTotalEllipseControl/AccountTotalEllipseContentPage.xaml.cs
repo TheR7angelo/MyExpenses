396f9888213d3b2be6fc606fa0233ad0e57577f5
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-16 15:30:00 +0200

    ci(Feature): Add TabView and database binding for VTotalByAccount analyses
    Implemented `TabView` in `AccountTotalEllipseContentPage` with a bindable data template linking `VTotalByAccountAnalyses`. Added database context initialization and AutoMapper integration to support dynamic content rendering.

--- MyExpenses.Smartphones/ContentPages/Analytics/AccountTotalEllipseControl/AccountTotalEllipseContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/Analytics/AccountTotalEllipseControl/AccountTotalEllipseContentPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using MyExpenses.Models.Config.Interfaces;
+﻿using System.Collections.ObjectModel;
+using MyExpenses.Models.AutoMapper;
+using MyExpenses.Models.Config.Interfaces;
+using MyExpenses.Models.Sql.Derivatives.Views;
 using MyExpenses.SharedUtils.Resources.Resx.AnalyticsManagement;
+using MyExpenses.Sql.Context;
 
 namespace MyExpenses.Smartphones.ContentPages.Analytics.AccountTotalEllipseControl;
 
@@ -17,9 +21,17 @@ public partial class AccountTotalEllipseContentPage
 
     private readonly DeviceOrientationService _deviceOrientationService;
 
+    public ObservableCollection<VTotalByAccountAnalyse> VTotalByAccountAnalyses { get; }
+
     public AccountTotalEllipseContentPage()
     {
         _deviceOrientationService = new DeviceOrientationService();
+
+        // ReSharper disable once HeapView.ObjectAllocation.Evident
+        // Necessary instantiation of DataBaseContext to interact with the database.
+        using var context = new DataBaseContext();
+        VTotalByAccountAnalyses = [..context.VTotalByAccounts.OrderBy(s => s.Name)
+            .Select(s => Mapping.Mapper.Map<VTotalByAccountAnalyse>(s))];
 
         UpdateLanguage();
         InitializeComponent();
