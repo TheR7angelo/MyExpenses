76baae4c5c346091279bcd3da05ee419bfb53632
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-16 15:29:20 +0200

    ci(Feature): Add AutoMapper profile and derived view for VTotalByAccount analysis
    Introduced `VTotalByAccountToVTotalByAccountAnalyse` AutoMapper profile and created `VTotalByAccountAnalyse` derived view with override for `ToString`.

---
+++ MyExpenses.Models/AutoMapper/Profiles/VTotalByAccountToVTotalByAccountAnalyse.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using MyExpenses.Models.Sql.Bases.Views;
+using MyExpenses.Models.Sql.Derivatives.Views;
+
+namespace MyExpenses.Models.AutoMapper.Profiles;
+
+public class VTotalByAccountToVTotalByAccountAnalyse : Profile
+{
+    public VTotalByAccountToVTotalByAccountAnalyse()
+    {
+        CreateMap<VTotalByAccount, VTotalByAccountAnalyse>();
+    }
+}
---
+++ MyExpenses.Models/Sql/Derivatives/Views/VTotalByAccountAnalyse.cs
@@ -0,0 +1,8 @@
+﻿using MyExpenses.Models.Sql.Bases.Views;
+
+namespace MyExpenses.Models.Sql.Derivatives.Views;
+
+public class VTotalByAccountAnalyse : VTotalByAccount
+{
+    public override string? ToString() => Name;
+}
