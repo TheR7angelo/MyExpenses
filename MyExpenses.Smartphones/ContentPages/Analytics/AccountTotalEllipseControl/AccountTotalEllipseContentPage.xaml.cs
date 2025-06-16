2e94fca5bf637b1a97a2357606fc2ed09dd665d5
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-16 11:20:35 +0200

    ci(Feature): Add bindable page title for AccountTotalEllipseContentPage
    Introduced `AccountTotalEllipseContentPageTitle` as a bindable property with dynamic title binding in XAML. Updated `UpdateLanguage` to set the title from language resources. Enhanced localization and binding context setup.

--- MyExpenses.Smartphones/ContentPages/Analytics/AccountTotalEllipseControl/AccountTotalEllipseContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/Analytics/AccountTotalEllipseControl/AccountTotalEllipseContentPage.xaml.cs
@@ -1,14 +1,35 @@
-﻿namespace MyExpenses.Smartphones.ContentPages.Analytics.AccountTotalEllipseControl;
+﻿using MyExpenses.Models.Config.Interfaces;
+using MyExpenses.SharedUtils.Resources.Resx.AnalyticsManagement;
+
+namespace MyExpenses.Smartphones.ContentPages.Analytics.AccountTotalEllipseControl;
 
 public partial class AccountTotalEllipseContentPage
 {
+    public static readonly BindableProperty AccountTotalEllipseContentPageTitleProperty =
+        BindableProperty.Create(nameof(AccountTotalEllipseContentPageTitle), typeof(string),
+            typeof(AccountTotalEllipseContentPage));
+
+    public string AccountTotalEllipseContentPageTitle
+    {
+        get => (string)GetValue(AccountTotalEllipseContentPageTitleProperty);
+        set => SetValue(AccountTotalEllipseContentPageTitleProperty, value);
+    }
+
     private readonly DeviceOrientationService _deviceOrientationService;
 
     public AccountTotalEllipseContentPage()
     {
         _deviceOrientationService = new DeviceOrientationService();
 
+        UpdateLanguage();
         InitializeComponent();
+
+        Interface.LanguageChanged += UpdateLanguage;
+    }
+
+    private void UpdateLanguage()
+    {
+        AccountTotalEllipseContentPageTitle = AnalyticsManagementResources.TabItemAccountTotalEllipseControlHeader;
     }
 
     protected override void OnAppearing()
