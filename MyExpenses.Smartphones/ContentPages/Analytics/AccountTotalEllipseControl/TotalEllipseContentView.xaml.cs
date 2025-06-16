8d768e139d3376f1cb5e0e5bec6c653e54f5993c
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-16 15:29:34 +0200

    ci(Feature): Add TotalEllipseContentView to AccountTotalEllipseControl module
    Introduced `TotalEllipseContentView` with XAML structure and bindable properties for `Symbol`, `Total`, and `TitleTotal`. Enhanced design support for analytics visualization.

---
+++ MyExpenses.Smartphones/ContentPages/Analytics/AccountTotalEllipseControl/TotalEllipseContentView.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MyExpenses.Smartphones.ContentPages.Analytics.AccountTotalEllipseControl;
+
+public partial class TotalEllipseContentView
+{
+    public static readonly BindableProperty SymbolProperty = BindableProperty.Create(nameof(Symbol), typeof(string),
+        typeof(TotalEllipseContentView));
+
+    public string Symbol
+    {
+        get => (string)GetValue(SymbolProperty);
+        set => SetValue(SymbolProperty, value);
+    }
+
+    public static readonly BindableProperty TotalProperty = BindableProperty.Create(nameof(Total), typeof(double),
+        typeof(TotalEllipseContentView), 0d);
+
+    public double? Total
+    {
+        get => (double?)GetValue(TotalProperty);
+        set => SetValue(TotalProperty, value);
+    }
+
+    public static readonly BindableProperty TitleTotalProperty = BindableProperty.Create(nameof(TitleTotal),
+        typeof(string), typeof(TotalEllipseContentView));
+
+    public string TitleTotal
+    {
+        get => (string)GetValue(TitleTotalProperty);
+        set => SetValue(TitleTotalProperty, value);
+    }
+
+    public TotalEllipseContentView()
+    {
+        InitializeComponent();
+    }
+}
