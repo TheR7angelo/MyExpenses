8e406402d24fb07f719e8e7f4b2db86b977d8fba
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-17 10:03:37 +0200

    ci(Feature): Add title binding and device orientation handling in AccountsCategorySumPositiveNegativeContentPage
    Introduced title binding using a new BindableProperty and implemented device orientation management via `DeviceOrientationService`. Updated language handling with support for runtime language changes.

--- MyExpenses.Smartphones/ContentPages/Analytics/AccountsCategorySumPositiveNegativeContent/AccountsCategorySumPositiveNegativeContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/Analytics/AccountsCategorySumPositiveNegativeContent/AccountsCategorySumPositiveNegativeContentPage.xaml.cs
@@ -1,14 +1,30 @@
-﻿using MyExpenses.Models.Ui;
+﻿using MyExpenses.Models.Config.Interfaces;
+using MyExpenses.Models.Ui;
+using MyExpenses.SharedUtils.Resources.Resx.AnalyticsManagement;
 using MyExpenses.Sql.Context;
 
 namespace MyExpenses.Smartphones.ContentPages.Analytics.AccountsCategorySumPositiveNegativeContent;
 
 public partial class AccountsCategorySumPositiveNegativeContentPage
 {
+    public static readonly BindableProperty AccountsCategorySumPositiveNegativeContentPageTitleProperty =
+        BindableProperty.Create(nameof(AccountsCategorySumPositiveNegativeContentPageTitle), typeof(string),
+            typeof(AccountsCategorySumPositiveNegativeContentPage));
+
+    public string AccountsCategorySumPositiveNegativeContentPageTitle
+    {
+        get => (string)GetValue(AccountsCategorySumPositiveNegativeContentPageTitleProperty);
+        set => SetValue(AccountsCategorySumPositiveNegativeContentPageTitleProperty, value);
+    }
+
     public List<TabItemData> TabItems { get; }
+
+    private readonly DeviceOrientationService _deviceOrientationService;
 
     public AccountsCategorySumPositiveNegativeContentPage()
     {
+        _deviceOrientationService = new DeviceOrientationService();
+
         // ReSharper disable once HeapView.ObjectAllocation.Evident
         // Necessary instantiation of DataBaseContext to interact with the database.
         // This creates a scoped database context for performing queries and modifications in the database.
@@ -16,6 +32,32 @@ public partial class AccountsCategorySumPositiveNegativeContentPage
         TabItems = [..context.TAccounts.OrderBy(s => s.Name)
             .Select(s => new TabItemData { Header = s.Name!, Content = s })];
 
+        UpdateLanguage();
         InitializeComponent();
+
+        Interface.LanguageChanged += UpdateLanguage;
     }
+
+    private void UpdateLanguage()
+    {
+        AccountsCategorySumPositiveNegativeContentPageTitle = AnalyticsManagementResources.TabItemAccountsCategorySumPositiveNegativeControl;
+    }
+
+    #region Action
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        _deviceOrientationService.SetDeviceOrientation(DisplayOrientation.Landscape);
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+
+        _deviceOrientationService.SetDeviceOrientation(DisplayOrientation.Unknown);
+    }
+
+    #endregion
 }
