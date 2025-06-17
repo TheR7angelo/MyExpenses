056331ce757ca3e4c4637c33a7d672e9f1cdb920
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-17 09:31:51 +0200

    ci(Refactor): Simplify TabItems initialization in AccountsCategorySumPositiveNegativeControl
    Replaced manual TabItems population with LINQ for cleaner and more efficient initialization. Removed unused ObservableCollection.

--- MyExpenses.Wpf/UserControls/Analytics/AccountsCategorySumPositiveNegativeControls/AccountsCategorySumPositiveNegativeControl.xaml.cs
+++ MyExpenses.Wpf/UserControls/Analytics/AccountsCategorySumPositiveNegativeControls/AccountsCategorySumPositiveNegativeControl.xaml.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections.ObjectModel;
-using MyExpenses.Models.Ui;
+﻿using MyExpenses.Models.Ui;
 using MyExpenses.Sql.Context;
 
 namespace MyExpenses.Wpf.UserControls.Analytics.AccountsCategorySumPositiveNegativeControls;
 
 public partial class AccountsCategorySumPositiveNegativeControl
 {
-    public ObservableCollection<TabItemData> TabItems { get; } = [];
+    public List<TabItemData> TabItems { get; }
 
     public AccountsCategorySumPositiveNegativeControl()
     {
@@ -14,24 +13,9 @@ public partial class AccountsCategorySumPositiveNegativeControl
         // Necessary instantiation of DataBaseContext to interact with the database.
         // This creates a scoped database context for performing queries and modifications in the database.
         using var context = new DataBaseContext();
-        var accounts = context.TAccounts.OrderBy(s => s.Name);
+        TabItems = [..context.TAccounts.OrderBy(s => s.Name)
+            .Select(s => new TabItemData { Header = s.Name!, Content = new AccountCategorySumPositiveNegativeControl(s.Id) })];
 
         InitializeComponent();
-
-        foreach (var account in accounts)
-        {
-            // ReSharper disable once HeapView.ObjectAllocation.Evident
-            // The instantiation of AccountCategorySumPositiveNegativeControl is essential because
-            // this control handles the logic for visualizing account category data.
-            var accountModePaymentMonthlySumControl = new AccountCategorySumPositiveNegativeControl(account.Id);
-            // ReSharper disable once HeapView.ObjectAllocation.Evident
-            var tabItemData = new TabItemData
-            {
-                Header = account.Name!,
-                Content = accountModePaymentMonthlySumControl
-            };
-
-            TabItems.Add(tabItemData);
-        }
     }
 }
