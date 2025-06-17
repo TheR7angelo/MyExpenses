d7edf4f7feac21a004ef90daee5240cebf92334b
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-17 09:46:14 +0200

    ci(Feature): Add AccountsCategorySumPositiveNegativeContentPage and bind new button in GeneralAnalysesContentPage
    Integrated `AccountsCategorySumPositiveNegativeContentPage` with navigation logic and added a corresponding button in `GeneralAnalysesContentPage`. Updated bindings for dynamic text and navigation.

---
+++ MyExpenses.Smartphones/ContentPages/Analytics/AccountsCategorySumPositiveNegativeContent/AccountsCategorySumPositiveNegativeContentPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MyExpenses.Models.Ui;
+using MyExpenses.Sql.Context;
+
+namespace MyExpenses.Smartphones.ContentPages.Analytics.AccountsCategorySumPositiveNegativeContent;
+
+public partial class AccountsCategorySumPositiveNegativeContentPage
+{
+    public List<TabItemData> TabItems { get; }
+
+    public AccountsCategorySumPositiveNegativeContentPage()
+    {
+        // ReSharper disable once HeapView.ObjectAllocation.Evident
+        // Necessary instantiation of DataBaseContext to interact with the database.
+        // This creates a scoped database context for performing queries and modifications in the database.
+        using var context = new DataBaseContext();
+        TabItems = [..context.TAccounts.OrderBy(s => s.Name)
+            .Select(s => new TabItemData { Header = s.Name!, Content = s })];
+
+        InitializeComponent();
+    }
+}
--- MyExpenses.Smartphones/ContentPages/GeneralAnalysesContentPage.xaml.cs
+++ MyExpenses.Smartphones/ContentPages/GeneralAnalysesContentPage.xaml.cs
@@ -1,12 +1,23 @@
 ﻿using MyExpenses.Models.Config.Interfaces;
 using MyExpenses.SharedUtils.Resources.Resx.AnalyticsManagement;
 using MyExpenses.Smartphones.ContentPages.Analytics;
+using MyExpenses.Smartphones.ContentPages.Analytics.AccountsCategorySumPositiveNegativeContent;
 using MyExpenses.Smartphones.ContentPages.Analytics.AccountTotalEllipseContent;
 
 namespace MyExpenses.Smartphones.ContentPages;
 
 public partial class GeneralAnalysesContentPage
 {
+    public static readonly BindableProperty ButtonTextAccountsCategorySumPositiveNegativeControlProperty =
+        BindableProperty.Create(nameof(ButtonTextAccountsCategorySumPositiveNegativeControl), typeof(string),
+            typeof(GeneralAnalysesContentPage));
+
+    public string ButtonTextAccountsCategorySumPositiveNegativeControl
+    {
+        get => (string)GetValue(ButtonTextAccountsCategorySumPositiveNegativeControlProperty);
+        set => SetValue(ButtonTextAccountsCategorySumPositiveNegativeControlProperty, value);
+    }
+
     public static readonly BindableProperty ButtonTextAccountTotalEllipseProperty =
         BindableProperty.Create(nameof(ButtonTextAccountTotalEllipse), typeof(string),
             typeof(GeneralAnalysesContentPage));
@@ -43,6 +54,11 @@ public partial class GeneralAnalysesContentPage
     {
         ButtonTextAccountAnalyzedByMonth = AnalyticsManagementResources.ButtonTextAnalyseByCategoryAndMonth;
         ButtonTextAccountTotalEllipse = AnalyticsManagementResources.TabItemAccountTotalEllipseControlHeader;
+        ButtonTextAccountsCategorySumPositiveNegativeControl = AnalyticsManagementResources.TabItemAccountsCategorySumPositiveNegativeControl;
+        // ButtonTextCumulativeSumChartControlHeader = AnalyticsManagementResources.TabItemCumulativeSumChartControlHeader;
+        // ButtonTextAccountsCategorySumControlHeader = AnalyticsManagementResources.TabItemAccountsCategorySumControlHeader;
+        // ButtonTextAccountsModePaymentMonthlySumControlHeader = AnalyticsManagementResources.TabItemAccountsModePaymentMonthlySumControlHeader;
+        // ButtonTextBudgetsControlHeader = AnalyticsManagementResources.TabItemBudgetsControlHeader;
     }
 
     private void ButtonAccountAnalyzedByMonth_OnClicked(object? sender, EventArgs e)
@@ -50,4 +66,7 @@ public partial class GeneralAnalysesContentPage
 
     private void ButtonAccountTotalEllipse_OnClicked(object? sender, EventArgs e)
         => _ = typeof(AccountTotalEllipseContentPage).NavigateToAsync();
+
+    private void ButtonAccountsCategorySumPositiveNegative_OnClicked(object? sender, EventArgs e)
+        => _ = typeof(AccountsCategorySumPositiveNegativeContentPage).NavigateToAsync();
 }
