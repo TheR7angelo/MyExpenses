2b213df4382b14c37d1973904ceb04490b10d7b9
TheR7angelo <raphael.antunes@hotmail.fr>
2025-06-17 09:19:38 +0200

    ci(Feature): Override ToString for TabItemData
    Added `ToString` override in `TabItemData` to return the `Header` property, improving debugging and logging readability.

--- MyExpenses.Models/Ui/TabItemData.cs
+++ MyExpenses.Models/Ui/TabItemData.cs
@@ -3,5 +3,8 @@ namespace MyExpenses.Models.Ui;
 public class TabItemData
 {
     public required string Header { get; init; }
+
     public required object Content { get; init; }
+
+    public override string ToString() => Header;
 }
