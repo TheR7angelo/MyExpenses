5450489286cb262a4b10c573bb2400b059429a66
TheR7angelo <raphael.antunes@hotmail.fr>
2025-09-16 09:28:08 +0200

    feat(Settings): add "System" control with localization and configurable properties
    Added a new `SystemControl` user control in the Settings section,
    including XAML design, dependency properties, and methods for managing
    `MaxDaysLog` and `MaxBackupDatabase`. Integrated localization with
    resource files and auto-generated designer classes for multilingual
    support.

---
+++ MyExpenses.Wpf/UserControls/Settings/SystemControl.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using MaterialDesignThemes.Wpf;
+using MyExpenses.Models.Config.Interfaces;
+using MyExpenses.Utils;
+using MyExpenses.Utils.Strings;
+using MyExpenses.Wpf.Resources.Resx.UserControls.Settings.SystemControl;
+
+namespace MyExpenses.Wpf.UserControls.Settings;
+
+public partial class SystemControl
+{
+    public static readonly DependencyProperty MaxBackupDatabaseStringProperty =
+        DependencyProperty.Register(nameof(MaxBackupDatabaseString), typeof(string), typeof(SystemControl),
+            new PropertyMetadata(default(string)));
+
+    public string MaxBackupDatabaseString
+    {
+        get => (string)GetValue(MaxBackupDatabaseStringProperty);
+        set => SetValue(MaxBackupDatabaseStringProperty, value);
+    }
+
+    public static readonly DependencyProperty MaxBackupDatabaseProperty =
+        DependencyProperty.Register(nameof(MaxBackupDatabase), typeof(int), typeof(SystemControl),
+            new PropertyMetadata(0));
+
+    public int MaxBackupDatabase
+    {
+        get => (int)GetValue(MaxBackupDatabaseProperty);
+        set => SetValue(MaxBackupDatabaseProperty, value);
+    }
+
+    public static readonly DependencyProperty MaxDaysLogProperty = DependencyProperty.Register(nameof(MaxDaysLog),
+        typeof(int), typeof(SystemControl), new PropertyMetadata(0));
+
+    public int MaxDaysLog
+    {
+        get => (int)GetValue(MaxDaysLogProperty);
+        set => SetValue(MaxDaysLogProperty, value);
+    }
+
+    public static readonly DependencyProperty MaxDaysLogStringProperty = DependencyProperty.Register(nameof(MaxDaysLogString),
+        typeof(string), typeof(SystemControl), new PropertyMetadata(default(string)));
+
+    public string MaxDaysLogString
+    {
+        get => (string)GetValue(MaxDaysLogStringProperty);
+        set => SetValue(MaxDaysLogStringProperty, value);
+    }
+
+    public SystemControl()
+    {
+        MaxDaysLog = Config.Configuration.System.MaxDaysLog;
+        MaxBackupDatabase = Config.Configuration.System.MaxBackupDatabase;
+
+        UpdateLanguage();
+        InitializeComponent();
+
+        Interface.LanguageChanged += InterfaceOnLanguageChanged;
+    }
+
+    private void InterfaceOnLanguageChanged()
+        => UpdateLanguage();
+
+    private void UpdateLanguage()
+    {
+        MaxDaysLogString = SystemControlResources.MaxDaysLogString;
+        MaxBackupDatabaseString = SystemControlResources.MaxBackupDatabaseString;
+    }
+
+    private void UIElement_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+        var numericUpDown = (NumericUpDown)sender;
+        var txt = numericUpDown.Value + e.Text;
+
+        var canConvert = txt.ToDouble(out _);
+
+        e.Handled = !canConvert;
+    }
+}
