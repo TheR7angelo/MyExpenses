dfff1fe3bee42433e1784c1fae9aa4c8a34fc09e
TheR7angelo <raphael.antunes@hotmail.fr>
2026-04-19 14:32:50 +0200

    refactor(validation): extend `DomainValidationFailure` to support formatted error messages
    - Added `args` parameter to `DomainValidationFailure` for error message formatting.
    - Updated `WithState` builder to pass formatting arguments.
    - Enhanced `InternalMessage` property to leverage `string.Format` for localized and parameterized messages.

--- MyExpenses.Presentation/Validations/DomainValidationFailure.cs
+++ MyExpenses.Presentation/Validations/DomainValidationFailure.cs
@@ -11,18 +11,21 @@ public static class RuleExtensions
         this IRuleBuilderOptions<T, TProperty> rule,
         ErrorCode code,
         ResourceManager resourceManager,
-        string resourcePropertyName)
+        string resourcePropertyName,
+        params object?[] args)
     {
         return rule
             .WithErrorCode(((int)code).ToString())
-            .WithState(_ => new DomainValidationFailure(code, resourceManager, resourcePropertyName));
+            .WithState(_ => new DomainValidationFailure(code, resourceManager, resourcePropertyName, args));
     }
 }
 
-public class DomainValidationFailure(ErrorCode errorCode, ResourceManager resourceManager, string resourcePropertyName) : FluentValidation.Results.ValidationFailure
+public class DomainValidationFailure(ErrorCode errorCode, ResourceManager resourceManager, string resourcePropertyName, params object?[] args)
+    : FluentValidation.Results.ValidationFailure
 {
     public string ErrorCodeString => $"{(int)DomainCode}({DomainCode})";
-    public string? InternalMessage => ResourceManager.GetString(ResourcePropertyName, CultureInfo.InvariantCulture);
+    public string InternalMessage
+        => string.Format(CultureInfo.CurrentCulture, ResourceManager.GetString(ResourcePropertyName, CultureInfo.CurrentCulture) ?? string.Empty, args);
 
     private ErrorCode DomainCode { get; set; } = errorCode;
     private ResourceManager ResourceManager { get; set; } = resourceManager;
