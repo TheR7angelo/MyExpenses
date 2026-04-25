e661a2f066bee2070e282cf5bce9b738514665d2
TheR7angelo <raphael.antunes@hotmail.fr>
2026-04-25 20:16:00 +0200

    ci(validation): update `CategoryTypeViewModelValidator` to use localized error messages
    - Replaced hardcoded validation messages with localized resources in `ExpenseManagementResources`.
    - Fixed logic for category type name uniqueness check.
    - Enhanced error handling using custom error codes.

--- MyExpenses.Presentation/Validations/CategoryTypeViewModelValidator.cs
+++ MyExpenses.Presentation/Validations/CategoryTypeViewModelValidator.cs
@@ -13,19 +13,19 @@ public class CategoryTypeViewModelValidator : AbstractValidator<CategoryTypeView
     {
         RuleFor(x => x.Name)
             .Cascade(CascadeMode.Stop)
-            .NotEmpty().WithMessage("Name is required")
+            .NotEmpty().WithMessage(ExpenseManagementResources.CategoryTypeViewModelNameRequired)
             .WithError(ErrorCode.NameRequired, ExpenseManagementResources.ResourceManager, nameof(ExpenseManagementResources.CategoryTypeViewModelNameRequired))
 
             .Length(1, CategoryTypeDomain.MaxNameLength)
-            .WithMessage($"Name must be between 1 and {CategoryTypeDomain.MaxNameLength} characters")
+            .WithMessage(string.Format(ExpenseManagementResources.CategoryTypeViewModelNameTooLong, CategoryTypeDomain.MaxNameLength))
             .WithError(ErrorCode.NameTooLong, ExpenseManagementResources.ResourceManager, nameof(ExpenseManagementResources.CategoryTypeViewModelNameTooLong), CategoryTypeDomain.MaxNameLength)
 
-            .MustAsync(async (name, cancellation) => !await expensePresentationValidationService.IsCategoryTypeNameAvailableAsync(name, cancellation))
-            .WithMessage("Category type name must be unique")
+            .MustAsync(async (name, cancellation) => await expensePresentationValidationService.IsCategoryTypeNameAvailableAsync(name, cancellation))
+            .WithMessage(ExpenseManagementResources.CategoryTypeViewModelNameAlreadyExists)
             .WithError(ErrorCode.NameAlreadyExists, ExpenseManagementResources.ResourceManager, nameof(ExpenseManagementResources.CategoryTypeViewModelNameAlreadyExists));
 
         RuleFor(x => x.Color)
-            .NotEmpty().WithMessage("Color is required")
+            .NotEmpty().WithMessage(ExpenseManagementResources.CategoryTypeViewModelColorRequired)
             .WithError(ErrorCode.ColorRequired, ExpenseManagementResources.ResourceManager, nameof(ExpenseManagementResources.CategoryTypeViewModelColorRequired));
     }
 }
