8b2244398fe5402ae2e1586cdb95e3e2ff6954bb
TheR7angelo <raphael.antunes@hotmail.fr>
2026-04-25 18:18:47 +0200

    refactor(validation): remove unused `UniqueWithCodeAttribute` validation attribute

--- MyExpenses.Presentation/Validations/Attributes/UniqueWithCodeAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-using Domain.Models.Validation;
-
-namespace MyExpenses.Presentation.Validations.Attributes;
-
-public class UniqueWithCodeAttribute(ErrorCode errorCode) : ValidationAttribute
-{
-
-}
