ce255bf9e772b0cc718eb9c41925eba921fa476f
TheR7angelo <raphael.antunes@hotmail.fr>
2026-05-13 22:19:51 +0200

    refactor: add `StringComparison` parameter to `AddAndSort` methods for improved sorting flexibility
    - Updated `AddAndSort` and `AddRangeAndSort` to accept a `StringComparison` parameter (default: `OrdinalIgnoreCase`).
    - Improved sorting logic by using the provided comparison type.

--- MyExpenses.SharedUtils/Collection/CollectionExtensions.cs
+++ MyExpenses.SharedUtils/Collection/CollectionExtensions.cs
@@ -69,13 +69,14 @@ public static class CollectionExtensions
     /// <param name="collection">The ObservableCollection<T/> to add the items to.</param>
     /// <param name="items">The collection of items to add.</param>
     /// <param name="keySelector">The key selector function used to determine the sorting order.</param>
+    /// <param name="comparisonType">The type of comparison to use when sorting the collection.</param>
     /// <typeparam name="T">The type of the elements in the ObservableCollection.</typeparam>
     public static void AddRangeAndSort<T>(this ObservableCollection<T> collection, IEnumerable<T> items,
-        Func<T, string> keySelector)
+        Func<T, string> keySelector, StringComparison comparisonType = StringComparison.OrdinalIgnoreCase)
     {
         foreach (var item in items)
         {
-            collection.AddAndSort(item, keySelector);
+            collection.AddAndSort(item, keySelector, comparisonType);
         }
     }
 
@@ -101,13 +102,14 @@ public static class CollectionExtensions
     /// <param name="oldItem">The old item to be removed from the collection.</param>
     /// <param name="newItem">The item to be added to the collection.</param>
     /// <param name="keySelector">A function that selects a key for sorting the collection.</param>
+    /// <param name="comparisonType">The type of comparison to use when sorting the collection.</param>
     /// <exception cref="ArgumentNullException">Thrown when the collection is null.</exception>
     public static void AddAndSort<T>(this ObservableCollection<T> collection, T oldItem, T newItem,
-        Func<T, string> keySelector)
+        Func<T, string> keySelector, StringComparison comparisonType = StringComparison.OrdinalIgnoreCase)
     {
         ArgumentNullException.ThrowIfNull(collection);
         collection.Remove(oldItem);
-        collection.AddAndSort(newItem, keySelector);
+        collection.AddAndSort(newItem, keySelector, comparisonType);
     }
 
     /// <summary>
@@ -117,14 +119,15 @@ public static class CollectionExtensions
     /// <param name="collection">The ObservableCollection to add the item to.</param>
     /// <param name="item">The item to add.</param>
     /// <param name="keySelector">A function to extract the sort key from each element.</param>
+    /// <param name="comparisonType">The type of comparison to use when sorting the collection.</param>
     /// <exception cref="ArgumentNullException">Thrown when the collection is null.</exception>
     // ReSharper disable once HeapView.ClosureAllocation
-    public static void AddAndSort<T>(this ObservableCollection<T> collection, T item, Func<T, string> keySelector)
+    public static void AddAndSort<T>(this ObservableCollection<T> collection, T item, Func<T, string> keySelector, StringComparison comparisonType = StringComparison.OrdinalIgnoreCase)
     {
         ArgumentNullException.ThrowIfNull(collection);
 
         var index = collection
-            .TakeWhile(x => string.Compare(keySelector(x), keySelector(item), StringComparison.OrdinalIgnoreCase) < 0)
+            .TakeWhile(x => string.Compare(keySelector(x), keySelector(item), comparisonType) < 0)
             .Count();
 
         collection.Insert(index, item);
