7d54d576fa107717f9ba6749863a38afe707df83
TheR7angelo <raphael.antunes@hotmail.fr>
2026-05-14 11:09:40 +0200

    refactor: update `AddEditCategoryTypeWindow` to improve localization and MVVM adherence
    - Renamed `EditCategoryType` to `IsEditCategoryType` for better clarity and consistency with MVVM conventions.
    - Introduced `ISystemPresentationService` and `IExpensePresentationService` for managing dependencies.
    - Replaced hardcoded title setting with localized resources using `{localisations:Tr}` binding.
    - Updated button visibility binding and removed obsolete commented-out code.
    - Enhanced color handling with `ColorViewModels` retrieved asynchronously from services.

--- MyExpenses.Wpf/Windows/CategoryTypeManagementWindow/AddEditCategoryTypeWindow.xaml.cs
+++ MyExpenses.Wpf/Windows/CategoryTypeManagementWindow/AddEditCategoryTypeWindow.xaml.cs
@@ -3,9 +3,11 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Microsoft.Data.Sqlite;
-using MyExpenses.Models.Config.Interfaces;
 using MyExpenses.Models.Sql.Bases.Tables;
 using MyExpenses.Presentation.Enums;
+using MyExpenses.Presentation.Services.Interfaces;
+using MyExpenses.Presentation.ViewModels.Expenses;
+using MyExpenses.Presentation.ViewModels.Systems;
 using MyExpenses.SharedUtils.Collection;
 using MyExpenses.SharedUtils.Properties;
 using MyExpenses.SharedUtils.Resources.Resx.CategoryTypesManagement;
@@ -20,25 +22,30 @@ namespace MyExpenses.Wpf.Windows.CategoryTypeManagementWindow;
 
 public partial class AddEditCategoryTypeWindow
 {
+    private readonly ISystemPresentationService _systemPresentationService;
+    private readonly IExpensePresentationService _expensePresentationService;
+
     // ReSharper disable once HeapView.BoxingAllocation
     // ReSharper disable once HeapView.ObjectAllocation.Evident
-    public static readonly DependencyProperty EditCategoryTypeProperty =
-        DependencyProperty.Register(nameof(EditCategoryType), typeof(bool), typeof(AddEditCategoryTypeWindow),
+    public static readonly DependencyProperty IsEditCategoryTypeProperty =
+        DependencyProperty.Register(nameof(IsEditCategoryType), typeof(bool), typeof(AddEditCategoryTypeWindow),
             new PropertyMetadata(false));
 
     // ReSharper disable once HeapView.BoxingAllocation
-    public bool EditCategoryType
+    public bool IsEditCategoryType
     {
-        get => (bool)GetValue(EditCategoryTypeProperty);
-        set => SetValue(EditCategoryTypeProperty, value);
+        get => (bool)GetValue(IsEditCategoryTypeProperty);
+        set => SetValue(IsEditCategoryTypeProperty, value);
     }
 
     #region Property
 
     // ReSharper disable once HeapView.ObjectAllocation.Evident
     public TCategoryType CategoryType { get; } = new();
-    public ObservableCollection<TColor> Colors { get; }
-    private List<TCategoryType> CategoryTypes { get; }
+
+    public CategoryTypeViewModel CategoryTypeViewModel { get; } = new();
+
+    public ObservableCollection<ColorViewModel> ColorViewModels { get; } = [];
 
     public bool CategoryTypeDeleted { get; private set; }
 
@@ -115,20 +122,36 @@ public partial class AddEditCategoryTypeWindow
 
     public string ComboBoxColorSelectedValuePath { get; } = nameof(TColor.Id);
 
-    public AddEditCategoryTypeWindow()
+    public AddEditCategoryTypeWindow(ISystemPresentationService systemPresentationService,
+        IExpensePresentationService expensePresentationService)
     {
-        // ReSharper disable once HeapView.ObjectAllocation.Evident
-        // Necessary instantiation of DataBaseContext to interact with the database.
-        // This creates a scoped database context for performing queries and modifications in the database.
-        using var context = new DataBaseContextOld();
-        CategoryTypes = [..context.TCategoryTypes];
-        Colors = [..context.TColors.OrderBy(s => s.Name)];
+        _systemPresentationService = systemPresentationService;
+        _expensePresentationService = expensePresentationService;
 
-        UpdateLanguage();
+        // // ReSharper disable once HeapView.ObjectAllocation.Evident
+        // // Necessary instantiation of DataBaseContext to interact with the database.
+        // // This creates a scoped database context for performing queries and modifications in the database.
+        // using var context = new DataBaseContextOld();
+        // CategoryTypes = [..context.TCategoryTypes];
+        // Colors = [..context.TColors.OrderBy(s => s.Name)];
+
         InitializeComponent();
 
-        // ReSharper disable once HeapView.DelegateAllocation
-        Interface.LanguageChanged += Interface_OnLanguageChanged;
+        Loaded += OnLoaded;
+    }
+
+    private async void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            var colors = await _systemPresentationService.GetAllColorViewModelAsync();
+            ColorViewModels.AddRangeAndSort(colors, s => s.Name!);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            throw;
+        }
     }
 
     private void UpdateLanguage()
@@ -205,30 +228,34 @@ public partial class AddEditCategoryTypeWindow
 
     private void ButtonValid_OnClick(object sender, RoutedEventArgs e)
     {
-        var categoryTypeName = CategoryType.Name;
-        if (string.IsNullOrWhiteSpace(categoryTypeName))
-        {
-            Dialogs.MsgBox.MsgBox.Show(CategoryTypesManagementResources.MessageBoxValidateCategoryTypeErrorEmptyMessage,
-                MsgBoxImage.Error);
-            return;
-        }
+        // TODO check valid
 
-        if (CheckCategoryTypeName(categoryTypeName))
-        {
-            ShowErrorMessage();
-            return;
-        }
+        // _systemPresentationService.CreateCategoryTypeViewModel()
 
-        // TODO correct
-        // if (CategoryType.ColorFk is null)
+        // var categoryTypeName = CategoryType.Name;
+        // if (string.IsNullOrWhiteSpace(categoryTypeName))
         // {
-        //     MsgBox.MsgBox.Show(CategoryTypesManagementResources.MessageBoxValidateColorErrorEmptyMessage,
+        //     Dialogs.MsgBox.MsgBox.Show(CategoryTypesManagementResources.MessageBoxValidateCategoryTypeErrorEmptyMessage,
         //         MsgBoxImage.Error);
         //     return;
         // }
-
-        DialogResult = true;
-        Close();
+        //
+        // if (CheckCategoryTypeName(categoryTypeName))
+        // {
+        //     ShowErrorMessage();
+        //     return;
+        // }
+        //
+        // // TODO correct
+        // // if (CategoryType.ColorFk is null)
+        // // {
+        // //     MsgBox.MsgBox.Show(CategoryTypesManagementResources.MessageBoxValidateColorErrorEmptyMessage,
+        // //         MsgBoxImage.Error);
+        // //     return;
+        // // }
+        //
+        // DialogResult = true;
+        // Close();
     }
 
     private void Interface_OnLanguageChanged()
@@ -241,25 +268,25 @@ public partial class AddEditCategoryTypeWindow
         var categoryTypeName = textBox.Text;
         if (string.IsNullOrEmpty(categoryTypeName)) return;
 
-        var alreadyExist = CheckCategoryTypeName(categoryTypeName);
-        if (alreadyExist) ShowErrorMessage();
+        // var alreadyExist = CheckCategoryTypeName(categoryTypeName);
+        // if (alreadyExist) ShowErrorMessage();
     }
 
     #endregion
 
     #region Function
 
-    private bool CheckCategoryTypeName(string accountName)
-        => CategoryTypes.Select(s => s.Name).Contains(accountName);
+    // private bool CheckCategoryTypeName(string accountName)
+        // => CategoryTypes.Select(s => s.Name).Contains(accountName);
 
     // ReSharper disable once HeapView.ClosureAllocation
     public void SetTCategoryType(TCategoryType categoryType)
     {
         categoryType.CopyPropertiesTo(CategoryType);
-        EditCategoryType = true;
+        IsEditCategoryType = true;
 
         // ReSharper disable once HeapView.DelegateAllocation
-        CategoryTypes.Remove(CategoryTypes.Find(s => s.Id.Equals(categoryType.Id))!);
+        // CategoryTypes.Remove(CategoryTypes.Find(s => s.Id.Equals(categoryType.Id))!);
     }
 
     private static void ShowErrorMessage()
@@ -285,8 +312,8 @@ public partial class AddEditCategoryTypeWindow
         if (addEditColorWindow.DeleteColor)
         {
             // ReSharper disable once HeapView.DelegateAllocation
-            var colorDeleted = Colors.FirstOrDefault(s => s.Id == CategoryType.ColorFk);
-            if (colorDeleted is not null) Colors.Remove(colorDeleted);
+            // var colorDeleted = Colors.FirstOrDefault(s => s.Id == CategoryType.ColorFk);
+            // if (colorDeleted is not null) Colors.Remove(colorDeleted);
 
             return;
         }
@@ -303,8 +330,8 @@ public partial class AddEditCategoryTypeWindow
             Log.Information("{Json}", json);
 
             // ReSharper disable once HeapView.DelegateAllocation
-            var oldColor = Colors.First(s => s.Id.Equals(editedColor.Id));
-            editedColor.CopyPropertiesTo(oldColor);
+            // var oldColor = Colors.First(s => s.Id.Equals(editedColor.Id));
+            // editedColor.CopyPropertiesTo(oldColor);
 
             Dialogs.MsgBox.MsgBox.Show(ColorManagementResources.MessageBoxEditColorSuccessTitle,
                 ColorManagementResources.MessageBoxEditColorSuccessMessage, MsgBoxImage.Check);
@@ -340,7 +367,7 @@ public partial class AddEditCategoryTypeWindow
 
             Dialogs.MsgBox.MsgBox.Show(ColorManagementResources.MessageBoxAddColorSuccessMessage, MsgBoxImage.Check);
 
-            Colors.AddAndSort(newColor, s => s.Name!);
+            // Colors.AddAndSort(newColor, s => s.Name!);
         }
         else
         {
