aa1317390f7fce7c81762a624b097e7c3a4e8015
TheR7angelo <raphael.antunes@hotmail.fr>
2026-05-14 11:43:29 +0200

    refactor: rename resource manager dependency to improve naming consistency
    - Updated `RessourceManagerNameDependencyRessources` to `RessourceManagerNameExpenseRessources` in `ExpenseResourceManager`.
    - Adjusted resource string definitions to reflect the new naming convention for improved clarity.

--- MyExpenses.Presentation/Resources/Resx/ExpenseResources/ExpenseResourceManager.cs
+++ MyExpenses.Presentation/Resources/Resx/ExpenseResources/ExpenseResourceManager.cs
@@ -2,26 +2,26 @@ namespace MyExpenses.Presentation.Resources.Resx.ExpenseResources;
 
 public class ExpenseResourceManager
 {
-    private static string RessourceManagerNameDependencyRessources => nameof(ExpenseResources);
+    private static string RessourceManagerNameExpenseRessources => nameof(ExpenseResources);
 
-    public static string ComboBoxFromAccountHintAssist { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ComboBoxFromAccountHintAssist)}";
-    public static string ComboBoxToAccountHintAssist { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ComboBoxToAccountHintAssist)}";
-    public static string ComboBoxCategoryTypeHintAssist { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ComboBoxCategoryTypeHintAssist)}";
-    public static string ComboBoxModePaymentHintAssist { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ComboBoxModePaymentHintAssist)}";
-    public static string DatePickerWhenHintAssist { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.DatePickerWhenHintAssist)}";
-    public static string TextBoxValueHintAssist { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.TextBoxValueHintAssist)}";
-    public static string TextBoxMainReasonHintAssist { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.TextBoxMainReasonHintAssist)}";
-    public static string TextBoxAdditionalReasonHintAssist { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.TextBoxAdditionalReasonHintAssist)}";
-    public static string ButtonPrepareValidContent { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ButtonPrepareValidContent)}";
-    public static string ButtonPrepareCancelContent { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ButtonPrepareCancelContent)}";
-    public static string ButtonPreviewValidContent { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ButtonPreviewValidContent)}";
-    public static string ButtonPreviewCancelContent { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ButtonPreviewCancelContent)}";
+    public static string ComboBoxFromAccountHintAssist { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ComboBoxFromAccountHintAssist)}";
+    public static string ComboBoxToAccountHintAssist { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ComboBoxToAccountHintAssist)}";
+    public static string ComboBoxCategoryTypeHintAssist { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ComboBoxCategoryTypeHintAssist)}";
+    public static string ComboBoxModePaymentHintAssist { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ComboBoxModePaymentHintAssist)}";
+    public static string DatePickerWhenHintAssist { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.DatePickerWhenHintAssist)}";
+    public static string TextBoxValueHintAssist { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.TextBoxValueHintAssist)}";
+    public static string TextBoxMainReasonHintAssist { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.TextBoxMainReasonHintAssist)}";
+    public static string TextBoxAdditionalReasonHintAssist { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.TextBoxAdditionalReasonHintAssist)}";
+    public static string ButtonPrepareValidContent { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ButtonPrepareValidContent)}";
+    public static string ButtonPrepareCancelContent { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ButtonPrepareCancelContent)}";
+    public static string ButtonPreviewValidContent { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ButtonPreviewValidContent)}";
+    public static string ButtonPreviewCancelContent { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ButtonPreviewCancelContent)}";
 
-    public static string TitleWindowAddCategoryTypeName { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.TitleWindowAddCategoryTypeName)}";
-    public static string TitleWindowEditCategoryTypeName { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.TitleWindowEditCategoryTypeName)}";
-    public static string TextBoxCategoryTypeNameHintAssist { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.TextBoxCategoryTypeNameHintAssist)}";
-    public static string ComboBoxColorValueHintAssist { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ComboBoxColorValueHintAssist)}";
-    public static string ButtonValidContentCategoryType { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ButtonValidContentCategoryType)}";
-    public static string ButtonCancelContentCategoryType { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ButtonCancelContentCategoryType)}";
-    public static string ButtonDeleteContentCategoryType { get; } = $"{RessourceManagerNameDependencyRessources}:{nameof(ExpenseResources.ButtonDeleteContentCategoryType)}";
+    public static string TitleWindowAddCategoryTypeName { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.TitleWindowAddCategoryTypeName)}";
+    public static string TitleWindowEditCategoryTypeName { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.TitleWindowEditCategoryTypeName)}";
+    public static string TextBoxCategoryTypeNameHintAssist { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.TextBoxCategoryTypeNameHintAssist)}";
+    public static string ComboBoxColorValueHintAssist { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ComboBoxColorValueHintAssist)}";
+    public static string ButtonValidContentCategoryType { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ButtonValidContentCategoryType)}";
+    public static string ButtonCancelContentCategoryType { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ButtonCancelContentCategoryType)}";
+    public static string ButtonDeleteContentCategoryType { get; } = $"{RessourceManagerNameExpenseRessources}:{nameof(ExpenseResources.ButtonDeleteContentCategoryType)}";
 }
