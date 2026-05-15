d7d685e2745048207aac73826040f6cbf14433be
TheR7angelo <raphael.antunes@hotmail.fr>
2026-05-15 14:16:59 +0200

    refactor(color-management): comment out unused logic in ColorManagementPage and AddEditColorWindow
    - Temporarily disabled unused code in `ButtonAddColor_OnClick`, `ButtonEditColor_OnClick`, and associated methods.
    - Added TODO placeholders for future corrections and improvements.
    - Updated XML documentation in `AddEditColorWindow` for better maintainability.

--- MyExpenses.Wpf/Pages/ColorManagementPage.xaml.cs
+++ MyExpenses.Wpf/Pages/ColorManagementPage.xaml.cs
@@ -36,92 +36,95 @@ public partial class ColorManagementPage
 
     private void ButtonAddColor_OnClick(object sender, RoutedEventArgs e)
     {
-        // ReSharper disable once HeapView.ObjectAllocation.Evident
-        // The instance of AddEditColorWindow is created locally within this method and is used temporarily.
-        // Since there are no references to it after this scope and the Garbage Collector will handle
-        // its cleanup efficiently, this allocation is intentional and does not require further optimization.
-        var addEditColorWindow = App.ServiceProvider.GetRequiredService<AddEditColorWindow>();
-        addEditColorWindow.ShowDialog();
+        // TODO correct
 
-        if (addEditColorWindow.DialogResult is not true) return;
-
-        var newColor = addEditColorWindow.Color;
-
-        Log.Information("Attempt to inject the new color \"{ColorName}\" with hexadecimal code \"{ColorHexadecimalColorCode}\"",
-            newColor.Name, newColor.HexadecimalColorCode);
-
-        var (success, exception) = newColor.AddOrEdit();
-        if (success)
-        {
-            Log.Information("color was successfully added");
-            var json = newColor.ToJsonString();
-            Log.Information("{Json}", json);
-
-            MsgBox.Show(ColorManagementResources.MessageBoxAddColorSuccessTitle,
-                ColorManagementResources.MessageBoxAddColorSuccessMessage, MsgBoxImage.Check);
-
-            Colors.AddAndSort(newColor, s => s.Name!);
-        }
-        else
-        {
-            Log.Error(exception, "An error occurred please retry");
-            MsgBox.Show(ColorManagementResources.MessageBoxAddColorErrorTitle,
-                ColorManagementResources.MessageBoxAddColorErrorMessage, MsgBoxImage.Error);
-        }
+        // // ReSharper disable once HeapView.ObjectAllocation.Evident
+        // // The instance of AddEditColorWindow is created locally within this method and is used temporarily.
+        // // Since there are no references to it after this scope and the Garbage Collector will handle
+        // // its cleanup efficiently, this allocation is intentional and does not require further optimization.
+        // var addEditColorWindow = App.ServiceProvider.GetRequiredService<AddEditColorWindow>();
+        // addEditColorWindow.ShowDialog();
+        //
+        // if (addEditColorWindow.DialogResult is not true) return;
+        //
+        // var newColor = addEditColorWindow.Color;
+        //
+        // Log.Information("Attempt to inject the new color \"{ColorName}\" with hexadecimal code \"{ColorHexadecimalColorCode}\"",
+        //     newColor.Name, newColor.HexadecimalColorCode);
+        //
+        // var (success, exception) = newColor.AddOrEdit();
+        // if (success)
+        // {
+        //     Log.Information("color was successfully added");
+        //     var json = newColor.ToJsonString();
+        //     Log.Information("{Json}", json);
+        //
+        //     MsgBox.Show(ColorManagementResources.MessageBoxAddColorSuccessTitle,
+        //         ColorManagementResources.MessageBoxAddColorSuccessMessage, MsgBoxImage.Check);
+        //
+        //     Colors.AddAndSort(newColor, s => s.Name!);
+        // }
+        // else
+        // {
+        //     Log.Error(exception, "An error occurred please retry");
+        //     MsgBox.Show(ColorManagementResources.MessageBoxAddColorErrorTitle,
+        //         ColorManagementResources.MessageBoxAddColorErrorMessage, MsgBoxImage.Error);
+        // }
     }
 
     private void ButtonEditColor_OnClick(object sender, RoutedEventArgs e)
     {
-        if (sender is not Button button) return;
-
-        // ReSharper disable once HeapView.ClosureAllocation
-        if (button.DataContext is not TColor colorToEdit) return;
-
-        var c = new ColorViewModel
-        {
-            Id = colorToEdit.Id,
-            Name = colorToEdit.Name,
-            HexadecimalColorCode = colorToEdit.HexadecimalColorCode,
-            DateAdded = colorToEdit.DateAdded
-        };
-
-        var addEditColorWindow = App.ServiceProvider.GetRequiredService<AddEditColorWindow>();
-        addEditColorWindow.LoadColorViewModel(c);
-        addEditColorWindow.ShowDialog();
-
-        if (addEditColorWindow.DialogResult is not true) return;
-        if (addEditColorWindow.DeleteColor)
-        {
-            // ReSharper disable once HeapView.DelegateAllocation
-            var colorDeleted = Colors.FirstOrDefault(s => s.Id.Equals(colorToEdit.Id));
-            if (colorDeleted is not null) Colors.Remove(colorDeleted);
-
-            return;
-        }
-
-        var editedColor = addEditColorWindow.Color;
-
-        Log.Information("Attempting to edit the color \"{AccountName}\"", editedColor.Name);
-        var (success, exception) = editedColor.AddOrEdit();
-        if (success)
-        {
-            Log.Information("Color was successfully edited");
-            var json = editedColor.ToJsonString();
-            Log.Information("{Json}", json);
-
-            // ReSharper disable once HeapView.DelegateAllocation
-            var oldColor = Colors.First(s => s.Id.Equals(editedColor.Id));
-            editedColor.CopyPropertiesTo(oldColor);
-
-            MsgBox.Show(ColorManagementResources.MessageBoxEditColorSuccessTitle,
-                ColorManagementResources.MessageBoxEditColorSuccessMessage, MsgBoxImage.Check);
-        }
-        else
-        {
-            Log.Error(exception, "An error occurred please retry");
-            MsgBox.Show(ColorManagementResources.MessageBoxEditColorErrorTitle,
-                ColorManagementResources.MessageBoxEditColorErrorMessage, MsgBoxImage.Warning);
-        }
+        // TODO correct
+        // if (sender is not Button button) return;
+        //
+        // // ReSharper disable once HeapView.ClosureAllocation
+        // if (button.DataContext is not TColor colorToEdit) return;
+        //
+        // var c = new ColorViewModel
+        // {
+        //     Id = colorToEdit.Id,
+        //     Name = colorToEdit.Name,
+        //     HexadecimalColorCode = colorToEdit.HexadecimalColorCode,
+        //     DateAdded = colorToEdit.DateAdded
+        // };
+        //
+        // var addEditColorWindow = App.ServiceProvider.GetRequiredService<AddEditColorWindow>();
+        // addEditColorWindow.LoadColorViewModel(c);
+        // addEditColorWindow.ShowDialog();
+        //
+        // if (addEditColorWindow.DialogResult is not true) return;
+        // if (addEditColorWindow.DeleteColor)
+        // {
+        //     // ReSharper disable once HeapView.DelegateAllocation
+        //     var colorDeleted = Colors.FirstOrDefault(s => s.Id.Equals(colorToEdit.Id));
+        //     if (colorDeleted is not null) Colors.Remove(colorDeleted);
+        //
+        //     return;
+        // }
+        //
+        // var editedColor = addEditColorWindow.Color;
+        //
+        // Log.Information("Attempting to edit the color \"{AccountName}\"", editedColor.Name);
+        // var (success, exception) = editedColor.AddOrEdit();
+        // if (success)
+        // {
+        //     Log.Information("Color was successfully edited");
+        //     var json = editedColor.ToJsonString();
+        //     Log.Information("{Json}", json);
+        //
+        //     // ReSharper disable once HeapView.DelegateAllocation
+        //     var oldColor = Colors.First(s => s.Id.Equals(editedColor.Id));
+        //     editedColor.CopyPropertiesTo(oldColor);
+        //
+        //     MsgBox.Show(ColorManagementResources.MessageBoxEditColorSuccessTitle,
+        //         ColorManagementResources.MessageBoxEditColorSuccessMessage, MsgBoxImage.Check);
+        // }
+        // else
+        // {
+        //     Log.Error(exception, "An error occurred please retry");
+        //     MsgBox.Show(ColorManagementResources.MessageBoxEditColorErrorTitle,
+        //         ColorManagementResources.MessageBoxEditColorErrorMessage, MsgBoxImage.Warning);
+        // }
     }
 
     // private void Colors_OnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
--- MyExpenses.Wpf/Windows/AddEditColorWindow.xaml.cs
+++ MyExpenses.Wpf/Windows/AddEditColorWindow.xaml.cs
@@ -1,21 +1,35 @@
-﻿using System.Windows;
-using System.Windows.Input;
-using MyExpenses.Application.Interfaces;
-using MyExpenses.Models.Sql.Bases.Tables;
+﻿using MyExpenses.Application.Interfaces;
 using MyExpenses.Presentation.ViewModel;
 using MyExpenses.Presentation.ViewModels.Systems;
 
 namespace MyExpenses.Wpf.Windows;
 
+/// <summary>
+/// Represents a window that facilitates the addition or modification of color configurations
+/// in the application. This window interacts with the color management workflow by binding
+/// to the <see cref="ColorManagementViewModel"/>.
+/// </summary>
+/// <remarks>
+/// The <see cref="AddEditColorWindow"/> is designed for managing the UI logic
+/// for creating or editing colors, leveraging the <see cref="ColorManagementViewModel"/>
+/// for the underlying data operations and workflows. It implements the <see cref="IClosable"/>
+/// interface, allowing it to interact seamlessly with commands that require closing or returning
+/// dialog results.
+/// </remarks>
 public partial class AddEditColorWindow : IClosable
 {
-    // ReSharper disable once HeapView.ObjectAllocation.Evident
-    public TColor Color { get; } = new();
-
-    public bool DeleteColor { get; private set; }
-
+    /// <summary>
+    /// Represents the ViewModel instance used for managing color configurations in the
+    /// AddEditColorWindow. This field provides data-binding to the window and facilitates
+    /// operations such as loading, editing, validating, and deleting color-related data.
+    /// </summary>
     private readonly ColorManagementViewModel _colorManagementViewModel;
 
+    /// <summary>
+    /// Represents a window for adding or editing color configurations within the application.
+    /// Provides the user interface layer to interact with the color management process, including
+    /// binding to the associated ViewModel to enable data manipulation and workflow execution.
+    /// </summary>
     public AddEditColorWindow(ColorManagementViewModel vm)
     {
         _colorManagementViewModel = vm;
@@ -24,102 +38,6 @@ public partial class AddEditColorWindow : IClosable
 
         DataContext = _colorManagementViewModel;
     }
-
-    #region Action
-
-    private void ButtonValid_OnClick(object sender, RoutedEventArgs e)
-    {
-        // if (string.IsNullOrWhiteSpace(Color.Name))
-        // {
-        //     Dialogs.MsgBox.MsgBox.Show(ColorManagementResources.MessageBoxCannotAddEmptyColorNameErrorTitle,
-        //         ColorManagementResources.MessageBoxCannotAddEmptyColorNameErrorMessage, MsgBoxImage.Error);
-        //     return;
-        // }
-        //
-        // if (string.IsNullOrWhiteSpace(Color.HexadecimalColorCode))
-        // {
-        //     Dialogs.MsgBox.MsgBox.Show(ColorManagementResources.MessageBoxCannotAddEmptyColorHexErrorTitle,
-        //         ColorManagementResources.MessageBoxCannotAddEmptyColorHexErrorMessage, MsgBoxImage.Error);
-        //     return;
-        // }
-        //
-        // var nameAlreadyExist = CheckColorName(Color.Name);
-        // if (nameAlreadyExist)
-        // {
-        //     ShowErrorMessageDuplicateName();
-        //     return;
-        // }
-        //
-        // // ReSharper disable once HeapView.DelegateAllocation
-        // var colorAlreadyExist = Colors.FirstOrDefault(s => s.HexadecimalColorCode == Color.HexadecimalColorCode);
-        // if (colorAlreadyExist is not null)
-        // {
-        //     var message = string.Format(ColorManagementResources.MessageBoxCannotAddDuplicateColorHexErrorMessage,
-        //         colorAlreadyExist.Name);
-        //     Dialogs.MsgBox.MsgBox.Show(ColorManagementResources.MessageBoxCannotAddDuplicateColorHexErrorTitle,
-        //         message, MsgBoxImage.Error);
-        //     return;
-        // }
-        //
-        // DialogResult = true;
-        // Close();
-    }
-
-    private void ButtonDelete_OnClick(object sender, RoutedEventArgs e)
-    {
-        // var message = string.Format(ColorManagementResources.MessageBoxDeleteColorQuestionMessage, Color.Name);
-        // var response = Dialogs.MsgBox.MsgBox.Show(ColorManagementResources.MessageBoxDeleteColorQuestionTitle, message,
-        //         MessageBoxButton.YesNoCancel, MsgBoxImage.Question);
-        // if (response is not MessageBoxResult.Yes) return;
-        //
-        // Log.Information("Attempting to remove the color \"{ColorToDeleteName}\"", Color.Name);
-        // var (success, exception) = Color.Delete();
-        //
-        // if (success)
-        // {
-        //     Log.Information("Color was successfully removed");
-        //     Dialogs.MsgBox.MsgBox.Show(ColorManagementResources.MessageBoxDeleteColorNoUseSuccessTitle,
-        //         ColorManagementResources.MessageBoxDeleteColorNoUseSuccessMessage, MsgBoxImage.Check);
-        //
-        //     DeleteColor = true;
-        //     DialogResult = true;
-        //     Close();
-        //     return;
-        // }
-        //
-        // if (exception!.InnerException is SqliteException
-        //     {
-        //         SqliteExtendedErrorCode: SQLitePCL.raw.SQLITE_CONSTRAINT_FOREIGNKEY
-        //     })
-        // {
-        //     Log.Error("Foreign key constraint violation");
-        //
-        //     response = Dialogs.MsgBox.MsgBox.Show(ColorManagementResources.MessageBoxDeleteColorUseQuestionTitle,
-        //         ColorManagementResources.MessageBoxDeleteColorUseQuestionMessage,
-        //         MessageBoxButton.YesNoCancel, MsgBoxImage.Question);
-        //
-        //     if (response is not MessageBoxResult.Yes) return;
-        //
-        //     Log.Information("Attempting to remove the color \"{ColorToDeleteName}\" with all relative element",
-        //         Color.Name);
-        //     Color.Delete(true);
-        //     Log.Information("Account and all relative element was successfully removed");
-        //     Dialogs.MsgBox.MsgBox.Show(ColorManagementResources.MessageBoxDeleteColorUseSuccessTitle,
-        //         ColorManagementResources.MessageBoxDeleteColorUseSuccessMessage, MsgBoxImage.Check);
-        //
-        //     DeleteColor = true;
-        //     DialogResult = true;
-        //     Close();
-        //
-        //     return;
-        // }
-        //
-        // Log.Error(exception, "An error occurred please retry");
-        // Dialogs.MsgBox.MsgBox.Show(ColorManagementResources.MessageBoxDeleteColorErrorTitle,
-        //     ColorManagementResources.MessageBoxDeleteColorErrorMessage, MsgBoxImage.Error);
-    }
-
-    #endregion
 
     /// <summary>
     /// Loads the provided <paramref name="colorViewModel"/> into the current color management context.
