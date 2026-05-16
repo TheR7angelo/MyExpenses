318762cf074c70c1eb0f6876c39d2d33553d3a29
TheR7angelo <raphael.antunes@hotmail.fr>
2026-05-16 16:45:08 +0200

    refactor(category-type): migrate `CategoryTypeViewModel` to inherit from `BaseViewModel` and clean up validation logic

--- MyExpenses.Presentation/ViewModels/Expenses/CategoryTypeViewModel.cs
+++ MyExpenses.Presentation/ViewModels/Expenses/CategoryTypeViewModel.cs
@@ -1,36 +1,29 @@
 using CommunityToolkit.Mvvm.ComponentModel;
-using Domain.Models.Expenses;
-using Domain.Models.Validation;
-using MyExpenses.Presentation.Validations.Attributes;
+using FluentValidation.Results;
+using MyExpenses.Presentation.Validations;
 using MyExpenses.Presentation.ViewModels.Systems;
 using TheR7angelo.DirtyTracking.Abstractions;
 
 namespace MyExpenses.Presentation.ViewModels.Expenses;
 
 [DirtyTracking]
-public partial class CategoryTypeViewModel : ObservableValidator
+public partial class CategoryTypeViewModel : BaseViewModel
 {
-    [ObservableProperty]
-    public partial int Id { get; set; }
+    public int Id { get; set; }
 
     [DirtyTrackedProperty]
     [ObservableProperty]
-    [NotifyDataErrorInfo]
-    [RequiredWithCode(ErrorCode.NameRequired, ErrorMessage = "Category name is required")]
-    [MaxLengthWithCode(CategoryTypeDomain.MaxNameLength, ErrorCode.NameTooLong, ErrorMessage = "Category name cannot exceed 55 characters")]
     public partial string? Name { get; set; }
 
     [DirtyTrackedProperty]
     [ObservableProperty]
-    [NotifyDataErrorInfo]
-    [RequiredWithCode(ErrorCode.ColorRequired, ErrorMessage = "Category color is required")]
     public partial ColorViewModel? Color { get; set; }
 
+    public DateTime DateAdded { get; set; } = DateTime.Now;
+
     [ObservableProperty]
-    public partial DateTime DateAdded { get; set; } = DateTime.Now;
+    public partial bool IsDeleting { get; set; }
 
-    public IEnumerable<DomainValidationResult> GetErrorCodes()
-        => GetErrors().OfType<DomainValidationResult>();
-
-    public bool IsDeleting { get; set; }
+    public new void ValidateWithFluent(ValidationResult result)
+        => base.ValidateWithFluent(result);
 }
