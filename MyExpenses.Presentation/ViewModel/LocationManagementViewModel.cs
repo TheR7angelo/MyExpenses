225cd90b63ff82e5b99b9355267c39695f7d2c18
TheR7angelo <raphael.antunes@hotmail.fr>
2026-05-18 22:44:36 +0200

    enhance(location-management): integrate point feature mapping in `LocationManagementViewModel`
    - Added `ILocationDtoViewModelMapper` dependency to handle point feature mapping for places.
    - Implemented logic to convert places into point features with conditional styling based on their status (`IsOpen`).
    - Updated `PlaceLayer` with mapped point features for improved map visualization.
    - Refactored variable names for clarity (`places` to `resultPlaces`).

--- MyExpenses.Presentation/ViewModel/LocationManagementViewModel.cs
+++ MyExpenses.Presentation/ViewModel/LocationManagementViewModel.cs
@@ -3,6 +3,8 @@ using BruTile.Predefined;
 using CommunityToolkit.Mvvm.Input;
 using Mapsui.Layers;
 using Microsoft.Extensions.Logging;
+using MyExpenses.Presentation.Converters;
+using MyExpenses.Presentation.Mappings.Interfaces;
 using MyExpenses.Presentation.Services.Interfaces;
 using MyExpenses.Presentation.ViewModels.Locations;
 using MyExpenses.SharedUtils.Collection;
@@ -12,6 +14,7 @@ namespace MyExpenses.Presentation.ViewModel;
 public partial class LocationManagementViewModel : ViewModelBase
 {
     private readonly ILocationPresentationService _locationPresentationService;
+    private readonly ILocationDtoViewModelMapper _locationDtoViewModelMapper;
     private readonly ILogger<LocationManagementViewModel> _logger;
 
     private WritableLayer PlaceLayer { get; } = new() { Style = null, Tag = typeof(PlaceViewModel) };
@@ -23,9 +26,11 @@ public partial class LocationManagementViewModel : ViewModelBase
     public IAsyncRelayCommand LoadCommand { get; }
 
     public LocationManagementViewModel(ILocationPresentationService locationPresentationService,
+        ILocationDtoViewModelMapper locationDtoViewModelMapper,
         ILogger<LocationManagementViewModel> logger)
     {
         _locationPresentationService = locationPresentationService;
+        _locationDtoViewModelMapper = locationDtoViewModelMapper;
         _logger = logger;
 
         LoadCommand = new AsyncRelayCommand(OnLoadAsync);
@@ -36,13 +41,22 @@ public partial class LocationManagementViewModel : ViewModelBase
         var titleSource = _locationPresentationService.GetAllKnowTitleSource();
         if (titleSource.IsSuccess) KnownTileSources.AddRangeAndSort(titleSource.Value!, s => s.ToString());
 
-        var places = await _locationPresentationService.GetAllPlaceGroup(cancellationToken);
-        if (!places.IsSuccess)
+        var resultPlaces = await _locationPresentationService.GetAllPlaceGroup(cancellationToken);
+        if (!resultPlaces.IsSuccess)
         {
-            _logger.LogWarning("Failed to load place group: {Error}", places.InternalMessage!);
+            _logger.LogWarning("Failed to load place group: {Error}", resultPlaces.InternalMessage!);
             return;
         }
 
-        CountryGroups.AddRangeAndSort(places.Value!, s => s.Country!);
+        CountryGroups.AddRangeAndSort(resultPlaces.Value!, s => s.Country!);
+
+        var pointFeatures = resultPlaces.Value!.SelectMany(s => s.CityGroups!.SelectMany(c => c.Places!))
+            .Where(s => s.Latitude is not null && s.Latitude is not 0 && s.Longitude is not null &&
+                        s.Longitude is not 0)
+            .Select(s => s.IsOpen
+                ? _locationDtoViewModelMapper.MapToPointFeature(s, MapsuiStyleExtensions.RedMarkerStyle)
+                : _locationDtoViewModelMapper.MapToPointFeature(s, MapsuiStyleExtensions.BlueMarkerStyle));
+
+        PlaceLayer.AddRange(pointFeatures);
     }
 }
