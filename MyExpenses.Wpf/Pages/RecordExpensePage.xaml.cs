83fa26ebea514e23a4e2209b2fad62eb8f7df41c
TheR7angelo <raphael.antunes@hotmail.fr>
2026-06-09 20:01:32 +0200

    refactor: Refactored `RecordExpensePage.xaml.cs` by removing unused validation code and refactoring button click handlers. Improved readability and maintainability of the page logic.

--- MyExpenses.Wpf/Pages/RecordExpensePage.xaml.cs
+++ MyExpenses.Wpf/Pages/RecordExpensePage.xaml.cs
@@ -8,7 +8,6 @@ using MyExpenses.Presentation.Enums;
 using MyExpenses.Presentation.Services.Interfaces;
 using MyExpenses.Presentation.ViewModel;
 using MyExpenses.Presentation.ViewModels.Expenses;
-using MyExpenses.SharedUtils.Properties;
 using MyExpenses.SharedUtils.Resources.Resx.DetailedRecordManagement;
 using MyExpenses.Sql.Context;
 using MyExpenses.Utils;
@@ -40,91 +39,6 @@ public partial class RecordExpensePage : IReceiveNavigationParameter
     }
 
     #region Action
-
-    private void ButtonValid_OnClick(object sender, RoutedEventArgs e)
-    {
-        // // ReSharper disable once HeapView.ObjectAllocation.Evident
-        // // The serviceProvider and items are set to null because they are not required in this context.
-        // // The ValidationResults list will store any validation errors detected during the process.
-        // var validationContext = new ValidationContext(History, serviceProvider: null, items: null);
-        //
-        // // ReSharper disable once HeapView.ObjectAllocation.Evident
-        // // Using 'var' keeps the code concise and readable, as the type (List<ValidationResult>)
-        // // is evident from the initialization. The result will still be compatible with any method
-        // // that expects an ICollection<ValidationResult>, as List<T> implements the ICollection interface.
-        // var validationResults = new List<ValidationResult>();
-        // var isValid = Validator.TryValidateObject(History, validationContext, validationResults, true);
-        //
-        // if (!isValid)
-        // {
-        //     var propertyError = validationResults.First();
-        //     var propertyMemberName = propertyError.MemberNames.First();
-        //
-        //     var messageErrorKey = propertyMemberName switch
-        //     {
-        //         nameof(THistory.AccountFk) => nameof(DetailedRecordManagementResources.MessageBoxValidationAccountFkError),
-        //         nameof(THistory.Description) => nameof(DetailedRecordManagementResources.MessageBoxValidationDescriptionError),
-        //         nameof(THistory.CategoryTypeFk) => nameof(DetailedRecordManagementResources.MessageBoxValidationCategoryTypeFkError),
-        //         nameof(THistory.ModePaymentFk) => nameof(DetailedRecordManagementResources.MessageBoxValidationModePaymentFkError),
-        //         nameof(THistory.Value) => nameof(DetailedRecordManagementResources.MessageBoxValidationValueError),
-        //         nameof(THistory.Date) => nameof(DetailedRecordManagementResources.MessageBoxValidationDateError),
-        //         nameof(THistory.PlaceFk) => nameof(DetailedRecordManagementResources.MessageBoxValidationPlaceFkError),
-        //         _ => null
-        //     };
-        //
-        //     var localizedErrorMessage = string.IsNullOrEmpty(messageErrorKey)
-        //         ? propertyError.ErrorMessage!
-        //         : DetailedRecordManagementResources.ResourceManager.GetString(messageErrorKey)!;
-        //
-        //     MsgBox.Show(localizedErrorMessage, MsgBoxImage.Error);
-        //     return;
-        // }
-        //
-        // Log.Information("Attempting to inject the new history");
-        //
-        // History.DateAdded = DateTime.Now;
-        // if (History.IsPointed) History.DatePointed = DateTime.Now;
-        // else History.DatePointed = null;
-        //
-        // var (success, exception) = History.AddOrEdit();
-        // if (success)
-        // {
-        //     Log.Information("History was successfully added");
-        //     var json = History.ToJsonString();
-        //     Log.Information("{Json}", json);
-        //
-        //     MsgBox.Show(DetailedRecordManagementResources.MessageBoxAddHistorySuccessMessage, MsgBoxImage.Check);
-        //
-        //     // if (EditHistory)
-        //     // {
-        //     //     if (History.BankTransferFk is not null)
-        //     //     {
-        //     //         using var context = new DataBaseContextOld();
-        //     //         var bankTransfer = context.TBankTransfers.FirstOrDefault(s => s.Id == History.BankTransferFk);
-        //     //         if (bankTransfer is not null)
-        //     //         {
-        //     //             bankTransfer.MainReason = History.Description;
-        //     //             bankTransfer.Value = Math.Abs(History.Value ?? 0);
-        //     //             bankTransfer.AddOrEdit();
-        //     //         }
-        //     //     }
-        //     //
-        //     //     nameof(MainWindow.FrameBody).GoBack();
-        //     //     return;
-        //     // }
-        //
-        //     var response = MsgBox.Show(DetailedRecordManagementResources.MessageBoxAddHistoryQuestionMessage, MsgBoxImage.Question,
-        //         MessageBoxButton.YesNoCancel);
-        //     if (response is not MessageBoxResult.Yes) nameof(MainWindow.FrameBody).GoBack();
-        //
-        //     History.Reset();
-        // }
-        // else
-        // {
-        //     Log.Error(exception, "An error occurred please retry");
-        //     MsgBox.Show(DetailedRecordManagementResources.MessageBoxAddHistoryErrorMessage, MsgBoxImage.Error);
-        // }
-    }
 
     private void ButtonDelete_OnClick(object sender, RoutedEventArgs e)
     {
@@ -189,16 +103,6 @@ public partial class RecordExpensePage : IReceiveNavigationParameter
 
         var bankTransfer = History.BankTransferFk?.ToISql<TBankTransfer>();
         bankTransfer?.Delete(true);
-    }
-
-    public void SetTHistory(THistory history)
-    {
-        var place = history.PlaceFk?.ToISql<TPlace>();
-        // SelectedCountry = EmptyStringTreeViewConverter.ToUnknown(place?.Country);
-        // SelectedCity = EmptyStringTreeViewConverter.ToUnknown(place?.City);
-
-        history.CopyPropertiesTo(History);
-        // EditHistory = true;
     }
 
     private void UpdateConfiguration()
