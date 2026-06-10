307e22fbcc97d88a6e1a1bdb25f77494c362f4aa
TheR7angelo <raphael.antunes@hotmail.fr>
2026-06-10 21:27:27 +0200

    refactor: Refactor `RecordExpensePage.xaml.cs`: remove redundant button click handler for deleting expenses

--- MyExpenses.Wpf/Pages/RecordExpensePage.xaml.cs
+++ MyExpenses.Wpf/Pages/RecordExpensePage.xaml.cs
@@ -1,28 +1,15 @@
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using Microsoft.Data.Sqlite;
 using MyExpenses.Models.Config;
-using MyExpenses.Models.Sql.Bases.Tables;
-using MyExpenses.Presentation.Enums;
 using MyExpenses.Presentation.Services.Interfaces;
 using MyExpenses.Presentation.ViewModel;
 using MyExpenses.Presentation.ViewModels.Expenses;
-using MyExpenses.SharedUtils.Resources.Resx.DetailedRecordManagement;
-using MyExpenses.Sql.Context;
 using MyExpenses.Utils;
-using MyExpenses.Wpf.Windows.Dialogs.MsgBox;
-using Serilog;
-using MessageBoxButton = System.Windows.MessageBoxButton;
-using MessageBoxResult = System.Windows.MessageBoxResult;
 
 namespace MyExpenses.Wpf.Pages;
 
 public partial class RecordExpensePage : IReceiveNavigationParameter
 {
-    // ReSharper disable once HeapView.ObjectAllocation.Evident
-    public THistory History { get; } = new();
-
     private ExpenseManagementViewModel ExpenseManagementViewModel
         => (ExpenseManagementViewModel)DataContext;
 
@@ -38,74 +25,10 @@ public partial class RecordExpensePage : IReceiveNavigationParameter
         DataContext = vm;
     }
 
-    #region Action
-
-    private void ButtonDelete_OnClick(object sender, RoutedEventArgs e)
-    {
-
-
-        // var response = MsgBox.Show(DetailedRecordManagementResources.MessageBoxDeleteHistoryQuestionMessage, MsgBoxImage.Question,
-        //     MessageBoxButton.YesNoCancel);
-        //
-        // if (response is not MessageBoxResult.Yes) return;
-        //
-        // Log.Information("Attempting to remove the record \"{HistoryToDeleteDescriiption}\"", History.Description);
-        // var (success, exception) = History.Delete();
-        //
-        // if (success)
-        // {
-        //     Log.Information("This Record was successfully removed");
-        //     MsgBox.Show(DetailedRecordManagementResources.MessageBoxDeleteHistoryNoUseSuccessMessage, MsgBoxImage.Check);
-        //
-        //     DeleteBankTransfer();
-        //
-        //     nameof(MainWindow.FrameBody).GoBack();
-        //     return;
-        // }
-        //
-        // if (exception!.InnerException is SqliteException
-        //     {
-        //         SqliteExtendedErrorCode: SQLitePCL.raw.SQLITE_CONSTRAINT_FOREIGNKEY
-        //     })
-        // {
-        //     Log.Error("Foreign key constraint violation");
-        //
-        //     response = MsgBox.Show(DetailedRecordManagementResources.MessageBoxDeleteUseRecordQuestionMessage,
-        //         MsgBoxImage.Question, MessageBoxButton.YesNoCancel);
-        //
-        //     if (response is not MessageBoxResult.Yes) return;
-        //
-        //     Log.Information(
-        //         "Attempting to remove this record \"{HistoryToDeleteDescriiption}\" with all relative element",
-        //         History.Description);
-        //     History.Delete(true);
-        //     DeleteBankTransfer();
-        //     Log.Information("This record and all relative element was successfully removed");
-        //
-        //     MsgBox.Show(DetailedRecordManagementResources.MessageBoxDeleteHistoryUseSuccessMessage, MsgBoxImage.Check);
-        //
-        //     nameof(MainWindow.FrameBody).GoBack();
-        //     return;
-        // }
-        //
-        // Log.Error(exception, "An error occurred please retry");
-        // MsgBox.Show(DetailedRecordManagementResources.MessageBoxDeleteHistoryErrorMessage, MsgBoxImage.Error);
-    }
-
     private void Configuration_OnConfigurationChanged()
         => UpdateConfiguration();
 
-    #endregion
-
     #region Function
-
-    private void DeleteBankTransfer()
-    {
-        if (History.BankTransferFk is null) return;
-
-        var bankTransfer = History.BankTransferFk?.ToISql<TBankTransfer>();
-        bankTransfer?.Delete(true);
-    }
 
     private void UpdateConfiguration()
     {
@@ -118,6 +41,12 @@ public partial class RecordExpensePage : IReceiveNavigationParameter
 
     #endregion
 
+    /// <summary>
+    /// Handles the context menu opening event for the MapControl.
+    /// This method updates the location information when the context menu is opened on the map.
+    /// </summary>
+    /// <param name="sender">The source of the event, which is typically the MapControl.</param>
+    /// <param name="e">Arguments associated with the event.</param>
     private void MapControl_OnContextMenuOpening(object sender, ContextMenuEventArgs e)
     {
         var position = Mouse.GetPosition(MapControl);
